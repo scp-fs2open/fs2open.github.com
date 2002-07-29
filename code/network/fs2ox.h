caaf1172a60913ed333314f3e81b66f352e9ab0e
penguin <penguin@387891d4-d844-0410-90c0-e4c51a9137d3>
2002-07-29 22:24:26 +0000

    First attempt at "fs2_open exchange" (PXO substitute) screen

---
+++ code/network/fs2ox.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) Volition, Inc. 1999.  All rights reserved.
+ *
+ * All source code herein is the property of Volition, Inc. You may not sell 
+ * or otherwise commercially exploit the source or things you created based on the 
+ * source.
+ *
+ */ 
+
+/*
+ * $Logfile$
+ * $Revision: 1.1 $
+ * $Date: 2002-07-29 22:24:26 $
+ * $Author: penguin $
+ *
+ * Header file for PXO-substitute (FS2OX -- "fs2_open exchange") screen
+ *
+ * $Log: not supported by cvs2svn $
+ * $NoKeywords: $
+ */
+
+#ifndef FS2OX_H
+#define FS2OX_H
+
+void fs2ox_close();
+void fs2ox_init();
+void fs2ox_do_frame();
+
+#endif
