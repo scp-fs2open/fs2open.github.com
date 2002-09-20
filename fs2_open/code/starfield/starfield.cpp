a2d1cf8f37e6e348f0fe6c32f122b6bf69b0da43
phreak <phreak@387891d4-d844-0410-90c0-e4c51a9137d3>
2002-09-20 20:05:29 +0000

    glare parser stuff in stars_init()

--- fs2_open/code/starfield/starfield.cpp
+++ fs2_open/code/starfield/starfield.cpp
@@ -9,14 +9,17 @@
 
 /*
  * $Logfile: /Freespace2/code/Starfield/StarField.cpp $
- * $Revision: 2.1 $
- * $Date: 2002-08-01 01:41:10 $
- * $Author: penguin $
+ * $Revision: 2.2 $
+ * $Date: 2002-09-20 20:05:29 $
+ * $Author: phreak $
  *
  * Code to handle and draw starfields, background space image bitmaps, floating
  * debris, etc.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.1  2002/08/01 01:41:10  penguin
+ * The big include file move
+ *
  * Revision 2.0  2002/06/03 04:02:29  penguin
  * Warpcore CVS sync
  *
@@ -358,6 +361,7 @@ void stars_init()
 	char filename[MAX_FILENAME_LEN+1] = "";
 	char glow_filename[MAX_FILENAME_LEN+1] = "";
 	float r, g, b, i;
+	int sun_glare;
 
 	// parse stars.tbl
 	read_file_text("stars.tbl");
@@ -432,6 +436,7 @@ void stars_init()
 			stuff_float(&b);
 			stuff_float(&i);
 
+			sun_glare=!optional_string("$NoGlare:");
 			if(count < MAX_STARFIELD_BITMAPS){
 				bm = &Sun_bitmaps[count++];
 				strcpy(bm->filename, filename);
@@ -445,6 +450,7 @@ void stars_init()
 				bm->g = g;
 				bm->b = b;
 				bm->i = i;
+				bm->glare=sun_glare;
 
 				// if fred is running we should lock the bitmap now
 				if(Fred_running){
