eeab39d62dbf1c66c57430a72d07a0fc99ce37f9
inquisitor <inquisitor@387891d4-d844-0410-90c0-e4c51a9137d3>
2002-10-30 20:22:30 +0000

    Bad Committ, Rolling back

--- fs2_open/code/cmdline/cmdline.h
+++ fs2_open/code/cmdline/cmdline.h
@@ -1,14 +1,201 @@
+/*
+ * Copyright (C) Volition, Inc. 1999.  All rights reserved.
+ *
+ * All source code herein is the property of Volition, Inc. You may not sell 
+ * or otherwise commercially exploit the source or things you created based on the 
+ * source.
+ *
+
+*/ 
+
+/*
+ * $Logfile: /Freespace2/code/Cmdline/cmdline.h $
+ * $Revision: 2.10 $
+ * $Date: 2002-10-30 20:22:30 $
+ * $Author: inquisitor $
+ *
+ * $Log: not supported by cvs2svn $
+ * Revision 2.7  2002/10/22 23:02:39  randomtiger
+ * Made Phreaks alternative scanning style optional under the command line tag "-phreak"
+ * Fixed bug that changes HUD colour when targetting debris in a full nebula. - RT
+ *
+ * Revision 2.6  2002/10/19 03:50:28  randomtiger
+ * Added special pause mode for easier action screenshots.
+ * Added new command line parameter for accessing all single missions in tech room. - RT
+ *
+ * Revision 2.5  2002/08/27 13:38:57  penguin
+ * Moved DirectX8 stuff to directx8 branch; reverted to previous
+ *
+ * Revision 2.3  2002/08/07 00:44:13  DTP
+ * Implented -GF4FIX commandline switch
+ *
+ * Revision 2.2  2002/07/29 06:35:15  DTP
+ * added -almission commandline arguement, will autoload mission i.e fs2_open.exe -almission kickass will autoload kickass.fs2 which should be a multiplayer mission.
+ *
+ * Revision 2.1  2002/07/07 19:55:58  penguin
+ * Back-port to MSVC
+ *
+ * Revision 2.0  2002/06/03 04:02:21  penguin
+ * Warpcore CVS sync
+ *
+ * Revision 1.2  2002/05/08 17:29:17  mharris
+ * more port tweaks
+ *
+ * Revision 1.1  2002/05/02 18:03:04  mharris
+ * Initial checkin - converted filenames and includes to lower case
+ *
+ * 
+ * 8     8/26/99 8:51p Dave
+ * Gave multiplayer TvT messaging a heavy dose of sanity. Cheat codes.
+ * 
+ * 7     7/15/99 3:07p Dave
+ * 32 bit detection support. Mouse coord commandline.
+ * 
+ * 6     7/13/99 1:15p Dave
+ * 32 bit support. Whee!
+ * 
+ * 5     6/22/99 9:37p Dave
+ * Put in pof spewing.
+ * 
+ * 4     1/12/99 5:45p Dave
+ * Moved weapon pipeline in multiplayer to almost exclusively client side.
+ * Very good results. Bandwidth goes down, playability goes up for crappy
+ * connections. Fixed object update problem for ship subsystems.
+ * 
+ * 3     11/17/98 11:12a Dave
+ * Removed player identification by address. Now assign explicit id #'s.
+ * 
+ * 2     10/07/98 10:52a Dave
+ * Initial checkin.
+ * 
+ * 1     10/07/98 10:48a Dave
+ * 
+ * 27    9/15/98 4:04p Allender
+ * added back in the -ip_addr command line switch because it needs to be
+ * in the standalone server only executable
+ * 
+ * 26    9/14/98 11:28a Allender
+ * support for server bashing of address when received from client.  Added
+ * a cmdline.cfg file to process command line arguments from a file
+ * 
+ * 25    9/08/98 2:20p Allender
+ * temporary code to force IP address to a specific value.
+ * 
+ * 24    8/20/98 5:30p Dave
+ * Put in handy multiplayer logfile system. Now need to put in useful
+ * applications of it all over the code.
+ * 
+ * 23    8/07/98 10:40a Allender
+ * new command line flags for starting netgames.  Only starting currently
+ * works, and PXO isn't implemented yet
+ * 
+ * 22    7/24/98 11:14a Allender
+ * start of new command line options for version 1.04
+ * 
+ * 21    5/21/98 1:50a Dave
+ * Remove obsolete command line functions. Reduce shield explosion packets
+ * drastically. Tweak PXO screen even more. Fix file xfer system so that
+ * we can guarantee file uniqueness.
+ * 
+ * 20    5/18/98 9:10p Dave
+ * Put in many new PXO features. Fixed skill level bashing in multiplayer.
+ * Removed several old command line options. Put in network config files.
+ * 
+ * 19    5/09/98 7:16p Dave
+ * Put in CD checking. Put in standalone host password. Made pilot into
+ * popup scrollable.
+ * 
+ * 18    4/23/98 8:27p Allender
+ * basic support for cutscene playback.  Into movie code in place.  Tech
+ * room can view cutscenes stored in CDROM_dir variable
+ * 
+ * 17    3/14/98 2:48p Dave
+ * Cleaned up observer joining code. Put in support for file xfers to
+ * ingame joiners (observers or not). Revamped and reinstalled pseudo
+ * lag/loss system.
+ * 
+ * 16    1/31/98 4:32p Dave
+ * Put in new support for VMT player validation, game logging in and game
+ * logging out.
+ * 
+ * 15    12/10/97 4:45p Dave
+ * Added in more detailed support for multiplayer packet lag/loss. Fixed
+ * some multiplayer stuff. Added some controls to the standalone.
+ * 
+ * 14    12/09/97 6:14p Lawrance
+ * add -nomusic flag
+ * 
+ * 13    11/28/97 7:04p Dave
+ * Emergency checkin due to big system crash.
+ * 
+ * 12    11/28/97 5:06p Dave
+ * Put in facilities for simulating multiplayer lag.
+ * 
+ * 11    11/24/97 5:42p Dave
+ * Fixed a file xfer buffer free/malloc problem. Lengthened command line
+ * switch string parse length.
+ * 
+ * 10    11/12/97 4:39p Dave
+ * Put in multiplayer campaign support parsing, loading and saving. Made
+ * command-line variables better named. Changed some things on the initial
+ * pilot select screen.
+ * 
+ * 9     11/11/97 4:54p Dave
+ * Put in support for single vs. multiplayer pilots. Put in initial player
+ * selection screen (no command line option yet). Started work on
+ * multiplayer campaign file save gaming.
+ * 
+ * 8     9/18/97 10:13p Dave
+ * Added -gimmemedals, which gives the current pilot all the medals in the
+ * game (debug)
+ * 
+ * 7     9/18/97 9:20a Dave
+ * Minor modifications
+ * 
+ * 6     9/15/97 11:40p Lawrance
+ * remove demo granularity switch
+ * 
+ * 5     9/03/97 5:03p Lawrance
+ * add support for -nosound command line parm
+ * 
+ * 4     8/21/97 4:55p Dave
+ * Added a switch for multiplayer chat streaming. Added a section for
+ * global command line vars.
+ * 
+ * 3     8/06/97 2:26p Dave
+ * Made the command line parse more robust. Made it easier to add and
+ * process new command-line switches.
+ * 
+ * 2     8/04/97 3:13p Dave
+ * Added command line functions. See cmdline.cpp for directions on adding
+ * new switches
+ * 
+ * 1     8/04/97 9:58a Dave
+ * 
+ * $NoKeywords: $
+ */
+
 #ifndef FS_CMDLINE_HEADER_FILE
 #define FS_CMDLINE_HEADER_FILE
 
+#ifdef _WIN32
 int parse_cmdline(char *cmdline);
+#else
+int parse_cmdline(int argc, char *argv[]);
+#endif
+
+// COMMAND LINE SETTINGS
+// This section is for reference by all the *_init() functions. For example, the multiplayer init function
+// could check to see if (int Cmdline_multi_stream_chat_to_file) has been set by the command line parser.
+//
+// Add any extern definitions here and put the actual variables inside of cmdline.cpp for ease of use
+// Also, check to make sure anything you add doesn't break Fred or TestCode
 
 extern int Cmdline_multi_stream_chat_to_file;
 extern int Cmdline_freespace_no_sound;
 extern int Cmdline_freespace_no_music;
 extern int Cmdline_gimme_all_medals;
 extern int Cmdline_use_last_pilot;
-extern int Cmdline_multi_protocol;
 extern int Cmdline_cd_check;
 extern int Cmdline_start_netgame;
 extern int Cmdline_closed_game;
@@ -19,8 +206,6 @@ extern char *Cmdline_game_password;
 extern char *Cmdline_rank_above;
 extern char *Cmdline_rank_below;
 extern char *Cmdline_connect_addr;
-extern char *Cmdline_almission;
-extern char *Cmdline_mod;
 extern int Cmdline_multi_log;
 extern int Cmdline_server_firing;
 extern int Cmdline_client_dodamage;
@@ -30,7 +215,9 @@ extern int Cmdline_mouse_coords;
 extern int Cmdline_timeout;
 
 extern int Cmdline_window;
-extern int Cmdline_gf4fix;
+extern char *Cmdline_almission;	//DTP for autoload mission
+extern int Cmdline_gf4fix;	//DTP for Random tigers GF4fix.
 extern int Cmdline_allslev;
 extern int Cmdline_phreak;
-extern char *Hold_mod;
+
+#endif
