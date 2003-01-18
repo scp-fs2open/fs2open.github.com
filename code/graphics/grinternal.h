fc351837a5a69fbe1dc8df7f8e20a6c1249f7682
phreak <phreak@387891d4-d844-0410-90c0-e4c51a9137d3>
2003-01-18 19:49:02 +0000

    added tcache flag TCACHE_FLAG_COMPRESSED

--- code/graphics/grinternal.h
+++ code/graphics/grinternal.h
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Graphics/GrInternal.h $
- * $Revision: 2.1 $
- * $Date: 2002-08-01 01:41:05 $
- * $Author: penguin $
+ * $Revision: 2.2 $
+ * $Date: 2003-01-18 19:49:02 $
+ * $Author: phreak $
  *
  * Include file for our Graphics directory
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.1  2002/08/01 01:41:05  penguin
+ * The big include file move
+ *
  * Revision 2.0  2002/06/03 04:02:22  penguin
  * Warpcore CVS sync
  *
@@ -217,6 +220,7 @@ extern int Gr_gamma_lookup[256];
 #define TCACHE_TYPE_XPARENT				2		// Bitmap with 0,255,0 = transparent.  Alpha=0 if transparent, 1 if not.
 #define TCACHE_TYPE_NONDARKENING			3		// Bitmap with 255,255,255 = non-darkening.  Alpha=1 if non-darkening, 0 if not.
 #define TCACHE_TYPE_BITMAP_SECTION		4		// section of a bitmap
+#define TCACHE_TYPE_COMPRESSED			(1<<31)
 
 #endif
 
