d32a38c267520337d9d2c9e7260562fcfce04407
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2003-02-05 07:04:31 +0000

    fixed version bumpage... the version is 3.5.0, not 3.50.0 ;) --Goober5000

--- fs2_open/code/globalincs/version.h
+++ fs2_open/code/globalincs/version.h
@@ -9,12 +9,15 @@
 
 /*
  * $Logfile: /Freespace2/code/GlobalIncs/version.h $
- * $Revision: 2.5 $
- * $Date: 2003-01-30 23:24:34 $
- * $Author: phreak $
+ * $Revision: 2.6 $
+ * $Date: 2003-02-05 07:04:31 $
+ * $Author: Goober5000 $
  *
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.5  2003/01/30 23:24:34  phreak
+ * bumped to 3.50
+ *
  * Revision 2.4  2002/12/31 20:09:14  Goober5000
  * bumped to 3.4
  * --Goober5000
@@ -136,7 +139,7 @@
 //   Major.Minor.Bugfix
 
 #define FS_VERSION_MAJOR 3					// major version
-#define FS_VERSION_MINOR 50					// increase by 1 for minor revs
+#define FS_VERSION_MINOR 5					// increase by 1 for minor revs
 #define FS_VERSION_BUILD 0					// bugfix release
 
 #define VERSION_LOC_FNAME			"version.nfo"
