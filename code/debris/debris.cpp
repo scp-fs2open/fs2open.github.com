92da0779243ac98595fb66e984f1df6b1fb33274
unknownplayer <unknownplayer@387891d4-d844-0410-90c0-e4c51a9137d3>
2003-03-19 12:29:02 +0000

    Woohoo! Killed two birds with one stone! Fixed the 'black screen around dialog boxes' problem and also the much more serious freezing problem experienced by Goober5000. It wasn't a crash, just an infinite loop. DX8 merge is GO! once again :)

--- code/debris/debris.cpp
+++ code/debris/debris.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Debris/Debris.cpp $
- * $Revision: 2.1 $
- * $Date: 2002-08-01 01:41:04 $
- * $Author: penguin $
+ * $Revision: 2.2 $
+ * $Date: 2003-03-19 12:29:01 $
+ * $Author: unknownplayer $
  *
  * Code for the pieces of exploding object debris.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.1  2002/08/01 01:41:04  penguin
+ * The big include file move
+ *
  * Revision 2.0  2002/06/03 04:02:21  penguin
  * Warpcore CVS sync
  *
@@ -235,6 +238,7 @@
 #include "freespace2/freespace.h"
 #include "object/objcollide.h"
 #include "io/timer.h"
+#include "fireball/fireballs.h"
 
 #ifndef NO_NETWORK
 #include "network/multi.h"
