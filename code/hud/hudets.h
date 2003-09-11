dbf498eb9159de1d4ac6cb6f6df374a7db165158
argv <argv@387891d4-d844-0410-90c0-e4c51a9137d3>
2003-09-11 19:04:36 +0000

    New energy system, auto ETS, etc.

--- code/hud/hudets.h
+++ code/hud/hudets.h
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Hud/HUDets.h $
- * $Revision: 2.1 $
- * $Date: 2002-08-01 01:41:05 $
- * $Author: penguin $
+ * $Revision: 2.2 $
+ * $Date: 2003-09-11 19:04:36 $
+ * $Author: argv $
  *
  * Header file that supports code to manage and display the Energy Transfer System (ETS)
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.1  2002/08/01 01:41:05  penguin
+ * The big include file move
+ *
  * Revision 2.0  2002/06/03 04:02:23  penguin
  * Warpcore CVS sync
  *
@@ -61,7 +64,7 @@ extern float Energy_levels[];
 extern int Weapon_energy_cheat;
 
 
-enum SYSTEM_TYPE {WEAPONS, SHIELDS, ENGINES};
+enum SYSTEM_TYPE {WEAPONS, SHIELDS, ENGINES, WEAPONS_QUIET, SHIELDS_QUIET, ENGINES_QUIET};
 
 void update_ets(object* obj, float fl_frametime);
 void ets_init_ship(object* obj);
