a63ec351c44c22537a22b702b41badc527157b63
argv <argv@387891d4-d844-0410-90c0-e4c51a9137d3>
2003-09-11 19:14:14 +0000

    New energy system, table subsystem flags including beam free by default, subsystem display name.

--- code/model/model.h
+++ code/model/model.h
@@ -9,13 +9,17 @@
 
 /*
  * $Logfile: /Freespace2/code/Model/MODEL.H $
- * $Revision: 2.28 $
- * $Date: 2003-08-28 20:42:18 $
- * $Author: Goober5000 $
+ * $Revision: 2.29 $
+ * $Date: 2003-09-11 19:14:14 $
+ * $Author: argv $
  *
  * header file for information about polygon models
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.28  2003/08/28 20:42:18  Goober5000
+ * implemented rotating barrels for firing primary weapons
+ * --Goober5000
+ *
  * Revision 2.27  2003/08/22 07:35:09  bobboau
  * specular code should be bugless now,
  * cell shadeing has been added activated via the comand line '-cell',
@@ -536,6 +540,15 @@ typedef struct model_subsystem {					/* contains rotation rate info */
 	int		secondary_banks[MAX_SECONDARY_BANKS];				// default secondary weapons -hoffoss
 	int		secondary_bank_capacity[MAX_SECONDARY_BANKS];	// capacity of a bank -hoffoss
 	int		path_num;								// path index into polymodel .paths array.  -2 if none exists, -1 if not defined
+
+	// _argv[-1] - power output of this subsystem, added to total effective power output for the ship.
+	float	power_output;
+	// _argv[-1] - optional subsys flags. bitfield this time, because bitfields are cool. :D
+	unsigned int		beam_free_by_default:2; // three states; 0 = never free by default, 1 = free by default if ship is, 2 = always free by default
+
+	// _argv[-1] - subsystem display name is displayed in the target view.
+	int		use_display_name:1;
+	char	display_name[MAX_NAME_LEN];
 } model_subsystem;
 
 typedef struct model_special {
