abc2ce88d476eaa9581386d24713163eedef0741
argv <argv@387891d4-d844-0410-90c0-e4c51a9137d3>
2003-09-11 19:12:40 +0000

    Ship flag2 beam-free-all.

--- code/missionui/missionweaponchoice.cpp
+++ code/missionui/missionweaponchoice.cpp
@@ -9,13 +9,22 @@
 
 /*
  * $Logfile: /Freespace2/code/MissionUI/MissionWeaponChoice.cpp $
- * $Revision: 2.15 $
- * $Date: 2003-08-16 03:52:24 $
- * $Author: bobboau $
+ * $Revision: 2.16 $
+ * $Date: 2003-09-11 19:12:40 $
+ * $Author: argv $
  *
  * C module for the weapon loadout screen
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.15  2003/08/16 03:52:24  bobboau
+ * update for the specmapping code includeing
+ * suport for seperate specular levels on lights and
+ * optional strings for the stars table
+ * code has been made more organised,
+ * though there seems to be a bug in the state selecting code
+ * resulting in the HUD being rendered incorectly
+ * and specmapping failing ocasionaly
+ *
  * Revision 2.14  2003/03/18 10:07:04  unknownplayer
  * The big DX/main line merge. This has been uploaded to the main CVS since I can't manage to get it to upload to the DX branch. Apologies to all who may be affected adversely, but I'll work to debug it as fast as I can.
  *
@@ -3434,6 +3443,23 @@ int wl_update_ship_weapons(int objnum, wss_unit *slot )
 		if ( wl_slots_all_empty(slot) ) {
 			return -1;
 		}
+	}
+
+	// _argv[-1] - since the ship class can obviously get changed in loadout, we need to deal with beam free again.
+	// By 'again' I mean that it was already done in missionparse.cpp, but that only applies to the default ship!
+	ship *shipp = &Ships[Objects[objnum].instance];
+	ship_info *sip = &Ship_info[shipp->ship_info_index];
+
+	for (ship_subsys *ss = GET_FIRST(&shipp->subsys_list); ss != END_OF_LIST(&shipp->subsys_list); ss = GET_NEXT(ss)) {
+		if (ss->system_info->type != SUBSYSTEM_TURRET)
+			continue;
+		if (ss->system_info->beam_free_by_default == 0)
+			continue;
+		if (!(sip->flags2 & SIF2_BEAM_FREE_BY_DEFAULT) && ss->system_info->beam_free_by_default == 1)
+			continue;
+
+		ss->weapons.flags |= SW_FLAG_BEAM_FREE;
+		ss->turret_next_fire_stamp = timestamp((int) frand_range(50.0f, 4000.0f));
 	}
 
 	wl_bash_ship_weapons(&Ships[Objects[objnum].instance].weapons, slot);
