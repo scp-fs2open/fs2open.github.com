55c7d0e0fc572bab98f48c3109caa872ff6a346c
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2004-02-28 20:26:10 +0000

    preliminary checking of globals.h --Goober5000

---
+++ fs2_open/code/globalincs/globals.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) Volition, Inc. 1999.  All rights reserved.
+ *
+ * All source code herein is the property of Volition, Inc. You may not sell 
+ * or otherwise commercially exploit the source or things you created based on the 
+ * source.
+ *
+ */ 
+
+/*
+ * $Logfile: /Freespace2/code/GlobalIncs/globals.h $
+ * $Revision: 1.1 $
+ * $Date: 2004-02-28 20:26:10 $
+ * $Author: Goober5000 $
+ *
+ * Header for common global #defines, to cut down on #includes
+ *
+ * $Log: not supported by cvs2svn $
+ * Revision 1.0  2004/02/28 15:35:00  Goober5000
+ * Initial checkin
+ *
+ */
