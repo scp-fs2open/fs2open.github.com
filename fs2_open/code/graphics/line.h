9236bd6d7c7eb28db38db546881e1b0f2d094a75
Kazan <Kazan@387891d4-d844-0410-90c0-e4c51a9137d3>
2004-03-08 15:03:27 +0000

    *crosses fingers*

--- fs2_open/code/graphics/line.h
+++ fs2_open/code/graphics/line.h
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Graphics/Line.h $
- * $Revision: 2.0 $
- * $Date: 2002-06-03 04:02:23 $
- * $Author: penguin $
+ * $Revision: 2.1 $
+ * $Date: 2004-03-08 15:03:27 $
+ * $Author: Kazan $
  *
  * Header file for line.cpp
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.0  2002/06/03 04:02:23  penguin
+ * Warpcore CVS sync
+ *
  * Revision 1.1  2002/05/02 18:03:07  mharris
  * Initial checkin - converted filenames and includes to lower case
  *
@@ -61,7 +64,7 @@
 #ifndef _LINE_H
 #define _LINE_H
 
-void gr8_line(int x1,int y1,int x2,int y2);
+void gr8_line(int x1,int y1,int x2,int y2, bool b);
 void gr8_aaline(vertex *v1, vertex *v2);
 											
 
