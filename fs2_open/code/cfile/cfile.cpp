71aecd39273e687ebcea9a002901c9803d58b0bd
wmcoolmon <wmcoolmon@387891d4-d844-0410-90c0-e4c51a9137d3>
2004-05-26 02:29:44 +0000

    Scratch that - .tbm is a better extension

--- fs2_open/code/cfile/cfile.cpp
+++ fs2_open/code/cfile/cfile.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/CFile/cfile.cpp $
- * $Revision: 2.16 $
- * $Date: 2004-05-26 02:26:14 $
+ * $Revision: 2.17 $
+ * $Date: 2004-05-26 02:29:44 $
  * $Author: wmcoolmon $
  *
  * Utilities for operating on files
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.16  2004/05/26 02:26:14  wmcoolmon
+ * Added table chunk extension
+ *
  * Revision 2.15  2004/05/06 22:12:14  taylor
  * make sure JPG and DDS files are added to the file cache for maps and effects
  *
@@ -281,7 +284,7 @@ cf_pathtype Pathtypes[CF_MAX_PATH_TYPES]  = {
 	{ CF_TYPE_TEXT,					"data" DIR_SEPARATOR_STR "text",																".txt .net",				CF_TYPE_DATA	},
 	{ CF_TYPE_MISSIONS,				"data" DIR_SEPARATOR_STR "missions",														".fs2 .fc2 .ntl .ssv",	CF_TYPE_DATA	},
 	{ CF_TYPE_MODELS,					"data" DIR_SEPARATOR_STR "models",															".pof",						CF_TYPE_DATA	},
-	{ CF_TYPE_TABLES,					"data" DIR_SEPARATOR_STR "tables",															".tbl .tbc",						CF_TYPE_DATA	},
+	{ CF_TYPE_TABLES,					"data" DIR_SEPARATOR_STR "tables",															".tbl .tbm",						CF_TYPE_DATA	},
 	{ CF_TYPE_SOUNDS,					"data" DIR_SEPARATOR_STR "sounds",															".wav",						CF_TYPE_DATA	},
 	{ CF_TYPE_SOUNDS_8B22K,			"data" DIR_SEPARATOR_STR "sounds" DIR_SEPARATOR_STR "8b22k",						".wav",						CF_TYPE_SOUNDS	},
 	{ CF_TYPE_SOUNDS_16B11K,		"data" DIR_SEPARATOR_STR "sounds" DIR_SEPARATOR_STR "16b11k",						".wav",						CF_TYPE_SOUNDS	},
