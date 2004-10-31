633729f7632a6e015f5d27f7bfbf92b61a6ff6ec
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2004-10-31 21:23:08 +0000

    #define if !#defined, add USE_DEVIL and FS2_SPEECH commented out

--- code/PreProcDefines.h
+++ code/PreProcDefines.h
@@ -5,12 +5,15 @@
 
 /*
  * $Logfile: /Freespace2/code/PreProcDefines.h $
- * $Revision: 1.3 $
- * $Date: 2004-10-06 22:02:53 $
- * $Author: Kazan $
+ * $Revision: 1.4 $
+ * $Date: 2004-10-31 21:23:08 $
+ * $Author: taylor $
  *
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2004/10/06 22:02:53  Kazan
+ * interface corruption fix - thanks taylor (MAX_BITMAPS upped to 7000)
+ *
  * Revision 1.2  2004/08/20 05:13:07  Kazan
  * wakka wakka - fix minor booboo
  *
@@ -23,11 +26,34 @@
 #if !defined(_pre_proc_defs_h_)
 #define _pre_proc_defs_h_
 
+#ifndef DECALS_ENABLED
 #define DECALS_ENABLED		1
+#endif
+
+#ifndef HTL
 #define HTL					1
+#endif
+
+#ifndef USE_OPENGL
 #define USE_OPENGL			1
+#endif
+
+#ifndef MORE_SPECIES
 #define MORE_SPECIES		1
+#endif
+
+#ifndef ENABLE_AUTO_PILOT
 #define ENABLE_AUTO_PILOT	1
-//#define USE_DEVIL			1
+#endif
+
+/*
+#ifndef USE_DEVIL
+#define USE_DEVIL			1
+#endif
+
+#ifndef FS2_SPEECH
+#define FS2_SPEECH			1
+#endif
+*/
 
 #endif // _pre_proc_defs_h_
