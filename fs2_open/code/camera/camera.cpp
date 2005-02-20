853b8e79f37892ee3ed28b436aa8a32edd89bf71
wmcoolmon <wmcoolmon@387891d4-d844-0410-90c0-e4c51a9137d3>
2005-02-20 23:14:00 +0000

    Camera code updates (subtitles)

--- fs2_open/code/camera/camera.cpp
+++ fs2_open/code/camera/camera.cpp
@@ -2,9 +2,13 @@
 #include "math/vecmat.h"
 #include "graphics/2d.h"
 #include "globalincs/alphacolors.h"
+#include "parse/parselo.h"
 
-//Some global cameras
-camera free_camera;
+//Some global vars
+std::vector<subtitle> Subtitles;
+std::vector<camera> Cameras;
+//Preset cameras
+camera* Free_camera;
 
 //This is where the camera class beings! :D
 camera::camera()
@@ -194,7 +198,7 @@ void camera::set_rotation_velocity(vector *in_rotation_rate)
 	rotation_rate_delta.h = in_rotation_rate->h / in_rotation_acceleration_time;*/
 }
 
-void camera::do_movement(float in_frametime)
+void camera::do_frame(float in_frametime)
 {
 	//float frametime, rot_frametime, trans_frametime;
 	//DO ROTATION STUFF
@@ -285,4 +289,193 @@ void camera::do_movement(float in_frametime)
 	}*/
 
 	vm_vec_scale_add2(&position, &translation_velocity, in_frametime);
+}
+
+#define MAX_SUBTITLE_LINES		10
+subtitle::subtitle(int in_x_pos, int in_y_pos, char* in_text, float in_display_time, char* in_imageanim, float in_fade_time, color *in_text_color, bool center_x, bool center_y, int in_width)
+{
+	//Setup text
+	int len = strlen(in_text);
+	if(len == 0)
+		return;
+
+	int num_text_lines;
+	char *text_line_ptrs[MAX_SUBTITLE_LINES];
+	int text_line_lens[MAX_SUBTITLE_LINES];
+	num_text_lines = split_str(in_text, in_width, text_line_lens, text_line_ptrs, MAX_SUBTITLE_LINES);
+	std::string temp_str;
+	for(int i = 0; i < num_text_lines; i++)
+	{
+		temp_str.assign(text_line_ptrs[i], text_line_lens[i]);
+		text_lines.push_back(temp_str);
+	}
+
+	//Setup text color
+	if(in_text_color)
+		text_color = *in_text_color;
+	else
+		gr_init_alphacolor(&text_color, 255, 255, 255, 255);
+
+	//Setup display and fade time
+	display_time = fl_abs(in_display_time);
+	fade_time = fl_abs(in_fade_time);
+
+	//Setup image
+	image_id = -1;
+	if(in_imageanim != NULL)
+	{
+		len = strlen(in_imageanim);
+		if(len != 0 && len < MAX_FILENAME_LEN)
+		{
+			image_id = bm_load(in_imageanim);
+			if(image_id != -1)
+				strcpy(imageanim, in_imageanim);
+		}
+	}
+
+	//Setup pos
+	int w=0, h=0, tw=0, th=0;
+	image_pos[0] = 0;
+	image_pos[1] = 0;
+	if(center_x || center_y)
+	{
+		//Get text size
+		for(int i = 0; i < num_text_lines; i++)
+		{
+			gr_get_string_size(&w, &h, text_line_ptrs[i], text_line_lens[i]);
+
+			if(w > tw)
+			tw = w;
+
+			th += h;
+		}
+
+		//Get image size
+		if(image_id != -1)
+		{
+			bm_get_info(image_id, &w, &h);
+			tw += w;
+			if(h > th)
+				th = h;
+		}
+
+		//Center it?
+		if(center_x)
+			image_pos[0] = (gr_screen.max_w - tw)/2;
+		if(center_y)
+			image_pos[0] = (gr_screen.max_h - th)/2;
+	}
+	if(in_x_pos < 0 && !center_x)
+		image_pos[0] += gr_screen.max_w + in_x_pos;
+	else
+		image_pos[0] += in_x_pos;
+
+	if(in_y_pos < 0 && !center_y)
+		image_pos[1] += gr_screen.max_h + in_y_pos;
+	else
+		image_pos[1] += in_y_pos;
+
+	if(image_id != -1)
+		text_pos[0] = image_pos[0] + w;	//Still set from bm_get_info call
+	else
+		text_pos[0] = image_pos[0];
+	text_pos[1] = image_pos[1];
+
+	time_displayed = 0.0f;
+	time_displayed_end = 2.0f*fade_time + display_time;
+}
+void subtitle::do_frame(float frametime)
+{
+	//Figure out how much alpha
+	if(time_displayed < fade_time)
+	{
+		text_color.alpha = (ubyte)fl2i(255.0f*(time_displayed/fade_time));
+	}
+	else if(time_displayed > time_displayed_end)
+	{
+		//We're finished
+		if(image_id != -1)
+		{
+			bm_unload(image_id);
+			image_id = -1;
+		}
+		return;
+	}
+	else if((time_displayed - fade_time) > display_time)
+	{
+		text_color.alpha = (ubyte)fl2i(255.0f*(1-(time_displayed - fade_time - display_time)/fade_time));
+	}
+	else
+	{
+		text_color.alpha = 255;
+	}
+
+	gr_set_color_fast(&text_color);
+
+	int font_height = gr_get_font_height();
+	int y = text_pos[1];
+
+	for(unsigned int i = 0; i < text_lines.size(); i++)
+	{
+		gr_string(text_pos[0], y, (char*)text_lines[i].c_str(), false);
+		y += font_height;
+	}
+
+	if(image_id != -1)
+	{
+		gr_set_bitmap(image_id, GR_ALPHABLEND_FILTER, GR_BITBLT_MODE_NORMAL, text_color.alpha/255.0f);
+		gr_bitmap(image_pos[0], image_pos[1], false);
+	}
+
+	time_displayed += frametime;
+}
+subtitle::~subtitle()
+{
+	if(image_id != -1)
+		bm_unload(image_id);
+}
+
+void cameras_init()
+{
+	camera handy_camera;
+
+	//Preset cameras
+	Cameras.push_back(handy_camera);
+	Free_camera = &Cameras[Cameras.size()-1];
+}
+
+void cameras_close()
+{
+	Cameras.clear();
+
+	//Preset cameras
+	Free_camera = NULL;
+}
+
+void cameras_do_frame(float frametime)
+{
+	unsigned int i,size=Cameras.size();
+	for(i = 0; i < size; i++)
+	{
+		Cameras[i].do_frame(frametime);
+	}
+}
+
+void subtitles_init()
+{
+	//Do nothing!!
+}
+
+void subtitles_close()
+{
+	Subtitles.clear();
+}
+
+void subtitles_do_frame(float frametime)
+{
+	unsigned int i,size=Subtitles.size();
+	for(i = 0; i < size; i++)
+	{
+		Subtitles[i].do_frame(frametime);
+	}
 }
