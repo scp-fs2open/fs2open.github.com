85e2f9b2a9eb0edb47e2d4026a9ce8ef4aafd122
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2005-03-30 01:23:32 +0000

    added header back in, with a small tweak for the .cpp --Goober5000

--- code/wxfred2/wxfred2.h
+++ code/wxfred2/wxfred2.h
@@ -6,18 +6,14 @@
  */ 
 
 /*
- * $Logfile: /Freespace2/code/wxFRED2/wxFRED2.cpp $
- * $Revision: 1.3 $
+ * $Logfile: /Freespace2/code/wxFRED2/wxFRED2.h $
+ * $Revision: 1.1 $
  * $Date: 2005-03-30 01:23:32 $
  * $Author: Goober5000 $
  *
  * New cross-platform version of FRED2
  *
  * $Log: not supported by cvs2svn $
- * Revision 1.2  2005/03/29 13:35:53  Goober5000
- * set up the main menu options
- * --Goober5000
- *
  * Revision 1.1  2005/03/29 07:55:19  Goober5000
  * whew... bare-bones "hello world" wxFRED base finally implemented!
  * --Goober5000
@@ -27,23 +23,15 @@
  *
  */
 
-#include "wx/wxprec.h"
+#ifndef _WXFRED2_H
+#define _WXFRED2_H
 
-#ifndef WX_PRECOMP
-#include "wx/wx.h"
-#endif
-
-#include "wxfred2.h"
-#include "fredframe.h"
-
-
-IMPLEMENT_APP(wxFRED2)
-
-bool wxFRED2::OnInit()
+class wxFRED2 : public wxApp
 {
-	FREDFrame *frame = new FREDFrame(_T("Untitled - FRED2_OPEN 3.6.5 - FreeSpace 2 Mission Editor"), 50, 50, 800, 600);
-	frame->Show(TRUE);
-	SetTopWindow(frame);
+	public:
+		virtual bool OnInit();
+};
 
-	return true;
-}
+DECLARE_APP(wxFRED2)
+
+#endif
