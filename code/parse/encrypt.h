dfc610f205b07fb3243a0ddba1220ca833ffc716
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2005-04-15 11:35:18 +0000

    add scramble port support both FS1 (8bit and 7bit) and FS2 style encryption

--- code/parse/encrypt.h
+++ code/parse/encrypt.h
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/parse/Encrypt.h $
- * $Revision: 2.2 $
- * $Date: 2004-08-11 05:06:31 $
- * $Author: Kazan $
+ * $Revision: 2.3 $
+ * $Date: 2005-04-15 11:35:18 $
+ * $Author: taylor $
  *
  * Header for encryption code common to FreeSpace and related tools
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.2  2004/08/11 05:06:31  Kazan
+ * added preprocdefines.h to prevent what happened with fred -- make sure to make all fred2 headers include this file as the _first_ include -- i have already modified fs2 files to do this
+ *
  * Revision 2.1  2003/09/28 21:22:58  Goober5000
  * added the option to import FSM missions, added a replace function, spruced
  * up my $player, $rank, etc. code, and fixed encrypt being misspelled as 'encrpyt'
@@ -61,8 +64,14 @@ void encrypt_init();
 // Return 1 if the file is encrypted, otherwise return 0
 int is_encrypted(char *scrambled_text);
 
+// Returns 1 if the data uses one of the FS1 style encryptions, 0 if FS2 style
+int is_old_encrypt(char *scrambled_text);
+
+// return text description of the encrypted text type
+const char *encrypt_type(char *scrambled_text);
+
 // Encrypt text data
-void encrypt(char *text, int text_len, char *scrambled_text, int *scrambled_len, int use_8bit);
+void encrypt(char *text, int text_len, char *scrambled_text, int *scrambled_len, int use_8bit, bool new_encrypt = true);
 
 // Decrypt scrambled_text
 void unencrypt(char *scrambled_text, int scrambled_len, char *text, int *text_len);
