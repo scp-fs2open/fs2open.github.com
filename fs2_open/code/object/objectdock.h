ba24fb1aeec09fe1633df8351eb4877c36716517
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2005-05-27 03:37:53 +0000

    moved some prototypes that WMC put in the header file back to the implementation file --Goober5000

--- fs2_open/code/object/objectdock.h
+++ fs2_open/code/object/objectdock.h
@@ -7,13 +7,17 @@
 
 /*
  * $Logfile: /Freespace2/code/Object/ObjectDock.h $
- * $Revision: 2.5 $
- * $Date: 2005-05-27 03:36:20 $
+ * $Revision: 2.6 $
+ * $Date: 2005-05-27 03:37:53 $
  * $Author: Goober5000 $
  *
  * New docking system
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.5  2005/05/27 03:36:20  Goober5000
+ * hmm, rolled back to 2.3
+ * --Goober5000
+ *
  * Revision 2.3  2005/04/05 05:53:21  taylor
  * s/vector/vec3d/g, better support for different compilers (Jens Granseuer)
  *
@@ -139,11 +143,5 @@ void dock_dock_objects(object *objp1, int dockpoint1, object *objp2, int dockpoi
 
 // remove objp1 and objp2 from each others' dock lists; currently only called by ai_do_objects_undocked_stuff
 void dock_undock_objects(object *objp1, object *objp2);
-
-//Removes other_objp from objp's docking list
-void dock_remove_instance(object *objp, object *other_objp);
-
-//Finds objp's dock instance to other_objp
-dock_instance *dock_find_instance(object *objp, object *other_objp);
 
 #endif	// _OBJECT_DOCK_H
