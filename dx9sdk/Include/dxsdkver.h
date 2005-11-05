a2b5c17d0e91542c0f8c199c5effcd8fa593166a
unknownplayer <unknownplayer@387891d4-d844-0410-90c0-e4c51a9137d3>
2005-11-05 09:31:45 +0000

    Committed the DX9 December 2004 SDK. None of the DX9 changes have been committed however. This might break the MSVC 2003 build since DX9 and DX8 share the DX8 include files. Just eliminate one from the library paths if you have any problems.

---
+++ dx9sdk/Include/dxsdkver.h
@@ -0,0 +1,18 @@
+/*==========================================================================;
+ *
+ *
+ *  File:   dxsdkver.h
+ *  Content:    DirectX SDK Version Include File
+ *
+ ****************************************************************************/
+
+#ifndef _DXSDKVER_H_
+#define _DXSDKVER_H_
+
+#define _DXSDK_PRODUCT_MAJOR  9     
+#define _DXSDK_PRODUCT_MINOR  04        
+#define _DXSDK_BUILD_MAJOR    91        
+#define _DXSDK_BUILD_MINOR    0000        
+
+#endif // _DXSDKVER_H_
+
