bc7cc673ee341808e62c089c1c7e202e50fbda18
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2005-11-13 06:37:24 +0000

    always produce BGR ordered images rather than RGB

--- libjpeg/jmorecfg.h
+++ libjpeg/jmorecfg.h
@@ -311,9 +311,9 @@ typedef int boolean;
  *    can't use color quantization if you change that value.
  */
 
-#define RGB_RED		0	/* Offset of Red in an RGB scanline element */
+#define RGB_RED		2	/* Offset of Red in an RGB scanline element */
 #define RGB_GREEN	1	/* Offset of Green */
-#define RGB_BLUE	2	/* Offset of Blue */
+#define RGB_BLUE	0	/* Offset of Blue */
 #define RGB_PIXELSIZE	3	/* JSAMPLEs per RGB scanline element */
 
 
