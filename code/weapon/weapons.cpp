8d0fc6455d1d62034450de1db6a5b50849ec1c94
wmcoolmon <wmcoolmon@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-06-07 05:20:52 +0000

    Not sure why I didn't catch this when compiling earlier...

--- code/weapon/weapons.cpp
+++ code/weapon/weapons.cpp
@@ -12,6 +12,11 @@
  * <insert description of file here>
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.180  2006/05/27 16:45:11  taylor
+ * some minor cleanup
+ * remove -nobeampierce
+ * update for geometry batcher changes
+ *
  * Revision 2.179  2006/04/20 06:32:30  Goober5000
  * proper capitalization according to Volition
  *
@@ -2696,10 +2701,6 @@ int parse_weapon(int subtype, bool replace)
 
 		// look it up
 		wip->muzzle_flash = mflash_lookup(mflash_string);
-
-		if(wip->muzzle_flash > -1){			
-			wip->wi_flags |= WIF_MFLASH;
-		}
 	}
 
 	// EMP optional stuff (if WIF_EMP is not set, none of this matters, anyway)
