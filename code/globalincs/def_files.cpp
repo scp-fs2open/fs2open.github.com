78f02e76cadaabb794f3137b686d3f605eeb6bed
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-07-06 21:23:28 +0000

    add CVS headers --Goober5000

--- code/globalincs/def_files.cpp
+++ code/globalincs/def_files.cpp
@@ -1,3 +1,20 @@
+/*
+ * Def_Files.cpp
+ *
+ * You may not sell or otherwise commercially exploit the source or things you
+ * create based on the source.
+ */
+
+/*
+ * $Logfile: /Freespace2/code/globalincs/def_files.cpp $
+ * $Revision: 2.19 $
+ * $Date: 2006-07-06 21:23:20 $
+ * $Author: Goober5000 $
+ *
+ * $Log: not supported by cvs2svn $
+ */
+
+
 #include <string.h>
 #include "globalincs/pstypes.h"
 
