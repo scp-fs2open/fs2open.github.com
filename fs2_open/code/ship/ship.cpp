2d4376fca91908d9bce796a200dc9dc0bea9f3c1
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-07-24 02:09:29 +0000

    fix a subtle and nasty bug --Goober5000

--- fs2_open/code/ship/ship.cpp
+++ fs2_open/code/ship/ship.cpp
@@ -10,13 +10,17 @@
 
 /*
  * $Logfile: /Freespace2/code/Ship/Ship.cpp $
- * $Revision: 2.336.2.17 $
- * $Date: 2006-07-20 00:41:26 $
+ * $Revision: 2.336.2.18 $
+ * $Date: 2006-07-24 02:09:29 $
  * $Author: Goober5000 $
  *
  * Ship (and other object) handling functions
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.336.2.17  2006/07/20 00:41:26  Goober5000
+ * add WCS screaming stuff to RC branch
+ * --Goober5000
+ *
  * Revision 2.336.2.16  2006/07/17 01:09:45  taylor
  * make glow point banks dynamic
  *
@@ -12560,16 +12564,19 @@ int ship_docking_valid(int docker, int dockee)
 // input:   persona  => OPTIONAL PARAMETER (default to -1) which persona to get
 int ship_get_random_player_wing_ship( int flags, float max_dist, int persona_index, int get_first, int multi_team )
 {
+	const int MAX_SIZE = MAX_SHIPS_PER_WING * MAX_SQUADRON_WINGS;
+
 	int i, j, ship_index, count;
-	int slist[MAX_SHIPS_PER_WING * MAX_STARTING_WINGS], which_one;
+	int slist[MAX_SIZE], which_one;
 
 	// iterate through starting wings of player.  Add ship indices of ships which meet
 	// given criteria
 	count = 0;
 	for (i = 0; i < Num_wings; i++ ) {
-		int wingnum;
+		if (count >= MAX_SIZE)
+			break;
 
-		wingnum = -1;
+		int wingnum = -1;
 
 		// multi-team?
 		if(multi_team >= 0){
@@ -12603,6 +12610,9 @@ int ship_get_random_player_wing_ship( int flags, float max_dist, int persona_ind
 		}
 
 		for ( j = 0; j < Wings[wingnum].current_count; j++ ) {
+			if (count >= MAX_SIZE)
+				break;
+
 			ship_index = Wings[wingnum].ship_index[j];
 			Assert( ship_index != -1 );
 
