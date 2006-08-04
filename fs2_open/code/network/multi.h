9f77c108e725a7f1f1309e1819897f4cd7f1142e
karajorma <karajorma@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-08-04 11:45:21 +0000

    Fix bug where end-mission SEXP only resulting in the mission ending for the server

--- fs2_open/code/network/multi.h
+++ fs2_open/code/network/multi.h
@@ -9,14 +9,17 @@
 
 /*
  * $Logfile: /Freespace2/code/Network/Multi.h $
- * $Revision: 2.14 $
- * $Date: 2006-06-07 18:47:51 $
+ * $Revision: 2.15 $
+ * $Date: 2006-08-04 11:45:21 $
  * $Author: karajorma $
  *
  * Header file which contains type definitions for multiplayer, and support for high-level
  * multiplayer functions.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.14  2006/06/07 18:47:51  karajorma
+ * Fix 130 ships limit for Inferno builds
+ *
  * Revision 2.13  2006/06/02 09:10:01  karajorma
  * Added the VARIABLE_UPDATE packet to send sexp variable value changes to client machines.
  *
@@ -552,6 +555,7 @@ extern int Om_tracker_flag;
 #define OBSERVER_UPDATE				0xCA     // sent from observers to server to give position and orientation
 #define SQUADMSG_PLAYER				0xCB		// a squadmate message command has been sent to a netplayer
 #define OBJ_UPDATE_SYNC				0xCC		// object update timebase syncing code
+#define FORCE_MISSION_END			0xCD	// Force all the players to skip straight to the debrief screen
 
 #define WEAPON_DET					0xD1		// a weapon has detonated, possibly with child weapons
 #define SHIP_KILL						0xD2		// a ship was killed
