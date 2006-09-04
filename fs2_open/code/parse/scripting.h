1cd84d8e827c3f008ed1c243a4b5533e1f2d0aa1
wmcoolmon <wmcoolmon@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-09-04 06:11:50 +0000

    enumerations; drawImage update; subsystem orientation and fireWeapon() member objects; many new weapons.tbl data variables; conditional hooks; modular scripting table support (*-sct)

--- fs2_open/code/parse/scripting.h
+++ fs2_open/code/parse/scripting.h
@@ -1,6 +1,7 @@
 #ifndef _SCRIPTING_H
 #define _SCRIPTING_H
 
+#include "globalincs/globals.h"
 #include "globalincs/pstypes.h"
 
 #include <stdio.h>
@@ -19,6 +20,56 @@ struct image_desc
 	int handle;
 };
 
+//**********Main Conditional Hook stuff
+
+#define MAX_HOOK_CONDITIONS	8
+
+//Conditionals
+#define CHC_NONE			-1
+#define CHC_MISSION			0
+#define CHC_SHIP			1
+#define CHC_SHIPCLASS		2
+#define CHC_SHIPTYPE		3
+#define CHC_STATE			4
+
+//Actions
+#define CHA_NONE			-1
+#define CHA_WARPOUT			0
+#define CHA_WARPIN			1
+#define CHA_DEATH			2
+#define CHA_HOOK			3
+
+struct script_condition
+{
+	int condition_type;
+	union
+	{
+		char name[NAME_LENGTH];
+	} data;
+
+	script_condition(){condition_type = CHC_NONE; memset(data.name, 0, sizeof(data.name));}
+};
+
+struct script_action
+{
+	int action_type;
+	script_hook hook;
+
+	script_action(){action_type = CHA_NONE;}
+};
+
+class ConditionedHook
+{
+private:
+	std::vector<script_action> Actions;
+	script_condition Conditions[MAX_HOOK_CONDITIONS];
+public:
+	bool AddCondition(script_condition sc);
+	bool AddAction(script_action sa);
+
+	bool MaybeRun(class script_state *sys, int action, char format='\0', void *data=NULL, struct object *objp=NULL);
+};
+
 //**********Main script_state function
 class script_state
 {
@@ -33,6 +84,7 @@ private:
 
 	//Utility variables
 	std::vector<image_desc> ScriptImages;
+	std::vector<ConditionedHook> ConditionalHooks;
 
 private:
 	PyObject *GetPyLocals(){return PyLoc;}
@@ -49,36 +101,40 @@ private:
 	void Clear();
 
 public:
-	//Init/Deinit
+	//***Init/Deinit
 	script_state(char *name);
 	script_state& operator=(script_state &in);
 	~script_state();
 
-	//Internal scripting stuff
+	//***Internal scripting stuff
 	int LoadBm(char *name);
 	void UnloadImages();
 
 	lua_State *GetLuaSession(){return LuaState;}
 
-	//Init functions for langs
+	//***Init functions for langs
 	int CreateLuaState();
 	//int CreatePyState(const script_py_lib_list *libraries);
 
-	//Get data
+	//***Get data
 	int OutputMeta(char *filename);
 
-	//Moves data
+	//***Moves data
 	//void MoveData(script_state &in);
 
-	//Variable handling functions
+	//***Variable handling functions
 	void SetGlobal(char *name, char format, void *data);
 	bool GetGlobal(char *name, char format='\0', void *data=NULL);
 	void RemGlobal(char *name);
 
-	//Hook handling functions
+	//***Hook creation functions
 	bool EvalString(char* string, char *format=NULL, void *rtn=NULL, char *debug_str=NULL);
 	script_hook ParseChunk(char* debug_str=NULL);
+	bool ParseCondition(char* debug_str=NULL);
+
+	//***Hook running functions
 	int RunBytecode(script_hook &hd, char format='\0', void *data=NULL);
+	int RunCondition(int condition, char format='\0', void *data=NULL, struct object *objp = NULL);
 	bool IsOverride(script_hook &hd);
 };
 
@@ -96,5 +152,7 @@ extern script_hook Script_simulationhook;
 extern script_hook Script_hudhook;
 extern script_hook Script_splashhook;
 extern script_hook Script_gameinithook;
+
+//*************************Conditional scripting*************************
 
 #endif //_SCRIPTING_H
