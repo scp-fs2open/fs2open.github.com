fabd9768ae1f0bfa19d7046ddc8b47660ff70e15
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-10-01 01:12:32 +0000

    enhanced voice acting manager, now with script generator :)

--- code/fred2/voiceactingmanager.cpp
+++ code/fred2/voiceactingmanager.cpp
@@ -15,6 +15,18 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
+// to keep track of data
+char Voice_abbrev_briefing[NAME_LENGTH];
+char Voice_abbrev_campaign[NAME_LENGTH];
+char Voice_abbrev_command_briefing[NAME_LENGTH];
+char Voice_abbrev_debriefing[NAME_LENGTH];
+char Voice_abbrev_message[NAME_LENGTH];
+char Voice_abbrev_mission[NAME_LENGTH];
+bool Voice_no_replace_filenames;
+char Voice_script_entry_format[NOTES_LENGTH];
+int Voice_export_selection;
+
+
 /////////////////////////////////////////////////////////////////////////////
 // VoiceActingManager dialog
 
@@ -30,6 +42,13 @@ VoiceActingManager::VoiceActingManager(CWnd* pParent /*=NULL*/)
 	m_abbrev_message = _T("");
 	m_abbrev_mission = _T("");
 	m_example = _T("");
+	m_no_replace = FALSE;
+	m_script_entry_format = _T("");
+	m_export_everything = FALSE;
+	m_export_command_briefings = FALSE;
+	m_export_briefings = FALSE;
+	m_export_debriefings = FALSE;
+	m_export_messages = FALSE;
 	//}}AFX_DATA_INIT
 }
 
@@ -45,26 +64,37 @@ void VoiceActingManager::DoDataExchange(CDataExchange* pDX)
 	DDX_Text(pDX, IDC_ABBREV_MESSAGE, m_abbrev_message);
 	DDX_Text(pDX, IDC_ABBREV_MISSION, m_abbrev_mission);
 	DDX_Text(pDX, IDC_EXAMPLE, m_example);
+	DDX_Check(pDX, IDC_NO_REPLACE, m_no_replace);
+	DDX_Text(pDX, IDC_ENTRY_FORMAT, m_script_entry_format);
+	DDX_Check(pDX, IDC_EXPORT_EVERYTHING, m_export_everything);
+	DDX_Check(pDX, IDC_EXPORT_COMMAND_BRIEFINGS, m_export_command_briefings);
+	DDX_Check(pDX, IDC_EXPORT_BRIEFINGS, m_export_briefings);
+	DDX_Check(pDX, IDC_EXPORT_DEBRIEFINGS, m_export_debriefings);
+	DDX_Check(pDX, IDC_EXPORT_MESSAGES, m_export_messages);
 	//}}AFX_DATA_MAP
 }
 
 
 BEGIN_MESSAGE_MAP(VoiceActingManager, CDialog)
 	//{{AFX_MSG_MAP(VoiceActingManager)
+	ON_WM_CLOSE()
 	ON_EN_SETFOCUS(IDC_ABBREV_BRIEFING, OnSetfocusAbbrevBriefing)
 	ON_EN_SETFOCUS(IDC_ABBREV_CAMPAIGN, OnSetfocusAbbrevCampaign)
 	ON_EN_SETFOCUS(IDC_ABBREV_COMMAND_BRIEFING, OnSetfocusAbbrevCommandBriefing)
 	ON_EN_SETFOCUS(IDC_ABBREV_DEBRIEFING, OnSetfocusAbbrevDebriefing)
 	ON_EN_SETFOCUS(IDC_ABBREV_MESSAGE, OnSetfocusAbbrevMessage)
 	ON_EN_SETFOCUS(IDC_ABBREV_MISSION, OnSetfocusAbbrevMission)
+	ON_CBN_SETFOCUS(IDC_SUFFIX, OnSetfocusSuffix)
 	ON_EN_CHANGE(IDC_ABBREV_BRIEFING, OnChangeAbbrevBriefing)
 	ON_EN_CHANGE(IDC_ABBREV_CAMPAIGN, OnChangeAbbrevCampaign)
 	ON_EN_CHANGE(IDC_ABBREV_COMMAND_BRIEFING, OnChangeAbbrevCommandBriefing)
 	ON_EN_CHANGE(IDC_ABBREV_DEBRIEFING, OnChangeAbbrevDebriefing)
 	ON_EN_CHANGE(IDC_ABBREV_MESSAGE, OnChangeAbbrevMessage)
 	ON_EN_CHANGE(IDC_ABBREV_MISSION, OnChangeAbbrevMission)
-	ON_BN_CLICKED(IDC_GENERATE_FILE_NAMES, OnGenerateFileNames)
 	ON_CBN_SELCHANGE(IDC_SUFFIX, OnChangeOtherSuffix)
+	ON_BN_CLICKED(IDC_NO_REPLACE, OnChangeNoReplace)
+	ON_BN_CLICKED(IDC_GENERATE_FILE_NAMES, OnGenerateFileNames)
+	ON_BN_CLICKED(IDC_GENERATE_SCRIPT, OnGenerateScript)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -78,7 +108,61 @@ BOOL VoiceActingManager::OnInitDialog()
 	box->AddString(".OGG");
 	box->SetCurSel(0);
 
+	// load saved data for file names
+	m_abbrev_briefing = _T(Voice_abbrev_briefing);
+	m_abbrev_campaign = _T(Voice_abbrev_campaign);
+	m_abbrev_command_briefing = _T(Voice_abbrev_command_briefing);
+	m_abbrev_debriefing = _T(Voice_abbrev_debriefing);
+	m_abbrev_message = _T(Voice_abbrev_message);
+	m_abbrev_mission = _T(Voice_abbrev_mission);
+	m_no_replace = Voice_no_replace_filenames;
+
+	// load saved data for script
+	m_script_entry_format = _T(Voice_script_entry_format);
+	m_export_everything = m_export_command_briefings = m_export_briefings = m_export_debriefings = m_export_messages = FALSE;
+	if (Voice_export_selection == 1)
+		m_export_command_briefings = TRUE;
+	else if (Voice_export_selection == 2)
+		m_export_briefings = TRUE;
+	else if (Voice_export_selection == 3)
+		m_export_debriefings = TRUE;
+	else if (Voice_export_selection == 4)
+		m_export_messages = TRUE;
+	else
+		m_export_everything = TRUE;
+
+	UpdateData(FALSE);
+
 	return TRUE;
+}
+
+void VoiceActingManager::OnClose()
+{
+	UpdateData(TRUE);
+
+	// save data for file names
+	strcpy(Voice_abbrev_briefing, m_abbrev_briefing);
+	strcpy(Voice_abbrev_campaign, m_abbrev_campaign);
+	strcpy(Voice_abbrev_command_briefing, m_abbrev_command_briefing);
+	strcpy(Voice_abbrev_debriefing, m_abbrev_debriefing);
+	strcpy(Voice_abbrev_message, m_abbrev_message);
+	strcpy(Voice_abbrev_mission, m_abbrev_mission);
+	Voice_no_replace_filenames = m_no_replace == TRUE ? true : false;
+
+	// save data for script
+	strcpy(Voice_script_entry_format, m_script_entry_format);
+	if (m_export_command_briefings)
+		Voice_export_selection = 1;
+	else if (m_export_briefings)
+		Voice_export_selection = 2;
+	else if (m_export_debriefings)
+		Voice_export_selection = 3;
+	else if (m_export_messages)
+		Voice_export_selection = 4;
+	else
+		Voice_export_selection = 0;
+
+	CDialog::OnClose();
 }
 
 CString VoiceActingManager::get_suffix()
@@ -165,41 +249,165 @@ void VoiceActingManager::OnGenerateFileNames()
 	int i;
 	int digits;
 
+	// stuff data to variables
+	UpdateData(TRUE);
+
 	// command briefings
 	digits = calc_digits(Cmd_briefs[0].num_stages);
 	for (i = 0; i < Cmd_briefs[0].num_stages; i++)
 	{
-		strcpy(Cmd_briefs[0].stage[i].wave_filename, LPCTSTR(generate_filename(m_abbrev_command_briefing, i + 1, digits)));
+		char *filename = Cmd_briefs[0].stage[i].wave_filename;
+
+		// generate only if we're replacing or if it has a replaceable name
+		if (!m_no_replace || !strlen(filename) || !strnicmp(filename, "none.wav", 4) || message_filename_is_generic(filename))
+		{
+			strcpy(filename, LPCTSTR(generate_filename(m_abbrev_command_briefing, i + 1, digits)));
+		}
 	}
 
 	// briefings
 	digits = calc_digits(Briefings[0].num_stages);
 	for (i = 0; i < Briefings[0].num_stages; i++)
 	{
-		strcpy(Briefings[0].stages[i].voice, LPCTSTR(generate_filename(m_abbrev_briefing, i + 1, digits)));
+		char *filename = Briefings[0].stages[i].voice;
+
+		// generate only if we're replacing or if it has a replaceable name
+		if (!m_no_replace || !strlen(filename) || !strnicmp(filename, "none.wav", 4) || message_filename_is_generic(filename))
+		{
+			strcpy(filename, LPCTSTR(generate_filename(m_abbrev_briefing, i + 1, digits)));
+		}
 	}
 
 	// debriefings
 	digits = calc_digits(Debriefings[0].num_stages);
 	for (i = 0; i < Debriefings[0].num_stages; i++)
 	{
-		strcpy(Debriefings[0].stages[i].voice, LPCTSTR(generate_filename(m_abbrev_debriefing, i + 1, digits)));
+		char *filename = Debriefings[0].stages[i].voice;
+
+		// generate only if we're replacing or if it has a replaceable name
+		if (!m_no_replace || !strlen(filename) || !strnicmp(filename, "none.wav", 4) || message_filename_is_generic(filename))
+		{
+			strcpy(filename, LPCTSTR(generate_filename(m_abbrev_debriefing, i + 1, digits)));
+		}
 	}
 
 	// messages
 	digits = calc_digits(Num_messages - Num_builtin_messages);
 	for (i = 0; i < Num_messages - Num_builtin_messages; i++)
 	{
-		// free existing filename
-		if (Messages[i + Num_builtin_messages].wave_info.name != NULL)
-			free(Messages[i + Num_builtin_messages].wave_info.name);
+		char *filename = Messages[i + Num_builtin_messages].wave_info.name;
 
-		// allocate new filename
-		Messages[i + Num_builtin_messages].wave_info.name = strdup(LPCTSTR(generate_filename(m_abbrev_message, i + 1, digits)));
+		// generate only if we're replacing or if it has a replaceable name
+		if (!m_no_replace || !filename || !strlen(filename) || !strnicmp(filename, "none.wav", 4) || message_filename_is_generic(filename))
+		{
+			// free existing filename
+			if (filename != NULL)
+				free(filename);
+
+			// allocate new filename
+			Messages[i + Num_builtin_messages].wave_info.name = strdup(LPCTSTR(generate_filename(m_abbrev_message, i + 1, digits)));
+		}
 	}
 
 	// notify
 	MessageBox("Filename generation complete.", "Woohoo!");
+}
+
+void VoiceActingManager::OnGenerateScript()
+{
+	int i;
+	char pathname[256];
+
+	// stuff data to variables
+	UpdateData(TRUE);
+
+	// prompt to save script
+	CFileDialog dlg(FALSE, "txt", NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, "Text files (*.txt)|*.txt||");
+	if (dlg.DoModal() != IDOK)
+		return;
+
+	string_copy(pathname, dlg.GetPathName(), 256);
+	fp = cfopen(pathname, "wt", CFILE_NORMAL);
+	if (!fp)
+	{
+		MessageBox("Can't open file to save.", "Error!");
+		return;
+	}
+
+	fout("%s\n", Mission_filename);
+	fout("%s\n", The_mission.name);
+
+	if (m_export_everything || m_export_command_briefings)
+	{
+		fout("\n\nCommand Briefings\n-----------------\n\n");
+
+		for (i = 0; i < Cmd_briefs[0].num_stages; i++)
+		{
+			CString entry = m_script_entry_format;
+			cmd_brief_stage *stage = &Cmd_briefs[0].stage[i];
+			entry.Replace("$filename", stage->wave_filename);
+			entry.Replace("$message", stage->text);
+			entry.Replace("$persona", "<no persona specified>");
+
+			fout("%s\n", (char *) (LPCTSTR) entry);
+		}
+	}
+
+	if (m_export_everything || m_export_briefings)
+	{
+		fout("\n\nBriefings\n---------\n\n");
+
+		for (i = 0; i < Briefings[0].num_stages; i++)
+		{
+			CString entry = m_script_entry_format;
+			brief_stage *stage = &Briefings[0].stages[i];
+			entry.Replace("$filename", stage->voice);
+			entry.Replace("$message", stage->new_text);
+			entry.Replace("$persona", "<no persona specified>");
+
+			fout("%s\n", (char *) (LPCTSTR) entry);
+		}
+	}
+
+	if (m_export_everything || m_export_command_briefings)
+	{
+		fout("\n\nDebriefings\n-----------\n\n");
+
+		for (i = 0; i < Debriefings[0].num_stages; i++)
+		{
+			CString entry = m_script_entry_format;
+			debrief_stage *stage = &Debriefings[0].stages[i];
+			entry.Replace("$filename", stage->voice);
+			entry.Replace("$message", stage->new_text);
+			entry.Replace("$persona", "<no persona specified>");
+	
+			fout("%s\n", (char *) (LPCTSTR) entry);
+		}
+	}
+
+	if (m_export_everything || m_export_command_briefings)
+	{
+		fout("\n\nMessages\n--------\n\n");
+
+		for (i = 0; i < Num_messages - Num_builtin_messages; i++)
+		{
+			CString entry = m_script_entry_format;
+			MMessage *message = &Messages[i + Num_builtin_messages];
+			entry.Replace("$filename", message->wave_info.name);
+			entry.Replace("$message", message->message);
+			if (message->persona_index >= 0)
+				entry.Replace("$persona", Personas[message->persona_index].name);
+			else
+				entry.Replace("$persona", <none>");
+	
+			fout("%s\n", (char *) (LPCTSTR) entry);
+		}
+	}
+
+	cfclose(fp);
+
+	// notify
+	MessageBox("Script generation complete.", "Woohoo!");
 }
 
 void VoiceActingManager::OnSetfocusAbbrevBriefing() 
@@ -248,6 +456,15 @@ void VoiceActingManager::OnSetfocusAbbrevMessage()
 }
 
 void VoiceActingManager::OnSetfocusAbbrevMission() 
+{
+	UpdateData(TRUE);
+
+	build_example();
+
+	UpdateData(FALSE);
+}
+
+void VoiceActingManager::OnSetfocusSuffix()
 {
 	UpdateData(TRUE);
 
@@ -317,4 +534,23 @@ void VoiceActingManager::OnChangeOtherSuffix()
 	build_example();
 
 	UpdateData(FALSE);
+}
+
+void VoiceActingManager::OnChangeNoReplace()
+{
+	UpdateData(TRUE);
+}
+
+int VoiceActingManager::fout(char *format, ...)
+{
+	char str[16384];
+	va_list args;
+	
+	va_start(args, format);
+	vsprintf(str, format, args);
+	va_end(args);
+	Assert(strlen(str) < 16384);
+
+	cfputs(str, fp);
+	return 0;
 }
