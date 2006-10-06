34f54b3cca9785ad5153ecbca6d1ff9dab45ebda
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-10-06 09:31:27 +0000

    re-fix this crap that I managed to break last time (was Mantis bug 1066 I think)

--- code/menuui/playermenu.cpp
+++ code/menuui/playermenu.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/MenuUI/PlayerMenu.cpp $
- * $Revision: 2.31 $
- * $Date: 2006-09-24 13:32:11 $
+ * $Revision: 2.32 $
+ * $Date: 2006-10-06 09:31:27 $
  * $Author: taylor $
  *
  * Code to drive the Player Select initial screen
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.31  2006/09/24 13:32:11  taylor
+ * fix for deletion of multiplayer pilots (Mantis bug #1066)
+ *
  * Revision 2.30  2006/04/20 06:32:07  Goober5000
  * proper capitalization according to Volition
  *
@@ -1117,10 +1120,11 @@ void player_select_delete_pilot()
 	// make sure we do this based upon whether we're in single or multiplayer mode
 	strcpy( filename, Pilots[Player_select_pilot] );
 
-	if (Player_select_mode == PLAYER_SELECT_MODE_SINGLE)
+	if (Player_select_mode == PLAYER_SELECT_MODE_SINGLE) {
 		SAFE_STRCAT( filename, NOX(".pl2"), sizeof(filename) );
-	else
+	} else {
 		SAFE_STRCAT( filename, NOX(".plr"), sizeof(filename) );
+	}
 
 	int del_rval;
 	int popup_rval = 0;
