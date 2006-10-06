83b502b3021175562fe83e4099c876215058bd7c
wmcoolmon <wmcoolmon@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-10-06 04:58:25 +0000

    Fix OGG bug where sound files were always allocated 16 seconds of wavedata

--- code/sound/sound.cpp
+++ code/sound/sound.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Sound/Sound.cpp $
- * $Revision: 2.34.2.2 $
- * $Date: 2006-09-11 01:00:28 $
- * $Author: taylor $
+ * $Revision: 2.34.2.3 $
+ * $Date: 2006-10-06 04:58:25 $
+ * $Author: wmcoolmon $
  *
  * Low-level sound code
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.34.2.2  2006/09/11 01:00:28  taylor
+ * various small compiler warning and strict compiling fixes
+ *
  * Revision 2.34.2.1  2006/07/08 18:07:32  taylor
  * require OpenAL 1.1 under Windows, I think it's causing problems for us to mix users with 1.0 and builds for 1.1
  *   (having 1.0 is fatal to sound, but the game should still work, a corresponding Launcher change goes with this too)
@@ -754,7 +757,7 @@ int snd_load( game_snd *gs, int allow_hardware_load )
 	si->avg_bytes_per_sec	= header->nAvgBytesPerSec;	// 32-bit average bytes per second (nAvgBytesPerSec)
 	si->n_block_align		= header->nBlockAlign;		// 16-bit block alignment (nBlockAlign)
 	si->bits				= header->wBitsPerSample;	// Read 16-bit bits per sample	
-	snd->duration = fl2i(1000.0f * (si->size / (si->bits/8.0f)) / si->sample_rate);	
+	snd->duration = fl2i(1000.0f * ((si->size / (si->bits/8.0f)) / si->sample_rate / si->n_channels));
 
 	type = 0;
 
