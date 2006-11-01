a846582d94ae20e054e4bcac7a8a273d26456f9f
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-11-01 15:55:25 +0000

    make "TBL Info" button in ships editor handle modular tables (Mantis bug #1130) fix bugs where ship names with leading '@' or trailing whitespace didn't get compared properly in "TBL Info"
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@3677 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/fred2/textviewdlg.cpp
+++ code/fred2/textviewdlg.cpp
@@ -51,8 +51,10 @@ END_MESSAGE_MAP()
 
 void text_view_dlg::set(int ship_class)
 {
-	char line[256], line2[256];
-	int i, j, found = 0, comment = 0;
+	char line[256], line2[256], file_text[82];
+	int i, j, n, found = 0, comment = 0, num_files = 0;
+	char tbl_file_arr[MAX_TBL_PARTS][MAX_FILENAME_LEN];
+	char *tbl_file_names[MAX_TBL_PARTS];
 	CFILE *fp;
 
 	if (ship_class < 0)
@@ -61,6 +63,7 @@ void text_view_dlg::set(int ship_class)
 	fp = cfopen("ships.tbl", "r");
 	Assert(fp);
 
+	
 	while (cfgets(line, 255, fp)) {
 		while (line[strlen(line) - 1] == '\n')
 			line[strlen(line) - 1] = 0;
@@ -87,13 +90,17 @@ void text_view_dlg::set(int ship_class)
 
 		line2[j] = 0;
 		if (!strnicmp(line2, "$Name:", 6)) {
+			drop_trailing_white_space(line2);
 			found = 0;
 			i = 6;
-			while (line2[i] == ' ' || line2[i] == '\t')
+
+			while (line2[i] == ' ' || line2[i] == '\t' || line2[i] == '@')
 				i++;
 
-			if (!stricmp(line2 + i, Ship_info[ship_class].name))
+			if (!stricmp(line2 + i, Ship_info[ship_class].name)) {
+				m_edit += "-- ships.tbl  -------------------------------\r\n";
 				found = 1;
+			}
 		}
 
 		if (found) {
@@ -103,6 +110,71 @@ void text_view_dlg::set(int ship_class)
 	}
 
 	cfclose(fp);
+
+
+	// done with ships.tbl, so now check all modular ship tables...
+	num_files = cf_get_file_list_preallocated(MAX_TBL_PARTS, tbl_file_arr, tbl_file_names, CF_TYPE_TABLES, NOX("*-shp.tbm"), CF_SORT_REVERSE);
+
+	for (n = 0; n < num_files; n++){
+		strcat(tbl_file_names[n], ".tbm");
+
+		fp = cfopen(tbl_file_names[n], "r");
+		Assert(fp);
+
+		memset( line, 0, sizeof(line) );
+		memset( line2, 0, sizeof(line2) );
+		found = 0;
+		comment = 0;
+
+		while (cfgets(line, 255, fp)) {
+			while (line[strlen(line) - 1] == '\n')
+				line[strlen(line) - 1] = 0;
+
+			for (i=j=0; line[i]; i++) {
+				if (line[i] == '/' && line[i+1] == '/')
+					break;
+
+				if (line[i] == '/' && line[i+1] == '*') {
+					comment = 1;
+					i++;
+					continue;
+				}
+
+				if (line[i] == '*' && line[i+1] == '/') {
+					comment = 0;
+					i++;
+					continue;
+				}
+
+				if (!comment)
+					line2[j++] = line[i];
+			}
+
+			line2[j] = 0;
+			if (!strnicmp(line2, "$Name:", 6)) {
+				drop_trailing_white_space(line2);
+				found = 0;
+				i = 6;
+
+				while (line2[i] == ' ' || line2[i] == '\t' || line2[i] == '@')
+					i++;
+
+				if (!stricmp(line2 + i, Ship_info[ship_class].name)) {
+					memset( file_text, 0, sizeof(file_text) );
+					snprintf(file_text, sizeof(file_text)-1, "--  %s  -------------------------------\r\n", tbl_file_names[n]);
+					m_edit += file_text;
+					found = 1;
+				}
+			}
+
+			if (found) {
+				m_edit += line;
+				m_edit += "\r\n";
+			}
+		}
+
+		cfclose(fp);
+	}
 }
 
 void text_view_dlg::OnSetfocusEdit1() 
