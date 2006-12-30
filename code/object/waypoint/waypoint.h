7539f7802440f5f297e3da498c0c4be5388c9907
wmcoolmon <wmcoolmon@387891d4-d844-0410-90c0-e4c51a9137d3>
2006-12-30 23:34:18 +0000

    Add missing waypoint files
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@3825 387891d4-d844-0410-90c0-e4c51a9137d3

---
+++ code/object/waypoint/waypoint.h
@@ -0,0 +1,35 @@
+#include "globalincs/globals.h"
+#include "globalincs/pstypes.h"
+
+#ifndef _WAYPOINT_H
+#define _WAYPOINT_H
+
+//********************DEFINES********************
+#define	MAX_WAYPOINTS_PER_LIST	20
+#define	MAX_WAYPOINT_LISTS		32
+
+#define WAYPOINTLIST_INDEX(wlp) (wlp - &Waypoint_lists[0])
+
+//********************STRUCTURES********************
+// waypoint list flags bitmasks.
+#define WL_MARKED	0x01
+
+typedef struct waypoint_list {
+	char		name[NAME_LENGTH];
+	int		count;
+	char		flags[MAX_WAYPOINTS_PER_LIST];
+	vec3d	waypoints[MAX_WAYPOINTS_PER_LIST];
+} waypoint_list;
+
+//********************GLOBALS********************
+extern waypoint_list Waypoint_lists[MAX_WAYPOINT_LISTS];
+extern int Num_waypoint_lists;
+
+//********************FUNCTIONS********************
+//Attempts to create a waypoint on waypoint_list list. Returns -1 on failure.
+int waypoint_create(vec3d *pos, int list);
+
+//Queries for waypoint with given name, returns -1 on failure
+int waypoint_lookup(char *name);
+
+#endif //_WAYPOINT_H
