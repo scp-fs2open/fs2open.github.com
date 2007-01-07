88024e707ba10f9759de5b09391e746ae5292c2b
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2007-01-07 12:41:37 +0000

    make expl info dyanmic fix possible out-of-bounds on expl lod checking fix memory leak from modular tables

--- code/weapon/weapon.h
+++ code/weapon/weapon.h
@@ -12,6 +12,9 @@
  * <insert description of file here>
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.72  2006/12/28 00:59:54  wmcoolmon
+ * WMC codebase commit. See pre-commit build thread for details on changes.
+ *
  * Revision 2.71  2006/09/11 06:51:17  taylor
  * fixes for stuff_string() bounds checking
  *
@@ -558,6 +561,8 @@
 #include "weapon/trails.h"
 #include "weapon/shockwave.h"
 
+#include <vector>
+
 struct object;
 struct ship_subsys;
 
@@ -958,13 +963,14 @@ extern missile_obj Missile_obj_list;
 
 // WEAPON EXPLOSION INFO
 #define MAX_WEAPON_EXPL_LOD						4
-#define MAX_WEAPON_EXPL_INFO					20
 
 typedef struct weapon_expl_lod {
 	char	filename[MAX_FILENAME_LEN];
 	int	bitmap_id;
 	int	num_frames;
 	int	fps;
+
+	weapon_expl_lod() { memset(this, 0, sizeof(weapon_expl_lod)); bitmap_id = -1; }
 } weapon_expl_lod;
 
 typedef struct weapon_expl_info	{
@@ -975,9 +981,7 @@ typedef struct weapon_expl_info	{
 class weapon_explosions
 {
 private:
-	int ExplosionNum;
-	weapon_expl_info ExplosionInfo[MAX_WEAPON_EXPL_INFO];
-
+	std::vector<weapon_expl_info> ExplosionInfo;
 	int GetIndex(char *filename);
 
 public:
