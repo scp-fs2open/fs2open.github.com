93c253dcea012389e75822aa757fe4e78e5877f1
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2007-02-11 09:56:25 +0000

    support for new finding/loading of sound files add support for automatically figuring out samples-per-measure based on Goober's explanation in the Wiki (not sure if it's actually right though) remove NO_SOUND

--- fs2_open/code/gamesnd/eventmusic.cpp
+++ fs2_open/code/gamesnd/eventmusic.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Gamesnd/EventMusic.cpp $
- * $Revision: 2.40.2.3 $
- * $Date: 2007-01-15 01:55:46 $
- * $Author: Goober5000 $
+ * $Revision: 2.40.2.4 $
+ * $Date: 2007-02-11 09:56:25 $
+ * $Author: taylor $
  *
  * C module for high-level control of event driven music 
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.40.2.3  2007/01/15 01:55:46  Goober5000
+ * thanks to WMC for catching this -- it was a goof from back in 2003!
+ *
  * Revision 2.40.2.2  2006/09/11 01:15:04  taylor
  * fixes for stuff_string() bounds checking
  *
@@ -567,6 +570,15 @@ void event_music_init()
 
 	// look for any modular tables
 	parse_modular_table(NOX("*-mus.tbm"), event_music_parse_musictbl);
+
+	for (i = 0; i < Num_soundtracks; i++) {
+		for (j = 0; j < Soundtracks[i].num_patterns; j++) {
+			int spm = snd_get_samples_per_measure(Soundtracks[i].pattern_fnames[j], Pattern_num_measures[i][j]);
+
+			if (spm > 0)
+				Pattern_samples_per_measure[i][j] = spm;
+		}
+	}
 
 	Event_music_inited = TRUE;
 	Event_music_begun = FALSE;
@@ -1397,7 +1409,7 @@ bool parse_soundtrack_line(int strack_idx, int pattern_idx)
 			break;
 		}
 
-		
+
 		if ( count == 0 ) {
 			Pattern_num_measures[strack_idx][pattern_idx] = (float)atof(token);	//Num_measures
 		} else if(count == 1) {
