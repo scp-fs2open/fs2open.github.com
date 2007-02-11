06789e7cb9140459dc4f625baefcc8886fa4261c
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2007-02-11 09:23:14 +0000

    remove AVI/MPG support, we now rely just on MVE/OGG

--- code/cutscene/movie.cpp
+++ code/cutscene/movie.cpp
@@ -9,13 +9,17 @@
 
 /*
  * $Logfile: /Freespace2/code/cutscene/movie.cpp $
- * $Revision: 2.31.2.10 $
- * $Date: 2007-01-07 12:10:18 $
+ * $Revision: 2.31.2.11 $
+ * $Date: 2007-02-11 09:23:14 $
  * $Author: taylor $
  *
  * movie player code
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 2.31.2.10  2007/01/07 12:10:18  taylor
+ * remove D3D support for Theora movies, make sure that it doesn't try to play them (since it wouldn't work properly anyway)
+ * fix triple-buffer problem with page flipping (Mantis #1190)
+ *
  * Revision 2.31.2.9  2006/12/26 05:13:29  taylor
  * add support for Theora movies (the default format)
  * some little bits of cleanup
@@ -101,44 +105,29 @@
 
 #ifdef _WIN32
 #include <windows.h>
-#include "directx/dx8show.h"
 #include "graphics/grd3dinternal.h"
 #endif
 
+#include "graphics/2d.h"
+#include "globalincs/systemvars.h"
 #include "cutscene/movie.h"
 #include "osapi/osapi.h"
 #include "cmdline/cmdline.h"	
-#include "debugconsole/dbugfile.h" 
 #include "cfile/cfile.h"
 #include "cutscene/cutscenes.h" // cutscene_mark_viewable()
-#include "freespace2/freespace.h" // for Game_mode, etc.
 #include "cutscene/mvelib.h"
 #include "cutscene/oggplayer.h"
 #include "menuui/mainhallmenu.h"
 
-// to know if we are in a movie, needed for non-MVE only
-bool Playing_movie = false;
+extern int Game_mode;
+
 
 #define MOVIE_NONE	-1
 #define MOVIE_OGG	0
 #define MOVIE_MVE	1
-#define MOVIE_AVI	2
-#define MOVIE_MPG	3
 
 // This module links freespace movie calls to the actual API calls the play the movie.
 // This module handles all the different requires of OS and gfx API and finding the file to play
-
-void process_messages()
-{
-#ifdef _WIN32
-	MSG msg;
-   	while(PeekMessage(&msg, (HWND) os_get_window(), 0, 0, PM_REMOVE))
-   	{
-   		TranslateMessage(&msg);
-   		DispatchMessage(&msg);
-   	}
-#endif
-}
 
 
 // filename		- file to search for
@@ -149,8 +138,8 @@ int movie_find(char *filename, char *out_name)
 	char full_path[MAX_PATH];
 	char tmp_name[MAX_PATH];
 	int size, offset = 0;
-	const int NUM_EXT = 4;
-	const char *movie_ext[NUM_EXT] = { ".ogg", ".mve", ".avi", ".mpg" };
+	const int NUM_EXT = 2;
+	const char *movie_ext[NUM_EXT] = { ".ogg", ".mve" };
 
 	if (out_name == NULL)
 		return MOVIE_NONE;
@@ -169,15 +158,7 @@ int movie_find(char *filename, char *out_name)
 	if (rc == MOVIE_NONE)
 		return MOVIE_NONE;
 
-	// if it's not in a packfile then we're done
-	// NOTE: MVEs use CFILE internally for the player, so they can work out of VPs
-	if ( ((rc == MOVIE_AVI) || (rc == MOVIE_MPG)) && (offset != 0) ) {
-		mprintf(("MOVIE ERROR:  Found '%s' in a VP archive, but it can't be used from there!\n", full_path));
-		rc = MOVIE_NONE;
-	} else {
-		strcpy( out_name, full_path );
-	}
-
+	strcpy( out_name, full_path );
 
 	return rc;
 }
@@ -207,178 +188,72 @@ bool movie_play(char *name)
 	if (rc == MOVIE_NONE) {
 		mprintf(("MOVIE ERROR: Unable to open movie file '%s' in any supported format.\n", name));
 		return false;
-	} else {
-		DBUGFILE_OUTPUT_1("About to play: %s", full_name);
 	}
-
-#ifndef NO_DIRECT3D
-	// no Theora movies in D3D mode, they don't display properly
-	if ( (rc == MOVIE_OGG) && (gr_screen.mode == GR_DIRECT3D) ) {
-		mprintf(("MOVIE ERROR: Theora movies are not currently supported in Direct3D mode!\n"));
-		return false;
-	}
-#endif
-
-	// MVE checks first since they use a different player
-	if (rc == MOVIE_MVE || rc == MOVIE_OGG) {
-		MVESTREAM *movie_mve = NULL;
-		THEORAFILE *movie_ogg = NULL;
-
-		// NOTE the MVE/Theora code uses CFILE, so only pass the base name and it will load up the movie properly
-		if (rc == MOVIE_MVE)
-			movie_mve = mve_open(name);
-		else
-			movie_ogg = theora_open(name);
-
-		if (movie_mve || movie_ogg) {
-			// kill all background sounds
-			main_hall_pause();
-
-			// clear the screen and hide the mouse cursor
-			Mouse_hidden++;
-			gr_reset_clip();
-			gr_set_color(255, 255, 255);
-			gr_set_clear_color(0, 0, 0);
-			gr_zbuffer_clear(0);
-			// clear first buffer
-			gr_clear();
-			gr_flip();
-			// clear second buffer (may not be one, but that's ok)
-			gr_clear();
-			gr_flip();
-			// clear third buffer (may not be one, but that's ok)
-			gr_clear();
-
-			// ready to play...
-			if (movie_mve) {
-				mve_init(movie_mve);
-				mve_play(movie_mve);
-			} else if (movie_ogg) {
-				theora_play(movie_ogg);
-			}
-
-			// ... done playing, close the movie and show the cursor again
-			if (movie_mve) {
-				mve_shutdown();
-				mve_close(movie_mve);
-			} else if (movie_ogg) {
-				theora_close(movie_ogg);
-			}
-
-			Mouse_hidden--;
-
-			main_hall_unpause();
-
-			return true;
-		} else {
-			// uh-oh, movie is invalid... Abory, Retry, Fail?
-			mprintf(("MOVIE ERROR: Found invalid movie! (%s)\n", full_name));
-			return false;
-		}
-	}
-
-	// no MVE version so move on to AVI/MPG specific player code
-#ifdef _WIN32
-	process_messages();
-
-#ifndef NO_DIRECT3D
-	// This is a bit of a hack but it works nicely
- 	if(gr_screen.mode == GR_DIRECT3D)
-	{
-   		GlobalD3DVars::D3D_activate = 0;
-		GlobalD3DVars::lpD3DDevice->EndScene();
-	  	GlobalD3DVars::lpD3DDevice->Present(NULL,NULL,NULL,NULL);
-		d3d_lost_device();
-	}
-#endif
-
-	Playing_movie = true;
 
 	// kill all background sounds
 	main_hall_pause();
+
 	// clear the screen and hide the mouse cursor
 	Mouse_hidden++;
 	gr_reset_clip();
 	gr_set_color(255, 255, 255);
 	gr_set_clear_color(0, 0, 0);
 	gr_zbuffer_clear(0);
+	// clear first buffer
 	gr_clear();
 	gr_flip();
-	gr_clear(); // fullscreen under Windows appears to need this extra one
+	// clear second buffer (may not be one, but that's ok)
+	gr_clear();
+	gr_flip();
+	// clear third buffer (may not be one, but that's ok)
+	gr_clear();
 
-	HWND hWnd = (HWND)os_get_window();
-
-  	process_messages();
-
-	if (OpenClip(hWnd, full_name)) {
-		do {
-			// Give system threads time to run (and don't sample user input madly)
-			Sleep(100);
-
-			MSG msg;
-
-			while (PeekMessage(&msg, hWnd, 0, 0, PM_REMOVE)) {
- 				TranslateMessage(&msg);
-
-				if (msg.message == WM_ERASEBKGND)
-					continue;
-
-			//	PassMsgToVideoWindow(hWnd, msg.message, msg.wParam, msg.lParam);
-
-				if (msg.message == WM_KEYDOWN) {
-					switch (msg.wParam)
-					{
-						// Quits movie if escape, space or return is pressed
-						case VK_ESCAPE:
-						case VK_SPACE:
-						case VK_RETURN:
-						{
-							goto Done;
-						}
-					}
-				} else {
-			   	  	DispatchMessage(&msg);
-				}
-			}
+	if (rc == MOVIE_OGG) {
+		if (gr_screen.mode == GR_DIRECT3D) {
+			mprintf(("MOVIE ERROR: Theora movies are not currently supported in Direct3D mode!\n"));
+			return false;
 		}
-		// Stream bit of the movie then handle windows messages
-		while (dx8show_stream_movie() == false);
+
+		THEORAFILE *movie_ogg = theora_open(name);
+
+		if (movie_ogg) {
+			// start playing ...
+			theora_play(movie_ogg);
+
+			// ... done playing, close the movie
+			theora_close(movie_ogg);
+		} else {
+			// uh-oh, movie is invalid... Abory, Retry, Fail?
+			mprintf(("MOVIE ERROR: Found invalid movie! (%s)\n", name));
+			return false;
+		}
+	} else if (rc == MOVIE_MVE) {
+		MVESTREAM *movie_mve = mve_open(name);
+
+		if (movie_mve) {
+			// start playing ...
+			mve_init(movie_mve);
+			mve_play(movie_mve);
+
+			// ... done playing, close the movie
+			mve_shutdown();
+			mve_close(movie_mve);
+		} else {
+			// uh-oh, movie is invalid... Abory, Retry, Fail?
+			mprintf(("MOVIE ERROR: Found invalid movie! (%s)\n", name));
+			return false;
+		}
 	}
 
-Done:
+	// show the mouse cursor again, and unpause the mainhall music
 	Mouse_hidden--;
-
 	main_hall_unpause();
-
-	// We finished playing the movie
-	CloseClip( hWnd );
-
-	// reset window settings (works the same for fullscreen too)
-	SetForegroundWindow( hWnd );
-	SetActiveWindow( hWnd );
-
-#ifndef NO_DIRECT3D
-	if (gr_screen.mode == GR_DIRECT3D) {
-	 	GlobalD3DVars::D3D_activate = 1;
-	}
-#endif
-
-	Playing_movie = false;
-
-#else
-
-	STUB_FUNCTION;
-
-#endif
 
 	return true;
 }
 
-bool movie_play_two(char *name1, char *name2)
+void movie_play_two(char *name1, char *name2)
 {
-	bool result1 = movie_play(name1);
-	process_messages();
-	bool result2 = movie_play(name2);
-
-	return result1 && result2;
+	if ( movie_play(name1) )
+		movie_play(name2);
 }
