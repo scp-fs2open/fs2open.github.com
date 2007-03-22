bb87b7c6b8a4f0a3b1a03074afb5364e23891bef
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2007-03-22 20:04:04 +0000

    little bit of cleanup set "#Weak" weapons to always get the "player allowed" flag (Mantis #1301)
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@4070 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/weapon/weapons.cpp
+++ code/weapon/weapons.cpp
@@ -12,6 +12,14 @@
  * <insert description of file here>
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.199  2007/03/22 20:02:46  taylor
+ * make use of generic_anim and generic_bitmap where possible
+ * bunch of cleanup and little optimizations
+ * add "+remove" tbl option to completely get rid of particular beam sections
+ * delayed loading of bitmaps, will only do it after all tbl/tbm have been parsed
+ * if we are using a POF missile which isn't loaded yet, try and load it before going crazy
+ * bug fix and optimization for laser color setting
+ *
  * Revision 2.198  2007/02/20 04:27:22  Goober5000
  * revert to retail behavior
  *
@@ -3759,31 +3767,41 @@ void weapon_load_bitmaps(int weapon_index)
 
 void weapon_do_post_parse()
 {
+	weapon_info *wip;
+	int first_cmeasure_index = -1;
 	int i;
 
 	weapon_sort_by_type();	// NOTE: This has to be first thing!
 	weapon_create_names();
 	weapon_clean_entries();
 
-	//Set default countermeasure index from the saved name
-	if ( strlen(Default_cmeasure_name) ) {
-		for (i = 0; i < Num_weapon_types; i++) {
-			if ( !stricmp(Weapon_info[i].name, Default_cmeasure_name) ) {
+	Default_cmeasure_index = -1;
+
+	// run through weapons list and deal with individual issues
+	for (i = 0; i < Num_weapon_types; i++) {
+		wip = &Weapon_info[i];
+
+		// set default counter-measure index from the saved name
+		if ( (Default_cmeasure_index < 0) && strlen(Default_cmeasure_name) ) {
+			if ( !stricmp(wip->name, Default_cmeasure_name) ) {
 				Default_cmeasure_index = i;
-				break;
 			}
+		}
+
+		// catch a fall back cmeasure index, just in case
+		if ( (first_cmeasure_index < 0) && (wip->wi_flags & WIF_CMEASURE) )
+			first_cmeasure_index = i;
+
+		// if we are a "#weak" weapon then popup a warning if we don't have the "player allowed" flag set
+		if ( stristr(wip->name, "#weak") && !(wip->wi_flags & WIF_PLAYER_ALLOWED) ) {
+			Warning(LOCATION, "Weapon '%s' requires the \"player allowed\" flag, but it's not listed!  Adding it by default.\n", wip->name);
+			wip->wi_flags |= WIF_PLAYER_ALLOWED;
 		}
 	}
 
-	//Oops. Emergency fallback.
-	if (Default_cmeasure_index < 0) {
-		for (i = 0; i < Num_weapon_types; i++) {
-			if (Weapon_info[i].wi_flags & WIF_CMEASURE) {
-				Default_cmeasure_index = i;
-				break;
-			}
-		}
-	}
+	// catch cmeasure fallback
+	if (Default_cmeasure_index < 0)
+		Default_cmeasure_index = first_cmeasure_index;
 
 	// translate all spawn type weapons to referrence the appropriate spawned weapon entry
 	translate_spawn_types();
