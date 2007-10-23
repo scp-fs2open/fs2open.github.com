da8f890f04432b82caa45a7f43562922fe413dfa
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2007-10-23 20:07:42 +0000

    reset Perspective_locked at the start of each mission so that view controls don't get locked until exit
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@4294 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/freespace2/freespace.cpp
+++ code/freespace2/freespace.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Freespace2/FreeSpace.cpp $
- * $Revision: 2.296 $
- * $Date: 2007-09-29 14:31:11 $
- * $Author: karajorma $
+ * $Revision: 2.297 $
+ * $Date: 2007-10-23 20:07:25 $
+ * $Author: taylor $
  *
  * FreeSpace main body
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.296  2007/09/29 14:31:11  karajorma
+ * Fix the double respawn bug (Mantis 788) and hopefully the corresponding server Int3 (Mantis 1213)
+ *
  * Revision 2.295  2007/09/04 00:08:47  Goober5000
  * fix the factoring on the shudder parameters (Mantis #1419)
  *
@@ -2749,6 +2752,8 @@ void game_level_init(int seed)
 	Cheats_enabled = 0;
 
 	Game_shudder_time = -1;
+
+	Perspective_locked = false;
 
 	// reset the geometry batcher, this should to be done pretty soon in this mission load process (though it's not required)
 	batch_reset();
