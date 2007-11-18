44420357ffb6b4601a5f671baf8de46ac85b1782
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2007-11-18 06:57:40 +0000

    gak -- stupid mistake

--- fs2_open/code/mission/missionparse.cpp
+++ fs2_open/code/mission/missionparse.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Mission/MissionParse.cpp $
- * $Revision: 2.229 $
- * $Date: 2007-11-14 14:14:25 $
- * $Author: karajorma $
+ * $Revision: 2.230 $
+ * $Date: 2007-11-18 06:57:40 $
+ * $Author: Goober5000 $
  *
  * main upper level code for parsing stuff
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.229  2007/11/14 14:14:25  karajorma
+ * Fix peristent variables in missions played from the techroom
+ *
  * Revision 2.228  2007/10/12 20:30:02  karajorma
  * Typo
  *
@@ -5586,7 +5589,7 @@ void parse_messages(mission *pm, int flags)
 	// mission file takes the same format as the messages in messages,tbl.  Make parsing
 	// a whole lot easier!!!
 	while ( required_string_either("#Reinforcements", "$Name")){
-		message_parse((flags & MPF_IMPORT_FSM) >= 0);		// call the message parsing system
+		message_parse(flags & MPF_IMPORT_FSM);		// call the message parsing system
 	}
 
 	mprintf(("Ending mission message count : %d\n", Num_message_waves));
