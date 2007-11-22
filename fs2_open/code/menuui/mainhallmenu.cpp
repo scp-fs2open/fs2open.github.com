ef94b0d06721876b8c44fad0770e5e938cae0989
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2007-11-22 05:23:44 +0000

    fix campaign is missing then give player choice to switch campaigns, switch pilots, or exit

--- fs2_open/code/menuui/mainhallmenu.cpp
+++ fs2_open/code/menuui/mainhallmenu.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/MenuUI/MainHallMenu.cpp $
- * $Revision: 2.44.2.11 $
- * $Date: 2007-11-20 01:11:10 $
- * $Author: Goober5000 $
+ * $Revision: 2.44.2.12 $
+ * $Date: 2007-11-22 05:23:44 $
+ * $Author: taylor $
  *
  * Header file for main-hall menu code
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.44.2.11  2007/11/20 01:11:10  Goober5000
+ * recognize a Vasudan main hall even when it isn't the second one; play the appropriate music as soon as the main hall switches
+ *
  * Revision 2.44.2.10  2007/10/15 06:43:15  taylor
  * FS2NetD v.2  (still a work in progress, but is ~98% complete)
  *
@@ -1579,8 +1582,25 @@ void main_hall_do(float frametime)
 	// see if we have a missing campaign and force the player to select a new campaign if so
 	extern bool Campaign_room_no_campaigns;
 	if ( !(Player->flags & PLAYER_FLAGS_IS_MULTI) && Campaign_file_missing && !Campaign_room_no_campaigns ) {
-		popup(PF_USE_AFFIRMATIVE_ICON, 1, POPUP_OK, XSTR( "The currently active campaign cannot be found.  Please select another...", -1));
-		gameseq_post_event(GS_EVENT_CAMPAIGN_ROOM);
+		int rc = popup(0, 3, XSTR("Go to Campaign Room", -1), XSTR("Select another pilot", -1), XSTR("Exit Game", -1), XSTR("The currently active campaign cannot be found.  Please select another...", -1));
+
+		switch (rc) {
+			case 0:
+				gameseq_post_event(GS_EVENT_CAMPAIGN_ROOM);
+				break;
+
+			case 1:
+				gameseq_post_event(GS_EVENT_INITIAL_PLAYER_SELECT);
+				break;
+
+			case 2:
+				main_hall_exit_game();
+				break;
+
+			default:
+				gameseq_post_event(GS_EVENT_CAMPAIGN_ROOM);
+				break;
+		}
 	}
 
 	// maybe run the player tips popup
