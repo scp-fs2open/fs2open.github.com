42e618e3300dbcb5f92adfd05aacdfe8084e9e24
Fabian <Fabian@387891d4-d844-0410-90c0-e4c51a9137d3>
2008-08-15 01:10:23 +0000

    - Fixed mix of friendly vs. non-friendly.
    - Added testwise coward mode, so that friendly AI fighters and bombers do depart when taking too much hits. ;-)

--- code/ai/newai/newai.cpp
+++ code/ai/newai/newai.cpp
@@ -4,26 +4,57 @@
 
 struct aicode_call_table AICodeNewAITable;
 
+#include "iff_defs/iff_defs.h"
+
+#define DEPARTURE_HULL_PERCENT 0.3 // Departure if 15% to 30% hull strength left
+
+void newai_ai_maybe_depart(object* objp_ship)
+{
+	ship *shipp = &Ships[objp_ship->instance];
+
+	// Coward mode:
+	//
+	// Depart if:
+	//   * not player and
+	//   * same team as player and player not traitor and
+	//   * fighter or bomber
+	//   * hull multiplied with random value below critical
+	
+	if (objp_ship->flags & OF_PLAYER_SHIP)
+		return;
+
+	// make sure player is on their team and not a traitor
+	if ((Player_ship->team != shipp->team) || (Player_ship->team == Iff_traitor))
+		return;
+
+	if ((Ship_info[shipp->ship_info_index].flags & (SIF_FIGHTER|SIF_BOMBER)) == 0)
+		return;
+
+	if (objp_ship->hull_strength*(frand()+1) < DEPARTURE_HULL_PERCENT * shipp->ship_max_hull_strength)
+		mission_do_departure(objp_ship);
+}
+
 //      Object *objp_ship was hit by either weapon *objp_weapon or collided into by ship hit_objp at point *hitpos.
 void    newai_aicode_ai_ship_hit (object *objp_ship, object *hit_objp, vec3d *hitpos, int shield_quadrant, vec3d *hit_normal)
 {
+	// Call the default implementation
 	AICodeDefaultTable.ai_ship_hit(objp_ship, hit_objp, hitpos, shield_quadrant, hit_normal);
-
+	
 	if (hit_objp->parent >= 0)
 	{
 		object *objp_hitter = &Objects[hit_objp->parent];
-		object *objp_hit = objp_ship;
 		
-		if ( objp_hitter->type != OBJ_SHIP )
-			return;
-
-		ship *shipp_hitter = &Ships[objp_hitter->instance];
-		ship *shipp_hit = &Ships[objp_hit->instance];
+		if ( objp_hitter->type == OBJ_SHIP )
+		{
+			ship *shipp_hitter = &Ships[objp_hitter->instance];
+			ship *shipp_hit = &Ships[objp_ship->instance];
 						
-		fprintf(stderr, "Ship %s friendly hit %s\n", shipp_hitter->ship_name, shipp_hit->ship_name);
+			fprintf(stderr, "Ship %s hit %s\n", shipp_hitter->ship_name, shipp_hit->ship_name);
+		}
 
 	}
 
+	newai_ai_maybe_depart(objp_ship);
 }
 
 
@@ -39,7 +70,7 @@ void newai_aicode_maybe_process_friendly_hit(object *objp_hitter, object *objp_h
 		ship *shipp_hitter = &Ships[objp_hitter->instance];
 		ship *shipp_hit = &Ships[objp_hit->instance];
 						
-		fprintf(stderr, "Ship %s hit %s\n", shipp_hitter->ship_name, shipp_hit->ship_name);
+		fprintf(stderr, "Ship %s friendly hit %s\n", shipp_hitter->ship_name, shipp_hit->ship_name);
 	}
 }
 
