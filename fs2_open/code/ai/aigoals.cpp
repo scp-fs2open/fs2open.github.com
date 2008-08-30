cf994a011d645afe9dbffe7213db2e2f06196907
WMCoolmon <WMCoolmon@387891d4-d844-0410-90c0-e4c51a9137d3>
2008-08-30 10:37:51 +0000

    - Fixed bug involving too many goals being assigned to a ship from wing orders

--- fs2_open/code/ai/aigoals.cpp
+++ fs2_open/code/ai/aigoals.cpp
@@ -1837,11 +1837,27 @@ void ai_copy_mission_wing_goal( ai_goal *aigp, ai_info *aip )
 {
 	int j;
 
+	//1: Find empty spot
 	for ( j = 0; j < MAX_AI_GOALS; j++ ) {
 		if ( aip->goals[j].ai_mode == AI_GOAL_NONE )
 			break;
 	}
-	Assert ( j < MAX_AI_GOALS );
+	
+	//WMC - Fix assert when somebody assigns too many goals to a ship
+	//2: Find lower priority goal
+	if( j >= MAX_AI_GOALS )
+	{
+		for(j = 0; j < MAX_AI_GOALS; j++) {
+			if(aip->goals[j].priority < aigp->priority)
+				break;
+		}
+	}
+
+	//3: Everything is same priority or above. Use the last slot.
+	if(j >= MAX_AI_GOALS)
+	{
+		j = MAX_AI_GOALS-1;
+	}
 	aip->goals[j] = *aigp;
 }
 
