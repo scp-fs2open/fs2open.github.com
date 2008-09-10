40419fb891a3bbfc6dd3d946f90ddc2b27dc2341
karajorma <karajorma@387891d4-d844-0410-90c0-e4c51a9137d3>
2008-09-10 17:36:09 +0000

    Allow players to filter who they send messages to when using the chatbox

--- code/network/multi_pmsg.cpp
+++ code/network/multi_pmsg.cpp
@@ -469,13 +469,16 @@ void multi_msg_display_mission_text(char *msg,int player_index)
 // if the passed net_player's callsign matches the reg expression of the passed expr
 int multi_msg_matches_expr(net_player *np,char *expr)
 {
-	char callsign[CALLSIGN_LEN+1];
-	int len,idx;
-
 	// some error checking
 	if((np == NULL) || (expr == NULL) || (strlen(expr) <= 0)){
 		return 0;
 	}
+
+	return stricmp(expr, np->m_player->callsign) ? 0 : 1 ; 
+	/*
+	char callsign[CALLSIGN_LEN+1];
+	int len,idx;
+
 
 	// get the completely lowercase callsign
 	memset(callsign,0,CALLSIGN_LEN+1);
@@ -488,13 +491,13 @@ int multi_msg_matches_expr(net_player *np,char *expr)
 	len = strlen(expr);
 	for(idx=0;idx<len;idx++){
 		// look for non-matching characters
-		if(callsign[idx] != expr[idx]){
+		if(callsign[idx] != (char)tolower(expr[idx])){
 			return 0;
 		}
 	}
 				
 	// matches
-	return 1;
+	return 1;*/
 }
 
 // if text input mode is active, clear it
