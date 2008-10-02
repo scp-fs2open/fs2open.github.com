3b7aaaa300f09268dd163efd219c81cd2eeae01e
Swifty <Swifty@387891d4-d844-0410-90c0-e4c51a9137d3>
2008-10-02 05:58:01 +0000

    Statically link TrackIR support; FS2_TrackIR.dll no longer required at run-time.

--- fs2_open/code/io/trackir.h
+++ fs2_open/code/io/trackir.h
@@ -1,4 +1,3 @@
-
 #ifndef _TRACKIR_H
 #define _TRACKIR_H
 
@@ -6,47 +5,42 @@
 #include "osapi/osapi.h"
 
 #ifdef WIN32
-#include <windows.h>
+ #include <windows.h>
+
+ #define TIR_APIENTRY __stdcall
+#else
+ #define TIR_APIENTRY
 #endif
 
-// Set up function pointers
-typedef int (*TRACKIR_INIT_FUNC)( HWND );
-typedef int (*TRACKIR_QUERY_FUNC)();
-typedef float (*TRACKIR_GETYAW_FUNC)();
-typedef float (*TRACKIR_GETPITCH_FUNC)();
-typedef float (*TRACKIR_GETX_FUNC)();
-typedef float (*TRACKIR_GETY_FUNC)();
-typedef float (*TRACKIR_GETZ_FUNC)();
-typedef int (*TRACKIR_SHUTDOWN_FUNC)();
+void initialize_trackir();
 
-extern TRACKIR_INIT_FUNC TrackIR_Init_Proc; // Returns 0 if TrackIR has successfully initialized. Non-zero if error.
-extern TRACKIR_QUERY_FUNC TrackIR_Query_Proc; 
-extern TRACKIR_GETYAW_FUNC TrackIR_GetYaw_Proc;
-extern TRACKIR_GETPITCH_FUNC TrackIR_GetPitch_Proc;
-extern TRACKIR_GETX_FUNC TrackIR_GetX_Proc;
-extern TRACKIR_GETY_FUNC TrackIR_GetY_Proc;
-extern TRACKIR_GETZ_FUNC TrackIR_GetZ_Proc;
-extern TRACKIR_SHUTDOWN_FUNC TrackIR_ShutDown_Proc;
-
-// 0 if TrackIR is not installed or is disabled. 
 extern int trackir_enabled;
 
-#ifdef WIN32
-
-extern HMODULE trackIR_dll;
-
+#if defined(__cplusplus)
+extern "C" {
 #endif
 
-void initialize_trackir(); // The function that loads the FS2_TrackIR DLL and appropriates the virtual functions accordingly.
-
-// These are safe to call even if the DLL isn't loaded or if we're not running on Windows.
-int TrackIR_Init( HWND hwnd); 
-int TrackIR_Query(); 
-float TrackIR_GetYaw();
-float TrackIR_GetPitch();
-float TrackIR_GetX();
-float TrackIR_GetY();
-float TrackIR_GetZ();
-int TrackIR_ShutDown();
-
+#ifdef WIN32 // HWND doesn't exist outside of Windows
+ int TIR_APIENTRY TrackIR_Init(HWND FS2_Window);
 #endif
+
+int TIR_APIENTRY TrackIR_Query();
+float TIR_APIENTRY TrackIR_GetPitch();
+float TIR_APIENTRY TrackIR_GetPitchRaw();
+float TIR_APIENTRY TrackIR_GetYaw();
+float TIR_APIENTRY TrackIR_GetYawRaw();
+float TIR_APIENTRY TrackIR_GetRoll();
+float TIR_APIENTRY TrackIR_GetRollRaw();
+float TIR_APIENTRY TrackIR_GetX();
+float TIR_APIENTRY TrackIR_GetXRaw();
+float TIR_APIENTRY TrackIR_GetY();
+float TIR_APIENTRY TrackIR_GetYRaw();
+float TIR_APIENTRY TrackIR_GetZ();
+float TIR_APIENTRY TrackIR_GetZRaw();
+int TIR_APIENTRY TrackIR_ShutDown();
+
+#if defined(__cplusplus)
+}  
+#endif /* extern "C" */
+
+#endif /* _TRACKIR_H */
