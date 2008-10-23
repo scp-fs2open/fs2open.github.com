862d6f43db026ef9c780f69ed5e9ad85ad0062b4
taylor <taylor@387891d4-d844-0410-90c0-e4c51a9137d3>
2008-10-23 13:27:21 +0000

    mode if statement fixage

--- code/freespace2/freespace.cpp
+++ code/freespace2/freespace.cpp
@@ -4944,7 +4944,7 @@ void say_view_target()
 			}
 
 			end_string_at_first_hash_symbol(view_target_name);
-			if ( view_target_name ) {
+			if ( strlen(view_target_name) ) {
 				HUD_fixed_printf(0.0f, XSTR( "Viewing %s%s\n", 185), (Viewer_mode & VM_OTHER_SHIP) ? XSTR( "from ", 186) : "", view_target_name);
 				Show_viewing_from_self = 1;
 			}
@@ -6473,7 +6473,7 @@ void game_frame(int paused)
 				hud_show_radar();
 			}
 
-			if( (Game_detail_flags & DETAIL_FLAG_HUD) && !(Game_mode & GM_MULTIPLAYER) || ( (Game_mode & GM_MULTIPLAYER) && !(Net_player->flags & NETINFO_FLAG_OBSERVER) ) ) {
+			if( (Game_detail_flags & DETAIL_FLAG_HUD) && (!(Game_mode & GM_MULTIPLAYER) || ((Game_mode & GM_MULTIPLAYER) && !(Net_player->flags & NETINFO_FLAG_OBSERVER))) ) {
 				hud_maybe_clear_head_area();
 				anim_render_all(0, flFrametime);
 			}
@@ -6608,7 +6608,7 @@ void game_reset_time()
 	if((Game_mode & GM_MULTIPLAYER) && (Netgame.game_state == NETGAME_STATE_SERVER_TRANSFER)){
 		return ;
 	}
-	
+
 	//	Last_time = timer_get_fixed_seconds();
 	game_start_time();
 	timestamp_reset();
@@ -8213,8 +8213,8 @@ void game_enter_state( int old_state, int new_state )
 			// if we are coming from the briefing, ship select, weapons loadout, or main menu (in the
 			// case of quick start), then do bitmap loads, etc  Don't do any of the loading stuff
 			// if we are in multiplayer -- this stuff is all handled in the multi-wait section
-			if ( !(Game_mode & GM_MULTIPLAYER) && (old_state == GS_STATE_BRIEFING) || (old_state == GS_STATE_SHIP_SELECT) ||
-				(old_state == GS_STATE_WEAPON_SELECT) || (old_state == GS_STATE_MAIN_MENU) || (old_state == GS_STATE_MULTI_STD_WAIT)	|| (old_state == GS_STATE_SIMULATOR_ROOM) ) {
+			if ( !(Game_mode & GM_MULTIPLAYER) && ((old_state == GS_STATE_BRIEFING) || (old_state == GS_STATE_SHIP_SELECT) ||
+				(old_state == GS_STATE_WEAPON_SELECT) || (old_state == GS_STATE_MAIN_MENU) || (old_state == GS_STATE_MULTI_STD_WAIT) || (old_state == GS_STATE_SIMULATOR_ROOM)) ) {
 					// JAS: Used to do all paging here.
 
 					#ifndef NDEBUG
@@ -8229,7 +8229,7 @@ void game_enter_state( int old_state, int new_state )
 			}
 
 			// special code that restores player ship selection and weapons loadout when doing a quick start
-			if ( !(Game_mode & GM_MULTIPLAYER) && (old_state == GS_STATE_MAIN_MENU) || (old_state == GS_STATE_DEATH_BLEW_UP)  || (old_state == GS_STATE_GAME_PLAY) ) {
+			if ( !(Game_mode & GM_MULTIPLAYER) && ((old_state == GS_STATE_MAIN_MENU) || (old_state == GS_STATE_DEATH_BLEW_UP)  || (old_state == GS_STATE_GAME_PLAY)) ) {
 				if ( !stricmp(Player_loadout.filename, Game_current_mission_filename) ) {
 					wss_direct_restore_loadout();
 				}
@@ -8240,7 +8240,7 @@ void game_enter_state( int old_state, int new_state )
 				event_music_first_pattern();	// start the first pattern
 			}
 
-			if ( !(Game_mode & GM_STANDALONE_SERVER) && (old_state != GS_STATE_GAME_PAUSED) && (old_state != GS_STATE_MULTI_PAUSED) ) {
+			if ( !(Game_mode & GM_STANDALONE_SERVER) && ((old_state != GS_STATE_GAME_PAUSED) && (old_state != GS_STATE_MULTI_PAUSED)) ) {
 				event_music_first_pattern();	// start the first pattern
 			}			
 			player_restore_target_and_weapon_link_prefs();
@@ -8288,7 +8288,7 @@ void mouse_force_pos(int x, int y);
 	
 			// under certain circumstances, the server should reset the object update rate limiting stuff
 			if( ((Game_mode & GM_MULTIPLAYER) && (Net_player->flags & NETINFO_FLAG_AM_MASTER)) &&
-				 (old_state == GS_STATE_MULTI_PAUSED) || (old_state == GS_STATE_MULTI_MISSION_SYNC) ){
+				 ((old_state == GS_STATE_MULTI_PAUSED) || (old_state == GS_STATE_MULTI_MISSION_SYNC)) ){
 				
 				// reinitialize the rate limiting system for all clients
 				multi_oo_rate_init_all();
