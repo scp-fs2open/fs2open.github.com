a7570926c980679c0b9108a4ca21fa146036083e
karajorma <karajorma@387891d4-d844-0410-90c0-e4c51a9137d3>
2009-07-18 17:48:22 +0000

    Some minor changes which appear to have slipped into one codebase but not the other.

--- code/object/object.cpp
+++ code/object/object.cpp
@@ -1987,3 +1987,12 @@ int object_get_model(object *objp)
 
 	return -1;
 }
+
+bool is_valid_objnum(int objnum)
+{
+	if (objnum >= 0 && objnum < MAX_OBJECTS) {
+		return true;
+	}
+
+	return false; 
+}
