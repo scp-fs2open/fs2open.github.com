976b86d607449841ac5914337d98b9c9c542473b
Wanderer <Wanderer@387891d4-d844-0410-90c0-e4c51a9137d3>
2009-09-09 15:41:07 +0000

    fixing a logic error from the previous fix

--- code/model/modelread.cpp
+++ code/model/modelread.cpp
@@ -1706,7 +1706,7 @@ int read_model_file(polymodel * pm, char *filename, int n_subsystems, model_subs
 
 						cfread_vector(&(p->pnt), fp);
 						cfread_vector( &temp_vec, fp );
-						if (IS_VEC_NULL_SQ_SAFE(&temp_vec))
+						if (!IS_VEC_NULL_SQ_SAFE(&temp_vec))
 							vm_vec_normalize(&temp_vec);
 						else
 							vm_vec_zero(&temp_vec);
