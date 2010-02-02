9617c252a8f8e0ac559b46d1b0c3283df6f02ab6
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2010-02-02 08:12:44 +0000

    add licensing boilerplate to the new Java files add license files to the docs folder add At4J libraries

---
+++ fsoinstaller/docs/readme.txt
@@ -0,0 +1,32 @@
+README
+------
+
+we need some sort of readme here
+
+
+
+LICENSE
+-------
+
+The FreeSpace Open Installer, as a whole, is licensed under the GNU General Public License, version
+3.0.  The complete application package, containing source code and library dependencies, can be
+downloaded from the FSO Installer website at http://www.fsoinstaller.com/.
+
+Various components of the FSO Installer are themselves available under their separate licenses, all
+of which must be subsumed under the GNU GPL version 3.0 for purposes of coherent distribution.
+Here is a list of all FSO Installer components and their licenses:
+
+FSO Installer (application code only, sans dependencies)
+http://www.fsoinstaller.com/
+GNU General Public License, version 2.0
+
+Archive Tools for Java
+http://www.at4j.org/
+GNU Lesser General Public License, version 3.0
+
+Apache Log4J
+http://logging.apache.org/log4j/
+Apache License, version 2.0
+
+Non-FS2SCP-controlled components provide source code at their respective websites.  However, the
+text of all licenses can be found in the FSO Installer jar package.
--- fsoinstaller/src/com/fsoinstaller/common/BaseURL.java
+++ fsoinstaller/src/com/fsoinstaller/common/BaseURL.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the FreeSpace Open Installer
+ * Copyright (C) 2010 The FreeSpace 2 Source Code Project
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
 package com.fsoinstaller.common;
 
@@ -32,12 +50,13 @@ public class BaseURL
 	}
 
 	/**
-	 * Ensure that we use HTTP; that we are pointing to a folder, not a file; and that
-	 * we are absolute. URIs are used because checking for correctness is "friendlier"
-	 * than with URLs, in that URIs only require correct syntax. URLs require protocol
-	 * checking and other such nastiness that we don't need to deal with until we're
-	 * actually ready to go online. (Besides, this way we can filter out non-HTTP
-	 * protocols before even constructing a URL object.)
+	 * Ensure that we use HTTP; that we are pointing to a folder, not a file;
+	 * and that we are absolute. URIs are used because checking for correctness
+	 * is "friendlier" than with URLs, in that URIs only require correct syntax.
+	 * URLs require protocol checking and other such nastiness that we don't
+	 * need to deal with until we're actually ready to go online. (Besides, this
+	 * way we can filter out non-HTTP protocols before even constructing a URL
+	 * object.)
 	 */
 	public static boolean validateURL(URI theURL)
 	{
