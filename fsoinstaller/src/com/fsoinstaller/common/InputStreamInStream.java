6c576399027f28e643772e04a6be3511524c052a
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2010-03-02 07:26:43 +0000

    comments and minor cleanup

--- fsoinstaller/src/com/fsoinstaller/common/InputStreamInStream.java
+++ fsoinstaller/src/com/fsoinstaller/common/InputStreamInStream.java
@@ -1,21 +1,39 @@
+/*
+ * This file is part of the FreeSpace Open Installer
+ * Copyright (C) 2010 The FreeSpace 2 Source Code Project
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
 package com.fsoinstaller.common;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
-import org.apache.log4j.Logger;
 
 import net.sf.sevenzipjbinding.IInStream;
 import net.sf.sevenzipjbinding.SevenZipException;
 
 
+/**
+ * This class acts as a bridge between the IInStream API used by 7-Zip-JBinding
+ * and the InputStream API used by Java.
+ * 
+ * @author Goober5000
+ */
 public class InputStreamInStream implements IInStream
 {
-	@SuppressWarnings("unused")
-	private static final Logger logger = Logger.getLogger(InputStreamInStream.class);
-
 	private static final int defaultBufferSize = 8192;
 	private static final int MAX_SEEK_TRIES = 10;
 
@@ -256,37 +274,6 @@ public class InputStreamInStream implements IInStream
 		{
 			currentInputStream.close();
 			currentInputStream = null;
-		}
-	}
-
-	public static void main(String[] args) throws SevenZipException
-	{
-		String thing = "The quick brown fox jumped over the lazy dogs.";
-		final byte[] array = thing.getBytes();
-
-		IInStream iis = new InputStreamInStream(new InputStreamSource()
-		{
-			@Override
-			public InputStream recycleInputStream(InputStream oldInputStream) throws IOException
-			{
-				return new ByteArrayInputStream(array);
-			}
-		}, array.length, 8);
-
-		int tempyoffset = 0;
-
-		byte[] myArray = new byte[12];
-		while (true)
-		{
-			int numRead = iis.read(myArray);
-			if (numRead == 0)
-				break;
-
-			if (tempyoffset != 0)
-				iis.seek(tempyoffset, SEEK_CUR);
-
-			for (int i = 0; i < numRead; i++)
-				System.out.print((char) myArray[i]);
 		}
 	}
 }
--- fsoinstaller/src/com/fsoinstaller/common/OutputStreamSequentialOutStream.java
+++ fsoinstaller/src/com/fsoinstaller/common/OutputStreamSequentialOutStream.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the FreeSpace Open Installer
+ * Copyright (C) 2010 The FreeSpace 2 Source Code Project
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
 package com.fsoinstaller.common;
 
@@ -8,6 +26,12 @@ import net.sf.sevenzipjbinding.ISequentialOutStream;
 import net.sf.sevenzipjbinding.SevenZipException;
 
 
+/**
+ * This class acts as a bridge between the OutputStream API used by Java and the
+ * ISequentialOutStream API used by 7-Zip-JBinding.
+ * 
+ * @author Goober5000
+ */
 public class OutputStreamSequentialOutStream implements ISequentialOutStream
 {
 	protected OutputStream currentOutputStream;
