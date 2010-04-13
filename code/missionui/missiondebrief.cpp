1362e16c4ce8b6c58cd2270bcbc3d71698dd9c4f
karajorma <karajorma@387891d4-d844-0410-90c0-e4c51a9137d3>
2010-04-13 11:14:20 +0000

    Remove a hack which wouldn't allow players to skip the penultimate FS2 mission if they failed it 5 times.

--- code/missionui/missiondebrief.cpp
+++ code/missionui/missiondebrief.cpp
@@ -2634,15 +2634,6 @@ void debrief_do_frame(float frametime)
 
 	gr_flip();
 
-	// don't let dude skip 3-09.  hack.	
-	if(Game_mode & GM_CAMPAIGN_MODE){
-		if((Campaign.current_mission >= 0) && (Campaign.current_mission < MAX_CAMPAIGN_MISSIONS)){
-			if ((Campaign.missions[Campaign.current_mission].name != NULL) && !stricmp(Campaign.missions[Campaign.current_mission].name, "sm3-09.fs2")) {
-				Debrief_skip_popup_already_shown = 1;
-			}
-		}
-	}	
-
 	// maybe show skip mission popup
 	if ( Must_replay_mission && (!Debrief_skip_popup_already_shown) && (Player->show_skip_popup) && (Game_mode & GM_NORMAL) && (Game_mode & GM_CAMPAIGN_MODE) && (Player->failures_this_session >= PLAYER_MISSION_FAILURE_LIMIT) && !(Game_mode & GM_MULTIPLAYER)) {
 		int popup_choice = popup(0, 3, XSTR("Do Not Skip This Mission", 1473),
