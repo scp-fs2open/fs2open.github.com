7a8973eade6222bd6296d25458ad9f1c33e87805
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2010-07-05 02:28:48 +0000

    Introducing new SIF2 flag ("no pain flash") to turn off the pain flash. I guess all the peeps who want to do capship missions can rejoice now.

--- code/ship/ship.h
+++ code/ship/ship.h
@@ -815,6 +815,7 @@ extern int ship_find_exited_ship_by_signature( int signature);
 #define SIF2_NO_THRUSTER_GEO_NOISE			(1 << 8)	// Echelon9 - No thruster geometry noise.
 #define SIF2_INTRINSIC_NO_SHIELDS			(1 << 9)	// Chief - disables shields for this ship even without No Shields in mission.
 #define SIF2_NO_PRIMARY_LINKING				(1 << 10)	// Chief - slated for 3.7 originally, but this looks pretty simple to implement.
+#define SIF2_NO_PAIN_FLASH					(1 << 11)	// The E - disable red pain flash
 
 #define	MAX_SHIP_FLAGS	11		//	Number of distinct flags for flags field in ship_info struct
 #define	SIF_DEFAULT_VALUE		0
