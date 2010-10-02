ab8a83e27108b4e47cd00d90ae5b0cb90a7bfb0d
Zacam <Zacam@387891d4-d844-0410-90c0-e4c51a9137d3>
2010-10-02 07:18:59 +0000

    Trunk Sync - r6558.

--- code/weapon/weapons.cpp
+++ code/weapon/weapons.cpp
@@ -2196,7 +2196,7 @@ int parse_weapon(int subtype, bool replace)
 				stuff_float(&bsip->flicker); 
 				//Sanity
 				if (bsip->flicker < 0.0f || bsip->flicker > 1.0f) {
-					mprintf(("WARNING: Invalid value found for +Flicker on section %d of beam %s. Valid range is 0.0 to 1.0, values will be adjusted.\n"), wip->b_info.beam_num_sections, wip->name);
+					mprintf(("WARNING: Invalid value found for +Flicker on section %d of beam %s. Valid range is 0.0 to 1.0, values will be adjusted.\n", wip->b_info.beam_num_sections, wip->name));
 					CLAMP(bsip->flicker, 0.0f, 1.0f);
 				}
 			}
