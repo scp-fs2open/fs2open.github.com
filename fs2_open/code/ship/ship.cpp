dc71cc2c85f6271f5464fb78a3ff8d7836db1ca1
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2010-12-03 23:27:58 +0000

    Turning these Warnings into Assertions, as it's something that really needs to be fixed.

--- fs2_open/code/ship/ship.cpp
+++ fs2_open/code/ship/ship.cpp
@@ -7793,7 +7793,7 @@ void ship_set_default_weapons(ship *shipp, ship_info *sip)
 	// Primary banks
 	if ( pm->n_guns > sip->num_primary_banks ) {
 		Assert(pm->n_guns <= MAX_SHIP_PRIMARY_BANKS);
-		Warning(LOCATION, "There are %d primary banks in the model file,\nbut only %d primary banks specified for %s\n", pm->n_guns, sip->num_primary_banks, sip->name);
+		Assertion(pm->n_guns == sip->num_primary_banks, "There are %d primary banks in the model file,\nbut only %d primary banks specified for %s\nThis must be fixed, as it will cause crashes.\n", pm->n_guns, sip->num_primary_banks, sip->name);
 		for ( i = sip->num_primary_banks; i < pm->n_guns; i++ ) {
 			// Make unspecified weapon for bank be a laser
 			for ( j = 0; j < Num_player_weapon_precedence; j++ ) {
@@ -7816,11 +7816,11 @@ void ship_set_default_weapons(ship *shipp, ship_info *sip)
 	// Secondary banks
 	if ( pm->n_missiles > sip->num_secondary_banks ) {
 		Assert(pm->n_missiles <= MAX_SHIP_SECONDARY_BANKS);
-		Warning(LOCATION, "There are %d secondary banks in model,\nbut only %d secondary banks specified for %s\n", pm->n_missiles, sip->num_secondary_banks, sip->name);
+		Assertion(pm->n_missiles == sip->num_secondary_banks, "There are %d secondary banks in model,\nbut only %d secondary banks specified for %s\nThis must be fixed, as it will cause crashes.\n", pm->n_missiles, sip->num_secondary_banks, sip->name);
 		for ( i = sip->num_secondary_banks; i < pm->n_missiles; i++ ) {
 			// Make unspecified weapon for bank be a missile
 			for ( j = 0; j < Num_player_weapon_precedence; j++ ) {
-				Assert(Player_weapon_precedence[j] > 0);
+				Assertion((Player_weapon_precedence[j] > 0), "Error reading player weapon precedence list. Check weapons.tbl for $Player Weapon Precedence entry, and correct as necessary.\n");
 				int weapon_id = Player_weapon_precedence[j];
 				if (Weapon_info[weapon_id].subtype == WP_MISSILE) {
 					swp->secondary_bank_weapons[i] = weapon_id;
