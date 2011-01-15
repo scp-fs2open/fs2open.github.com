3c2c5cbb587164616482192e181025a978e98113
Echelon9 <Echelon9@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-01-15 03:05:01 +0000

    Cleanup cmeasure.*: Remove dead code, use size_t as for loop array index not a signed int

--- fs2_open/code/cmeasure/cmeasure.cpp
+++ fs2_open/code/cmeasure/cmeasure.cpp
@@ -10,8 +10,6 @@
 
 
 #include "cmeasure/cmeasure.h"
-//#include "freespace2/freespace.h"
-//#include "model/model.h"
 #include "ship/ship.h"
 #include "math/staticrand.h"
 #include "object/object.h"
@@ -21,191 +19,8 @@
 #include "network/multimsgs.h"
 #include "mission/missionparse.h"
 
-
-//cmeasure_info Cmeasure_info[MAX_CMEASURE_TYPES];
-//cmeasure Cmeasures[MAX_CMEASURES];
-
-//int	Num_cmeasure_types = 0;
-//int	Num_cmeasures = 0;
-//int	Cmeasure_inited = 0;
 int	Cmeasures_homing_check = 0;
 int	Countermeasures_enabled = 1;			//	Debug, set to 0 means no one can fire countermeasures.
-
-// This will get called at the start of each level.
-//Not anymore - WMC
-/*
-void cmeasure_init()
-{
-	int i;
-
-	if ( !Cmeasure_inited )
-		Cmeasure_inited = 1;
-
-	// Reset everything between levels
-	Num_cmeasures = 0;
-
-	for (i=0; i<MAX_CMEASURES; i++ )	{
-		Cmeasures[i].subtype = CMEASURE_UNUSED;
-	}
-		
-}*/
-/*
-void cmeasure_render(object * objp)
-{
-	// JAS TODO: Replace with proper fireball
-	cmeasure			*cmp;
-	cmeasure_info	*cmip;
-	
-	cmp = &Cmeasures[objp->instance];
-	cmip = &Cmeasure_info[cmp->subtype];
-
-	if ( cmp->subtype == CMEASURE_UNUSED )	{
-		Int3();	//	Hey, what are we doing in here?
-		return;
-	}
-	
-	if ( cmip->model_num > -1 )	{
-		model_clear_instance(cmip->model_num);
-		model_render(cmip->model_num, &objp->orient, &objp->pos, MR_NO_LIGHTING );
-	} else {
-		mprintf(( "Not rendering countermeasure because model_num is negative\n" ));
-	}
-}*/
-/*
-void cmeasure_delete( object * objp )
-{
-	int num;
-
-	num = objp->instance;
-
-//	Assert( Cmeasures[num].objnum == OBJ_INDEX(objp));
-
-	Cmeasures[num].subtype = CMEASURE_UNUSED;
-	Num_cmeasures--;
-	Assert( Num_cmeasures >= 0 );
-}*/
-
-// broke cmeasure_move into two functions -- process_pre and process_post (as was done with
-// all *_move functions).  Nothing to do for process_pre
-/*
-void cmeasure_process_pre( object *objp, float frame_time)
-{
-}*/
-/*
-void cmeasure_process_post(object * objp, float frame_time)
-{
-	int num;
-	num = objp->instance;
-	
-//	Assert( Cmeasures[num].objnum == objnum );
-	cmeasure *cmp = &Cmeasures[num];
-
-	if ( cmp->lifeleft >= 0.0f) {
-		cmp->lifeleft -= frame_time;
-		if ( cmp->lifeleft < 0.0f )	{
-			objp->flags |= OF_SHOULD_BE_DEAD;
-//			demo_do_flag_dead(OBJ_INDEX(objp));
-		}
-	}
-
-}*/
-
-// creates one countermeasure.  A ship fires 1 of these per launch.  rand_val is used
-// in multiplayer.  If -1, then create a random number.  If non-negative, use this
-// number for static_rand functions
-/*
-int cmeasure_create( object * source_obj, vec3d * pos, int cm_type, int rand_val )
-{
-	int		n, objnum, parent_objnum, arand;
-	object	* obj;
-	ship		*shipp;
-	cmeasure	*cmp;
-	cmeasure_info	*cmeasurep;
-
-	Cmeasures_homing_check = 2;		//	Tell homing code to scan everything for two frames.  If only one frame, get sync problems due to objects being created at end of frame!
-
-	parent_objnum = OBJ_INDEX(source_obj);
-
-	Assert( source_obj->type == OBJ_SHIP );	
-	Assert( source_obj->instance >= 0 && source_obj->instance < MAX_SHIPS );	
-	
-	shipp = &Ships[source_obj->instance];
-
-	if ( Num_cmeasures >= MAX_CMEASURES)
-		return -1;
-
-	for (n=0; n<MAX_CMEASURES; n++ )	
-		if ( Cmeasures[n].subtype == CMEASURE_UNUSED)
-			break;
-	if ( n == MAX_CMEASURES)
-		return -1;
-
-	nprintf(("Network", "Cmeasure created by %s\n", Ships[source_obj->instance].ship_name));
-
-	cmp = &Cmeasures[n];
-	cmeasurep = &Cmeasure_info[cm_type];
-
-	if ( pos == NULL )
-		pos = &source_obj->pos;
-
-	objnum = obj_create( OBJ_CMEASURE, parent_objnum, n, &source_obj->orient, pos, 1.0f, OF_RENDERS | OF_PHYSICS );
-	
-	Assert( objnum >= 0 && objnum < MAX_OBJECTS );
-
-	// Create Debris piece n!
-	if ( rand_val == -1 )
-		arand = myrand();				// use a random number to get lifeleft, and random vector for displacement from ship
-	else
-		arand = rand_val;
-
-	cmp->lifeleft = static_randf(arand) * (cmeasurep->life_max - cmeasurep->life_min) / cmeasurep->life_min;
-	if (source_obj->flags & OF_PLAYER_SHIP){
-		cmp->lifeleft *= The_mission.ai_profile->cmeasure_life_scale[Game_skill_level];
-	}
-	cmp->lifeleft = cmeasurep->life_min + cmp->lifeleft * (cmeasurep->life_max - cmeasurep->life_min);
-
-	//	cmp->objnum = objnum;
-	cmp->team = shipp->team;
-	cmp->subtype = cm_type;
-	cmp->objnum = objnum;
-	cmp->source_objnum = parent_objnum;
-	cmp->source_sig = Objects[objnum].signature;
-
-	cmp->flags = 0;
-
-	nprintf(("Jim", "Frame %i: Launching countermeasure #%i\n", Framecount, Objects[objnum].signature));
-
-	obj = &Objects[objnum];
-	
-	Num_cmeasures++;
-
-	//Set countermeasure velocity
-	vec3d vel, rand_vec;
-
-	//Get cmeasure rear velocity in world
-	vm_vec_scale_add(&vel, &source_obj->phys_info.vel, &source_obj->orient.vec.fvec, -25.0f);
-
-	//Get random velocity vector
-	static_randvec(arand+1, &rand_vec);
-
-	//Add it to the rear velocity
-	vm_vec_scale_add2(&vel, &rand_vec, 2.0f);
-
-	obj->phys_info.vel = vel;
-
-	vm_vec_zero(&obj->phys_info.rotvel);
-	vm_vec_zero(&obj->phys_info.max_vel);
-	vm_vec_zero(&obj->phys_info.max_rotvel);
-
-	// blow out his reverse thrusters. Or drag, same thing.
-	obj->phys_info.rotdamp = 10000.0f;
-	obj->phys_info.side_slip_time_const = 10000.0f;
-
-	obj->phys_info.max_vel.xyz.z = -25.0f;
-	vm_vec_copy_scale(&obj->phys_info.desired_vel, &obj->orient.vec.fvec, obj->phys_info.max_vel.xyz.z );
-
-	return arand;										// need to return this value for multiplayer purposes
-}*/
 
 //Used to set a countermeasure velocity after being launched from a ship as a countermeasure
 //ie not as a primary or secondary.
@@ -229,12 +44,6 @@ void cmeasure_set_ship_launch_vel(object *objp, object *parent_objp, int arand)
 	vm_vec_zero(&objp->phys_info.max_vel);
 	vm_vec_zero(&objp->phys_info.max_rotvel);
 	
-	//Idunnit. -WMC
-	//objp->phys_info.flags |= PF_CONST_VEL;
-
-	//WMC - I don't think this stuff is needed with the flag I set
-	//WMC - Or maybe it is.
-	
 	// blow out his reverse thrusters. Or drag, same thing.
 	objp->phys_info.rotdamp = 10000.0f;
 	objp->phys_info.side_slip_time_const = 10000.0f;
@@ -246,7 +55,7 @@ void cmeasure_set_ship_launch_vel(object *objp, object *parent_objp, int arand)
 void cmeasure_select_next(ship *shipp)
 {
 	Assert(shipp != NULL);
-	int i, new_index;
+	size_t i, new_index;
 
 	for (i = 1; i < Num_weapon_types; i++)
 	{
@@ -258,8 +67,6 @@ void cmeasure_select_next(ship *shipp)
 			return;
 		}
 	}
-
-	//snd_play( &Snds[SND_CMEASURE_CYCLE] );
 
 	mprintf(("Countermeasure type set to %i in frame %i\n", shipp->current_cmeasure, Framecount));
 }
--- fs2_open/code/cmeasure/cmeasure.h
+++ fs2_open/code/cmeasure/cmeasure.h
@@ -16,63 +16,18 @@
 #include "globalincs/systemvars.h"
 
 struct object;
-//struct vec3d;
-
-//#define MAX_CMEASURES 64
-
-//	Goes in cmeasure.subtype
-//#define	CMEASURE_UNUSED			-1
-
-//#define	MAX_CMEASURE_TYPES		3
 
 #define	CMEASURE_WAIT				333			//	delay in milliseconds between countermeasure firing.
-
-//#define	CMF_DUD_HEAT				0x01			//	If set, this cmeasure is a dud to heat seekers.  Set at create time.
-//#define	CMF_DUD_ASPECT				0x02			//	If set, this cmeasure is a dud to aspect seekers.  Set at create time.
-
-//#define	CMF_DUD	(CMF_DUD_HEAT | CMF_DUD_ASPECT)
 
 //	Maximum distance at which a countermeasure can be tracked
 //	If this value is too large, missiles will always be tracking countermeasures.
 #define	MAX_CMEASURE_TRACK_DIST	300.0f
-/*
-typedef struct cmeasure_info {
-	char	cmeasure_name[NAME_LENGTH];
-	//float	max_speed;								// launch speed, relative to ship
-	float	fire_wait;								//	time between launches
-	float	life_min, life_max;					//	lifetime will be in range min..max.
-	int	launch_sound;							//	Sound played when launched.
-	char	pof_name[NAME_LENGTH];
-	int	model_num;								// What this renders as
-} cmeasure_info;
 
-typedef struct cmeasure {
-	int		flags;				//	You know, flag type things.
-	int		subtype;				// See CMEASURE_??? defines
-	int		objnum;
-	int		source_objnum;		// What object this came from
-	int		source_sig;			// Signature of the source object
-	int		team;					// Team of the ship where the cmeasure came from
-	float		lifeleft;			// When 0 or less object dies
-} cmeasure;
-*/
-//extern cmeasure_info Cmeasure_info[MAX_CMEASURE_TYPES];
-//extern cmeasure Cmeasures[MAX_CMEASURES];
-//extern int Num_cmeasure_types;
-//extern int Num_cmeasures;
 extern int Cmeasures_homing_check;
 extern int Countermeasures_enabled;
 
-//extern void cmeasure_init();
-//extern void cmeasure_render( object * obj );
-//extern void cmeasure_delete( object * obj );
-//extern void cmeasure_process_pre( object * obj, float frame_time);
-//extern void cmeasure_process_post( object * obj, float frame_time);
-//extern int cmeasure_create( object * source_obj, vec3d *pos, int cm_type, int rand_val = -1 );
 extern void cmeasure_set_ship_launch_vel(object *objp, object *parent_objp, int arand);
 extern void cmeasure_select_next(object *objp);
 extern void cmeasure_maybe_alert_success(object *objp);
-
-extern float Skill_level_cmeasure_life_scale[NUM_SKILL_LEVELS];
 
 #endif // _CMEASURE_H
