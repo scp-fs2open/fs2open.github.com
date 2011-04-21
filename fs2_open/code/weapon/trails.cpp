4e66d27edf3cb9d5de63dfea92d30588cc63e556
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-04-21 16:12:54 +0000

    Guarding against a null vec3D in vm_vec_normalize here.

--- fs2_open/code/weapon/trails.cpp
+++ fs2_open/code/weapon/trails.cpp
@@ -79,10 +79,12 @@ void trail_calc_facing_pts( vec3d *top, vec3d *bot, vec3d *fvec, vec3d *pos, flo
 	vec3d uvec, rvec;
 
 	vm_vec_sub( &rvec, &Eye_position, pos );
-	vm_vec_normalize( &rvec );
+	if (!IS_VEC_NULL(&rvec))
+		vm_vec_normalize( &rvec );
 
 	vm_vec_crossprod(&uvec,fvec,&rvec);
-	vm_vec_normalize(&uvec);
+	if (!IS_VEC_NULL(&uvec))
+		vm_vec_normalize(&uvec);
 
 	vm_vec_scale_add( top, pos, &uvec, w * 0.5f );
 	vm_vec_scale_add( bot, pos, &uvec, -w * 0.5f );
