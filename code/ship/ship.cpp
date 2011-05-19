80ba7361a82cb6c124c237d1767c7b5c4ed38f28
Zacam <Zacam@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-05-19 21:34:28 +0000

    Trunk Sync - r7197

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -9231,6 +9231,9 @@ void change_ship_type(int n, int ship_type, int by_sexp)
 
 	//Reassign sound stuff
 	ship_assign_sound(sp);
+	
+	// create new model instance data
+	sp->model_instance_num = model_create_instance(sip->model_num);
 }
 
 #ifndef NDEBUG
