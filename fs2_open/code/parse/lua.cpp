4fc74f172b10510c846cffac0c350e83342147fc
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-06-25 14:15:44 +0000

    Patch from m!m to give scripting access to debris metadata

--- fs2_open/code/parse/lua.cpp
+++ fs2_open/code/parse/lua.cpp
@@ -4747,29 +4747,6 @@ ADE_VIRTVAR(Target, l_Asteroid, "object", "Asteroid target object; may be object
 
 }
 
-//**********HANDLE: Debris
-ade_obj<object_h> l_Debris("debris", "Debris handle", &l_Object);
-
-ADE_VIRTVAR(IsHull, l_Debris, "boolean", "Whether or not debris is a piece of hull", "boolean", "Whether debris is a hull fragment, or false if handle is invalid")
-{
-	object_h *oh;
-	bool b=false;
-	if(!ade_get_args(L, "o|b", l_Debris.GetPtr(&oh), &b))
-		return ade_set_error(L, "b", false);
-
-	if(!oh->IsValid())
-		return ade_set_error(L, "b", false);
-
-	debris *db = &Debris[oh->objp->instance];
-
-	if(ADE_SETTING_VAR) {
-		db->is_hull = b ? 1 : 0;
-	}
-
-	return ade_set_args(L, "b", db->is_hull ? true : false);
-
-}
-
 //**********HANDLE: Shipclass
 ade_obj<int> l_Shipclass("shipclass", "Ship class handle");
 extern int ships_inited;
@@ -5329,6 +5306,58 @@ ADE_FUNC(getShipClassIndex, l_Shipclass, NULL, "Gets the index valus of the ship
 		return ade_set_args(L, "i", -1);
 
 	return ade_set_args(L, "i", idx);
+}
+
+//**********HANDLE: Debris
+ade_obj<object_h> l_Debris("debris", "Debris handle", &l_Object);
+
+ADE_VIRTVAR(IsHull, l_Debris, "boolean", "Whether or not debris is a piece of hull", "boolean", "Whether debris is a hull fragment, or false if handle is invalid")
+{
+	object_h *oh;
+	bool b=false;
+	if(!ade_get_args(L, "o|b", l_Debris.GetPtr(&oh), &b))
+		return ade_set_error(L, "b", false);
+
+	if(!oh->IsValid())
+		return ade_set_error(L, "b", false);
+
+	debris *db = &Debris[oh->objp->instance];
+
+	if(ADE_SETTING_VAR) {
+		db->is_hull = b ? 1 : 0;
+	}
+
+	return ade_set_args(L, "b", db->is_hull ? true : false);
+
+}
+
+ADE_VIRTVAR(OriginClass, l_Debris, "shipclass", "The shipclass of the ship this debris originates from", "shipclass", "The shipclass of the ship that created this debris")
+{
+	object_h *oh;
+	int shipIdx = -1;
+	if(!ade_get_args(L, "o|o", l_Debris.GetPtr(&oh), &shipIdx))
+		return ade_set_error(L, "o", l_Shipclass.Set(-1));
+
+	if(!oh->IsValid())
+		return ade_set_error(L, "o", l_Shipclass.Set(-1));
+
+	debris *db = &Debris[oh->objp->instance];
+
+	if(ADE_SETTING_VAR) {
+		if (shipIdx < 0 || shipIdx > MAX_SHIP_CLASSES)
+			db->ship_info_index = shipIdx;
+	}
+
+	return ade_set_error(L, "o", l_Shipclass.Set(db->ship_info_index));
+}
+
+ADE_FUNC(isValid, l_Debris, NULL, "Return if this debris handle is valid", "boolean", "true if valid false otherwise")
+{
+	object_h *oh;
+	if(!ade_get_args(L, "o", l_Debris.GetPtr(&oh)))
+		return ADE_RETURN_FALSE;
+
+	return ade_set_args(L, "b", oh != NULL && oh->IsValid());
 }
 
 //**********HANDLE: Waypoint
