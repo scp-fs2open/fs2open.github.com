583f5c976fd92250ffe10398f40918f9b6b5b4bb
Zacam <Zacam@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-08-21 04:04:45 +0000

    Revert 7498: You should also test without optional command lines present, yellow disco screens of death in-mission are bad.

--- code/globalincs/def_files.cpp
+++ code/globalincs/def_files.cpp
@@ -2194,16 +2194,10 @@ uniform float window_height;	\n\
 uniform float nearZ;			\n\
 uniform float farZ;				\n\
 								\n\
-varying float radius;			\n\
-								\n\
-#ifdef FLAG_DISTORTION			\n\
-uniform sampler2D distMap;		\n\
-uniform sampler2D frameBuffer;	\n\
-#endif							\n\
+varying float radius;				\n\
 																			\n\
 void main()																	\n\
 {																			\n\
-	#ifndef FLAG_DISTORTION													\n\
 	vec2 offset = vec2(														\n\
 		radius * abs(0.5 - gl_TexCoord[0].x) * 2.0,							\n\
 		radius * abs(0.5 - gl_TexCoord[0].y) * 2.0							\n\
@@ -2242,21 +2236,5 @@ void main()																	\n\
 	fragmentColor = fragmentColor * ( ds / (depthOffset*2.0) );				\n\
 																			\n\
 	gl_FragColor = fragmentColor;											\n\
-	#else																	\n\
-	vec2 depthCoord = vec2(													\n\
-		gl_FragCoord.x / window_width,										\n\
-		gl_FragCoord.y / window_height										\n\
-	);																		\n\
-																			\n\
-	vec4 sceneDepth = texture2D(depthMap, depthCoord);						\n\
-	if(sceneDepth.x < gl_FragCoord.z)										\n\
-		return;																\n\
-	vec4 fragmentColor = texture2D(baseMap, gl_TexCoord[0].xy)*gl_Color.a;	\n\
-	vec2 distortion = texture2D(distMap, gl_TexCoord[0].xy);				\n\
-	float alpha = clamp(dot(fragmentColor.rgb,vec3(0.3333))*5.0,0.0,1.0);	\n\
-	distortion = ((distortion - 0.5) * 0.01) * alpha;						\n\
-	gl_FragColor = texture2D(frameBuffer,depthCoord+distortion);			\n\
-	gl_FragColor.a = alpha;													\n\
-	#endif																	\n\
 }																			\n\
 ";
