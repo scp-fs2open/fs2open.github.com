8894d719f54355213b556dfc818f653e7e348bd3
Zacam <Zacam@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-09-26 05:40:07 +0000

    From CommanderDJ: Fix for Mantis 2224 "Chat messages may end up unreadable due to unfortunate color choice" Allows for the specification of colors via a colors.tbl (Wiki Documentation here: http://www.hard-light.net/wiki/index.php/Colors.tbl )

--- fs2_open/code/globalincs/alphacolors.cpp
+++ fs2_open/code/globalincs/alphacolors.cpp
@@ -8,9 +8,9 @@
 */ 
 
 #include "graphics/2d.h"
+#include "parse/parselo.h"
+#include "globalincs/def_files.h"
 #include "globalincs/alphacolors.h"
-
-
 
 // -----------------------------------------------------------------------------------
 // ALPHA DEFINES/VARS
@@ -24,7 +24,7 @@ color Color_ui_light_green, Color_ui_green;
 color Color_ui_light_pink, Color_ui_pink;
 
 // netplayer colors
-color *Color_netplayer[20] = {
+color *Color_netplayer[NETPLAYER_COLORS] = {
 
 	&Color_blue,
 	&Color_bright_blue,
@@ -53,8 +53,152 @@ color *Color_netplayer[20] = {
 // ALPHA FUNCTIONS
 //
 
+/**
+* CommanderDJ: initialise alpha colors based on colors.tbl
+* \return 1 if successful, 0 if init fails for any reason
+*/
+int new_alpha_colors_init()
+{
+	int err_code;
+	if ((err_code = setjmp(parse_abort)) != 0)
+	{
+		mprintf(("Unable to parse 'colors.tbl'!  Error code = %d.\n", err_code));
+		return 0;
+	}
+
+	if (cf_exists_full("colors.tbl", CF_TYPE_TABLES))
+	{
+		read_file_text("colors.tbl", CF_TYPE_TABLES);
+		mprintf(("TABLES => Starting parse of 'colors.tbl'...\n"));
+	}
+	else
+	{
+		//colors.tbl doesn't exist
+		mprintf(("TABLES => Unable to find 'colors.tbl'. Initialising colors with default values.\n"));
+		return 0;
+	}
+
+	reset_parse();
+
+	// we search for the colors based on their order of definition above
+	// if the color isn't found, we just use default values
+	if (required_string("#Start Colors"))
+	{
+		// vars for holding rgba values
+		int rgba[4] = {0,0,0,0};
+
+		color *colors[TOTAL_COLORS] = {
+			&Color_blue,
+			&Color_bright_blue,
+			&Color_green,
+			&Color_bright_green,
+			&Color_black,
+			&Color_grey,
+			&Color_silver,
+			&Color_white,
+			&Color_bright_white,
+			&Color_violet_gray,
+			&Color_violet,
+			&Color_dim_red,
+			&Color_red,
+			&Color_bright_red,
+			&Color_pink,
+			&Color_light_pink,
+			&Color_yellow,
+			&Color_bright_yellow,
+			&Color_ui_light_green,
+			&Color_ui_green,
+			&Color_ui_light_pink,
+			&Color_ui_pink,
+		};
+
+		char* color_names[TOTAL_COLORS] = {
+			"$Blue:",
+			"$Bright Blue:",
+			"$Green:",
+			"$Bright Green:",
+			"$Black:",
+			"$Grey:",
+			"$Silver:",
+			"$White:",
+			"$Bright White:",
+			"$Violet Gray:",
+			"$Violet:",
+			"$Dim Red:",
+			"$Red:",
+			"$Bright Red:",
+			"$Pink:",
+			"$Light Pink:",
+			"$Yellow:",
+			"$Bright Yellow:",
+			"$UI Light Green:",
+			"$UI Green:",
+			"$UI Light Pink:",
+			"$UI Pink:"
+		};
+
+		int rgba_defaults[TOTAL_COLORS][4] = {
+			93, 93, 128, 255,
+			185, 185, 255, 255,
+			0, 120, 0, 255,
+			50, 190, 50, 255,
+			0, 0, 0, 255,
+			65, 65, 65, 255,
+			160, 160, 160, 255,
+			105, 105, 105, 255,
+			255, 255, 255 ,255,
+			160, 144, 160, 255,
+			192, 104, 192, 255,
+			80, 6, 6, 255,
+			126, 6, 6, 255,
+			200, 0, 0, 255,
+			185, 150, 150, 255,
+			230, 190, 190, 255,
+			255, 255, 122, 255,
+			255, 255, 0, 255,
+			161, 184, 161, 255,
+			190, 228, 190, 255,
+			184, 161, 161, 255,
+			228, 190, 190, 255
+		};
+
+		// now for each color, check if it's corresponding string is there
+		for (int i=0; i<TOTAL_COLORS; i++)
+		{
+			if (optional_string(color_names[i]))
+			{
+				// if so, get its rgba values and initialise it using them
+				mprintf(("'%s' has been redefined.\n", color_names[i]));
+				stuff_int_list(rgba, 4, RAW_INTEGER_TYPE);
+				for (int j=0; j<4; j++)
+				{
+					if (rgba[j] < 0)
+					{
+						Warning(LOCATION, "RGBA value for '%s' in colors.tbl too low (%d), capping to 0.", color_names[i], rgba[j]);
+						rgba[j] = 0;
+					}
+					else if (rgba[j] > 255)
+					{
+						Warning(LOCATION, "RGBA value for '%s' in colors.tbl too high (%d), capping to 255.", color_names[i], rgba[j]);
+						rgba[j] = 255;
+					}
+				}
+				gr_init_alphacolor(colors[i], rgba[0], rgba[1], rgba[2], rgba[3]);
+			}
+			// otherwise use its defaults
+			else
+				gr_init_alphacolor(colors[i], rgba_defaults[i][0], rgba_defaults[i][1], rgba_defaults[i][2], rgba_defaults[i][3]);
+		}
+		required_string("#End");
+
+		return 1;
+	}
+	// if we get here, then something's wrong with the table
+	return 0;
+}
+
 // initialize all alpha colors (call at startup)
-void alpha_colors_init()
+void old_alpha_colors_init()
 {
 	// See the variable declarations above for color usage
 	gr_init_alphacolor( &Color_blue, 93, 93, 128, 255 );
--- fs2_open/code/globalincs/alphacolors.h
+++ fs2_open/code/globalincs/alphacolors.h
@@ -11,6 +11,8 @@
 #define _GLOBAL_ALPHACOLORS_HEADER_FILE
 
 #include "graphics/2d.h"
+#include "parse/parselo.h"
+#include "globalincs/def_files.h"
 
 // -----------------------------------------------------------------------------------
 // ALPHA DEFINES/VARS
@@ -21,24 +23,25 @@
 
 // The following colors are for text drawing:
 // normal text
-#define Color_text_normal			Color_white
+#define Color_text_normal		Color_white
 // text highlighted while down still down on a line
 #define Color_text_subselected	Color_blue
 // text highlighted as the selected line
 #define Color_text_selected		Color_bright_blue
 // text that indicates an error
-#define Color_text_error			Color_red
+#define Color_text_error		Color_red
 // text that indicates an error, and line is selected or subselected
 #define Color_text_error_hi		Color_bright_red
 // text that indicates line is active item
-#define Color_text_active			Color_bright_white
+#define Color_text_active		Color_bright_white
 // text that indicates line is active item, and line is selected or subselected
-#define Color_text_active_hi		Color_bright_white
+#define Color_text_active_hi	Color_bright_white
 // text drawn as a heading for a section or title, etc
-#define Color_text_heading			Color_violet_gray
+#define Color_text_heading		Color_violet_gray
 
 #define Color_bright Color_bright_blue
 #define Color_normal Color_white
+#define TOTAL_COLORS 22
 extern color Color_blue, Color_bright_blue, Color_green, Color_bright_green;
 extern color Color_black, Color_grey, Color_silver, Color_white, Color_bright_white;
 extern color Color_violet_gray, Color_violet, Color_pink, Color_light_pink;
@@ -48,14 +51,17 @@ extern color Color_ui_light_green, Color_ui_green;
 extern color Color_ui_light_pink, Color_ui_pink;
 
 // netplayer colors
-extern color *Color_netplayer[20];
+#define NETPLAYER_COLORS 20
+extern color *Color_netplayer[NETPLAYER_COLORS];
 
 // -----------------------------------------------------------------------------------
 // ALPHA FUNCTIONS
 //
 
-// initialize all alpha colors (call at startup)
-void alpha_colors_init();
+//initialize alpha colors based on colors.tbl
+int new_alpha_colors_init();
 
+//initialize alpha colors based on hardcoded values
+void old_alpha_colors_init();
 
 #endif
