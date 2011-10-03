27900ec1a9505b0cd5d96ada0d0dfd7b9bcb9525
Echelon9 <Echelon9@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-10-03 12:15:55 +0000

    Refactor of the jump_node code to use the STL's std::list, rather than Volition's in-house written linked_list.

--- code/jumpnode/jumpnode.h
+++ code/jumpnode/jumpnode.h
@@ -15,14 +15,10 @@
 #include <stdlib.h>
 
 #include "globalincs/globals.h"
-#include "globalincs/linklist.h"
 #include "graphics/2d.h"
 
 struct vec3d;
 struct object;
-
-//#define MAX_JUMP_NODES	50
-//No max anymore. Why you'd want more than 50 is anybody's guess though.
 
 //Jump node flags
 #define JN_USE_DISPLAY_COLOR		(1<<0)		//Use display_color instead of HUD color
@@ -30,9 +26,10 @@ struct object;
 #define JN_HIDE						(1<<2)		//Hides a jump node
 #define JN_SPECIAL_MODEL			(1<<3)		//If non-default model
 
-class jump_node : public linked_list<jump_node>
+class jump_node
 {
 	char m_name[NAME_LENGTH];
+	float radius;
 
 	int	m_modelnum;
 	int	m_objnum;						// objnum of this jump node
@@ -45,39 +42,33 @@ public:
 	~jump_node();
 	
 	//Getting
-	color get_color(){return m_display_color;}
-	int get_modelnum(){return m_modelnum;}
-	int get_objnum(){return m_objnum;}
-	object *get_obj(){return &Objects[m_objnum];}
-	char *get_name_ptr(){return m_name;}
-	bool is_hidden(){if(m_flags & JN_HIDE){return true;}else{return false;}}
-	bool is_colored(){return ((m_flags & JN_USE_DISPLAY_COLOR) != 0);}
-	bool is_special_model(){return ((m_flags & JN_SPECIAL_MODEL) != 0);}
+	color get_color();
+	int get_modelnum();
+	int get_objnum();
+	object *get_obj();
+	char *get_name_ptr();
+	bool is_hidden();
+	bool is_colored();
+	bool is_special_model();
 
 	//Setting
 	void set_alphacolor(int r, int g, int b, int alpha);
 	void set_model(char *model_name, bool show_polys=false);
 	void set_name(char *new_name);
-	void show(bool enabled){if(enabled){m_flags&=~JN_HIDE;}else{m_flags|=JN_HIDE;}}
+	void show(bool enabled);
 
 	//Rendering
 	void render(vec3d *pos, vec3d *view_pos = NULL);
 };
 
 //-----Globals------
-//extern int Num_jump_nodes;
-//extern linked_list Jump_nodes;
-extern linked_list<jump_node> Jump_nodes;
+extern SCP_list<jump_node> Jump_nodes;
 
 //-----Functions-----
-//Given a name, returns pointer to the jump node object
 jump_node *jumpnode_get_by_name(char *name);
-
-//Given an object, returns which jump node it's in
 jump_node *jumpnode_get_which_in(object *objp);
 
-void jumpnode_level_init();
-void jumpnode_render_all();	// called by FRED
+void jumpnode_render_all();
 void jumpnode_level_close();
 
 bool jumpnode_check_for_duplicates();
