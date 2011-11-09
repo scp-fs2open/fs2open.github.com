50971919b3183ee5637b5e943c6e03ab6d06b3f8
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-11-09 09:25:59 +0000

    Fix for Mantis 2533 from Eli2: Integer overflow in mission-time-msecs

--- fs2_open/code/parse/sexp.cpp
+++ fs2_open/code/parse/sexp.cpp
@@ -12650,10 +12650,15 @@ int sexp_event_delay_status( int n, int want_true, bool use_msecs = false)
 		return SEXP_FALSE;
 	}
 
-	delay = i2f(eval_num(CDR(n)));
+	uint64_t tempDelay = eval_num(CDR(n));
 
 	if (use_msecs) {
-		delay = delay / 1000l;
+		tempDelay = tempDelay << 16;
+		tempDelay = tempDelay / 1000;
+
+		delay = (fix) tempDelay;
+	} else {
+		delay = i2f(tempDelay);
 	}
 
 	for (i = 0; i < Num_mission_events; i++ ) {
