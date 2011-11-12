ef5f145d5a4712597c024b5fada066df0f753ee2
Zacam <Zacam@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-11-12 14:36:31 +0000

    From zookeeper: customizable warpout engage time http://www.hard-light.net/forums/index.php?topic=78913.0

--- code/ai/aicode.cpp
+++ code/ai/aicode.cpp
@@ -12890,7 +12890,10 @@ void ai_warp_out(object *objp)
 		aip->submode_start_time = Missiontime;
 		break;
 	case AIS_WARP_2:			//	Make sure won't collide with any object.
-		if (timestamp_elapsed(aip->force_warp_time) || !collide_predict_large_ship(objp, objp->radius*2.0f + 100.0f)) {
+		if (timestamp_elapsed(aip->force_warp_time)
+			|| (!collide_predict_large_ship(objp, objp->radius*2.0f + 100.0f)
+			|| (Ship_info[shipp->ship_info_index].warpout_type == WT_HYPERSPACE
+				&& !collide_predict_large_ship(objp, 100000.0f)))) {
 			aip->submode = AIS_WARP_3;
 			aip->submode_start_time = Missiontime;
 
@@ -12931,9 +12934,12 @@ void ai_warp_out(object *objp)
 		}
 		break;
 	case AIS_WARP_4: {
-		shipfx_warpout_start(objp);
-		aip->submode = AIS_WARP_5;
-		aip->submode_start_time = Missiontime;
+		// Only lets the ship warp after waiting for the warpout engage time
+		if ( (Missiontime / 100) >= (aip->submode_start_time / 100 + Ship_info[shipp->ship_info_index].warpout_engage_time) ) {
+			shipfx_warpout_start(objp);
+			aip->submode = AIS_WARP_5;
+			aip->submode_start_time = Missiontime;
+		}
 		break;
 	}
 	case AIS_WARP_5:
