0a87afaec80b7dbbe354c9b654a0e2cbd461cf83
Sushi <Sushi@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-11-14 18:26:55 +0000

    Add "only target if can fire" turret flag

--- fs2_open/code/model/model.h
+++ fs2_open/code/model/model.h
@@ -133,7 +133,8 @@ typedef struct polymodel_instance {
 #define MSS_FLAG_NO_AGGREGATE		(1 << 30)		// Don't include with aggregate subsystem types - Goober5000
 #define MSS_FLAG_TURRET_ANIM_WAIT   (1 << 31)		// Turret won't fire until animation is complete - Sushi
 
-#define MSS_FLAG2_PLAYER_TURRET_SOUND (1 << 0)
+#define MSS_FLAG2_PLAYER_TURRET_SOUND			 (1 << 0)
+#define MSS_FLAG2_TURRET_ONLY_TARGET_IF_CAN_FIRE (1 << 1)	// Turrets only target things they're allowed to shoot at (e.g. if check-hull fails, won't keep targeting)
 
 #define NUM_SUBSYSTEM_FLAGS			33
 
