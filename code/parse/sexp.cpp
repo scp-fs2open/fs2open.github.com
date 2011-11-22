af7e811cf1bd3806f3d65329f29a1925b5a796e6
Zacam <Zacam@387891d4-d844-0410-90c0-e4c51a9137d3>
2011-11-22 21:28:46 +0000

    Backport: Trunk 7962, 7963, 7974, 7976; Fix for Mantis 2533 from Eli2: Integer overflow in mission-time-msecs (with fixes from The E and inclusion of stdint.h and inttypes.h)

--- code/parse/sexp.cpp
+++ code/parse/sexp.cpp
@@ -20,6 +20,11 @@
 #include <ctype.h>
 #include <assert.h>
 #include <limits.h>
+#ifdef _MSC_VER
+	#include "globalincs/stdint.h"
+#else
+	#include <stdint.h>
+#endif
 
 #include "parse/parselo.h"
 #include "parse/sexp.h"
@@ -12584,10 +12589,15 @@ int sexp_event_delay_status( int n, int want_true, bool use_msecs = false)
 		return SEXP_FALSE;
 	}
 
-	delay = i2f(eval_num(CDR(n)));
+	uint64_t tempDelay = eval_num(CDR(n));
 
 	if (use_msecs) {
-		delay = delay / 1000l;
+		tempDelay = tempDelay << 16;
+		tempDelay = tempDelay / 1000;
+
+		delay = (fix) tempDelay;
+	} else {
+		delay = i2f(tempDelay);
 	}
 
 	for (i = 0; i < Num_mission_events; i++ ) {
