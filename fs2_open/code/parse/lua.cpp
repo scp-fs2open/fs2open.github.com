cb8cacb32d185e178450d6108aa03244916345e2
Echelon9 <Echelon9@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-01-14 00:31:14 +0000

    Fix for #2567 - LLVM warning: Semantic issue in lua.cpp

--- fs2_open/code/parse/lua.cpp
+++ fs2_open/code/parse/lua.cpp
@@ -3315,8 +3315,8 @@ ADE_FUNC(__add, l_Vector, "number/vector", "Adds vector by another vector, or ad
 	if(lua_isnumber(L, 1) || lua_isnumber(L, 2))
 	{
 		float f;
-		if(lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3))
-			|| lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f))
+		if((lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3)))
+			|| (lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f)))
 		{
 			v3.xyz.x += f;
 			v3.xyz.y += f;
@@ -3341,8 +3341,8 @@ ADE_FUNC(__sub, l_Vector, "number/vector", "Subtracts vector from another vector
 	if(lua_isnumber(L, 1) || lua_isnumber(L, 2))
 	{
 		float f;
-		if(lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3))
-			|| lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f))
+		if((lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3)))
+			|| (lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f)))
 		{
 			v3.xyz.x += f;
 			v3.xyz.y += f;
@@ -3368,8 +3368,8 @@ ADE_FUNC(__mul, l_Vector, "number/vector", "Scales vector object (Multiplies all
 	if(lua_isnumber(L, 1) || lua_isnumber(L, 2))
 	{
 		float f;
-		if(lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3))
-			|| lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f))
+		if((lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3)))
+			|| (lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f)))
 		{
 			vm_vec_scale(&v3, f);
 		}
@@ -3395,8 +3395,8 @@ ADE_FUNC(__div, l_Vector, "number/vector", "Scales vector object (Divide all axe
 	if(lua_isnumber(L, 1) || lua_isnumber(L, 2))
 	{
 		float f;
-		if(lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3))
-			|| lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f))
+		if((lua_isnumber(L, 1) && ade_get_args(L, "fo", &f, l_Vector.Get(&v3)))
+			|| (lua_isnumber(L, 2) && ade_get_args(L, "of", l_Vector.Get(&v3), &f)))
 		{
 			vm_vec_scale(&v3, 1.0f/f);
 		}
