f5a192c27c088f86c8df35f2cfb30772d74ac87a
chief1983 <chief1983@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-01-23 04:56:33 +0000

    Backport: Trunk 8263 + 8271; Fix for Mantis 2573: Special Hitpoint settings get carried over when deleting and adding ships in FRED && update to revision 8263: one should probably reset the special shields if you reset special hitpoints

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -5010,6 +5010,13 @@ void ship_set(int ship_index, int objnum, int ship_type)
 	shipp->collision_damage_type_idx =  sip->collision_damage_type_idx;
 	shipp->debris_damage_type_idx = sip->debris_damage_type_idx;
 	sip->shockwave.damage_type_idx = sip->shockwave.damage_type_idx_sav;
+
+	// Reset special hitpoints. Fixes Mantis issue 2573
+	shipp->special_hitpoints = 0;
+	shipp->special_shield = -1;
+
+	// Reset special explosion too.
+	shipp->use_special_explosion = false;
 }
 
 /**
