f54f348f19e7d8d0333bef932a150121c0c9964e
Zacam <Zacam@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-02-14 00:58:12 +0000

    Backport: Trunk r8432; Make $FlyBySnd actually do something and allow primaries as well.

--- code/weapon/weapons.cpp
+++ code/weapon/weapons.cpp
@@ -1649,18 +1649,8 @@ int parse_weapon(int subtype, bool replace)
 	//Disarmed impact sound
 	parse_sound("$Disarmed ImpactSnd:", &wip->impact_snd, wip->name);
 
-	if (subtype == WP_MISSILE)
-	{
-		parse_sound("$FlyBySnd:", &wip->flyby_snd, wip->name);
-	}
-	else
-	{
-		if (optional_string("$FlyBySnd:"))
-		{
-			Warning(LOCATION, "$FlyBySnd: flag found on %s, but is not used with primary weapons; ignoring...", wip->name);
-		}
-	}
-
+	parse_sound("$FlyBySnd:", &wip->flyby_snd, wip->name);
+	
 	if(optional_string("$Model:"))
 	{
 		wip->render_type = WRT_POF;
@@ -3587,7 +3577,7 @@ void weapon_render(object *obj)
 
 
 			model_render(wip->model_num, &obj->orient, &obj->pos, render_flags);
-
+			wp->weapon_flags |= WF_CONSIDER_FOR_FLYBY_SOUND;
 			if (clip_plane)
 			{
 				g3_stop_user_clip_plane();
@@ -4357,12 +4347,7 @@ MONITOR( NumWeapons )
  * Maybe play a "whizz sound" if close enough to view position
  */
 void weapon_maybe_play_flyby_sound(object *weapon_objp, weapon *wp)
-{	
-	// do a quick out if not a laser
-	if ( Weapon_info[wp->weapon_info_index].subtype != WP_LASER ) {
-		return;
-	}
-
+{
 	// don't play flyby sounds too close together
 	if ( !timestamp_elapsed(Weapon_flyby_sound_timer) ) {
 		return;
@@ -4399,7 +4384,13 @@ void weapon_maybe_play_flyby_sound(object *weapon_objp, weapon *wp)
 			dot = vm_vec_dot(&vec_to_weapon, &weapon_objp->orient.vec.fvec);
 			
 			if ( (dot < -0.80) && (dot > -0.98) ) {
-				snd_play_3d( &Snds[SND_WEAPON_FLYBY], &weapon_objp->pos, &Eye_position );
+				if(Weapon_info[wp->weapon_info_index].flyby_snd != -1) {
+					snd_play_3d( &Snds[Weapon_info[wp->weapon_info_index].flyby_snd], &weapon_objp->pos, &Eye_position );
+				} else {
+					if ( Weapon_info[wp->weapon_info_index].subtype == WP_LASER ) {
+						snd_play_3d( &Snds[SND_WEAPON_FLYBY], &weapon_objp->pos, &Eye_position );
+					}
+				}
 				Weapon_flyby_sound_timer = timestamp(200);
 				wp->weapon_flags |= WF_PLAYED_FLYBY_SOUND;
 			}
