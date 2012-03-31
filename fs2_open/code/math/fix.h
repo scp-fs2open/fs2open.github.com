22d11ea641eb5fd5fbfa86f81f79e11e63a76caa
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-03-31 20:30:09 +0000

    As per discussion for Mantis 2633, Reverting revision 8544 due to timer drift.

--- fs2_open/code/math/fix.h
+++ fs2_open/code/math/fix.h
@@ -13,11 +13,6 @@
 #define _FIX_H
 
 //#include "pstypes.h"
-#ifdef _MSC_VER
-	#include "globalincs/msvc/stdint.h"
-#else
-	#include <stdint.h>
-#endif
 
 #define F1_0 65536
 #define f1_0 65536
@@ -25,20 +20,6 @@
 fix fixmul(fix a, fix b);
 fix fixdiv(fix a, fix b);
 fix fixmuldiv(fix a, fix b, fix c);
-
-static inline uint32_t fixTimeToMs(const fix time) {
-	uint64_t temp = time;
-	temp = temp * 1000;
-	temp = temp >> 16;
-	return (uint32_t) temp;
-}
-
-static inline fix fixTimeFromMs(const uint32_t ms) {
-	uint64_t temp = ms;
-	temp = temp << 16;
-	temp = temp / 1000;
-	return (fix) temp;
-}
 
 #define f2i(a) ((int)((a)>>16))
 #define i2f(a) ((fix)((a)<<16))
