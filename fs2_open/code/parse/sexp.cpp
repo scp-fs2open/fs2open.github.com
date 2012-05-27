a5cfd79ee3124eaf6150eaaa8960b4c457831bfb
Echelon9 <Echelon9@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-05-27 14:23:41 +0000

    GCC warning: comparison between signed and unsigned integer expressions

--- fs2_open/code/parse/sexp.cpp
+++ fs2_open/code/parse/sexp.cpp
@@ -2790,8 +2790,8 @@ int check_sexp_syntax(int node, int return_type, int recursive, int *bad_node, i
 				if ( type2 != SEXP_ATOM_STRING )
 					return SEXP_CHECK_TYPE_MISMATCH;
 
-				st = model_anim_match_type(CTEXT(node));
-				if ( st == TRIGGER_TYPE_NONE )
+				i = model_anim_match_type(CTEXT(node));
+				if ( i == TRIGGER_TYPE_NONE )
 					return SEXP_CHECK_INVALID_ANIMATION_TYPE;
 
 				break;
