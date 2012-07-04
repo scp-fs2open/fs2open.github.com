bf852eda06ccb7bfbfd1c807bc550a35661fd5df
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-07-04 05:38:51 +0000

    replace variables in recommendations too

--- fs2_open/code/missionui/missiondebrief.cpp
+++ fs2_open/code/missionui/missiondebrief.cpp
@@ -1996,8 +1996,10 @@ void debrief_init()
 	}
 
 	// Goober5000 - replace any variables with their values
-	for (i = 0; i < Debriefing->num_stages; i++)
+	for (i = 0; i < Debriefing->num_stages; i++) {
 		sexp_replace_variable_names_with_values(Debriefing->stages[i].text);
+		sexp_replace_variable_names_with_values(Debriefing->stages[i].recommendation_text);
+	}
 
 	// no longer is mission
 	Game_mode &= ~(GM_IN_MISSION);	
