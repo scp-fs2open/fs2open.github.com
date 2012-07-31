371b827014307b1780f6bbf0df3804e9997753cc
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-07-31 18:27:07 +0000

    Patch from Pecenipicek that allows a "none" team color to be defined in ships.tbl

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -1632,11 +1632,15 @@ int parse_ship_values(ship_info* sip, bool isTemplate, bool first_time, bool rep
 		char temp[NAME_LENGTH];
 		stuff_string(temp, F_NAME, NAME_LENGTH);
 		SCP_string name = temp;
-		if (Team_Colors.find(name) != Team_Colors.end()) {
-			sip->default_team_name = name;
+		if (name == "None") {
 			sip->uses_team_colors = true;
 		} else {
-			Warning(LOCATION, "Team name %s is invalid. Teams must be defined in colors.tbl.\n", temp);
+			if (Team_Colors.find(name) != Team_Colors.end()) {
+				sip->default_team_name = name;
+				sip->uses_team_colors = true;
+			} else {
+				Warning(LOCATION, "Team name %s is invalid. Teams must be defined in colors.tbl.\n", temp);
+			}
 		}
 	}
 
