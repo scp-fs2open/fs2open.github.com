70e2280e2589547fbb2f2754adb17f654bc2713b
niffiwan <niffiwan@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-08-11 09:39:43 +0000

    Also check we have a valid bitmap before trying to render it

--- fs2_open/code/hud/hudreticle.cpp
+++ fs2_open/code/hud/hudreticle.cpp
@@ -757,7 +757,9 @@ void HudGaugeWeaponLinking::render(float frametime)
 
 	setGaugeColor();
 
-	renderBitmap(arc.first_frame+1, position[0], position[1]);
+	if (arc.first_frame >= 0) {
+		renderBitmap(arc.first_frame+1, position[0], position[1]);
+	}
 
 	swp = &Player_ship->weapons;
 
@@ -799,7 +801,9 @@ void HudGaugeWeaponLinking::render(float frametime)
 	}
 	
 	if ( gauge_index != -1 ) {
-		renderBitmap(weapon_linking_modes[gauge_index].first_frame+frame_offset, position[0] + Weapon_link_offsets[gauge_index][0], position[1] + Weapon_link_offsets[gauge_index][1]);
+		if (weapon_linking_modes[gauge_index].first_frame >= 0) {
+			renderBitmap(weapon_linking_modes[gauge_index].first_frame+frame_offset, position[0] + Weapon_link_offsets[gauge_index][0], position[1] + Weapon_link_offsets[gauge_index][1]);
+		}
 	}
 
 	int num_banks = swp->num_secondary_banks;
@@ -837,8 +841,9 @@ void HudGaugeWeaponLinking::render(float frametime)
 		} else {
 			frame_offset = swp->current_secondary_bank+1;
 		}
-
-		renderBitmap(weapon_linking_modes[gauge_index].first_frame+frame_offset, position[0] + Weapon_link_offsets[gauge_index][0],  position[1] + Weapon_link_offsets[gauge_index][1]);
+		if (weapon_linking_modes[gauge_index].first_frame >= 0) {
+			renderBitmap(weapon_linking_modes[gauge_index].first_frame+frame_offset, position[0] + Weapon_link_offsets[gauge_index][0],  position[1] + Weapon_link_offsets[gauge_index][1]);
+		}
 	}
 }
 
