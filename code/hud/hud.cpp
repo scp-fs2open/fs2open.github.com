e2ee74c3874b9289a2f8d2d1d338738607fae664
Swifty <Swifty@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-09-10 05:21:46 +0000

    Added bottom offset parameter to the Damage gauge in hud_gauges.tbl

--- code/hud/hud.cpp
+++ code/hud/hud.cpp
@@ -1957,6 +1957,11 @@ void HudGaugeDamage::initSubsysIntegValueOffsetX(int x)
 	subsys_integ_val_offset_x = x;
 }
 
+void HudGaugeDamage::initBottomBgOffset(int offset)
+{
+	bottom_bg_offset = offset;
+}
+
 void HudGaugeDamage::initLineHeight(int h)
 {
 	line_h = h;
@@ -2170,7 +2175,7 @@ void HudGaugeDamage::render(float frametime)
 	}
 
 	setGaugeColor();
-	renderBitmap(damage_bottom.first_frame, bx, by);		
+	renderBitmap(damage_bottom.first_frame, bx, by + bottom_bg_offset);		
 }
 
 /** 
