ddb89f2456f769d72833269146c4b1d854767fc7
Swifty <Swifty@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-09-10 04:51:11 +0000

    Base support for desaturated model rendering.

--- fs2_open/code/model/modelinterp.cpp
+++ fs2_open/code/model/modelinterp.cpp
@@ -110,6 +110,7 @@ static color Interp_outline_color;
 static int Interp_detail_level_locked = 0;
 static uint Interp_flags = 0;
 static uint Interp_tmap_flags = 0;
+bool Interp_desaturate = false;
 
 // If non-zero, then the subobject gets scaled by Interp_thrust_scale.
 static int Interp_thrust_scale_subobj = 0;
@@ -2742,6 +2743,10 @@ void model_really_render(int model_num, matrix *orient, vec3d * pos, uint flags,
 
 	if ( Interp_flags & MR_ANIMATED_SHADER )
 		Interp_tmap_flags |= TMAP_ANIMATED_SHADER;
+
+	if ( Interp_desaturate ) {
+		Interp_tmap_flags |= TMAP_FLAG_DESATURATE;
+	}
 
 	save_gr_zbuffering_mode = gr_zbuffering_mode;
 	zbuf_mode = gr_zbuffering_mode;
