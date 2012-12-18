d702b276ef9ad9fb76fbdef153758abb19d6ea41
The_E <The_E@387891d4-d844-0410-90c0-e4c51a9137d3>
2012-12-18 21:55:26 +0000

    Rename the model subsystem flag masks to MSS_MODEL_FLAG_MASK and MSS_MODEL_FLAG2_MASK respectively, for readability

--- code/model/model.h
+++ code/model/model.h
@@ -142,8 +142,8 @@ typedef struct polymodel_instance {
 #define NUM_SUBSYSTEM_FLAGS			33
 
 // all subsys flags set in model file, used to copy only these flags for different table entries using the same model
-#define MSS_MODEL_FLAG				(MSS_FLAG_CREWPOINT | MSS_FLAG_ROTATES | MSS_FLAG_TRIGGERED | MSS_FLAG_ARTILLERY | MSS_FLAG_STEPPED_ROTATE)
-#define MSS_MODEL_FLAG2				0
+#define MSS_MODEL_FLAG_MASK				(MSS_FLAG_CREWPOINT | MSS_FLAG_ROTATES | MSS_FLAG_TRIGGERED | MSS_FLAG_ARTILLERY | MSS_FLAG_STEPPED_ROTATE)
+#define MSS_MODEL_FLAG2_MASK			0
 
 // definition of stepped rotation struct
 typedef struct stepped_rotation {
