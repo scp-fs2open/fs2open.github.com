cae43dce4f91e77a80a7650d6a23460f7dc8284f
niffiwan <niffiwan@387891d4-d844-0410-90c0-e4c51a9137d3>
2013-01-12 09:52:10 +0000

    related to mantis 2503: added assertion to detect crazy amounts of red alert wingman when writing .csg player may lose one mission worth of progress, but shouldn't lose their campaign

--- code/pilotfile/csg.cpp
+++ code/pilotfile/csg.cpp
@@ -1438,6 +1438,12 @@ bool pilotfile::save_savefile()
 		return false;
 	}
 
+	// validate the number of red alert entries
+	// assertion before writing so that we don't corrupt the .csg by asserting halfway through writing
+	// assertion should also prevent loss of major campaign progress
+	// i.e. lose one mission, not several missions worth (in theory)
+	Assertion(Red_alert_wingman_status.size() >= 0 && Red_alert_wingman_status.size() <= MAX_SHIPS, "Invalid number of Red_alert_wingman_status entries: %u\n", Red_alert_wingman_status.size());
+
 	// open it, hopefully...
 	cfp = cfopen((char*)filename.c_str(), "wb", CFILE_NORMAL, CF_TYPE_PLAYERS);
 
