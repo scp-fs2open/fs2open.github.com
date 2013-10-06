576baed1d388ad8edbb49435692ffcbefde53968
niffiwan <niffiwan@387891d4-d844-0410-90c0-e4c51a9137d3>
2013-10-06 11:21:20 +0000

    add Coverity modeling file
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@9877 387891d4-d844-0410-90c0-e4c51a9137d3

---
+++ coverity/modeling.cpp
@@ -0,0 +1,39 @@
+/**
+ * Coverity Scan modeling file.  Help reduce false positives
+ */
+
+// Assert & Assertion are "killpaths" in DEBUG, but not release
+// This should get them treated as killpaths in release as well
+// (just a copy of the DEBUG version of the macros)
+
+#	define Assert(expr) do {\
+		if (!(expr)) {\
+			WinAssert(#expr,__FILE__,__LINE__);\
+		}\
+		ASSUME( expr );\
+	} while (0)
+
+#	ifndef _MSC_VER   // non MS compilers
+#		define Assertion(expr, msg, ...) do {\
+			if (!(expr)) {\
+				WinAssert(#expr,__FILE__,__LINE__, msg , ##__VA_ARGS__ );\
+			}\
+		} while (0)
+#	else
+#		if _MSC_VER >= 1400	// VC 2005 or greater
+#			define Assertion(expr, msg, ...) do {\
+				if (!(expr)) {\
+					WinAssert(#expr,__FILE__,__LINE__, msg, __VA_ARGS__ );\
+				}\
+				ASSUME(expr);\
+			} while (0)
+#		else // older MSVC compilers
+#			define Assertion(expr, msg) do {\
+				if (!(expr)) {\
+					WinAssert(#expr,__FILE__,__LINE__);\
+				}\
+			} while (0)
+#		endif
+#	endif
+
+
