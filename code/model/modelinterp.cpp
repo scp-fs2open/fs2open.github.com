a0c510e1c62667e84179eee1f72cec8d47855f35
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2013-10-08 06:13:24 +0000

    fix Mantis #2931: don't prematurely exit the model_interp_tmappoly method if we're drawing outlines
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@9913 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/model/modelinterp.cpp
+++ code/model/modelinterp.cpp
@@ -773,11 +773,14 @@ void model_interp_tmappoly(ubyte * p,polymodel * pm)
 
 	if (Interp_warp_bitmap < 0) {
 		if ( (!Interp_thrust_scale_subobj) && (tbase->GetTexture() < 0) ) {
-			// Don't draw invisible polygons.
-			if ( !(Interp_flags & MR_SHOW_INVISIBLE_FACES) )
-				return;
-			else
-				is_invisible = 1;
+			// Ignore the following if we're drawing in outline mode.  Fixes Mantis #2931.
+			if (!(Interp_flags & (MR_SHOW_OUTLINE|MR_SHOW_OUTLINE_PRESET))) {
+				// Don't draw invisible polygons.
+				if ( !(Interp_flags & MR_SHOW_INVISIBLE_FACES) )
+					return;
+				else
+					is_invisible = 1;
+			}
 		}
 	}
 
