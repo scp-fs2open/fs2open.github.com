5312b47983fd945b15a63ed6789fe7206120dee4
zookeeper <zookeeper@387891d4-d844-0410-90c0-e4c51a9137d3>
2013-10-15 09:47:20 +0000

    Typo fix.
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@9938 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/parse/lua.cpp
+++ code/parse/lua.cpp
@@ -10370,7 +10370,7 @@ ADE_FUNC(getDuration, l_SoundEntry, NULL, "Gives the length of the sound in seco
 	return ade_set_args(L, "f", (i2fl(snd_get_duration(seh->getId())) / 1000.0f));
 }
 
-ADE_FUNC(get3DValues, l_SoundEntry, "vector Postion[, number radius=0.0]", "Computes the volume and the panning of the sound when it would be played from the specified position.<br>"
+ADE_FUNC(get3DValues, l_SoundEntry, "vector Position[, number radius=0.0]", "Computes the volume and the panning of the sound when it would be played from the specified position.<br>"
 	"If range is given then the volume will diminish when the listener is withing that distance to the source.<br>"
 	"The position of the listener is always the the current viewing position.", "number, number", "The volume and the panning, in that sequence, or both -1 on error")
 {
