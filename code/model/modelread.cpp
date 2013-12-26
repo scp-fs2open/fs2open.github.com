507defb0339ddcaa0a032a7767ed206e4ade6fdd
Echelon9 <rhyskidd@gmail.com>
2013-12-26 12:15:39 +1100

    Fix Coverity 1093455 Dereference after null check: In read_model_file(polymodel *, char *, int, model_subsystem *, int): Pointer is checked against null but then dereferenced anyway (CWE-476)

--- code/model/modelread.cpp
+++ code/model/modelread.cpp
@@ -521,7 +521,7 @@ static void set_subsystem_info( model_subsystem *subsystemp, char *props, char *
 		// *** determine how the subsys rotates ***
 
 		// CASE OF STEPPED ROTATION
-		if ( (p = strstr(props, "$stepped")) != NULL) {
+		if ( (strstr(props, "$stepped")) != NULL) {
 
 			subsystemp->stepped_rotation = new(stepped_rotation);
 			subsystemp->flags |= MSS_FLAG_STEPPED_ROTATE;
@@ -1422,7 +1422,7 @@ int read_model_file(polymodel * pm, char *filename, int n_subsystems, model_subs
 					// Find end of number
 					parsed_string = strchr(parsed_string, ',');
 					if (parsed_string == NULL) {
-						Warning( LOCATION,
+						Error( LOCATION,
 							"Submodel '%s' of model '%s' has an improperly formatted $uvec: declaration in its properties."
 							"\n\n$uvec: should be followed by 3 numbers separated with commas."
 							"\n\nCouldn't find first comma (,)!",
@@ -1439,7 +1439,7 @@ int read_model_file(polymodel * pm, char *filename, int n_subsystems, model_subs
 					// Find end of number
 					parsed_string = strchr(parsed_string, ',');
 					if (parsed_string == NULL) {
-						Warning( LOCATION,
+						Error( LOCATION,
 							"Submodel '%s' of model '%s' has an improperly formatted $uvec: declaration in its properties."
 							"\n\n$uvec: should be followed by 3 numbers separated with commas."
 							"\n\nCouldn't find second comma (,)!",
@@ -1465,7 +1465,7 @@ int read_model_file(polymodel * pm, char *filename, int n_subsystems, model_subs
 						// Find end of number
 						parsed_string = strchr(parsed_string, ',');
 						if (parsed_string == NULL) {
-							Warning( LOCATION,
+							Error( LOCATION,
 								"Submodel '%s' of model '%s' has an improperly formatted $fvec: declaration in its properties."
 								"\n\n$fvec: should be followed by 3 numbers separated with commas."
 								"\n\nCouldn't find first comma (,)!",
@@ -1482,7 +1482,7 @@ int read_model_file(polymodel * pm, char *filename, int n_subsystems, model_subs
 						// Find end of number
 						parsed_string = strchr(parsed_string, ',');
 						if (parsed_string == NULL) {
-							Warning( LOCATION,
+							Error( LOCATION,
 								"Submodel '%s' of model '%s' has an improperly formatted $fvec: declaration in its properties."
 								"\n\n$fvec: should be followed by 3 numbers separated with commas."
 								"\n\nCouldn't find second comma (,)!",
