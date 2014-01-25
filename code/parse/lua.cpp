0794dcded360241edf1828214b596a4f4138129c
zookeeper <zookeeper@387891d4-d844-0410-90c0-e4c51a9137d3>
2014-01-25 22:13:42 +0000

    Another little Lua documentation fix.
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10348 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/parse/lua.cpp
+++ code/parse/lua.cpp
@@ -14709,7 +14709,7 @@ ade_lib l_Tables("Tables", NULL, "tb", "Tables library");
 
 //*****SUBLIBRARY: Tables/ShipClasses
 ade_lib l_Tables_ShipClasses("ShipClasses", &l_Tables, NULL, NULL);
-ADE_INDEXER(l_Tables_ShipClasses, "number Index/string Name", "Array of ship classes", "ship", "Ship handle, or invalid ship handle if index is invalid")
+ADE_INDEXER(l_Tables_ShipClasses, "number Index/string Name", "Array of ship classes", "shipclass", "Ship handle, or invalid ship handle if index is invalid")
 {
 	if(!ships_inited)
 		return ade_set_error(L, "o", l_Shipclass.Set(-1));
