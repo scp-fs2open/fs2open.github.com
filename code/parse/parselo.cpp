8e1c4be1b6b4ec482cf50d119ad9481e7f9832b6
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2014-06-05 02:21:58 +0000

    quotes can span multiple lines as well, so let's keep track of that flag (fixes Mantis #3001)
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10753 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/parse/parselo.cpp
+++ code/parse/parselo.cpp
@@ -1767,9 +1767,8 @@ bool matches_version_specific_tag(const char *line_start, bool &compatible_versi
 
 // Strip comments from a line of input.
 // Goober5000 - rewritten for the second time
-void strip_comments(char *line, bool &in_multiline_comment_a, bool &in_multiline_comment_b)
+void strip_comments(char *line, bool &in_quote, bool &in_multiline_comment_a, bool &in_multiline_comment_b)
 {
-	bool in_quote = false;
 	char *writep = line;
 	char *readp = line;
 
@@ -2097,6 +2096,7 @@ void process_raw_file_text(char *processed_text, char *raw_text)
 	char	*mp;
 	char	*mp_raw;
 	char outbuf[PARSE_BUF_SIZE], *str;
+	bool in_quote = false;
 	bool in_multiline_comment_a = false;
 	bool in_multiline_comment_b = false;
 	int raw_text_len = strlen(raw_text);
@@ -2118,7 +2118,7 @@ void process_raw_file_text(char *processed_text, char *raw_text)
 	while ( (num_chars_read = parse_get_line(outbuf, PARSE_BUF_SIZE, raw_text, raw_text_len, mp_raw)) != 0 ) {
 		mp_raw += num_chars_read;
 
-		strip_comments(outbuf, in_multiline_comment_a, in_multiline_comment_b);
+		strip_comments(outbuf, in_quote, in_multiline_comment_a, in_multiline_comment_b);
 
 		maybe_convert_foreign_characters(outbuf);
 
