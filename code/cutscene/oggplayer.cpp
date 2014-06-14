129a2d0d85c47c25322cafdc000e6b3def08db64
Echelon9 <Echelon9@387891d4-d844-0410-90c0-e4c51a9137d3>
2014-06-14 04:34:14 +0000

    Address Coverity 1204290 Copy into fixed size buffer - strncpy() should be restricted by length to that of the target string, and does not guarantee null termination if the source string overran
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10806 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/cutscene/oggplayer.cpp
+++ code/cutscene/oggplayer.cpp
@@ -797,7 +797,8 @@ THEORAFILE *theora_open(char *filename)
 
 	// lower case filename for checking
 	memset( lower_name, 0, sizeof(lower_name) );
-	strncpy( lower_name, filename, strlen(filename) );
+	strncpy( lower_name, filename, sizeof(lower_name)-1 );
+	lower_name[sizeof(lower_name)-1] = '\0';
 	strlwr(lower_name);
 	
 	char *p = strchr( lower_name, '.' );
