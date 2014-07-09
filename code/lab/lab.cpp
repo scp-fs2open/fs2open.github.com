ae351d2924cfd0590e1cb771aec51cfe860e718b
chief1983 <chief1983@387891d4-d844-0410-90c0-e4c51a9137d3>
2014-07-09 21:44:53 +0000

    Fix Mantis 2886 - Option to render External weapon models, besides Tech models, in Weapon lab, from FUBAR.
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10907 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/lab/lab.cpp
+++ code/lab/lab.cpp
@@ -2116,6 +2116,14 @@ void labviewer_show_tech_model(Tree *caller)
 	labviewer_change_model(Weapon_info[weap_index].tech_model, caller->GetSelectedItem()->GetData(), weap_index);
 }
 
+void labviewer_show_external_model(Tree *caller)
+{
+	int weap_index = (int)(caller->GetSelectedItem()->GetParentItem()->GetData());
+	Assert( weap_index >= 0 );
+
+	labviewer_change_model(Weapon_info[weap_index].external_model_name, caller->GetSelectedItem()->GetData(), weap_index);
+}
+
 extern void weapon_load_bitmaps(int weapon_index);
 void labviewer_change_weapon(Tree *caller)
 {
@@ -2214,6 +2222,9 @@ void labviewer_make_weap_window(Button* caller)
 
 		if (Weapon_info[i].tech_model[0] != '\0') {
 			cmp->AddItem(cwip, "Tech Model", 0, false, labviewer_show_tech_model);
+		}
+		if (Weapon_info[i].external_model_name[0] != '\0') {
+			cmp->AddItem(cwip, "External Model", 0, false, labviewer_show_external_model);
 		}
 	}
 
