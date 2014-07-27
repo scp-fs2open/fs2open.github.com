9f768b8cff5920faf5089ff29204a633b2f7170b
niffiwan <niffiwan@387891d4-d844-0410-90c0-e4c51a9137d3>
2014-07-27 11:10:56 +0000

    Doh! Assign, not equality!
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10958 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -10274,7 +10274,7 @@ int ship_fire_primary(object * obj, int stream_weapons, int force)
 					fbfire_info.target = NULL;
 				}
 				fbfire_info.turret = &shipp->fighter_beam_turret_data;
-				fbfire_info.bfi_flags == BFIF_IS_FIGHTER_BEAM;
+				fbfire_info.bfi_flags = BFIF_IS_FIGHTER_BEAM;
 				fbfire_info.bank = bank_to_fire;
 
 				for ( v = 0; v < points; v++ ){
