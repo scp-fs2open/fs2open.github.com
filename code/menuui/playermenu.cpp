b88edd53bc78d3c9c6b1f1223abdedc98aa9b933
niffiwan <niffiwan@387891d4-d844-0410-90c0-e4c51a9137d3>
2014-08-21 09:15:16 +0000

    Followup fix for mantis 2883 (from Yarn)
    Set Lcl_special_chars when changing fonts
    Change some copyright chars to use Lcl_special_chars
    
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@11016 387891d4-d844-0410-90c0-e4c51a9137d3

--- code/menuui/playermenu.cpp
+++ code/menuui/playermenu.cpp
@@ -1140,11 +1140,7 @@ void player_select_display_copyright()
 
 //	sprintf(Copyright_msg1, NOX("FreeSpace 2"));
 	get_version_string(Copyright_msg1, sizeof(Copyright_msg1));
-	if (Lcl_gr) {
-		sprintf(Copyright_msg2, XSTR("Copyright %c 1999, Volition, Inc.  All rights reserved.", 385), '\xA8');
-	} else {
-		sprintf(Copyright_msg2, XSTR("Copyright %c 1999, Volition, Inc.  All rights reserved.", 385), '\x83');
-	}
+	sprintf(Copyright_msg2, XSTR("Copyright %c 1999, Volition, Inc.  All rights reserved.", 385), Lcl_special_chars + 4);
 
 	gr_get_string_size(&w, NULL, Copyright_msg1);
 	sx = fl2i((gr_screen.max_w_unscaled / 2) - w/2.0f + 0.5f);
