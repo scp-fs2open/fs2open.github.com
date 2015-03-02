05b463504846ff2e536bb5d3295b8c1b15a4a07c
zookeeper <zookeeper@387891d4-d844-0410-90c0-e4c51a9137d3>
2015-03-02 13:47:19 +0000

    Added bay arrive/depart speed multipliers to +Path Metadata.
    git-svn-id: svn://svn.icculus.org/fs2open/branches/zookeeper@11271 387891d4-d844-0410-90c0-e4c51a9137d3
    
    Conflicts:
    	code/ai/ai_profiles.cpp

--- code/ai/ai_profiles.cpp
+++ code/ai/ai_profiles.cpp
@@ -462,8 +462,8 @@ void parse_ai_profiles_tbl(const char *filename)
 
 				set_flag(profile, "$strict turret-tagged-only targeting:", AIPF2_STRICT_TURRET_TAGGED_ONLY_TARGETING, AIP_FLAG2);
 
-				profile->bay_arrive_speed_mult = 0.0f;
-				profile->bay_depart_speed_mult = 0.0f;
+				profile->bay_arrive_speed_mult = 1.0f;
+				profile->bay_depart_speed_mult = 1.0f;
 				if (optional_string("$bay arrive start speed multiplier:")) {
 					stuff_float(&profile->bay_arrive_speed_mult);
 				}
