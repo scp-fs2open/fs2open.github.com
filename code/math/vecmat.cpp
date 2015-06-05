945f655e0e47fbc2f5b80b31c304cf8de77b4cc3
Peter Mitsis <peter.mitsis@gmail.com>
2015-06-05 06:29:25 -0400

    Remove unnecessary vm_vec_normalize() checks
    The routine vm_vec_normalize() always returns a value greater than zero.
    Thus, there is no need to test its return value for values that are less
    than or equal to zero.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@gmail.com>

--- code/math/vecmat.cpp
+++ code/math/vecmat.cpp
@@ -479,7 +479,7 @@ float vm_vec_copy_normalize(vec3d *dest, const vec3d *src)
 	return m;
 }
 
-//normalize a vector. returns mag of source vec
+//normalize a vector. returns mag of source vec (always greater than zero)
 float vm_vec_normalize(vec3d *v)
 {
 	float t;
@@ -885,8 +885,7 @@ matrix *vm_vector_2_matrix(matrix *m, const vec3d *fvec, const vec3d *uvec, cons
 			vm_vec_crossprod(yvec,zvec,xvec);
 
 			//normalize new perpendicular vector
-			if (vm_vec_normalize(yvec) == 0.0f)
-				vm_vector_2_matrix_gen_vectors(m);
+			vm_vec_normalize(yvec);
 
 			//now recompute right vector, in case it wasn't entirely perpendiclar
 			vm_vec_crossprod(xvec,yvec,zvec);
@@ -898,8 +897,7 @@ matrix *vm_vector_2_matrix(matrix *m, const vec3d *fvec, const vec3d *uvec, cons
 		vm_vec_crossprod(xvec,yvec,zvec);
 
 		//normalize new perpendicular vector
-		if (vm_vec_normalize(xvec) == 0.0f)
-			vm_vector_2_matrix_gen_vectors(m);
+		vm_vec_normalize(xvec);
 
 		//now recompute up vector, in case it wasn't entirely perpendiclar
 		vm_vec_crossprod(yvec,zvec,xvec);
@@ -926,8 +924,7 @@ matrix *vm_vector_2_matrix_norm(matrix *m, const vec3d *fvec, const vec3d *uvec,
 			vm_vec_crossprod(yvec,zvec,xvec);
 
 			//normalize new perpendicular vector
-			if (vm_vec_normalize(yvec) == 0.0f)
-				vm_vector_2_matrix_gen_vectors(m);
+			vm_vec_normalize(yvec);
 
 			//now recompute right vector, in case it wasn't entirely perpendiclar
 			vm_vec_crossprod(xvec,yvec,zvec);
@@ -937,8 +934,7 @@ matrix *vm_vector_2_matrix_norm(matrix *m, const vec3d *fvec, const vec3d *uvec,
 		vm_vec_crossprod(xvec,yvec,zvec);
 
 		//normalize new perpendicular vector
-		if (vm_vec_normalize(xvec) == 0.0f)
-			vm_vector_2_matrix_gen_vectors(m);
+		vm_vec_normalize(xvec);
 
 		//now recompute up vector, in case it wasn't entirely perpendiclar
 		vm_vec_crossprod(yvec,zvec,xvec);
@@ -1301,8 +1297,7 @@ void vm_orthogonalize_matrix(matrix *m_src)
 
 		} else {  // use the right vector to figure up vector
 			vm_vec_crossprod(&m->vec.uvec, &m->vec.fvec, &m_src->vec.rvec);
-			if (vm_vec_normalize(&m->vec.uvec) == 0.0f)
-				Error( LOCATION, "Bad vector!" );
+			vm_vec_normalize(&m->vec.uvec);
 		}
 
 	} else {  // use source up vector
@@ -1313,8 +1308,7 @@ void vm_orthogonalize_matrix(matrix *m_src)
 	vm_vec_crossprod(&m->vec.rvec, &m->vec.uvec, &m->vec.fvec);
 		
 	//normalize new perpendicular vector
-	if (vm_vec_normalize(&m->vec.rvec) == 0.0f)
-		Error( LOCATION, "Bad vector!" );
+	vm_vec_normalize(&m->vec.rvec);
 
 	//now recompute up vector, in case it wasn't entirely perpendicular
 	vm_vec_crossprod(&m->vec.uvec, &m->vec.fvec, &m->vec.rvec);
