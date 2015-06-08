a92ebdc08a9a484df5691a7063ae5101929a58b6
Peter Mitsis <peter.mitsis@gmail.com>
2015-06-08 06:33:33 -0400

    Remove commented out from vm_vec_copy_normalize()
    The commented out code was cluttering things up.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@gmail.com>

--- code/math/vecmat.cpp
+++ code/math/vecmat.cpp
@@ -456,13 +456,8 @@ float vm_vec_copy_normalize(vec3d *dest, const vec3d *src)
 
 	//	Mainly here to trap attempts to normalize a null vector.
 	if (m <= 0.0f) {
-//		static int been_warned2 = false;//added this so the warning could be sounded and you can still get on with playing-Bobboau
-//		if(!been_warned2)
-		{
-			Warning(LOCATION, "Null vec3d in vec3d normalize.\n"
-							  "Trace out of vecmat.cpp and find offending code.\n");
-//			been_warned2 = true;
-		}
+		Warning(LOCATION, "Null vec3d in vec3d normalize.\n"
+						  "Trace out of vecmat.cpp and find offending code.\n");
 
 		dest->xyz.x = 1.0f;
 		dest->xyz.y = 0.0f;
