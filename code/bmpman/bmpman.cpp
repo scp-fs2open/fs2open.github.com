6abba98455e6c066c3e76a95977a59d0758735cb
Echelon9 <rhyskidd@gmail.com>
2015-06-23 11:49:48 +1000

    Increase consistency with adjacent code style, and avoid >80 character line wrap.

--- code/bmpman/bmpman.cpp
+++ code/bmpman/bmpman.cpp
@@ -2223,18 +2223,20 @@ void bm_page_in_xparent_texture(int bitmapnum, int nframes) {
 bool bm_page_out(int handle) {
 	int n = handle % MAX_BITMAPS;
 
-	Assert(n >= 0 && n < MAX_BITMAPS);
+	Assert(n >= 0);
+	Assert(handle == bm_bitmaps[n].handle);
 
 	// in case it's already been released
 	if (bm_bitmaps[n].type == BM_TYPE_NONE)
 		return 0;
 
-	Assert(bm_bitmaps[n].handle == handle);	// INVALID BITMAP HANDLE
-
-	// it's possible to hit < 0 here when model_page_out_textures() is called from
-	// anywhere other than in a mission
+	// it's possible to hit < 0 here when model_page_out_textures() is
+	// called from anywhere other than in a mission
 	if (bm_bitmaps[n].preload_count > 0) {
-		nprintf(("BmpMan", "PAGE-OUT: %s - preload_count remaining: %d\n", bm_bitmaps[n].filename, bm_bitmaps[n].preload_count));
+		nprintf(("BmpMan",
+			 "PAGE-OUT: %s - preload_count remaining: %d\n",
+			 bm_bitmaps[n].filename,
+			 bm_bitmaps[n].preload_count));
 
 		// lets decrease it for next time around
 		bm_bitmaps[n].preload_count--;
