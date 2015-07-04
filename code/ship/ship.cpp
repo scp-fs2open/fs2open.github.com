4f4fe00b97d940670ce9edffb251a336bbe654c5
Echelon9 <rhyskidd@gmail.com>
2015-07-04 16:01:36 +1000

    Fix: ship/ship.cpp:18381:6: warning: variable ‘clip_started’ set but not used [-Wunused-but-set-variable]

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -18375,18 +18375,13 @@ void ship_render(object* obj, draw_list* scene)
 		render_flags |= MR_SHOW_THRUSTERS;
 	}
 
-	// If the ship is going "through" the warp effect, then
-	// set up the model renderer to only draw the polygons in front
-	// of the warp in effect
-	int clip_started = 0;
-
 	// Warp_shipp points to the ship that is going through a
 	// warp... either this ship or the ship it is docked with.
 	if ( warp_shipp != NULL ) {
 		if ( warp_shipp->flags & SF_ARRIVING ) {
-			clip_started = warp_shipp->warpin_effect->warpShipClip(&render_info);
+			warp_shipp->warpin_effect->warpShipClip(&render_info);
 		} else if ( warp_shipp->flags & SF_DEPART_WARP ) {
-			clip_started = warp_shipp->warpout_effect->warpShipClip(&render_info);
+			warp_shipp->warpout_effect->warpShipClip(&render_info);
 		}
 	}
 
