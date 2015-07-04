6950c69ebb98c4706b3c11214bd05f9b2a9c03fd
Echelon9 <rhyskidd@gmail.com>
2015-07-05 00:54:29 +1000

    Fix: model/modelinterp.cpp:2034:44: warning: declaration of ‘gpoi’ shadows a previous local [-Wshadow]

--- code/model/modelinterp.cpp
+++ code/model/modelinterp.cpp
@@ -2031,7 +2031,7 @@ void model_render_DEPRECATED(int model_num, matrix *orient, vec3d * pos, uint fl
 		{
 			shipp = &Ships[Objects[objnum].instance];
 			sip = &Ship_info[shipp->ship_info_index];
-			SCP_unordered_map<int, void*>::iterator gpoi = sip->glowpoint_bank_override_map.find(-1);
+			gpoi = sip->glowpoint_bank_override_map.find(-1);
 		
 			if(gpoi != sip->glowpoint_bank_override_map.end()) {
 				override_all = true;
