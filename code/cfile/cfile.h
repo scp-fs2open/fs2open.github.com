9c698b9885d94f9fecc62fae080832a5454d3e98
Echelon9 <rhyskidd@gmail.com>
2015-08-15 14:29:54 +1000

    Refactor cfile_init() such that it is passed a path to directory, not path to a file. Also affects FRED.

--- code/cfile/cfile.h
+++ code/cfile/cfile.h
@@ -114,8 +114,7 @@ extern char Cfile_user_dir[CFILE_ROOT_DIRECTORY_LEN];
 #endif
 
 //================= LOW-LEVEL FUNCTIONS ==================
-// Call this once at the beginning of the program
-int cfile_init(const char *exe_dir, const char *cdrom_dir=NULL);
+int cfile_init(const char *path_dir, const char *cdrom_dir=NULL);
 
 // Call this if pack files got added or removed or the
 // cdrom changed.  This will refresh the list of filenames 
