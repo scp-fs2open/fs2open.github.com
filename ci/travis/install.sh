07fcd05fff8a7ac8fac48b9cf9c7a3ac65a0ca1f
asarium <asarium@gmail.com>
2015-08-21 18:08:41 +0200

    Add CI support for MacOSX
    Requires enabling the multi-os feature of travis CI

---
+++ ci/travis/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    sudo apt-get install libopenal-dev libogg-dev libvorbis-dev build-essential automake1.10 autoconf libsdl1.2-dev libtheora-dev libreadline6-dev libpng12-dev libjpeg62-dev liblua5.1-0-dev libjansson-dev libtool
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    gem install xcpretty
+fi
