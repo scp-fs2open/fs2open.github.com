4e21d9ee799cc9e34acbc39f1860b3d24bb74bb8
asarium <asarium@gmail.com>
2015-10-14 18:09:16 +0200

    Make error fatal when doing a CI build

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
         AUTOGEN_CONFIG=--enable-debug
     fi
     
-    ./autogen.sh $AUTOGEN_CONFIG
+    ./autogen.sh --enable-fatal-warnings $AUTOGEN_CONFIG
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     cd projects/Xcode4
     tar -xvzf Frameworks.tgz
