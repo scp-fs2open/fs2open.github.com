a020095b074412d3db8a93a1f4f5b0fa97428093
asarium <asarium@gmail.com>
2015-10-14 18:44:57 +0200

    Disable array bounds checks in a Travis build

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    AUTOGEN_CONFIG=
+    # Due to a bug in gcc the array bounds check isn't working correctly
+    # This can be removed when gcc is updated
+    AUTOGEN_CONFIG="CXXFLAGS=-Wno-array-bounds --enable-fatal-warnings"
     
     if [ "$CONFIGURATION" = "Debug" ]; then
-        AUTOGEN_CONFIG=--enable-debug
+        AUTOGEN_CONFIG="$AUTOGEN_CONFIG --enable-debug"
     fi
     
-    ./autogen.sh --enable-fatal-warnings $AUTOGEN_CONFIG
+    ./autogen.sh $AUTOGEN_CONFIG
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     cd projects/Xcode4
     tar -xvzf Frameworks.tgz
