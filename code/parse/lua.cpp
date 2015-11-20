6dd31585704f4496fd4d9effafe79d71c776f248
MageKing17 <MageKing17@gmail.com>
2015-11-19 20:27:32 -0800

    Make l_Wing functions handle invalid handles.
    Indexing an invalid wing handle now returns invalid ship handles, getting its length returns 0, and getting its name returns an empty string.

--- code/parse/lua.cpp
+++ code/parse/lua.cpp
@@ -8797,7 +8797,7 @@ ADE_FUNC(isValid, l_Message, NULL, "Checks if the message handle is valid", "boo
 //**********HANDLE: Wing
 ade_obj<int> l_Wing("wing", "Wing handle");
 
-ADE_INDEXER(l_Wing, "number Index", "Array of ships in the wing", "ship", "Ship handle, or invalid ship handle if index is invawing handle is invalid")
+ADE_INDEXER(l_Wing, "number Index", "Array of ships in the wing", "ship", "Ship handle, or invalid ship handle if index is invalid or wing handle is invalid")
 {
 	int wdx;
 	int sdx;
@@ -8805,7 +8805,7 @@ ADE_INDEXER(l_Wing, "number Index", "Array of ships in the wing", "ship", "Ship 
 	if(!ade_get_args(L, "oi|o", l_Wing.Get(&wdx), &sdx, l_Ship.GetPtr(&ndx)))
 		return ade_set_error(L, "o", l_Ship.Set(object_h()));
 
-	if(sdx < 1 || sdx > Wings[wdx].current_count) {
+	if(wdx < 0 || wdx >= Num_wings || sdx < 1 || sdx > Wings[wdx].current_count) {
 		return ade_set_error(L, "o", l_Ship.Set(object_h()));
 	}
 
@@ -8819,11 +8819,11 @@ ADE_INDEXER(l_Wing, "number Index", "Array of ships in the wing", "ship", "Ship 
 	return ade_set_args(L, "o", l_Ship.Set(object_h(&Objects[Ships[Wings[wdx].ship_index[sdx]].objnum])));
 }
 
-ADE_FUNC(__len, l_Wing, NULL, "Gets the number of ships in the wing", "number", "Number of ships in wing")
+ADE_FUNC(__len, l_Wing, NULL, "Gets the number of ships in the wing", "number", "Number of ships in wing, or 0 if invalid handle")
 {
 	int wdx;
-	if(!ade_get_args(L, "o", l_Wing.Get(&wdx)))
-		return ade_set_error(L, "i", NULL);
+	if(!ade_get_args(L, "o", l_Wing.Get(&wdx)) || wdx < 0 || wdx >= Num_wings)
+		return ade_set_error(L, "i", 0);
 
 	return ade_set_args(L, "i", Wings[wdx].current_count);
 }
@@ -8832,7 +8832,7 @@ ADE_VIRTVAR(Name, l_Wing, "string", "Name of Wing", "string", "Wing name, or emp
 {
 	int wdx;
 	char *s = NULL;
-	if ( !ade_get_args(L, "o|s", l_Wing.Get(&wdx), &s) )
+	if ( !ade_get_args(L, "o|s", l_Wing.Get(&wdx), &s) || wdx < 0 || wdx >= Num_wings )
 		return ade_set_error(L, "s", "");
 
 	if(ADE_SETTING_VAR && s != NULL) {
