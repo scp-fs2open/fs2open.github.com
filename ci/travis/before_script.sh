335cc4a114c9d87eb8cf1483fa1a2ca01ee58e59
asarium <asarium@gmail.com>
2016-01-24 14:31:13 +0100

    Use generic architecture by default

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     # Due to a bug in gcc the array bounds check isn't working correctly
     # This can be removed when gcc is updated
-    AUTOGEN_CONFIG="CXXFLAGS=-Wno-array-bounds --enable-fatal-warnings"
+    AUTOGEN_CONFIG="CXXFLAGS=-Wno-array-bounds --enable-fatal-warnings --enable-generic-architecture"
     
     if [ "$CONFIGURATION" = "Debug" ]; then
         AUTOGEN_CONFIG="$AUTOGEN_CONFIG --enable-debug"
