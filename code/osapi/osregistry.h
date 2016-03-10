5b1ff86f4209a00de231fba60d0f87cea90b8bc5
asarium <asarium@gmail.com>
2016-03-10 18:14:17 +0100

    Fix Windows Registry configuration

--- code/osapi/osregistry.h
+++ code/osapi/osregistry.h
@@ -35,15 +35,8 @@ extern const char *Osreg_user_dir;
 // initialize the registry. setup default keys to use
 void os_init_registry_stuff( const char *company, const char *app, const char *version );
 
-// Removes a value from to the INI file.  Passing
-// name=NULL will delete the section.
-void os_config_remove( const char *section, const char *name );
-
 // Writes a string to the registry
 void os_config_write_string( const char *section, const char *name, const char *value );
-
-// same as previous function except we don't use the application name to build up the keyname
-void os_config_write_string2( const char *section, const char *name, const char *value );
 
 // Writes an unsigned int to the INI file.  
 void os_config_write_uint( const char *section, const char *name, unsigned int value );
@@ -55,15 +48,9 @@ void os_config_write_uint( const char *section, const char *name, unsigned int v
 // same buffer.
 const char * os_config_read_string( const char *section, const char *name, const char *default_value=0 /*NULL*/ );
 
-// same as previous function except we don't use the application name to build up the keyname
-const char * os_config_read_string2( const char *section, const char *name, const char *default_value=0 /*NULL*/ );
-
 // Reads a string from the INI file.  Default_value must 
 // be passed, and if 'name' isn't found, then returns default_value
 unsigned int  os_config_read_uint( const char *section, const char *name, unsigned int default_value );
-
-// uses Ex versions of Windows registry functions
-const char * os_config_read_string_ex( const char *keyname, const char *name, const char *default_value );
 
 #endif
 
