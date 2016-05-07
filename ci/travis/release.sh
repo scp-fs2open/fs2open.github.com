7137dd63725cd8854bc482bff35963a8aff6ed78
asarium <asarium@gmail.com>
2016-05-07 14:29:38 +0200

    Add release script

---
+++ ci/travis/release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    # Due to a bug in gcc the array bounds check isn't working correctly
+    # This can be removed when gcc is updated
+    AUTOGEN_CONFIG="--enable-generic-architecture --prefix=/tmp/release"
+
+    # First compile the release build
+    ./autogen.sh $AUTOGEN_CONFIG
+
+    make -j4
+
+    make distclean
+
+    # Now compile debug
+    ./autogen.sh $AUTOGEN_CONFIG --enable-debug
+
+    make -j4
+
+    ls -al /tmp/release/bin
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+fi
