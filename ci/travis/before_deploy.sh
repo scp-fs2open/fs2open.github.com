edec7aa354c0bb548106eab1b09b9045ca8f35de
asarium <asarium@gmail.com>
2016-06-04 15:06:24 +0200

    Improve rename script

--- ci/travis/before_deploy.sh
+++ ci/travis/before_deploy.sh
@@ -4,5 +4,5 @@ set -ex
 
 if [ "$NIGHTLY_BUILD" = true ]; then
     echo "Fixing bintray config..."
-    sed -i "s/\$VERSION_NAME/$VERSION_NAME/g" ci/bintray.json
+    sed -e "s/\$VERSION_NAME/$VERSION_NAME/" ci/bintray.template.json > ci/bintray.json
 fi
