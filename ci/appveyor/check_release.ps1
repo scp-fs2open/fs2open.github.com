3f6ce50e73c02df5dc4a04d2d831788f3193d84c
asarium <asarium@gmail.com>
2016-06-04 16:51:44 +0200

    Enable nightly building always

--- ci/appveyor/check_release.ps1
+++ ci/appveyor/check_release.ps1
@@ -2,24 +2,24 @@
 write "$env:APPVEYOR_REPO_TAG"
 write "$env:APPVEYOR_REPO_TAG_NAME"
 
+# Default values
+AppveyorBuildVariable 'ReleaseBuild' 'false'
+AppveyorBuildVariable 'NightlyBuild' 'false'
+AppveyorBuildVariable 'DeployBuild' 'false'
+
 if ([System.Convert]::ToBoolean($env:APPVEYOR_REPO_TAG) -And ("$env:APPVEYOR_REPO_TAG_NAME" -match "^release.*")) {
     # Tag matches
     Set-AppveyorBuildVariable 'ReleaseBuild' 'true'
-} else {
-    # Standard CI build
-    Set-AppveyorBuildVariable 'ReleaseBuild' 'false'
 }
+
 if ([System.Convert]::ToBoolean($env:APPVEYOR_REPO_TAG) -And ("$env:APPVEYOR_REPO_TAG_NAME" -match "^nightly.*")) {
     # Tag matches
     Set-AppveyorBuildVariable 'NightlyBuild' 'true'
-} else {
-    # Standard CI build
-    Set-AppveyorBuildVariable 'NightlyBuild' 'false'
 }
+Set-AppveyorBuildVariable 'NightlyBuild' 'true'
 
 if ([System.Convert]::ToBoolean($env:ReleaseBuild) -Or [System.Convert]::ToBoolean($env:NightlyBuild)) {
     Set-AppveyorBuildVariable 'DeployBuild' 'true'
-} else {
-    Set-AppveyorBuildVariable 'DeployBuild' 'false'
 }
+
 Set-AppveyorBuildVariable 'TestVariable' 'test'
