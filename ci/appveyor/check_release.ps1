bc9ccbbcde57e0c434179f49382c427bf3360df2
asarium <asarium@gmail.com>
2016-06-04 23:28:40 +0200

    Fix package name variable

--- ci/appveyor/check_release.ps1
+++ ci/appveyor/check_release.ps1
@@ -13,14 +13,14 @@ Set-AppveyorBuildVariable 'VersionName' ''
 if ([System.Convert]::ToBoolean($env:APPVEYOR_REPO_TAG) -And ("$env:APPVEYOR_REPO_TAG_NAME" -match "^release_(.*)")) {
     # Tag matches
     Set-AppveyorBuildVariable 'ReleaseBuild' 'true'
-    Set-AppveyorBuildVariable 'PackageName' "release_$matches[1]"
+    Set-AppveyorBuildVariable 'PackageName' "release_$($matches[1])"
 }
 
 if ([System.Convert]::ToBoolean($env:APPVEYOR_REPO_TAG) -And ("$env:APPVEYOR_REPO_TAG_NAME" -match "^nightly_(.*)")) {
     # Tag matches
     Set-AppveyorBuildVariable 'NightlyBuild' 'true'
-    Set-AppveyorBuildVariable 'PackageName' "nightly_$matches[1]"
-	Set-AppveyorBuildVariable 'VersionName' "$matches[1]"
+    Set-AppveyorBuildVariable 'PackageName' "nightly_$($matches[1])"
+	Set-AppveyorBuildVariable 'VersionName' "$($matches[1])"
 }
 
 if ([System.Convert]::ToBoolean($env:ReleaseBuild) -Or [System.Convert]::ToBoolean($env:NightlyBuild)) {
