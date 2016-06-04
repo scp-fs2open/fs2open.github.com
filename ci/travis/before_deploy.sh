1d1d011b07b22b097eb5be8dad0a2b4882c7b4c9
asarium <asarium@gmail.com>
2016-06-04 14:42:21 +0200

    Try to fix if

--- ci/travis/before_deploy.sh
+++ ci/travis/before_deploy.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [[ "$NIGHTLY_BUILD" == true ]]; then
+if [ "$NIGHTLY_BUILD" = true ]; then
     echo "Fixing bintray config..."
     sed -i "s/\$VERSION_NAME/$VERSION_NAME/g" ci/bintray.json
 fi
