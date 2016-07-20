c062a071fb99010cef8056ae0e091b53efe07391
z64555 <z1281110@gmail.com>
2016-07-20 14:18:11 -0500

    Cleanup and doxy a few dialogs

--- fred2/AltShipClassDlg.h
+++ fred2/AltShipClassDlg.h
@@ -4,83 +4,131 @@
  * create based on the source.
  */
 
-#if !defined(AFX_AltShipClassDlg_H__2FE2C917_C7DA_4C05_A11F_DCC46BFB26BB__INCLUDED_)
-#define AFX_AltShipClassDlg_H__2FE2C917_C7DA_4C05_A11F_DCC46BFB26BB__INCLUDED_
+#ifndef _ALTSHIPCLASSDLG_H
+#define _ALTSHIPCLASSDLG_H
 
-#if _MSC_VER > 1000
-#pragma once
-#endif // _MSC_VER > 1000
-// AltShipClassDlg.h : header file
-//
-
-/////////////////////////////////////////////////////////////////////////////
-// AltShipClassDlg dialog
-
+/**
+ * @class AltShipClassDlg
+ *
+ * @brief Alternate Ship Class Editor, called from the Alt Ship Class button (Ships Editor)
+ */
 class AltShipClassDlg : public CDialog
 {
-// Construction
 public:
-	AltShipClassDlg(CWnd* pParent = NULL);   // standard constructor
+	/**
+	 * @brief Standard constructor, nothing special
+	 *
+	 * @see OnInitDialog
+	 */
+	AltShipClassDlg(CWnd* pParent = NULL);
 
-// Dialog Data
 	//{{AFX_DATA(AltShipClassDlg)
-	enum { IDD = IDD_ALT_SHIP_CLASS };
-	CButton	m_default_to_class;
-	CListBox	m_alt_class_list;
-	CComboBox	m_set_from_ship_class;
-	CComboBox	m_set_from_variables;
-	int		m_selected_variable;
-	int		m_selected_class;
+	enum
+	{
+		IDD = IDD_ALT_SHIP_CLASS
+	};
+
+	CButton m_default_to_class; //!< That's no button, its a checkbox! Filled if the selected alt ship type in the class list is considered the default.
+	CListBox  m_alt_class_list; //!< Listbox of alternative ship types allowed for the selected ship
+
+	CComboBox m_set_from_ship_class;    //!< Choicebox of available ship types
+	int m_selected_class;               //!< The selected ship class in the m_set_from_ship_class choicebox
+
+	CComboBox m_set_from_variables;     //!< Choicebox of available SEXP variables
+	int m_selected_variable;            //!< The selected SEXP variable in the m_set_from_variables choicebox
 	//}}AFX_DATA
 
-
-// Overrides
-	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(AltShipClassDlg)
-	protected:
+protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
-// Implementation
-protected:
-
-	// Generated message map functions
 	//{{AFX_MSG(AltShipClassDlg)
+	/**
+	 * @brief Handler for dialog initialization
+	 */
 	virtual BOOL OnInitDialog();
+
+	/**
+	 * @brief Handler for the Cancel button
+	 */
 	virtual void OnCancel();
+
+	/**
+	 * @brief Handler for the OK button
+	 */
 	virtual void OnOK();
+
+	/**
+	 * @brief Handler for the AltClass::Add button
+	 */
 	afx_msg void OnAltClassAdd();
+
+	/**
+	 * @brief Handler for the AltClass::Insert button
+	 */
 	afx_msg void OnAltClassInsert();
+
+	/**
+	 * @brief Handler for the AltClass::Delete button
+	 */
 	afx_msg void OnAltClassDelete();
+
+	/**
+	 * @brief Handler for the MoveUp button
+	 *
+	 * @details Moves the selected item up by one
+	 */
 	afx_msg void OnMoveUp();
+
+	/**
+	 * @brief Handler for the MoveDown button
+	 *
+	 * @details Moves the selected item down by one
+	 */
 	afx_msg void OnMoveDown();
+
 	afx_msg void OnSelendokSetFromShipClass();
 	afx_msg void OnSelendokSetFromVariables();
 	afx_msg void OnSelchangeAltClassList();
+
+	/**
+	 * @brief Handler for the Default to Class checkbox
+	 */
 	afx_msg void OnDefaultToClass();
+
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 
 private:
-	
-	bool player_ships_only;		// Whether we only add player ships to the alt classes or if we can add any ship
+	bool player_ships_only; //!< True if we can only add player ships to the alt classes, false if we can add any
 
+	//! Pool of alt ship classes for the selected item.
+	//! The pool is shown in the m_alt_class_list but this vector is easier to work with internally.
 	SCP_vector<alt_class> alt_class_pool;
-	int num_string_variables;							// Number of string variables in the mission
-	int string_variable_indices[MAX_SEXP_VARIABLES];	// maps string variables to their index in Sexp_variables
-	int ship_class_indices[MAX_SHIP_CLASSES];		// maps ships in the ships combobox to their index in Ship_info
 
-	
-	// variables to handle selection of multiple ships
-	bool multi_edit;
-	int m_selected_ships[MAX_SHIPS];
-	int num_selected_ships;
+	int num_string_variables;                           //!< Number of string variables in the mission
+	int string_variable_indices[MAX_SEXP_VARIABLES];    //!< maps string variables to their index in Sexp_variables
+	int ship_class_indices[MAX_SHIP_CLASSES];           //!< maps ships in the ships combobox to their index in Ship_info
 
-	void AltShipClassDlg::alt_class_list_rebuild();	
-	void AltShipClassDlg::alt_class_update_entry(alt_class &list_item);	
+	//!@
+	//! Multi-edit member.
+	bool multi_edit;                    //!< True if multiple ships are selected
+	int m_selected_ships[MAX_SHIPS];    //!< Array containing the indices of the selected ships.
+	int num_selected_ships;             //!< Number of ships selected
+	//!@}
+
+	/**
+	 * @brief Helper function, rebuilds the alt ship pool and repopulates the m_alt_class_list listbox
+	 */
+	void AltShipClassDlg::alt_class_list_rebuild();
+
+	/**
+	 * @brief Helper function, updates the selected alt ship entry
+	 */
+	void AltShipClassDlg::alt_class_update_entry(alt_class &list_item);
 };
 
 //{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
-#endif // !defined(AFX_AltShipClassDlg_H__2FE2C917_C7DA_4C05_A11F_DCC46BFB26BB__INCLUDED_)
+#endif // _ALTSHIPCLASSDLG_H
--- fred2/adjustgriddlg.h
+++ fred2/adjustgriddlg.h
@@ -1,51 +1,78 @@
 /*
  * Copyright (C) Volition, Inc. 1999.  All rights reserved.
  *
- * All source code herein is the property of Volition, Inc. You may not sell 
- * or otherwise commercially exploit the source or things you created based on the 
+ * All source code herein is the property of Volition, Inc. You may not sell
+ * or otherwise commercially exploit the source or things you created based on the
  * source.
  *
-*/
+ */
+#ifndef _ADJUSTGRIDDLG_H
+#define _ADJUSTGRIDDLG_H
 
-
-
-/////////////////////////////////////////////////////////////////////////////
-// adjust_grid_dlg dialog
-
+/**
+ * @class adjust_grid_dlg
+ *
+ * @brief Dialog for the mission grid properties.
+ */
 class adjust_grid_dlg : public CDialog
 {
-// Construction
 public:
-	void OnOK();
-	adjust_grid_dlg(CWnd* pParent = NULL);   // standard constructor
+	/**
+	 * @brief Standard constructor, nothing special
+	 *
+	 * @see adjust_grid_dlg::OnInitDialog
+	 */
+	adjust_grid_dlg(CWnd* pParent = NULL);
 
-// Dialog Data
-	//{{AFX_DATA(adjust_grid_dlg)
-	enum { IDD = IDD_ADJUST_GRID };
-	CSpinButtonCtrl	m_spinz;
-	CSpinButtonCtrl	m_spiny;
-	CSpinButtonCtrl	m_spinx;
-	int		m_x;
-	int		m_y;
-	int		m_z;
+	/**
+	 * @brief Handler for the OK button.
+	 *
+	 * @details Transfers the X, Y, Z value from the spin control to the appropriate grid value and then exchanges data. The type of grid that is drawn (XZ, XY, YZ) is controlled by which button is active.
+	 */
+	void OnOK();
+
+	enum
+	{
+		IDD = IDD_ADJUST_GRID
+	};
+
+	CSpinButtonCtrl m_spinx;    //!< Spin control X value
+	CSpinButtonCtrl m_spiny;    //!< Spin control Y value
+	CSpinButtonCtrl m_spinz;    //!< Spin control Z value
+
+	int m_x;    //!< Grid's X value
+	int m_y;    //!< Grid's Y value
+	int m_z;    //!< Grid's Z value
 	//}}AFX_DATA
 
-// Overrides
-	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(adjust_grid_dlg)
-	protected:
+protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
-// Implementation
-protected:
-
-	// Generated message map functions
 	//{{AFX_MSG(adjust_grid_dlg)
+
+	/**
+	 * @brief Handler for dialog initialization.
+	 */
 	virtual BOOL OnInitDialog();
+
+	/**
+	 * @brief Handler for the XY button
+	 */
 	afx_msg void OnXyPlane();
+
+	/**
+	 * @brief Handler for the XZ button
+	 */
 	afx_msg void OnXzPlane();
+
+	/**
+	 * @brief Handler for the YZ button
+	 */
 	afx_msg void OnYzPlane();
 	//}}AFX_MSG
+
 	DECLARE_MESSAGE_MAP()
 };
+#endif	// _ADJUSTGRIDDLG_H
