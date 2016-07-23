581e6320e1e193446758edb7545f7dd96faa4438
z64555 <z1281110@gmail.com>
2016-07-23 13:17:32 -0500

    Cleanup grid.h and .cpp

--- fred2/grid.cpp
+++ fred2/grid.cpp
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) Volition, Inc. 1999.  All rights reserved.
  *
- * All source code herein is the property of Volition, Inc. You may not sell 
- * or otherwise commercially exploit the source or things you created based on the 
+ * All source code herein is the property of Volition, Inc. You may not sell
+ * or otherwise commercially exploit the source or things you created based on the
  * source.
  *
-*/
+ */
 
 
 
@@ -22,38 +22,13 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-extern int double_fine_gridlines;
-
-/////////////////////////////////////////////////////////////////////////////
-// CGrid dialog
-
-//	Modeless constructor, MK
-CGrid::CGrid(CView* pView)
-{
-	m_pGView = pView;
-}
-
-
-CGrid::CGrid(CWnd* pParent /*=NULL*/)
-	: CDialog(CGrid::IDD, pParent)
-{
-	//{{AFX_DATA_INIT(CGrid)
-	m_GridSize = 0;
-	//}}AFX_DATA_INIT
-
-	m_pGView = NULL;
-
-}
-
-
-void CGrid::DoDataExchange(CDataExchange* pDX)
-{
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CGrid)
-	DDX_Text(pDX, IDC_GRID_SIZE, m_GridSize);
-	DDV_MinMaxUInt(pDX, m_GridSize, 2, 20);
-	//}}AFX_DATA_MAP
-}
+/**
+ * @brief Creates (or recreates) a grid from the two given unit vectors
+ *
+ * @param forward Vector that points forward
+ * @param right Vector that points to the right
+ */
+void GridOrient(vec3d *forward, vec3d *right);
 
 
 BEGIN_MESSAGE_MAP(CGrid, CDialog)
@@ -68,18 +43,124 @@ BEGIN_MESSAGE_MAP(CGrid, CDialog)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
-/////////////////////////////////////////////////////////////////////////////
-// CGrid message handlers
 
-extern grid *The_grid;
+BOOL CGrid::Create(LPCTSTR lpszClassName, LPCTSTR lpszWindowName, DWORD dwStyle, const RECT& rect, CWnd* pParentWnd, UINT nID, CCreateContext* pContext) {
+	return CDialog::Create(IDD, pParentWnd);
+}
 
-void GridOrient(vec3d *forward, vec3d *right)
-{
+BOOL CGrid::Create() {
+	return CDialog::Create(CGrid::IDD);
+}
+
+CGrid::CGrid(CView* pView) {
+	m_pGView = pView;
+}
+
+CGrid::CGrid(CWnd* pParent /*=NULL*/)
+	: CDialog(CGrid::IDD, pParent) {
+	//{{AFX_DATA_INIT(CGrid)
+	m_GridSize = 0;
+	//}}AFX_DATA_INIT
+
+	m_pGView = NULL;
+
+}
+
+void CGrid::DoDataExchange(CDataExchange* pDX) {
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CGrid)
+	DDX_Text(pDX, IDC_GRID_SIZE, m_GridSize);
+	DDV_MinMaxUInt(pDX, m_GridSize, 2, 20);
+	//}}AFX_DATA_MAP
+}
+
+void CGrid::OnClose() {
+	DestroyWindow();
+}
+
+void CGrid::OnDestroy() {
+	UpdateData(TRUE);
+
+	CDialog::OnDestroy();
+
+}
+
+void CGrid::OnGridXyPlane() {
+	vec3d	forward, right;
+
+	vm_vec_make(&forward, 0.0f, 1.0f, 0.0f);
+	vm_vec_make(&right, 1.0f, 0.0f, 0.0f);
+
+	GridOrient(&forward, &right);
+}
+
+void CGrid::OnGridXzPlane() {
+	vec3d	forward, right;
+
+	vm_vec_make(&forward, 0.0f, 0.0f, 1.0f);
+	vm_vec_make(&right, 1.0f, 0.0f, 0.0f);
+
+	GridOrient(&forward, &right);
+}
+
+void CGrid::OnGridYzPlane() {
+	vec3d	forward, right;
+
+	vm_vec_make(&forward, 0.0f, 1.0f, 0.0f);
+	vm_vec_make(&right, 0.0f, 0.0f, 1.0f);
+
+	GridOrient(&forward, &right);
+}
+
+BOOL CGrid::OnInitDialog() {
+	CDialog::OnInitDialog();
+
+	CSpinButtonCtrl* pSpin = (CSpinButtonCtrl*) GetDlgItem(IDC_SPIN_GRID_SIZE);
+	pSpin->SetRange(2, 20);
+	if ((m_GridSize < 2) || (m_GridSize > 20))
+		m_GridSize = The_grid->ncols / 5;
+	pSpin->SetPos(m_GridSize);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CGrid::OnKillFocus(CWnd* pNewWnd) {
+	CDialog::OnKillFocus(pNewWnd);
+
+	DestroyWindow();
+
+}
+
+void CGrid::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar) {
+	CString	strValue;
+
+	CSpinButtonCtrl* pSpin = (CSpinButtonCtrl*) GetDlgItem(IDC_SPIN_GRID_SIZE);
+	strValue.Format("%i", pSpin->GetPos());
+	pSpin->GetBuddy()->SetWindowText(strValue);
+
+	The_grid->nrows = pSpin->GetPos() * 5;
+	The_grid->ncols = The_grid->nrows;
+
+	modify_grid(The_grid);
+
+	CDialog::OnVScroll(nSBCode, nPos, pScrollBar);
+}
+
+BOOL CGrid::DestroyWindow() {
+	// TODO: Add your specialized code here and/or call the base class
+
+	UpdateData(TRUE);
+
+	return CDialog::DestroyWindow();
+}
+
+void GridOrient(vec3d *forward, vec3d *right) {
 	vec3d	center;
 	int		nrows, ncols;
 	float	square_size;
 
-	if (The_grid != NULL){
+	if (The_grid != NULL) {
 		center = The_grid->center;
 		nrows = The_grid->nrows;
 		ncols = The_grid->ncols;
@@ -92,111 +173,10 @@ void GridOrient(vec3d *forward, vec3d *right)
 	}
 
 	The_grid = create_grid(The_grid, forward,
-		right,
-		&center,
-		nrows, ncols,
-		square_size);
+						   right,
+						   &center,
+						   nrows, ncols,
+						   square_size);
 
 	physics_init(&The_grid->physics);
-}
-
-void CGrid::OnGridXyPlane() 
-{
-	vec3d	forward, right;
-
-	vm_vec_make(&forward, 0.0f, 1.0f, 0.0f);
-	vm_vec_make(&right, 1.0f, 0.0f, 0.0f);
-
-	GridOrient(&forward, &right);
-}
-
-void CGrid::OnGridXzPlane() 
-{
-	vec3d	forward, right;
-
-	vm_vec_make(&forward, 0.0f, 0.0f, 1.0f);
-	vm_vec_make(&right, 1.0f, 0.0f, 0.0f);
-
-	GridOrient(&forward, &right);
-}
-
-void CGrid::OnGridYzPlane() 
-{
-	vec3d	forward, right;
-
-	vm_vec_make(&forward, 0.0f, 1.0f, 0.0f);
-	vm_vec_make(&right, 0.0f, 0.0f, 1.0f);
-
-	GridOrient(&forward, &right);	
-}
-
-BOOL CGrid::Create(LPCTSTR lpszClassName, LPCTSTR lpszWindowName, DWORD dwStyle, const RECT& rect, CWnd* pParentWnd, UINT nID, CCreateContext* pContext) 
-{
-	return CDialog::Create(IDD, pParentWnd);
-}
-
-BOOL CGrid::Create()
-{
-	return CDialog::Create(CGrid::IDD);
-}
-
-
-void CGrid::OnClose() 
-{
-	DestroyWindow();
-}
-
-void CGrid::OnDestroy() 
-{
-	UpdateData(TRUE);
-
-	CDialog::OnDestroy();
-	
-}
-
-void CGrid::OnKillFocus(CWnd* pNewWnd) 
-{
-	CDialog::OnKillFocus(pNewWnd);
-	
-	DestroyWindow();
-
-}
-
-BOOL CGrid::DestroyWindow() 
-{
-	// TODO: Add your specialized code here and/or call the base class
-
-	UpdateData(TRUE);
-	
-	return CDialog::DestroyWindow();
-}
-
-BOOL CGrid::OnInitDialog() 
-{
-	CDialog::OnInitDialog();
-	
-	CSpinButtonCtrl* pSpin = (CSpinButtonCtrl*) GetDlgItem(IDC_SPIN_GRID_SIZE);
-	pSpin->SetRange(2, 20);
-	if ((m_GridSize < 2) || (m_GridSize > 20))
-		m_GridSize = The_grid->ncols/5;
-	pSpin->SetPos(m_GridSize);
-	
-	return TRUE;  // return TRUE unless you set the focus to a control
-	              // EXCEPTION: OCX Property Pages should return FALSE
-}
-
-void CGrid::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar) 
-{
-	CString	strValue;
-
-	CSpinButtonCtrl* pSpin = (CSpinButtonCtrl*) GetDlgItem(IDC_SPIN_GRID_SIZE);
-	strValue.Format("%i", pSpin->GetPos());
-	pSpin->GetBuddy()->SetWindowText(strValue);
-
-	The_grid->nrows = pSpin->GetPos()*5;
-	The_grid->ncols = The_grid->nrows;
-
-	modify_grid(The_grid);
-
-	CDialog::OnVScroll(nSBCode, nPos, pScrollBar);
 }
--- fred2/grid.h
+++ fred2/grid.h
@@ -1,59 +1,126 @@
+#ifndef _GRID_H
+#define _GRID_H
 /*
  * Copyright (C) Volition, Inc. 1999.  All rights reserved.
  *
- * All source code herein is the property of Volition, Inc. You may not sell 
- * or otherwise commercially exploit the source or things you created based on the 
+ * All source code herein is the property of Volition, Inc. You may not sell
+ * or otherwise commercially exploit the source or things you created based on the
  * source.
  *
-*/
+ */
 
 
 
-/////////////////////////////////////////////////////////////////////////////
-// CGrid dialog
-
+/**
+ * @class CGrid
+ *
+ * @brief Dialog for adjusting/modifying the grid
+ */
 class CGrid : public CDialog
 {
-private:
-	CView*	m_pGView;
-
-// Construction
 public:
-	CGrid(CWnd* pParent = NULL);   // standard constructor
+	/**
+	 * @brief Standard constructor
+	 */
+	CGrid(CWnd* pParent = NULL);
+
+	/**
+	 * @brief Standard constructor
+	 */
 	CGrid(CView* pView);
 
+	/**
+	 * @brief Creates the dialog
+	 *
+	 * @TODO This might not be used. Verify and remove
+	 */
 	BOOL Create();
 
-// Dialog Data
 	//{{AFX_DATA(CGrid)
-	enum { IDD = IDD_GRID };
-	UINT	m_GridSize;
+	enum
+	{
+		IDD = IDD_GRID
+	};
+
+	UINT m_GridSize;    //!< Size of the the grid.
 	//}}AFX_DATA
 
-
-// Overrides
-	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CGrid)
-	public:
+	/**
+	 * @brief Creates the dialog
+	 *
+	 * @param[in] lpszClassName
+	 * @param[in] lpszWindowName
+	 * @param[in] dwStyle
+	 * @param[in] rect
+	 * @param[in] pParentWnd
+	 * @param[in] nID
+	 * @param[in] pContext
+	 */
 	virtual BOOL Create(LPCTSTR lpszClassName, LPCTSTR lpszWindowName, DWORD dwStyle, const RECT& rect, CWnd* pParentWnd, UINT nID, CCreateContext* pContext = NULL);
+
+	/**
+	 * @brief Destroys the dialog
+	 */
 	virtual BOOL DestroyWindow();
-	protected:
+
+protected:
+	/**
+	 * @brief Does data exchange
+	 *
+	 * @param[in] pDX
+	 */
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
-// Implementation
-protected:
-
 	// Generated message map functions
 	//{{AFX_MSG(CGrid)
+
+	/**
+	 * @brief Handler for the XY button
+	 */
 	afx_msg void OnGridXyPlane();
+
+	/**
+	 * @brief Handler for the XZ button
+	 */
 	afx_msg void OnGridXzPlane();
+
+	/**
+	 * @brief Handler for the YZ button
+	 */
 	afx_msg void OnGridYzPlane();
+
+	/**
+	 * @brief Handler for the Close button
+	 */
 	afx_msg void OnClose();
+
+	/**
+	 * @brief Handler for OnDestroy events
+	 */
 	afx_msg void OnDestroy();
+
+	/**
+	 * @brief Handler for OnKillFocus events
+	 */
 	afx_msg void OnKillFocus(CWnd* pNewWnd);
+
+	/**
+	 * @brief Handler for OnInitDialog events
+	 */
 	virtual BOOL OnInitDialog();
+
+	/**
+	 * @brief Handler for the vertical scroll buttons
+	 */
 	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
 	//}}AFX_MSG
+
 	DECLARE_MESSAGE_MAP()
+
+private:
+	CView*	m_pGView;   //!< Reference to parent window
+
 };
+#endif // _GRID_H
