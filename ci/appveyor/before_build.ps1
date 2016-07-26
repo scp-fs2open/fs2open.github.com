4d25eabcf6ff20f59b3d1f764cf1008e4c3f09ef
asarium <asarium@gmail.com>
2016-07-26 22:16:13 +0200

    Enable FRED2 by default
    If MFC could not be found then it's disabled without causing an error.

--- ci/appveyor/before_build.ps1
+++ ci/appveyor/before_build.ps1
@@ -6,6 +6,5 @@ if ($Env:VS_VERSION -eq "10") {
     $AdditionalFeatures="OFF"
 }
 
-cmake -DFSO_BUILD_FRED2="${AdditionalFeatures}" -DFSO_USE_SPEECH="${AdditionalFeatures}" `
-	-DCMAKE_INSTALL_PREFIX="$env:APPVEYOR_BUILD_FOLDER/../install" `
+cmake -DCMAKE_INSTALL_PREFIX="$env:APPVEYOR_BUILD_FOLDER/../install" `
 	-DFSO_USE_VOICEREC="${AdditionalFeatures}" -DMSVC_SIMD_INSTRUCTIONS=SSE2 -G "$Env:CMAKE_GENERATOR" -T "$Env:PlatformToolset" ..
