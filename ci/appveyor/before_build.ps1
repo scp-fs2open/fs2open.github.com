4edd6fd5a9de205e67cd29a4015f2a7c37c9311f
asarium <asarium@gmail.com>
2016-07-28 11:09:39 +0200

    Update tested visual studio versions

--- ci/appveyor/before_build.ps1
+++ ci/appveyor/before_build.ps1
@@ -1,10 +1,5 @@
 New-Item build -type directory
 Set-Location -Path build
 
-$AdditionalFeatures="ON"
-if ($Env:VS_VERSION -eq "10") {
-    $AdditionalFeatures="OFF"
-}
-
-cmake -DCMAKE_INSTALL_PREFIX="$env:APPVEYOR_BUILD_FOLDER/../install" -DFSO_USE_SPEECH="${AdditionalFeatures}" `
-	-DFSO_USE_VOICEREC="${AdditionalFeatures}" -DMSVC_SIMD_INSTRUCTIONS=SSE2 -G "$Env:CMAKE_GENERATOR" -T "$Env:PlatformToolset" ..
+cmake -DCMAKE_INSTALL_PREFIX="$env:APPVEYOR_BUILD_FOLDER/../install" -DFSO_USE_SPEECH="ON" `
+	-DFSO_USE_VOICEREC="ON" -DMSVC_SIMD_INSTRUCTIONS=SSE2 -G "$Env:CMAKE_GENERATOR" -T "$Env:PlatformToolset" ..
