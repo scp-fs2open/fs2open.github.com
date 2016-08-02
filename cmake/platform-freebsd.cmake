1f4e40757dc90021eb8bd002ac5b85b2c9309599
asarium <asarium@gmail.com>
2016-08-02 19:10:42 +0200

    Fix build on FreeBSD

--- cmake/platform-freebsd.cmake
+++ cmake/platform-freebsd.cmake
@@ -1,2 +1,10 @@
 
 target_compile_definitions(platform INTERFACE SCP_BSD)
+
+find_library(EXECINFO_LIB execinfo)
+
+if (NOT EXECINFO_LIB)
+    message(FATAL_ERROR "Couldn't find libexecinfo! Please install it and rerun the configuration.")
+else()
+    target_link_libraries(platform INTERFACE "${EXECINFO_LIB}")
+endif()
