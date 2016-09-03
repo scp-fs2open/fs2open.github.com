1d3ff1b6c0723223c1e058dc2b4f0e7447647a44
Goober5000 <ipw47@yahoo.com>
2016-09-02 23:25:46 -0400

    minor tweaks and adjustments to AI goal code
    A few things I noticed while working on ai-chase-ship-class, split out into a separate PR.  Some code is reordered, some is cleaned up; nothing too extensive.  The most notable changes are a) the modification of the Ai_goal_text function to work on the AI_GOAL_X #define directly rather than unnecessarily calculate the bit; b) changing the HUD text of "fly to ship" to "rendezvous with" plus the ship name; c) adding a Warning in ai_update_goal_references; and d) changing ship_name to target_name in ship_return_orders for clarity

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -15000,7 +15000,7 @@ char *ship_return_orders(char *outbuf, ship *sp)
 	ai_info	*aip;
 	ai_goal	*aigp;
 	const char		*order_text;
-	char ship_name[NAME_LENGTH];
+	char target_name[NAME_LENGTH];
 	
 	Assert(sp->ai_index >= 0);
 	aip = &Ai_info[sp->ai_index];
@@ -15011,29 +15011,29 @@ char *ship_return_orders(char *outbuf, ship *sp)
 	if ( aigp->ai_mode < 0 ) 
 		return NULL;
 
-	order_text = Ai_goal_text(bitmask_2_bitnum(aigp->ai_mode));
+	order_text = Ai_goal_text(aigp->ai_mode);
 	if ( order_text == NULL )
 		return NULL;
 
 	strcpy(outbuf, order_text);
 
 	if ( aigp->target_name ) {
-		strcpy_s(ship_name, aigp->target_name);
-		end_string_at_first_hash_symbol(ship_name);
+		strcpy_s(target_name, aigp->target_name);
+		end_string_at_first_hash_symbol(target_name);
 	}
-	switch (aigp->ai_mode ) {
+	switch ( aigp->ai_mode ) {
 
 		case AI_GOAL_FORM_ON_WING:
 		case AI_GOAL_GUARD_WING:
 		case AI_GOAL_CHASE_WING:
 			if ( aigp->target_name ) {
-				strcat(outbuf, ship_name);
-				strcat(outbuf, XSTR( "'s Wing", 494));
+				strcat(outbuf, target_name);
+				strcat(outbuf, XSTR("'s wing", 494));
 			} else {
-				strcpy(outbuf, XSTR( "no orders", 495));
+				strcpy(outbuf, XSTR("no orders", 495));
 			}
 			break;
-	
+
 		case AI_GOAL_CHASE:
 		case AI_GOAL_DOCK:
 		case AI_GOAL_UNDOCK:
@@ -15042,8 +15042,9 @@ char *ship_return_orders(char *outbuf, ship *sp)
 		case AI_GOAL_DISARM_SHIP:
 		case AI_GOAL_EVADE_SHIP:
 		case AI_GOAL_REARM_REPAIR:
-			if ( aigp->target_name ) {
-				strcat(outbuf, ship_name);
+		case AI_GOAL_FLY_TO_SHIP:
+			if (aigp->target_name) {
+				strcat(outbuf, target_name);
 			} else {
 				strcpy(outbuf, XSTR( "no orders", 495));
 			}
@@ -15054,9 +15055,9 @@ char *ship_return_orders(char *outbuf, ship *sp)
 				char subsys_name[NAME_LENGTH];
 				strcpy_s(subsys_name, aip->targeted_subsys->system_info->subobj_name);
 				hud_targetbox_truncate_subsys_name(subsys_name);
-				sprintf(outbuf, XSTR( "atk %s %s", 496), ship_name, subsys_name);
+				sprintf(outbuf, XSTR("atk %s %s", 496), target_name, subsys_name);
 			} else {
-				strcpy(outbuf, XSTR( "no orders", 495) );
+				strcpy(outbuf, XSTR("no orders", 495));
 			}
 			break;
 		}
@@ -15065,11 +15066,6 @@ char *ship_return_orders(char *outbuf, ship *sp)
 		case AI_GOAL_WAYPOINTS_ONCE:
 			// don't do anything, all info is in order_text
 			break;
-
-		case AI_GOAL_FLY_TO_SHIP:
-			strcpy(outbuf, "Flying to ship");
-			break;
-
 
 		default:
 			return NULL;
