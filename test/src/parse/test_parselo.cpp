4964fccd7eddfad6d977e663cb3cd3314e2ff079
asarium <asarium@gmail.com>
2016-09-04 16:32:11 +0200

    Don't initialize mission text when it's not used
    As far as I can tell this issue has been present for a very long time,
    maybe even since retail. It was only uncovered since the recent changes
    initialized the mission text by default which corrupted the table text
    that was currently being parsed.
    
    This also adds a unit test that validated that pausing works correctly.

--- test/src/parse/test_parselo.cpp
+++ test/src/parse/test_parselo.cpp
@@ -3,7 +3,56 @@
 
 #include <parse/parselo.h>
 
-TEST(ParseloTest, drop_trailing_whitespace_cstr) {
+#include "util/FSTestFixture.h"
+
+class ParseloTest : public test::FSTestFixture {
+ public:
+	ParseloTest() : test::FSTestFixture(0) {
+		pushModDir("parselo");
+	}
+
+ protected:
+	virtual void SetUp() override {
+		test::FSTestFixture::SetUp();
+	}
+	virtual void TearDown() override {
+		stop_parse();
+
+		test::FSTestFixture::TearDown();
+	}
+};
+
+TEST_F(ParseloTest, parse_pausing) {
+	read_file_text("test.tbl", CF_TYPE_TABLES);
+	reset_parse();
+
+	// Consume a token
+	required_string("#Start");
+
+	// Now pause parsing and check if the right text is loaded
+	char file_text[1024];
+	char file_text_raw[1024];
+
+	memset(file_text, 0, sizeof(file_text));
+	memset(file_text_raw, 0, sizeof(file_text_raw));
+
+	pause_parse();
+
+	read_file_text("test2.tbl", CF_TYPE_TABLES, file_text, file_text_raw);
+	reset_parse(file_text);
+
+	required_string("#Begin");
+	required_string("#End");
+
+	unpause_parse();
+
+	// We should be back in the original file
+	required_string("$Token:");
+	required_string("+OtherToken:");
+	required_string("#End");
+}
+
+TEST(ParseloUtilTest, drop_trailing_whitespace_cstr) {
 	char test_str[256];
 
 	strcpy_s(test_str, "Test string       ");
@@ -19,7 +68,7 @@ TEST(ParseloTest, drop_trailing_whitespace_cstr) {
 	ASSERT_STREQ(test_str, "");
 }
 
-TEST(ParseloTest, drop_trailing_whitespace) {
+TEST(ParseloUtilTest, drop_trailing_whitespace) {
 	SCP_string test_str;
 
 	test_str = "Test string       ";
@@ -35,7 +84,7 @@ TEST(ParseloTest, drop_trailing_whitespace) {
 	ASSERT_EQ(test_str, "");
 }
 
-TEST(ParseloTest, drop_leading_whitespace_cstr) {
+TEST(ParseloUtilTest, drop_leading_whitespace_cstr) {
 	char test_str[256];
 
 	strcpy_s(test_str, "          Test string");
@@ -51,7 +100,7 @@ TEST(ParseloTest, drop_leading_whitespace_cstr) {
 	ASSERT_STREQ(test_str, "");
 }
 
-TEST(ParseloTest, drop_leading_whitespace) {
+TEST(ParseloUtilTest, drop_leading_whitespace) {
 	SCP_string test_str;
 
 	test_str = "          Test string";
@@ -67,7 +116,7 @@ TEST(ParseloTest, drop_leading_whitespace) {
 	ASSERT_EQ(test_str, "");
 }
 
-TEST(ParseloTest, drop_whitespace_cstr) {
+TEST(ParseloUtilTest, drop_whitespace_cstr) {
 	char test_str[256];
 
 	strcpy_s(test_str, "          Test string          ");
@@ -91,7 +140,7 @@ TEST(ParseloTest, drop_whitespace_cstr) {
 	ASSERT_STREQ(test_str, "");
 }
 
-TEST(ParseloTest, drop_whitespace) {
+TEST(ParseloUtilTest, drop_whitespace) {
 	SCP_string test_str;
 
 	test_str = "          Test string";
