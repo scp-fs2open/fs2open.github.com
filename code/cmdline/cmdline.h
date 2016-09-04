f8f3cd33ceef9c9a3faaae91f07086f739c1733e
asarium <asarium@gmail.com>
2016-09-04 15:25:57 +0200

    Add shader caching
    This uses the program binary support included in OpenGL 4.1 to build a
    cache of compiled shaders. It does this by computing the MD5 of the
    source code of the shaders and using that hash to save the files. That
    should make sure that a change to a shader is automatically reflected in
    the shader cache.

--- code/cmdline/cmdline.h
+++ code/cmdline/cmdline.h
@@ -136,6 +136,7 @@ extern bool Cmdline_gl_finish;
 extern bool Cmdline_no_geo_sdr_effects;
 extern bool Cmdline_set_cpu_affinity;
 extern bool Cmdline_nograb;
+extern bool Cmdline_noshadercache;
 #ifdef WIN32
 extern bool Cmdline_alternate_registry_path;
 #endif
