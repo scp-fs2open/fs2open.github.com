8bb14d1a1faa3c5247121f35a7fa64225a16762c
Risto Toijala <risto.toijala@gmail.com>
2016-09-12 13:13:51 +0300

    Uncomment now working test for missing $end_multi_text
    This test works since 16d5f8830eacaa6d73aec5271dcfe5fcf30a6a74
    and c9f9e1b2f05e81e7ec9ba85a8771a082f81c3b2b.

--- test/src/menuui/test_intel_parse.cpp
+++ test/src/menuui/test_intel_parse.cpp
@@ -255,12 +255,7 @@ TEST_F(IntelParseTest, missing_end_multi_text) {
 
 	techroom_intel_init();
 
-	// Something is doing an out of bounds read, since this varies
-	// when running the tests with --gtest_shuffle.
-	//ASSERT_EQ(Intel_info_size, 1);
-
-	// Remove this when the above issue is fixed.
-	ASSERT_GE(Intel_info_size, 1);
+	ASSERT_EQ(Intel_info_size, 1);
 
 	test_intel_data_equal(expected_foo, Intel_info[0]);
 }
