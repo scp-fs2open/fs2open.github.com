23e41970f289ea3df00173f132325d37e33456af
Eshed <ashdnazg@gmail.com>
2016-10-01 21:33:32 +0300

    Remove boost dependency
    Add syncboundedqueue.h which is basically boost::sync_bounded_queue ported to std::* line for line.

--- ci/appveyor/build.ps1
+++ ci/appveyor/build.ps1
@@ -4,8 +4,6 @@ class BuildConfig {
 	[string]$PackageType
 	[string]$Toolset
 	[string]$SimdType
-	[string]$BoostRoot
-	[string]$BoostLibDir
 }
 
 $NightlyConfigurations = @(
@@ -14,16 +12,12 @@ $NightlyConfigurations = @(
 		PackageType="Win64";
 		Toolset="v140_xp";
 		SimdType="SSE2";
-		BoostRoot="C:\Libraries\boost_1_60_0"
-		BoostLibDir="lib64-msvc-14.0";
 	},
 	[BuildConfig]@{ 
 		Generator="Visual Studio 14 2015";
 		PackageType="Win32";
 		Toolset="v140_xp";
 		SimdType="SSE2";
-		BoostRoot="C:\Libraries\boost_1_60_0"
-		BoostLibDir="lib32-msvc-14.0";
 	}
 )
 $ReleaseConfigurations = @(
@@ -32,32 +26,24 @@ $ReleaseConfigurations = @(
 		PackageType="Win32";
 		Toolset="v140_xp";
 		SimdType="SSE2";
-		BoostRoot="C:\Libraries\boost_1_60_0"
-		BoostLibDir="lib32-msvc-14.0";
 	}
 	[BuildConfig]@{
 		Generator="Visual Studio 14 2015";
 		PackageType="Win32-AVX";
 		Toolset="v140_xp";
 		SimdType="AVX";
-		BoostRoot="C:\Libraries\boost_1_60_0"
-		BoostLibDir="lib32-msvc-14.0";
 	}
 	[BuildConfig]@{
 		Generator="Visual Studio 14 2015 Win64";
 		PackageType="Win64";
 		Toolset="v140_xp";
 		SimdType="SSE2";
-		BoostRoot="C:\Libraries\boost_1_60_0"
-		BoostLibDir="lib64-msvc-14.0";
 	}
 	[BuildConfig]@{
 		Generator="Visual Studio 14 2015 Win64";
 		PackageType="Win64-AVX";
 		Toolset="v140_xp";
 		SimdType="AVX";
-		BoostRoot="C:\Libraries\boost_1_60_0"
-		BoostLibDir="lib64-msvc-14.0";
 	}
 )
 
@@ -111,7 +97,6 @@ if ($DeployBuild) {
 	
 	cmake -DCMAKE_INSTALL_PREFIX="$env:APPVEYOR_BUILD_FOLDER/../install" -DFSO_USE_SPEECH="ON" `
 		-DFSO_USE_VOICEREC="ON" -DMSVC_SIMD_INSTRUCTIONS="$($buildConfig.SimdType)" `
-		-DBOOST_ROOT="$($buildConfig.BoostRoot)" -DBOOST_LIBRARYDIR="$($buildConfig.BoostRoot)\$($buildConfig.BoostLibDir)" `
 		-G "$($buildConfig.Generator)" -T "$($buildConfig.Toolset)" ..
 
 	$Configs = @("Release", "FastDebug")
@@ -127,7 +112,7 @@ if ($DeployBuild) {
     Push-AppveyorArtifact "$($PackageName)-builds-$($buildConfig.PackageType).zip"
 } else {
 	cmake -DFSO_USE_SPEECH="ON" -DFSO_FATAL_WARNINGS="ON" -DFSO_USE_VOICEREC="ON" -DMSVC_SIMD_INSTRUCTIONS=SSE2 `
-		-DBOOST_ROOT="$Env:BOOST_ROOT" -DBOOST_LIBRARYDIR="$Env:BOOST_LIBRARYDIR" -G "$Env:CMAKE_GENERATOR" -T "$Env:PlatformToolset" ..
+	-G "$Env:CMAKE_GENERATOR" -T "$Env:PlatformToolset" ..
 
     cmake --build . --config "$Env:CONFIGURATION" -- /verbosity:minimal
 
--- lib/FFmpeg.cmake
+++ lib/FFmpeg.cmake
@@ -4,15 +4,18 @@ add_library(ffmpeg_defs INTERFACE)
 IF(PLATFORM_WINDOWS)
     get_prebuilt_path(PREBUILT_PATH)
     set(FFMPEG_ROOT_DIR "${PREBUILT_PATH}/ffmpeg")
-    
+
     macro(add_av_lib name libname)
         add_library(${name} SHARED IMPORTED GLOBAL)
 
         set(include_dir "${FFMPEG_ROOT_DIR}/include/lib${name}")
         set(dll_name "${FFMPEG_ROOT_DIR}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}${libname}${CMAKE_SHARED_LIBRARY_SUFFIX}")
-        set(lib_name "${FFMPEG_ROOT_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${name}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+        set(lib_name "${FFMPEG_ROOT_DIR}/lib/${CMAKE_IMPORT_LIBRARY_PREFIX}${name}${CMAKE_IMPORT_LIBRARY_SUFFIX}")
+        if (NOT EXISTS "${dll_name}")
+            set(dll_name "${FFMPEG_ROOT_DIR}/bin/${libname}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+        endif()
         if (NOT EXISTS "${lib_name}")
-            set(lib_name "${FFMPEG_ROOT_DIR}/bin/${CMAKE_STATIC_LIBRARY_PREFIX}${name}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+            set(lib_name "${FFMPEG_ROOT_DIR}/bin/${CMAKE_IMPORT_LIBRARY_PREFIX}${name}${CMAKE_IMPORT_LIBRARY_SUFFIX}")
         endif()
 
         if (NOT IS_DIRECTORY "${include_dir}")
@@ -37,7 +40,7 @@ IF(PLATFORM_WINDOWS)
     add_av_lib(swscale swscale-4)
 
     add_av_lib(swresample swresample-2)
-    
+
     add_library(ffmpeg INTERFACE)
     target_link_libraries(ffmpeg INTERFACE avcodec)
     target_link_libraries(ffmpeg INTERFACE avformat)
@@ -46,7 +49,7 @@ IF(PLATFORM_WINDOWS)
     target_link_libraries(ffmpeg INTERFACE swresample)
 ELSE(WIN32)
     option(FFMPEG_USE_PRECOMPILED "Use precompiled version of FFmpeg. If disabled the system libraries will be used." OFF)
-    
+
     # CMake can't check for ffmpeg so we'll just use PkgConfig
     INCLUDE(FindPkgConfig REQUIRED)
     INCLUDE(util)
@@ -62,19 +65,19 @@ ELSE(WIN32)
         PKG_SEARCH_MODULE(avcodec "libavcodec")
         if (NOT avcodec_FOUND)
             message("FFmpeg libraries could not be found. Using prebuilt libraries...")
-            
+
             get_prebuilt_path(PREBUILT_PATH)
             set(FFMPEG_PATH "${PREBUILT_PATH}/ffmpeg")
             set(USING_PREBUILT_LIBS TRUE)
         endif()
     endif()
-    
+
     if (USING_PREBUILT_LIBS)
         message(STATUS "Using pre-built LGPL FFmpeg libraries.")
     else()
         message(STATUS "Using system FFmpeg libraries. Don't distribute these FSO binaries if these are the GPL libraries!")
     endif()
-    
+
     # I need to do some workarounds to be able to use our prebuilt LGPL builds
     # so we can distribute that in releases
     # The ffmpeg libs specify a SONAME that is different from the name of the file
@@ -84,18 +87,18 @@ ELSE(WIN32)
             PKG_SEARCH_MODULE(${name} REQUIRED "lib${name}")
 
             PKG_CONFIG_LIB_RESOLVE(${name} ${name}_LIB)
-            
+
             ADD_IMPORTED_LIB("${name}" "${${name}_INCLUDE_DIRS}" "${${name}_LIB}" SHARED)
         else()
             # Use our libraries
             find_library(${name}_LOCATION ${name}
                 PATHS "${FFMPEG_PATH}/lib"
                 NO_DEFAULT_PATH)
-                
+
             file(GLOB ${name}_LIBS "${FFMPEG_PATH}/lib/lib${name}*")
-                
+
             get_filename_component(FULL_LIB_PATH "${${name}_LOCATION}" REALPATH)
-            
+
             ADD_IMPORTED_LIB("${name}" "${FFMPEG_PATH}/include" "${FULL_LIB_PATH}")
 
             add_target_copy_files("${${name}_LIBS}")
@@ -107,10 +110,10 @@ ELSE(WIN32)
     add_av_lib(avutil)
     add_av_lib(swresample)
     add_av_lib(swscale)
-    
+
     add_library(ffmpeg INTERFACE)
     target_compile_definitions(ffmpeg INTERFACE __STDC_CONSTANT_MACROS)
-    
+
     target_link_libraries(ffmpeg INTERFACE avcodec)
     target_link_libraries(ffmpeg INTERFACE avformat)
     target_link_libraries(ffmpeg INTERFACE avutil)
