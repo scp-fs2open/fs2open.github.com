66b62ee4f051666fa820d01ee1331a63defb1a33
asarium <asarium@gmail.com>
2016-10-02 19:45:40 +0200

    Fix ADPCM with older FFmpeg versions
    ADPCM playback was broken by #991 when an older FFmpeg version (< 3.1)
    was used. The newer version has a better API for handling decoder that
    return multiple frames of data per packet which I already used. However,
    I didn't bother implementing that for the older API since I haven't seen
    a decoder that actually returned multiple frames per packet but ADPCM is
    such a decoder. This fixes that by implementing the correct behavior
    with the old API.

--- code/sound/ffmpeg/FFmpegAudioReader.h
+++ code/sound/ffmpeg/FFmpegAudioReader.h
@@ -16,8 +16,15 @@ class FFmpegAudioReader
 	AVFormatContext* _format_ctx = nullptr;
 	AVCodecContext* _codec_ctx = nullptr;
 
+#if LIBAVCODEC_VERSION_INT <= AV_VERSION_INT(57, 24, 255)
+    AVPacket* _currentPacket = nullptr;
+
+	void processPendingPacket();
+#endif
+
 public:
 	FFmpegAudioReader(AVFormatContext* av_format_context, AVCodecContext* codec_ctx, int stream_idx);
+	~FFmpegAudioReader();
 
 	bool readFrame(AVFrame* decode_frame);
 };
