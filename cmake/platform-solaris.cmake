accbc1d25d32d30b874911a43ce59ca5363e39fc
Cliff Gordon <cliff.gordon@ehi.com>
2017-04-21 15:12:15 -0500

    See if this helps with the linking errors for libsocket and libnsl symbols.

--- cmake/platform-solaris.cmake
+++ cmake/platform-solaris.cmake
@@ -1,2 +1,18 @@
 
 target_compile_definitions(platform INTERFACE SCP_SOLARIS)
+
+find_library(SOCKET_LIB socket)
+
+if (NOT SOCKET_LIB)
+    message(FATAL_ERROR "Couldn't find libsocket! Please install it and rerun the configuration.")
+else()
+    target_link_libraries(platform INTERFACE "${SOCKET_LIB}")
+endif()
+
+find_library(NSL_LIB nsl)
+
+if (NOT NSL_LIB)
+    message(FATAL_ERROR "Couldn't find libnsl! Please install it and rerun the configuration.")
+else()
+    target_link_libraries(platform INTERFACE "${NSL_LIB}")
+endif()
