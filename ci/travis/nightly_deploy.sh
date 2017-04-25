8a5212b786af9bad3a6953bd5369c8c4836c1721
asarium <asarium@gmail.com>
2017-04-25 10:34:41 +0200

    Upload nightly builds to FTP instead of BinTray

---
+++ ci/travis/nightly_deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -x
+set -e
+set -u
+
+cd /tmp/builds
+
+for file in *; do
+	# Upload to indiegames
+	curl -k "ftp://scp.indiegames.us/public_html/builds/nightly/$VERSION_NAME/" --user "$INDIEGAMES_USER:$INDIEGAMES_PASSWORD" -T "$file" --ftp-create-dirs
+
+	# Upload to fs2downloads
+	curl -k "ftp://swc.fs2downloads.com/swc.fs2downloads.com/builds/nightly/$VERSION_NAME/" --user "$FS2DOWNLOADS_USER:$FS2DOWNLOADS_PASSWORD" -T "$file" --ftp-create-dirs
+done
