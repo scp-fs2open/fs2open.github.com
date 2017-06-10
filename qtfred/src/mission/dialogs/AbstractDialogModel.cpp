41137fedbd6756329cc0ee9ad97799ec25a3695d
asarium <asarium@gmail.com>
2017-06-10 10:40:08 +0200

    Begin adding dialog infrastructure

--- qtfred/src/mission/dialogs/AbstractDialogModel.cpp
+++ qtfred/src/mission/dialogs/AbstractDialogModel.cpp
@@ -2,3 +2,18 @@
 //
 
 #include "AbstractDialogModel.h"
+
+namespace fso {
+namespace fred {
+namespace dialogs {
+
+
+AbstractDialogModel::AbstractDialogModel(QObject* parent,
+										 fso::fred::Editor* editor,
+										 fso::fred::FredRenderer* renderer) :
+	QObject(parent), _editor(editor), _renderer(renderer) {
+}
+
+}
+}
+}
--- qtfred/src/mission/dialogs/AbstractDialogModel.h
+++ qtfred/src/mission/dialogs/AbstractDialogModel.h
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <QtCore/QObject>
+
+#include <mission/editor.h>
+
 namespace fso {
 namespace fred {
-namespace dialog {
+namespace dialogs {
 
-class AbstractDialogModel {
+class AbstractDialogModel : public QObject {
+	Q_OBJECT
 
+ protected:
+	Editor* _editor = nullptr;
+	FredRenderer* _renderer = nullptr;
+
+ public:
+	AbstractDialogModel(QObject* parent, Editor* editor, FredRenderer* renderer);
 };
 
 }
