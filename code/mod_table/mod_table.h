841872aae0878f934de28b126f0ca4aafc8eeb9f
asarium <asarium@gmail.com>
2017-07-13 23:00:58 +0200

    Add mod table setting for specifying targetted FSO version
    This adds a small system for letting a mod specify which version of FSO
    was designed for. That would allow us to introduce code which would
    normally break backwards-compatibility by putting it in an if block
    which checks if the mod supports the FSO version which ontroduced the
    behavioral change.
    
    There is currently no code which uses the new functions yet so I added a
    few tests to verify that the options do what they are designed to do.
    
    This fixes #1389.

--- code/mod_table/mod_table.h
+++ code/mod_table/mod_table.h
@@ -32,3 +32,17 @@ extern float Generic_pain_flash_factor;
 extern float Shield_pain_flash_factor;
 
 void mod_table_init();
+
+/**
+ * @brief Checks if the mod specified support for the given engine version
+ *
+ * This is the function for implementing backwards-incompatible changes while not actually breaking backwards
+ * compatibility. If you want to introduce a change that may impact compatibility with an earlier version (e.g. retail)
+ * then you can use this function to check if the current mod targets a recent enough version.
+ *
+ * @param major The major version to check support for
+ * @param minor The minor version to check support for
+ * @param build The build version to check support for
+ * @return @c true if the mod specified support for this or a later version, @c false otherwise
+ */
+bool mod_supports_version(int major, int minor, int build);
