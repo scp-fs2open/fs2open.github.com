51557d3aa3d4253c75d6f43fe4eb5dbc65871b46
Fabian Woltermann <fwoltermann@gmail.com>
2018-07-01 09:00:36 +0200

    remove extra braces

--- code/math/vecmat.cpp
+++ code/math/vecmat.cpp
@@ -428,10 +428,8 @@ float vm_vec_copy_normalize(vec3d *dest, const vec3d *src)
 
 	//	Mainly here to trap attempts to normalize a null vector.
 	if (m <= 0.0f) {
-		{
-			mprintf(("Null vec3d in vec3d normalize.\n"
-							  "Trace out of vecmat.cpp and find offending code.\n"));
-		}
+		mprintf(("Null vec3d in vec3d normalize.\n"
+				 "Trace out of vecmat.cpp and find offending code.\n"));
 
 		dest->xyz.x = 1.0f;
 		dest->xyz.y = 0.0f;
