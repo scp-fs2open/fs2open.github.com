686bc35cff5f4c500bc1176b18880f03aadc48ba
Artyrm <ksotar@yandex.ru>
2018-10-14 16:07:59 +0300

    Moved variables, gave mean. names and descr.

--- code/menuui/barracks.cpp
+++ code/menuui/barracks.cpp
@@ -81,7 +81,6 @@ static int Barracks_stats_coords[GR_NUM_RESOLUTIONS][4] = {
 	}
 };
 
-uint16_t Calculated_width;		// For Unicode languages
 
 static int Barracks_stats2_coords[GR_NUM_RESOLUTIONS][3] = {
 	{ // GR_640
@@ -91,8 +90,6 @@ static int Barracks_stats2_coords[GR_NUM_RESOLUTIONS][3] = {
 		286, 351, 81		// X2, , W2
 	}
 };
-
-uint16_t Calculated_X2;
 
 // pilot picture field
 static int Barracks_image_coords[GR_NUM_RESOLUTIONS][4] = {
@@ -271,6 +268,9 @@ UI_XSTR Barracks_text[GR_NUM_RESOLUTIONS][BARRACKS_NUM_TEXT] = {
 static size_t Num_stat_lines;
 static char (*Stat_labels)[STAT_COLUMN1_W];
 static char (*Stats)[STAT_COLUMN2_W];
+static uint16_t Stats_max_width;		// If Unicode language is used then we adapt stats width for new words/strings length
+static uint16_t Stats_X2;				// And numbers position adapted as well
+
 
 static int Player_sel_mode;
 
@@ -490,22 +490,22 @@ void barracks_init_stats(scoring_struct *stats)
 	Num_stat_lines++;
 
 	int w;
-	Calculated_width = 0;
-	Calculated_X2	 = 0;
+	Stats_max_width = 0;
+	Stats_X2	 = 0;
 	if (Unicode_text_mode && (gr_screen.res != GR_640)) {
 		for (i = 0; i < Num_stat_lines; i++) {					// Find the longest string in labels to base list appearance on that
 			gr_get_string_size(&w, nullptr, Stat_labels[i]);
-			if (Calculated_width < w) 
-				Calculated_width = (uint16_t)w;
+			if (Stats_max_width < w) 
+				Stats_max_width = (uint16_t)w;
 		}
-		Calculated_width += 20;
-		Calculated_X2 = Calculated_width + 46;
+		Stats_max_width += 20;
+		Stats_X2 = Stats_max_width + 46;
 	}
 	else {
-		Calculated_width = (uint16_t) Barracks_stats_coords[gr_screen.res][BARRACKS_W_COORD];
-		Calculated_X2	 = (uint16_t) Barracks_stats2_coords[gr_screen.res][BARRACKS_X_COORD];
+		Stats_max_width = (uint16_t) Barracks_stats_coords[gr_screen.res][BARRACKS_W_COORD];
+		Stats_X2	 = (uint16_t) Barracks_stats2_coords[gr_screen.res][BARRACKS_X_COORD];
 		for (i = 0; i < Num_stat_lines; i++) {
-			font::force_fit_string(Stat_labels[i], Stat_column1_w[gr_screen.res], Calculated_width);
+			font::force_fit_string(Stat_labels[i], Stat_column1_w[gr_screen.res], Stats_max_width);
 			font::force_fit_string(Stats[i], Stat_column2_w[gr_screen.res], Barracks_stats2_coords[gr_screen.res][BARRACKS_W_COORD]);
 		}
 	}
@@ -1226,18 +1226,18 @@ void barracks_display_pilot_stats()
 
 			gr_get_string_size(&w, &h, str);
 			i = Barracks_stats_coords[gr_screen.res][BARRACKS_Y_COORD] + y + h / 2 - 1;			
-			gr_line(Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD], i, Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD] + Calculated_width - w - 2, i, GR_RESIZE_MENU);
-			gr_line(Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD] + Calculated_width + 1, i, Calculated_X2 + Barracks_stats2_coords[gr_screen.res][BARRACKS_W_COORD], i, GR_RESIZE_MENU);
+			gr_line(Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD], i, Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD] + Stats_max_width - w - 2, i, GR_RESIZE_MENU);
+			gr_line(Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD] + Stats_max_width + 1, i, Stats_X2 + Barracks_stats2_coords[gr_screen.res][BARRACKS_W_COORD], i, GR_RESIZE_MENU);
 
 		} else {
 			gr_set_color_fast(&Color_text_normal);
 		}
 
 		gr_get_string_size(&w, NULL, str);
-		gr_printf_menu(Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD] + Calculated_width - w, Barracks_stats_coords[gr_screen.res][BARRACKS_Y_COORD] + y, "%s", str);
+		gr_printf_menu(Barracks_stats_coords[gr_screen.res][BARRACKS_X_COORD] + Stats_max_width - w, Barracks_stats_coords[gr_screen.res][BARRACKS_Y_COORD] + y, "%s", str);
 		str = Stats[z];
 		if (*str) {
-			gr_printf_menu(Calculated_X2, Barracks_stats_coords[gr_screen.res][BARRACKS_Y_COORD] + y, "%s", str);
+			gr_printf_menu(Stats_X2, Barracks_stats_coords[gr_screen.res][BARRACKS_Y_COORD] + y, "%s", str);
 		}
 
 		y += font_height;
