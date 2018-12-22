79c0dca7963d508f4f49c0032d7608ceab96152f
CasparKielwein <Caspar@Kielwein.de>
2018-12-22 13:00:21 +0100

    remove unused functions in mission code.
    There where serval relics of prior refactorings like unimplemented functions.

--- code/mission/missioncampaign.h
+++ code/mission/missioncampaign.h
@@ -141,16 +141,6 @@ extern campaign Campaign;
 // campaign wasn't ended
 extern int Campaign_ending_via_supernova;
 
-// structure for players.  Holds the campaign name, number of missions flown in the campaign, and result
-// of the missions flown.  This structure is stored in the player file and thus is persistent across
-// games
-typedef struct campaign_info
-{
-	int		num_missions_completed;
-	char	filename[NAME_LENGTH];
-	ubyte	missions_completed[MAX_CAMPAIGN_MISSIONS];
-} campaign_info;
-
 // extern'ed so the mission loading can get a list of campains.  Only use this
 // data after mission_campaign_build_list() is called
 #define MAX_CAMPAIGNS	128
@@ -174,18 +164,10 @@ void player_loadout_init();
 // called at game startup time to load the default single player campaign
 void mission_campaign_init( void );
 
-// called to reload the default campaign
-int mission_campaign_load_by_name( char *filename );
-int mission_campaign_load_by_name_csfe( char *filename, char *callsign );
-
-
 // load up and initialize a new campaign
 int mission_campaign_load( char *filename, player *pl = NULL, int load_savefile = 1, bool reset_stats = true );
 
 bool campaign_is_ignored(const char *filename);
-
-// function to save the state of the campaign between missions or to load a campaign save file
-extern int mission_campaign_save( void );
 
 // declaration for local campaign save game load function
 extern void mission_campaign_savefile_delete( char *cfilename );
@@ -202,9 +184,6 @@ extern void mission_campaign_mission_over( bool do_next_mission = true );
 
 // frees all memory at game close time
 extern void mission_campaign_clear( void );
-
-// read in a campaign file.  Used by Fred.
-int mission_campaign_load_fred(char *filename, char *name_verify = NULL);
 
 // used by Fred to get a mission's list of goals.
 void read_mission_goal_list(int num);
@@ -223,15 +202,8 @@ extern void mission_campaign_save_persistent( int type, int index );
 
 void mission_campaign_savefile_generate_root(char *filename, player *pl = NULL);
 
-int mission_campaign_savefile_save();
-
 // The following are functions I added to set up the globals and then
 // execute the corresponding mission_campaign_savefile functions.
-
-// Saves the campaign camp under the player name pname
-int campaign_savefile_save(char *pname);
-// Deletes the campaign save camp under the player name pname
-void campaign_delete_save( char *cfn, char *pname);
 
 // get name and type of specified campaign file
 int mission_campaign_get_info(const char *filename, char *name, int *type, int *max_players, char **desc = NULL);
