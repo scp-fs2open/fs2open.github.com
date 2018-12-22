6660d21f7e69ba14c126255df99686fd999ac4c5
CasparKielwein <Caspar@Kielwein.de>
2018-12-22 15:18:08 +0100

    remove unused and commented out functions in pcxutils.

--- code/pcxutils/pcxutils.h
+++ code/pcxutils/pcxutils.h
@@ -16,12 +16,6 @@
 #include "cfile/cfile.h"
 
 
-/*
-#ifdef __cplusplus
-extern "C" {
-#endif
-*/
-
 #define PCX_ERROR_NONE 				0
 #define PCX_ERROR_OPENING			1
 #define PCX_ERROR_NO_HEADER		2
@@ -32,22 +26,10 @@ extern "C" {
 #define PCX_ERROR_MEMORY			7
 
 extern int pcx_read_header(const char *filename, CFILE *img_cfp = NULL, int *w = 0, int *h = 0, int *bpp = 0, ubyte *pal = NULL );
-//extern int pcx_read_bitmap_8bpp( const char * filename, ubyte *org_data, ubyte *palette );
-//extern int pcx_read_bitmap_16bpp( const char * filename, ubyte *org_data );
-//extern int pcx_read_bitmap_16bpp_aabitmap( const char *filename, ubyte *org_data );
-//extern int pcx_read_bitmap_16bpp_nondark( const char *filename, ubyte *org_data );
-//extern int pcx_read_bitmap_32(const char *real_filename, ubyte *data );
 extern int pcx_read_bitmap(const char *filename, ubyte *org_data, ubyte *pal, int byte_size, int aabitmap = 0, bool mask_bitmap = false, int cf_type = CF_TYPE_ANY);
 
 // Dumps a 8bpp bitmap to a file.
 // Set rowoff to -w for upside down bitmaps.
 extern int pcx_write_bitmap( const char * filename, int w, int h, ubyte ** row_ptrs, ubyte * palette );
-
-
-/*
-#ifdef __cplusplus
-}
-#endif
-*/
 
 #endif
