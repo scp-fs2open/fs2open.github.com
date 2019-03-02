1c799653ce2464ecb62bc75db7876943ebf58de7
asarium <asarium@gmail.com>
2019-03-02 16:38:41 +0100

    Unify Cfile blocks and CFILE handles
    The only thing a CFILE contained was a version field which isn't used
    anymore so that could be removed which basically meant that CFILE was
    just an indirection to a Cfile_block which I then just renamed to CFILE.
    The CFILE type is an opaque handle now since the contents are not
    relevant to a user of Cfile.

--- code/cfile/cfilearchive.h
+++ code/cfile/cfilearchive.h
@@ -22,30 +22,30 @@
 #define CFILE_BLOCK_UNUSED		0
 #define CFILE_BLOCK_USED		1
 
-typedef struct Cfile_block {
-	int		type;				// CFILE_BLOCK_UNUSED, CFILE_BLOCK_USED
-	int		dir_type;		// directory location
-	FILE		*fp;				// File pointer if opening an individual file
-	const void	*data;			// Pointer for memory-mapped file access.  NULL if not mem-mapped.
-	bool 	mem_mapped;		// Flag for memory mapped files (if data is not null and this is false it means that it's an embedded file)
+struct CFILE {
+	int type = CFILE_BLOCK_UNUSED;                // CFILE_BLOCK_UNUSED, CFILE_BLOCK_USED
+	int dir_type;        // directory location
+	FILE* fp;                // File pointer if opening an individual file
+	const void* data;            // Pointer for memory-mapped file access.  NULL if not mem-mapped.
+	bool mem_mapped; // Flag for memory mapped files (if data is not null and this is false it means that it's an embedded file)
 #ifdef _WIN32
 	HANDLE	hInFile;			// Handle from CreateFile()
 	HANDLE	hMapFile;		// Handle from CreateFileMapping()
 #else
-	size_t	data_length;	// length of data for mmap
+	size_t data_length;    // length of data for mmap
 #endif
-	size_t	lib_offset;
-	size_t	raw_position;
-	size_t	size;				// for packed files
+	size_t lib_offset;
+	size_t raw_position;
+	size_t size;                // for packed files
 
-	size_t	max_read_len;	// max read offset, for special error handling
-	
+	size_t max_read_len;    // max read offset, for special error handling
+
 	const char* source_file;
 	int line_num;
-} Cfile_block;
+};
 
 #define MAX_CFILE_BLOCKS	64
-extern Cfile_block Cfile_block_list[MAX_CFILE_BLOCKS];
+extern std::array<CFILE, MAX_CFILE_BLOCKS> Cfile_block_list;
 
 // Called once to setup the low-level reading code.
 void cf_init_lowlevel_read_code( CFILE * cfile, size_t lib_offset, size_t size, size_t pos );
