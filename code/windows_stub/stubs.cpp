d3d6e2ea41c3d968717262a17e92381002877d0e
Goober5000 <ipw47@mailbox.org>
2019-10-13 13:44:28 -0400

    add informative comment

--- code/windows_stub/stubs.cpp
+++ code/windows_stub/stubs.cpp
@@ -77,6 +77,8 @@ SCP_string dump_stacktrace()
 	
 	// iterate over the returned symbol lines. skip the first, it is the
 	// address of this function.
+	// NOTE: the numstrings type is different on different systems,
+	// so we use decltype here. See GitHub #1138.
 	for (decltype(numstrings) i = 1; i < numstrings; i++)
 	{
 		char *begin_name = 0, *begin_offset = 0, *end_offset = 0;
