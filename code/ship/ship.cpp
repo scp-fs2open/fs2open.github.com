90467f8f41ada53d4b992f8f9701bde1559d0851
MageKing17 <MageKing17@gmail.com>
2019-12-08 18:53:19 -0800

    Standardize nonstandard "+nocreate" behavior in objecttypes.tbl
    If you modified an existing ship type without using +nocreate, it would both modify it and add it to the vector again, leading to (at the very least) wasted memory. I copied over some more standard behavior from other parsing functions to hopefully avoid problems with modular objecttypes.

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -4738,27 +4738,46 @@ static engine_wash_info *get_engine_wash_pointer(char *engine_wash_name)
 	return NULL;
 }
 
-static void parse_ship_type()
+static void parse_ship_type(const char *filename, const bool replace)
 {
 	char name_buf[NAME_LENGTH];
-	bool nocreate = false;
-	ship_type_info stp_buf, *stp = NULL;
+	bool create_if_not_found = true;
+	ship_type_info *stp = NULL;
 
 	required_string("$Name:");
 	stuff_string(name_buf, F_NAME, NAME_LENGTH);
 
 	if(optional_string("+nocreate")) {
-		nocreate = true;
+		if(!replace) {
+			Warning(LOCATION, "+nocreate flag used for ship type '%s' in non-modular table", name_buf);
+		}
+		create_if_not_found = false;
 	}
 
 	int idx = ship_type_name_lookup(name_buf);
 	if (idx >= 0)
 	{
+		if (!replace)
+		{
+			Warning(LOCATION, "ship type '%s' already exists in %s; ship type names must be unique.", name_buf, filename);
+			if ( !skip_to_start_of_string_either("$Name:", "#End")) {
+				Int3();
+			}
+			return;
+		}
 		stp = &Ship_types[idx];
 	}
 	else
 	{
-		stp = &stp_buf;
+		if (!create_if_not_found && replace)
+		{
+			if ( !skip_to_start_of_string_either("$Name:", "#End")) {
+				Int3();
+			}
+			return;
+		}
+		Ship_types.push_back(ship_type_info());
+		stp = &Ship_types.back();
 		strcpy_s(stp->name, name_buf);
 	}
 
@@ -4776,7 +4795,7 @@ static void parse_ship_type()
 	}
 
 	if (ship_type != NULL) {
-		Warning(LOCATION, "Bad ship type name in objecttypes.tbl\n\nUsed ship type is redirected to another ship type.\nReplace \"%s\" with \"%s\"\nin objecttypes.tbl to fix this.\n", stp->name, ship_type);
+		Warning(LOCATION, "Bad ship type name in %s\n\nUsed ship type is redirected to another ship type.\nReplace \"%s\" with \"%s\"\nin %s to fix this.\n", filename, stp->name, ship_type, filename);
 	}
 
 	//Okay, now we should have the values to parse
@@ -4810,7 +4829,7 @@ static void parse_ship_type()
 				}
 			}
 			if (i == num_groups) {
-				Warning(LOCATION,"Unidentified priority group '%s' set for objecttype '%s'\n", target_group_strings[j].c_str(), stp->name);
+				Warning(LOCATION,"Unidentified priority group '%s' set for ship type '%s' in %s\n", target_group_strings[j].c_str(), stp->name, filename);
 			}
 		}
 	}
@@ -4953,14 +4972,11 @@ static void parse_ship_type()
 		stuff_float(&stp->vaporize_chance);
 		if (stp->vaporize_chance < 0.0f || stp->vaporize_chance > 100.0f) {
 			stp->vaporize_chance = 0.0f;
-			Warning(LOCATION, "$Vaporize Percent Chance should be between 0 and 100.0 (read %f). Setting to 0.", stp->vaporize_chance);
+			Warning(LOCATION, "$Vaporize Percent Chance should be between 0 and 100.0 (read %f) for ship type '%s' in %s. Setting to 0.", stp->vaporize_chance, stp->name, filename);
 		}
 		//Percent is nice for modders, but here in the code we want it betwwen 0 and 1.0
 		stp->vaporize_chance /= 100.0;
 	}
-
-	if (!nocreate)
-		Ship_types.push_back(stp_buf);
 }
 
 static void parse_shiptype_tbl(const char *filename)
@@ -4993,7 +5009,7 @@ static void parse_shiptype_tbl(const char *filename)
 		if (optional_string("#Ship Types"))
 		{
 			while (required_string_either("#End", "$Name:"))
-				parse_ship_type();
+				parse_ship_type(filename ? filename : "built-in objecttypes.tbl", Parsing_modular_table);
 
 			required_string("#End");
 		}
