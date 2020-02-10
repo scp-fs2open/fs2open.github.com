4bb31f4e1b90409d7fbf283146a7aa700f142549
pkubaj <pkubaj@users.noreply.github.com>
2020-02-10 15:49:52 +0100

    Fix build on big-endian architectures
    swap_bsp_data() and swap_sldc_data() use those.

--- code/model/modelread.cpp
+++ code/model/modelread.cpp
@@ -5341,7 +5341,7 @@ void swap_bsp_sortnorms( polymodel * pm, ubyte * p )
 }
 #endif // BIG_ENDIAN
 
-void swap_bsp_data( polymodel *  /*pm*/, void * /*model_ptr*/ )
+void swap_bsp_data( polymodel * pm, void * model_ptr )
 {
 #if BYTE_ORDER == BIG_ENDIAN
 	ubyte *p = (ubyte *)model_ptr;
@@ -5397,7 +5397,7 @@ void swap_bsp_data( polymodel *  /*pm*/, void * /*model_ptr*/ )
 #endif
 }
 
-void swap_sldc_data(ubyte * /*buffer*/)
+void swap_sldc_data(ubyte * buffer)
 {
 #if BYTE_ORDER == BIG_ENDIAN
 	char *type_p = (char *)(buffer);
