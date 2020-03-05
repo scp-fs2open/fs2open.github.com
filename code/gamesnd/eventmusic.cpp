bb48ea9c6155e86c00bbbac3e7244865e4e4b398
Fabian Woltermann <fwoltermann@gmail.com>
2020-03-05 20:15:20 +0100

    Reset the current soundtrack during mission shutdown

--- code/gamesnd/eventmusic.cpp
+++ code/gamesnd/eventmusic.cpp
@@ -705,6 +705,7 @@ void event_music_level_close()
 		audiostream_close_all(0);
 	}
 
+	Current_soundtrack_num = -1;
 	Current_pattern = -1;
 	Event_music_level_inited = FALSE;
 	Event_Music_battle_started = FALSE;
