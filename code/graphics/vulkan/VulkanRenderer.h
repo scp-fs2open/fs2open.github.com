fbdb25a10d744c179439e4a98058200eade39bf5
asarium <asarium@gmail.com>
2020-08-17 22:18:11 +0200

    Add more Vulkan setup code
    This adds the Vulkan code for choosing a physical device, creating a
    logical device from that and then initializing a swap chain.
    
    This also contains code for filling out the graphics API pointers with
    stub functions so that the API functions can be developed gradually.
    These changes allow FSO to "run" with Vulkan but nothing is rendered
    yet.

--- code/graphics/vulkan/VulkanRenderer.h
+++ code/graphics/vulkan/VulkanRenderer.h
@@ -13,6 +13,29 @@
 namespace graphics {
 namespace vulkan {
 
+struct QueueIndex {
+	// Poor mans std::optional
+	bool initialized = false;
+	uint32_t index = 0;
+};
+
+struct PhysicalDeviceValues {
+	vk::PhysicalDevice device;
+	vk::PhysicalDeviceProperties properties;
+	vk::PhysicalDeviceFeatures features;
+
+	std::vector<vk::ExtensionProperties> extensions;
+
+	vk::SurfaceCapabilitiesKHR surfaceCapabilities;
+	std::vector<vk::SurfaceFormatKHR> surfaceFormats;
+	std::vector<vk::PresentModeKHR> presentModes;
+
+	std::vector<vk::QueueFamilyProperties> queueProperties;
+	QueueIndex graphicsQueueIndex;
+	QueueIndex transferQueueIndex;
+	QueueIndex presentQueueIndex;
+};
+
 class VulkanRenderer {
   public:
 	explicit VulkanRenderer(std::unique_ptr<os::GraphicsOperations> graphicsOps);
@@ -26,13 +49,31 @@ class VulkanRenderer {
 
 	bool initializeSurface();
 
+	bool pickPhysicalDevice(PhysicalDeviceValues& deviceValues);
+
+	bool createLogicalDevice(PhysicalDeviceValues& deviceValues);
+
+	bool createSwapChain(PhysicalDeviceValues& deviceValues);
+
 	std::unique_ptr<os::GraphicsOperations> m_graphicsOps;
-	std::unique_ptr<os::Viewport> m_viewPort;
 
 	vk::UniqueInstance m_vkInstance;
 	vk::UniqueDebugReportCallbackEXT m_debugReport;
 
 	vk::UniqueSurfaceKHR m_vkSurface;
+
+	vk::UniqueDevice m_device;
+
+	vk::Queue m_graphicsQueue;
+	vk::Queue m_transferQueue;
+	vk::Queue m_presentQueue;
+
+	vk::UniqueSwapchainKHR m_swapChain;
+	vk::Format m_swapChainImageFormat;
+	vk::Extent2D m_swapChainExtent;
+	SCP_vector<vk::Image> m_swapChainImages;
+	SCP_vector<vk::UniqueImageView> m_swapChainImageViews;
+	SCP_vector<vk::UniqueFramebuffer> m_swapChainFramebuffers;
 
 #if SDL_SUPPORTS_VULKAN
 	bool m_debugReportEnabled = false;
