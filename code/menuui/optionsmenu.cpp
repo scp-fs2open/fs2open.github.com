48ea203b7bedbffc59a53cf10b4ccc131b897fc3
Goober5000 <ipw47@mailbox.org>
2020-08-31 18:02:28 -0400

    handle audio pause/unpause for all of the F2 screens
    This is a follow-up to #2100.  Don't pause/unpause the audio if we are moving from/to the Control Config or HUD Config screens.  We could hit F2 and move around a bit before resuming the game, so we don't want the audio to resume until we're ready.

--- code/menuui/optionsmenu.cpp
+++ code/menuui/optionsmenu.cpp
@@ -901,13 +901,6 @@ void options_menu_init()
 
 	Assert(!Options_menu_inited);
 
-	// pause all sounds, since we could get here through the game
-	weapon_pause_sounds();
-	// only pause music if we're in-mission; we could also be in the main hall
-	if (Game_mode & GM_IN_MISSION) {
-		audiostream_pause_all();
-	}
-
 	Tab = 0;
 	Gamma_last_set = -1;
 
@@ -1035,18 +1028,9 @@ void options_menu_close()
 	Pilot.save_savefile();
 	game_flush();
 	
-	// unpause all sounds, since we could be headed back to the game
-	weapon_unpause_sounds();
-	// only unpause music if we're in-mission; we could also be in the main hall
-	if (Game_mode & GM_IN_MISSION) {
-		audiostream_unpause_all();
-	}
-	
 	Options_menu_inited = 0;
 	Options_multi_inited = 0;
 	Options_detail_inited = 0;
-
-
 }
 
 
