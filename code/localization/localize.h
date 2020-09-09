885e26feb0ec7cce8ce096bc916f44d1923a810a
Goober5000 <Goober5000@users.noreply.github.com>
2020-09-08 20:20:21 -0400

    Revert "Change special_char_indexes to a vector"

--- code/localization/localize.h
+++ code/localization/localize.h
@@ -31,14 +31,13 @@
 // for language name strings
 #define LCL_LANG_NAME_LEN				32
 
-// Cyborg17 - We define at least this many special_char_indexes for each language to match retail behavior
-#define LCL_MINIMUM_FONTS				3   
+#define LCL_MAX_FONTS					5
 
 // language info table
 typedef struct lang_info {
 	char lang_name[LCL_LANG_NAME_LEN + 1];				// literal name of the language
 	char lang_ext[LCL_LANG_NAME_LEN + 1];				// the extension used for adding to names on disk access
-	SCP_vector<ubyte> special_char_indexes;				// where in the font do we have the special characters for this language
+	ubyte special_char_indexes[LCL_MAX_FONTS];			// where in the font do we have the special characters for this language
 														// note: treats 0 as "none" since a zero offset in a font makes no sense
 														// i.e. all the normal chars start at zero
 	int checksum;										// used for language auto-detection
