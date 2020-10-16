8090f60940a212639b065eb08698ff84c5ae7436
Tyler Blount <tylerrblount@gmail.com>
2020-10-16 16:15:48 -0600

    Don't clear locks for swarm/corkscrew missiles until the last missile needs to be fired

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -12255,7 +12255,11 @@ int ship_fire_secondary( object *obj, int allow_swarm )
 			// use missile lock slots
 			if ( !shipp->missile_locks_firing.empty() ) {
 				lock_info lock_data = shipp->missile_locks_firing.back();
-				shipp->missile_locks_firing.pop_back();
+				if ( wip->multi_lock || 
+					 !(wip->wi_flags[Weapon::Info_Flags::Corkscrew] || wip->wi_flags[Weapon::Info_Flags::Swarm]) ||
+					 (shipp->num_corkscrew_to_fire <= 1 && shipp->num_swarm_missiles_to_fire <= 1) ) {
+					shipp->missile_locks_firing.pop_back();
+				}
 
 				target_objnum = OBJ_INDEX(lock_data.obj);
 				target_subsys = lock_data.subsys;
