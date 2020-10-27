482b691f767afe1d3564fc94082592c20f548460
wookieejedi <azthepaleo@gmail.com>
2020-10-27 14:49:27 -0400

    more appeasement

--- code/particle/ParticleManager.cpp
+++ code/particle/ParticleManager.cpp
@@ -362,7 +362,7 @@ SCP_vector<int> parseAnimationList(bool critical) {
 	
 	SCP_vector<int> handles;
 
-	for (auto name: bitmap_strings) {
+	for (auto const &name: bitmap_strings) {
 		auto handle = bm_load_animation(name.c_str());
 		if (handle >= 0) {
 			handles.push_back(handle);
