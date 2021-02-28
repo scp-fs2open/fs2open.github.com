f0d5c5a49956662374df2cb5f5d01f6625441396
Birk Magnussen <birk.magnussen@online.de>
2021-03-01 00:13:49 +0900

    Remove redundant close

--- code/scripting/api/objs/file.cpp
+++ code/scripting/api/objs/file.cpp
@@ -14,7 +14,6 @@ cfile_h::~cfile_h()
 {
 	if (_cfp && cf_is_valid(_cfp.get())) {
 		Warning(LOCATION, "Lua cfile handle of file %s has been left open!\nYou should close the file with the object's close() method explicitly.\n", cf_get_filename(_cfp.get()));
-		close();
 	}
 }
 bool cfile_h::isValid() const { return _cfp != nullptr; }
