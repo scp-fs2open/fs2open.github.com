3f9218e67118810d1269348c9767637c3e2424a8
Shivansps <shivanuo@hotmail.com>
2021-03-11 20:51:28 -0300

    1st attempt at implementation
    I just dumped everything into cfilearchive to avoid a fight with the includes that i cant win for now.
    
    It dosent work, DDS utils complains about DDS filecode.

--- code/cfile/cfilearchive.cpp
+++ code/cfile/cfilearchive.cpp
@@ -32,6 +32,16 @@
 
 #define CHECK_POSITION
 
+
+/*INTERNAL FUNCTIONS*/
+/*LZ41*/
+void lz41_load_offsets(CFILE* cf, compression_info* ci);
+int lz41_stream_random_access(CFILE* cf, char* bytes_out, int offset, int length);
+compression_info lz41_create_ci(CFILE* cf, char* header);
+int fso_fseek(CFILE* cfile, int offset, int where);
+/*END OF INTERNAL FUNCTIONS*/
+
+
 // Called once to setup the low-level reading code.
 
 void cf_init_lowlevel_read_code( CFILE * cfile, size_t lib_offset, size_t size, size_t pos )
@@ -66,6 +76,9 @@ int cfeof(CFILE *cfile)
 {
 	Assert(cfile != NULL);
 
+	if (cfile->compression_info.isCompressed == 1)
+		return comp_feof(cfile);
+
 	int result = 0;
 
 	#if defined(CHECK_POSITION) && !defined(NDEBUG)
@@ -93,6 +106,9 @@ int cftell( CFILE * cfile )
 {
 	Assert(cfile != NULL);
 
+	if (cfile->compression_info.isCompressed == 1)
+		return comp_ftell(cfile);
+
 	#if defined(CHECK_POSITION) && !defined(NDEBUG)
     if (cfile->fp) {
 		auto raw_position = ftell(cfile->fp) - cfile->lib_offset;
@@ -116,6 +132,9 @@ int cfseek( CFILE *cfile, int offset, int where )
 {
 
 	Assert(cfile != NULL);
+
+	if (cfile->compression_info.isCompressed == 1)
+		return comp_fseek(cfile,offset,where);
 
 	// TODO: seek to offset in memory mapped file
 	Assert( !cfile->mem_mapped );
@@ -202,8 +221,33 @@ int cfread(void *buf, int elsize, int nelem, CFILE *cfile)
 		// This is a file from memory
 		bytes_read = size;
 		memcpy(buf, reinterpret_cast<const char*>(cfile->data) + cfile->raw_position, size);
-	} else {
-		bytes_read = fread( buf, 1, size, cfile->fp );
+	}else if (cfile->compression_info.isCompressed==1){
+		bytes_read = size;
+		int result = comp_fread(cfile, reinterpret_cast<char*>(buf),cfile->compression_info.uncompressed_position,size);
+		mprintf(("Continuing reading CP file: %s \n", cfile->original_filename.c_str()));
+		mprintf(("Size requested: %d \n", size));
+		mprintf(("(CI)Result: %d \n", result));
+		mprintf(("(CI)CP post: %d \n", cfile->compression_info.uncompressed_position));
+	}else{
+		if (cfile->raw_position == 0)
+		{
+			bytes_read = fread(buf, 1, size, cfile->fp);
+			if (comp_check_header((char*)buf) == COMP_HEADER_MATCH)
+			{
+				mprintf(("Compressed File requested: %s \n", cfile->original_filename.c_str()));
+				mprintf(("Size requested: %d \n", size));
+				comp_create_ci(cfile,(char*)buf);
+				int result = comp_fread(cfile, (char*)buf, cfile->compression_info.uncompressed_position, size);
+				mprintf(("(CI)Result: %d \n", result));
+				mprintf(("(CI)NumOffsets: %d \n", cfile->compression_info.numOffsets));
+				mprintf(("(CI)FileSize: %d \n", cfile->compression_info.uncompressed_size));
+				mprintf(("(CI)Buffer read: %s \n", buf));
+			}
+		}
+		else
+		{
+			bytes_read = fread(buf, 1, size, cfile->fp);
+		}
 	}
 	if ( bytes_read > 0 )	{
 		cfile->raw_position += bytes_read;
@@ -213,7 +257,7 @@ int cfread(void *buf, int elsize, int nelem, CFILE *cfile)
 	#if defined(CHECK_POSITION) && !defined(NDEBUG)
     if (cfile->fp) {
 		auto tmp_offset = ftell(cfile->fp) - cfile->lib_offset;
-		Assert(tmp_offset==cfile->raw_position);
+		//Assert(tmp_offset==cfile->raw_position);
 	}
 	#endif
 
@@ -264,6 +308,178 @@ int cfread_lua_number(double *buf, CFILE *cfile)
 	#endif
 
 	return items_read;
-
 }
 
+int comp_check_header(char* header)
+{
+	if (memcmp(LZ41_FILE_HEADER, header, 4) == 0)
+		return COMP_HEADER_MATCH;
+
+	return COMP_HEADER_MISMATCH;
+}
+
+void comp_create_ci(CFILE* cf, char* header)
+{
+	if (memcmp(LZ41_FILE_HEADER, header, 4) == 0)
+		cf->compression_info = lz41_create_ci(cf, header);
+}
+
+int comp_fread(CFILE* cf, char* buffer, int offset, int length)
+{
+	if (memcmp(LZ41_FILE_HEADER, cf->compression_info.header, 4) == 0)
+		return lz41_stream_random_access(cf, buffer, offset, length);
+
+	return 0;
+}
+
+int comp_ftell(CFILE* cf)
+{
+	mprintf(("FTELL HERE\n"));
+	if (cf->compression_info.isCompressed == 1)
+		return (int)cf->compression_info.uncompressed_position;
+	else
+		return (int)cf->raw_position;
+}
+
+int comp_feof(CFILE* cf)
+{
+	int result = 0;
+	mprintf(("FEOF HERE\n"));
+	if (cf->compression_info.isCompressed == 1)
+	{
+		if (cf->compression_info.uncompressed_position >= cf->compression_info.uncompressed_size)
+			result = 1;
+		else
+			result = 0;
+	}
+	else
+	{
+		if (cf->raw_position >= cf->size)
+			result = 1;
+		else
+			result = 0;
+	}
+	return result;
+}
+
+int comp_fseek(CFILE* cf, int offset, int where)
+{
+	mprintf(("FSEEK HERE\n"));
+	if (cf->compression_info.isCompressed == 1)
+	{
+		size_t goal_position;
+		switch (where) {
+		case SEEK_SET: goal_position = offset; break;
+		case SEEK_CUR: goal_position = cf->compression_info.uncompressed_position + offset; break;
+		case SEEK_END: goal_position = cf->compression_info.uncompressed_size + offset; break;
+		default: return 1;
+		}
+		cf->compression_info.uncompressed_position = goal_position;
+		return goal_position;
+	}
+	else
+	{
+		return fso_fseek(cf, offset, where);
+	}
+}
+
+int fso_fseek(CFILE* cfile, int offset, int where)
+{
+	size_t goal_position;
+
+	switch (where) {
+	case SEEK_SET: goal_position = offset + cfile->lib_offset; break;
+	case SEEK_CUR: goal_position = cfile->raw_position + offset + cfile->lib_offset; break;
+	case SEEK_END: goal_position = cfile->size + offset + cfile->lib_offset; break;
+	default: return 1;
+	}
+
+	int result = 0;
+
+	if (cfile->fp)
+		result = fseek(cfile->fp, (long)goal_position, SEEK_SET);
+
+	cfile->raw_position = goal_position - cfile->lib_offset;
+
+	return result;
+}
+
+compression_info lz41_create_ci(CFILE* cf, char* header)
+{
+	compression_info ci;
+	cf->raw_position = 4;
+	ci.isCompressed = 1;
+	memcpy(ci.header, header, 4);
+	ci.uncompressed_position = 0;
+	ci.maxBlocks = cf->size + 8 / LZ41_BLOCK_BYTES;
+	fso_fseek(cf, -8, SEEK_END);
+	fread(&ci.numOffsets, sizeof(int), 1, cf->fp);
+	fread(&ci.uncompressed_size, sizeof(int), 1, cf->fp);
+	lz41_load_offsets(cf, &ci);
+	return ci;
+}
+
+void lz41_load_offsets(CFILE* cf, compression_info* ci)
+{
+	ci->offsets = (int*)malloc(ci->maxBlocks);
+	int block;
+	int* offsetsPtr = ci->offsets;
+
+	fso_fseek(cf, (-4 * (ci->numOffsets + 1)) - sizeof(int), SEEK_END);
+	for (block = 0; block <= ci->numOffsets; ++block)
+		fread(offsetsPtr++, sizeof(int), 1, cf->fp);
+}
+
+int lz41_stream_random_access(CFILE* cf, char* bytes_out, int offset, int length)
+{
+	if (length == 0)
+		return 0;
+
+	if (offset >= cf->compression_info.uncompressed_size)
+		return 0;
+
+	LZ4_streamDecode_t lz4StreamDecode_body;
+	LZ4_streamDecode_t* lz4StreamDecode = &lz4StreamDecode_body;
+	/* The blocks [currentBlock, endBlock) contain the data we want */
+	int currentBlock = offset / LZ41_BLOCK_BYTES;
+	int endBlock = ((offset + length - 1) / LZ41_BLOCK_BYTES) + 1;
+	int* offsets = cf->compression_info.offsets, * offsetsPtr = offsets;
+	int block = 0, written_bytes = 0;
+	char decBuf[LZ41_BLOCK_BYTES];
+
+	if (cf->compression_info.numOffsets <= endBlock)
+		return LZ41_OFFSETS_MISMATCH;
+
+	/* Seek to the first block to read */
+	fso_fseek(cf, offsets[currentBlock], SEEK_SET);
+	offset = offset % LZ41_BLOCK_BYTES;
+	cf->raw_position = offsets[currentBlock];
+
+	/* Start decoding */
+	for (; currentBlock < endBlock; ++currentBlock)
+	{
+		char cmpBuf[LZ4_COMPRESSBOUND(LZ41_BLOCK_BYTES)];
+		/* The difference in offsets is the size of the block */
+		int cmpBytes = offsets[currentBlock + 1] - offsets[currentBlock];
+		fread(cmpBuf, cmpBytes, 1, cf->fp);
+		cf->raw_position += (size_t)cmpBytes;
+
+		const int decBytes = LZ4_decompress_safe_continue(lz4StreamDecode, cmpBuf, decBuf, cmpBytes, LZ41_BLOCK_BYTES);
+		if (decBytes <= 0)
+			return LZ41_DECOMPRESSION_ERROR;
+
+		/* Write out the part of the data we care about */
+		int blockLength = ((length) < ((decBytes - offset)) ? (length) : ((decBytes - offset)));
+		memcpy(bytes_out + written_bytes, decBuf + offset, blockLength);
+		written_bytes += blockLength;
+		offset = 0;
+		length -= blockLength;
+	}
+
+	cf->compression_info.uncompressed_position += written_bytes;
+
+	if (cf->compression_info.uncompressed_position == cf->compression_info.uncompressed_size)
+		cf->raw_position = cf->size;
+
+	return written_bytes;
+}
--- code/cfile/cfilearchive.h
+++ code/cfile/cfilearchive.h
@@ -16,6 +16,18 @@
 #endif
 
 #include "globalincs/pstypes.h"
+#include "lz4/lz4.h"
+
+struct compression_info {
+	char header[4] = { 'N','O','C','P' };
+	int isCompressed = 0;
+	size_t uncompressed_position = 0;
+	size_t uncompressed_size = 0;
+	int maxBlocks = 0;
+	int numOffsets = 0;
+	int* offsets = nullptr;
+};
+
 
 // The following Cfile_block data is private to cfile.cpp
 // DO NOT MOVE the Cfile_block* information to cfile.h / do not extern this data
@@ -44,12 +56,59 @@ struct CFILE {
 	SCP_string original_filename;
 	const char* source_file;
 	int line_num;
+	compression_info compression_info;
 };
 
 #define MAX_CFILE_BLOCKS	64
 extern std::array<CFILE, MAX_CFILE_BLOCKS> Cfile_block_list;
 
 // Called once to setup the low-level reading code.
-void cf_init_lowlevel_read_code( CFILE * cfile, size_t lib_offset, size_t size, size_t pos );
+void cf_init_lowlevel_read_code(CFILE* cfile, size_t lib_offset, size_t size, size_t pos);
+
+/*LZ41*/
+#define LZ41_FILE_HEADER "LZ41"
+#define LZ41_BLOCK_BYTES 8192
+#define LZ41_DECOMPRESSION_ERROR -1
+#define LZ41_MAX_BLOCKS_OVERFLOW -2
+#define LZ41_HEADER_MISMATCH -3
+#define LZ41_OFFSETS_MISMATCH -4
+/******/
+
+#define COMP_HEADER_MATCH 1
+#define COMP_HEADER_MISMATCH 0
+
+/*
+	Returns COMP_HEADER_MATCH if header is a valid compressed file header,
+	returns COMP_HEADER_MISMATCH if it dosent.
+*/
+int comp_check_header(char* header);
+
+/*
+	This is called to generate the correct compression_info data
+	after the file has been indentified as a compressed file.
+	This must be done before calling any other function.
+*/
+void comp_create_ci(CFILE* cf, char* header);
+
+/*
+	Read X bytes from the uncompressed file starting from X offset.
+	Returns the amount of bytes read, and 0 or lower to indicate errors.
+*/
+int comp_fread(CFILE* cf, char* buffer, int offset, int length);
+
+/*
+	Returns the current uncompressed file position.
+*/
+int comp_ftell(CFILE* cf);
+
+/*
+	Returns 1 of the uncompressed file has been completely read, otherwise it returns a 0.
+*/
+int comp_feof(CFILE* cf);
+
+/*
+	Used to move the uncompressed file current position.
+*/
+int comp_fseek(CFILE* cf, int offset, int where);
 
 #endif
