9597b5962ea95fb76f98b73b93f539a538ff03aa
wookieejedi <azthepaleo@gmail.com>
2021-03-23 23:16:59 -0400

    clarify ship override for subsystems debris

--- code/ship/shiphit.cpp
+++ code/ship/shiphit.cpp
@@ -517,8 +517,8 @@ float do_subobj_hit_stuff(object *ship_objp, object *other_obj, vec3d *hitpos, i
 	if (!global_damage) {
 		auto subsys = ship_get_subsys_for_submodel(ship_p, submodel_num);
 
-		if (subsys == nullptr || !subsys->system_info->flags[Model::Subsystem_Flags::No_impact_debris] ||
-			!Ship_info[ship_p->ship_info_index].flags[Ship::Info_Flags::No_impact_debris]) {
+		if (subsys == nullptr || 
+			!(subsys->system_info->flags[Model::Subsystem_Flags::No_impact_debris] || Ship_info[ship_p->ship_info_index].flags[Ship::Info_Flags::No_impact_debris]) ) {
 			create_generic_debris(ship_objp, hitpos, 1.0f, 5.0f, 1.0f, false);
 		}
 	}
