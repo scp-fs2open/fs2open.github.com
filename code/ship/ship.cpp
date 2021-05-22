c991c7f657f2ff32400bacfc6bb93f575cdc0343
Birk Magnussen <birk.magnussen@online.de>
2021-05-23 00:32:56 +0900

    Put deferred Cockpits behind a flag

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -7394,7 +7394,8 @@ void ship_render_cockpit(object *objp)
 	gr_set_proj_matrix(Proj_fov, gr_screen.clip_aspect, 0.02f, 10.0f * pm->rad);
 	gr_set_view_matrix(&leaning_position, &Eye_matrix);
 
-	gr_deferred_lighting_begin();
+	if(Cmdline_deferred_lighting_cockpit)
+		gr_deferred_lighting_begin();
 
 	uint render_flags = MR_NORMAL;
 	render_flags |= MR_NO_FOGGING;
@@ -7410,11 +7411,13 @@ void ship_render_cockpit(object *objp)
 
 	model_render_immediate(&render_info, sip->cockpit_model_num, &eye_ori, &pos);
 
-	gr_deferred_lighting_end();
-	gr_deferred_lighting_finish(&Eye_position);
+	if (Cmdline_deferred_lighting_cockpit) {
+		gr_deferred_lighting_end();
+		gr_deferred_lighting_finish(&Eye_position);
 
-	gr_reset_lighting();
-	gr_set_lighting(false, false);
+		gr_reset_lighting();
+		gr_set_lighting(false, false);
+	}
 
 	gr_end_view_matrix();
 	gr_end_proj_matrix();
