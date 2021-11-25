ec922610c08e370bdabb8289716d02bb3ca42192
The Force <theforce172@gmail.com>
2021-11-25 13:11:56 +0000

    Completed Special Exp Side

--- qtfred/src/mission/dialogs/ShipSpecialStatsDialogModel.h
+++ qtfred/src/mission/dialogs/ShipSpecialStatsDialogModel.h
@@ -13,8 +13,11 @@ namespace fso {
 				bool _modified = false;
 				bool m_multi_edit;
 				int m_ship;
+
+				int num_selected_ships;
+				SCP_vector<int> m_selected_ships;
 				//Special Explosion
-				bool m_sepecial_exp;
+				bool m_special_exp;
 				bool m_shockwave;
 				bool m_deathRoll;
 				int m_inner_rad;
@@ -30,10 +33,33 @@ namespace fso {
 				int m_shields;
 				int m_hull;
 
+				void set_modified();
+
 			public:
-				ShipSpecialStatsDialogModel(QObject* parent, EditorViewport* viewport, bool multi);
+				ShipSpecialStatsDialogModel(QObject* parent, EditorViewport* viewport);
 				bool apply() override;
 				void reject() override;
+				bool query_modified();
+
+				bool getSpecialExp() const;
+				void setSpecialExp(const bool);
+				bool getShockwave() const;
+				void setShockwave(const bool);
+				bool getDeathRoll() const;
+				void setDeathRoll(const bool);
+
+				int getDamage() const;
+				void setDamage(const int);
+				int getBlast() const;
+				void setBlast(const int);
+				int getInnerRadius() const;
+				void setInnerRadius(const int);
+				int getOuterRadius() const;
+				void setOuterRadius(const int);
+				int getShockwaveSpeed() const;
+				void setShockwaveSpeed(const int);
+				int getRollDuration() const;
+				void setRollDuration(const int);
 			};
 			template <typename T>
 			inline void ShipSpecialStatsDialogModel::modify(T& a, const T& b)
---
+++ qtfred/src/ui/dialogs/ShipSpecialStatsDialog.cpp
@@ -0,0 +1,80 @@
+#include "ShipSpecialStatsDialog.h"
+
+#include "ui_ShipSpecialStatsDialog.h"
+
+#include <ui/util/SignalBlockers.h>
+
+#include <QCloseEvent>
+
+namespace fso {
+	namespace fred {
+		namespace dialogs {
+
+			ShipSpecialStatsDialog::ShipSpecialStatsDialog(QWidget* parent, EditorViewport* viewport) : 
+				QDialog(parent), ui(new Ui::ShipSpecialStatsDialog()), _model(new ShipSpecialStatsDialogModel(this, viewport)), 
+				_viewport(viewport) 
+			{
+				ui->setupUi(this);
+
+				connect(this, &QDialog::accepted, _model.get(), &ShipSpecialStatsDialogModel::apply);
+				connect(this, &QDialog::rejected, _model.get(), &ShipSpecialStatsDialogModel::reject);
+				connect(_model.get(), &AbstractDialogModel::modelChanged, this, &ShipSpecialStatsDialog::updateUI);
+
+				connect(ui->explodeCheckBox, &QCheckBox::toggled, _model.get(), &ShipSpecialStatsDialogModel::setSpecialExp);
+				connect(ui->damageSpinBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), _model.get(), &ShipSpecialStatsDialogModel::setDamage);
+				connect(ui->blastSpinBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), _model.get(), &ShipSpecialStatsDialogModel::setBlast);
+				connect(ui->iRadiusSpinBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), _model.get(), &ShipSpecialStatsDialogModel::setInnerRadius);
+				connect(ui->oRadiusSpinBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), _model.get(), &ShipSpecialStatsDialogModel::setOuterRadius);
+				connect(ui->createShockwaveCheckBox, &QCheckBox::toggled, _model.get(), &ShipSpecialStatsDialogModel::setShockwave);
+				connect(ui->shockwaveSpeedSpinBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), _model.get(), &ShipSpecialStatsDialogModel::setShockwaveSpeed);
+				connect(ui->deathRollCheckBox, &QCheckBox::toggled, _model.get(), &ShipSpecialStatsDialogModel::setDeathRoll);
+				connect(ui->rollTimeSpinBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), _model.get(), &ShipSpecialStatsDialogModel::setRollDuration);
+
+
+
+				updateUI();
+
+				// Resize the dialog to the minimum size
+				resize(QDialog::sizeHint());
+			}
+
+			ShipSpecialStatsDialog::~ShipSpecialStatsDialog() = default;
+
+			void ShipSpecialStatsDialog::closeEvent(QCloseEvent* event)
+			{
+				if (_model->query_modified()) {
+					auto button = _viewport->dialogProvider->showButtonDialog(DialogType::Question,
+						"Changes detected",
+						"Do you want to keep your changes?",
+						{ DialogButton::Yes, DialogButton::No, DialogButton::Cancel });
+
+					if (button == DialogButton::Cancel) {
+						event->ignore();
+						return;
+					}
+
+					if (button == DialogButton::Yes) {
+						accept();
+						return;
+					}
+				}
+
+				QDialog::closeEvent(event);
+			}
+
+			void ShipSpecialStatsDialog::updateUI()
+			{
+				util::SignalBlockers blockers(this);
+				ui->explodeCheckBox->setChecked(_model->getSpecialExp());
+				ui->damageSpinBox->setValue(_model->getDamage());
+				ui->blastSpinBox->setValue(_model->getBlast());
+				ui->iRadiusSpinBox->setValue(_model->getInnerRadius());
+				ui->oRadiusSpinBox->setValue(_model->getOuterRadius());
+				ui->createShockwaveCheckBox->setChecked(_model->getShockwave());
+				ui->shockwaveSpeedSpinBox->setValue(_model->getShockwaveSpeed());
+				ui->deathRollCheckBox->setChecked(_model->getDeathRoll());
+				ui->rollTimeSpinBox->setValue(_model->getRollDuration());
+			}
+		}
+	}
+}
