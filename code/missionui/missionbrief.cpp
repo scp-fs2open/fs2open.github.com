scp-fs2open/fs2open.github.com

commit 3005e447d3e52d18786df676213a7a941c16d079

Kill the -wcs flag. Should have done this years ago.

code/missionui/missionbrief.cpp
@@ -1302,8 +1302,7 @@ int brief_setup_closeup(brief_icon *bi)
 		end_string_at_first_hash_symbol(Closeup_icon->closeup_label);
 
 		// Goober5000 - wcsaga doesn't want this
-		if (!Cmdline_wcsaga && sip->flags & (SIF_SMALL_SHIP|SIF_BIG_SHIP|SIF_HUGE_SHIP|SIF_SENTRYGUN))
-		{
+		if (Ship_types[sip->class_type].hud_bools & STI_HUD_NO_CLASS_DISPLAY ) {
 			strcat_s(Closeup_icon->closeup_label, XSTR( " class", 434));
 		}
 

code/ai/ai_profiles.cpp
@@ -391,6 +391,8 @@ void parse_ai_profiles_tbl(char *filename)
 
 			set_flag(profile, "$No extra collision avoidance vs player:", AIPF2_NO_SPECIAL_PLAYER_AVOID, AIP_FLAG2);
 
+			set_flag(profile, "$Perform less checks for death screams:", AIPF2_PERFORM_LESS_SCREAM_CHECKS, AIP_FLAG2);
+
 			// if we've been through once already and are at the same place, force a move
 			if ( saved_Mp && (saved_Mp == Mp) )
 				Mp++;

code/ai/ai_profiles.h
@@ -52,7 +52,8 @@
 
 // flags2
 #define AIPF2_TURRETS_IGNORE_TARGET_RADIUS							(1 << 0)
-#define AIPF2_NO_SPECIAL_PLAYER_AVOID									(1 << 1)
+#define AIPF2_NO_SPECIAL_PLAYER_AVOID								(1 << 1)
+#define AIPF2_PERFORM_LESS_SCREAM_CHECKS							(1 << 2)
 
 #define MAX_AI_PROFILES	5
 
@@ -126,6 +127,7 @@ typedef struct ai_profile_t {
 
 	// the chance (x/7) that ship is allowed to fire missiles at player ship.
 	int chance_to_use_missiles_on_plr[NUM_SKILL_LEVELS];
+
 
 } ai_profile_t;
 

code/cmdline/cmdline.cpp
@@ -336,11 +336,9 @@ int Cmdline_voice_recognition = 0;
 // MOD related
 cmdline_parm mod_arg("-mod", NULL, true);	// Cmdline_mod  -- DTP modsupport
 cmdline_parm tbp("-tbp", NULL);			// Cmdline_tbp  -- TBP warp effects -Et1
-cmdline_parm wcsaga("-wcsaga", NULL);	// Cmdline_wcsaga
 
 char *Cmdline_mod = NULL; //DTP for mod arguement
 int Cmdline_tbp = 0;
-int Cmdline_wcsaga = 0;
 
 // Multiplayer/Network related
 cmdline_parm almission_arg("-almission", NULL);		// Cmdline_almission  -- DTP for autoload Multi mission
@@ -1371,11 +1369,6 @@ bool SetCmdlineParams()
 
 	if ( ballistic_gauge.found() ) {
 		Cmdline_ballistic_gauge = 1;
-	}
-
-	if (wcsaga.found())
-	{
-		Cmdline_wcsaga = 1;
 	}
 
 	if ( cache_bitmaps_arg.found() ) {

code/cmdline/cmdline.h
@@ -105,7 +105,6 @@ extern int Cmdline_voice_recognition;
 // MOD related
 extern char *Cmdline_mod;	 // DTP for mod support
 extern int Cmdline_tbp;
-extern int Cmdline_wcsaga;
 
 // Multiplayer/Network related
 extern char *Cmdline_almission;	// DTP for autoload mission (for multi only)

code/globalincs/def_files.cpp
@@ -981,6 +981,10 @@ $fix AI class bug:	NO													\n\
 ;; during dogfights. This results in more aggressive AI.				\n\
 $No extra collision avoidance vs player:	NO							\n\
 																		\n\
+;; If set, the game will check if the dying ship is a player's wingman, \n\
+;; before making it give a death scream					 				\n\
+$Perform less checks for death screams:	NO								\n\
+																		\n\
 #End																	\n\
 ";
 

code/ship/ship.cpp
@@ -3301,6 +3301,10 @@ void parse_ship_type()
 		stuff_boolean_flag(&stp->weapon_bools, STI_WEAP_NO_HUGE_IMPACT_EFF);
 	}
 
+	if(optional_string("$Don't display class in briefing:")) {
+		stuff_boolean_flag(&stp->hud_bools, STI_HUD_NO_CLASS_DISPLAY);
+	}
+
 	if(optional_string("$Fog:"))
 	{
 		if(optional_string("+Start dist:")) {
@@ -13730,7 +13734,6 @@ void ship_maybe_lament()
 #define PLAYER_MAX_SCREAMS				10
 
 // play a death scream for a ship
-extern int Cmdline_wcsaga;
 void ship_scream(ship *sp)
 {
 	int multi_team_filter = -1;
@@ -13753,7 +13756,7 @@ void ship_scream(ship *sp)
 	Player->allow_scream_timestamp = timestamp(PLAYER_SCREAM_INTERVAL);
 
 	// Goober5000
-	if (!Cmdline_wcsaga || Player_ship->team == sp->team)
+	if (Player_ship->team == sp->team)
 		Player->scream_count++;
 
 	sp->flags |= SF_SHIP_HAS_SCREAMED;
@@ -13766,7 +13769,6 @@ void ship_scream(ship *sp)
 // ship has just died, maybe play a scream.
 //
 // NOTE: this is only called for ships that are not the player ship
-extern int Cmdline_wcsaga;
 void ship_maybe_scream(ship *sp)
 {
 	// bail if screaming is disabled
@@ -13776,26 +13778,19 @@ void ship_maybe_scream(ship *sp)
 	// if screaming is enabled, skip all checks
 	if (!(sp->flags2 & SF2_ALWAYS_DEATH_SCREAM))
 	{
-		// for WCSaga, only do a subset of the checks
-		if (Cmdline_wcsaga)
-		{
-			// only scream 50% of the time
-			if (rand() & 1)
-				return;
+		// only scream 50% of the time
+		if (rand() & 1)
+			return;
 
-			// check if enough time has elapsed since last scream; if not, leave
-			if (!timestamp_elapsed(Player->allow_scream_timestamp))
-				return;
-		}
-		// otherwise do all checks
-		else
+		// check if enough time has elapsed since last scream; if not, leave
+		if (!timestamp_elapsed(Player->allow_scream_timestamp))
+			return;
+
+		// for WCSaga, only do a subset of the checks
+		if (!(The_mission.ai_profile->flags2 & AIPF2_PERFORM_LESS_SCREAM_CHECKS))
 		{
 			// bail if this ship isn't from the player wing
 			if (!(sp->flags & SF_FROM_PLAYER_WING))
-				return;
-
-			// only scream 50% of the time
-			if (rand() & 1)
 				return;
 
 			// first check if the player has reached the maximum number of screams for a mission
@@ -13804,10 +13799,6 @@ void ship_maybe_scream(ship *sp)
 
 			// if on different teams (i.e. team v. team games in multiplayer), no scream
 			if (Player_ship->team != sp->team)
-				return;
-
-			// check if enough time has elapsed since last scream; if not, leave
-			if (!timestamp_elapsed(Player->allow_scream_timestamp))
 				return;
 		}
 	}

code/ship/ship.h
@@ -856,6 +856,7 @@ typedef struct thruster_particles {
 #define STI_HUD_HOTKEY_ON_LIST			(1<<0)
 #define STI_HUD_TARGET_AS_THREAT		(1<<1)
 #define STI_HUD_SHOW_ATTACK_DIRECTION	(1<<2)
+#define STI_HUD_NO_CLASS_DISPLAY		(1<<3)
 
 #define STI_SHIP_SCANNABLE				(1<<0)
 #define STI_SHIP_WARP_PUSHES			(1<<1)

commit ef917998f7cea964c95350acd7c3465cdd9c99d3

restore the proper 'class' logic, which was broken in commit 3005e447d3e52d18786df676213a7a941c16d079

code/missionui/missionbrief.cpp
@@ -1279,9 +1279,9 @@ int brief_setup_closeup(brief_icon *bi)
 		if (Closeup_icon->closeup_label[0] == '\0') {
 			strcpy_s(Closeup_icon->closeup_label, sip->get_display_name());
 
-			// Goober5000 - wcsaga doesn't want this
-			if (!Ship_types[sip->class_type].flags[Ship::Type_Info_Flags::No_class_display]) {
-				strcat_s(Closeup_icon->closeup_label, XSTR(" class", 434));
+			if (!Ship_types[sip->class_type].flags[Ship::Type_Info_Flags::No_class_display]
+				&& (sip->is_small_ship() || sip->is_big_ship() || sip->is_huge_ship() || sip->flags[Ship::Info_Flags::Sentrygun])) {
+					strcat_s(Closeup_icon->closeup_label, XSTR(" class", 434));
 			}
 		}
 		break;