baa696f6431268cbc6426ad42be43e0f456330ce
EatThePath <eatthepath@gmail.com>
2022-01-16 13:26:56 -0600

    Fix first person glows (#3882)
    * Split glow current brightness calculation into a new function for reuse and clarity
    
    * Readability cleanup pass on model_render_get_point_activation
    
    * created a new code path to add the lights for the player's ships' glowpoints without rendering their sprites.
    
    * cleaning up unused args from new glowpoint functions
    
    * cleanup redundant externs
    
    * Documentation and naming pass

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -19721,9 +19721,13 @@ void ship_render(object* obj, model_draw_list* scene)
 		return;
 	}
 
+	model_render_params render_info;
 	if ( obj == Viewer_obj && !Rendering_to_shadow_map ) {
 		if (!(Viewer_mode & VM_TOPDOWN))
 		{
+			render_info.set_object_number(OBJ_INDEX(obj));
+			//To allow the player ship to cast light from first person we must still handle it's glowpoints here.
+			model_render_only_glowpoint_lights(&render_info, sip->model_num, -1, &obj->orient, &obj->pos);
 			return;
 		}
 	}
@@ -19763,7 +19767,6 @@ void ship_render(object* obj, model_draw_list* scene)
 		
 	ship_render_batch_thrusters(obj);
 
-	model_render_params render_info;
 	
 	if ( !(shipp->flags[Ship_Flags::Disabled]) && !ship_subsys_disrupted(shipp, SUBSYSTEM_ENGINE) && show_thrusters) {
 		mst_info mst;
