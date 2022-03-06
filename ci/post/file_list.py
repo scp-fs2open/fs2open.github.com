afce512f55b9b8ad2c126050069545078f4d356a
Allen A. Babb <z1281110@gmail.com>
2022-03-05 21:48:01 -0600

    Repair Automated Forum and Nebula Scripts (#4052)
    * Document things
    * Maybe use a format that doxygen can understand
    * Use dict comprehension to clean up main.groups a bit
    
    * More docstrings
    
    * Move FileGroup into module its supposed to be in
    
    * Remove nightly-trigger.yml
    
    * Rename workflow files to better describe their actions
    
    * Re-add post build capabilities
    * Post-build may be triggered by build-release.yaml or manually
    * minor doc update to file_list.get_ftp_files()
    
    * Maybe fix yaml errors
    
    * "You have a syntax error on L345" Gee, thanks github debugger. Could you tell me what exactly that error may be?
    
    * More fixes
    
    * More fixes
    
    * fix typo
    
    * fix typo
    
    * Allow manual trigger to specify releaseTag
    
    * Try omitting the default for string
    
    * Use correct .inputs member
    
    * try environment instead of env
    
    * Try elevating scope of env
    
    * Comment out stuff to directly test env passing
    
    * See if echo is working correctly
    
    * Test if top level env is in scope
    
    * Try github.event instead of github.events
    
    * Uncomment lines, use correct context signature
    
    * Pass ref_name to post-build-release workflow
    
    * Fix arguments to git
    Stash linux package since it can fail, too
    
    * str and repr cannot be compared
    
    * Get repo metadata differently to allow tests on forks
    
    * echo repository var
    
    * config["templates"] does not have a member ["installer"] so... we'll use ["release"] instead
    
    * debug render_installer_config.  file groups malformed?
    
    * Print out both keys of groups dict
    
    * debug get_release_files. print binary_file list
    
    * Revert file_list debug print
    Try sorting binary_files by ReleaseFile.name
    
    * Add checks before running mako to improve debugging QoL
    * revert change to installer.py
    * maybe fix call to render_nebula_release
    * type the files argument to get IDE hints
    
    * Revert typing.... :(
    
    * missing parenthesis
    
    * Disabled installer mako rendering
    
    * Error check in ForumAPI to allow tests to continue
    
    * debug print hlp forum key stuff
    
    * Instead of none, check ""

---
+++ ci/post/file_list.py
@@ -0,0 +1,228 @@
+import re # regex module
+from ftplib import FTP, error_perm
+from itertools import groupby
+from typing import List, Tuple, Dict
+
+import requests  # HTTP requests module
+
+from util import retry_multi, GLOBAL_TIMEOUT	# from util.py
+
+
+class ReleaseFile:
+    """! Class representing a Released file on Nebula
+
+    `name`: str
+        Mod (or build) name,
+    `url`: str
+        Primary host URL,
+    `group`: str
+        Mod group string,
+    `subgroup`: str
+        Mod subgroup string,
+    `mirrors`: List[str]
+        List of URL's of FTP mirrors
+    """
+    def __init__(self, name, url, group, subgroup=None, mirrors=None):
+        if mirrors is None:
+            mirrors = []
+        self.mirrors = mirrors
+        self.subgroup = subgroup
+        self.group = group
+        self.url = url
+        self.name = name
+
+        self.base_url = "/".join(url.split('/')[0:-1]) + "/"
+        self.filename = url.split('/')[-1]
+
+        # A list of tuples of (filename, hash)
+        self.content_hashes = None
+
+        self.hash = None
+        self.size = 0
+    
+    def __repr__(self):
+        return repr((self.name))
+
+
+class SourceFile:
+    """! Class represeting a source file
+
+    `name`: str
+        File name,
+    `url`: str
+        FTP URL,
+    `group`
+        <unknown>
+ 
+    @details More details
+    """
+    def __init__(self, name, url, group):
+        self.group = group
+        self.url = url
+        self.name = name
+
+
+class FileGroup:
+    """! Represents a file group
+
+    `name`: str
+        Name of this group
+    `files`: List[ReleaseFile]
+        List of files within this group
+    `mainFile`: str
+        If this FileGroup has a subgroup, `mainFile` is the head of that group
+    `subFiles`: List[ReleaseFile]
+        Files within a subgroup
+    """
+
+    def __init__(self, name, files: List[ReleaseFile]):
+        self.files = files
+        self.name = name
+
+        if len(files) == 1:
+            self.mainFile = files[0]
+            self.subFiles = {}
+        else:
+            self.mainFile = None
+            subFiles = []
+            for file in files:
+                # We only have subcategories for Windows where SSE2 is the main group
+                if file.subgroup == "SSE2":
+                    self.mainFile = file
+                else:
+                    subFiles.append(file)
+
+            self.subFiles = dict(((x[0], next(x[1])) for x in groupby(subFiles, lambda f: f.subgroup)))
+
+
+def get_release_files(tag_name, config) -> Tuple[List[ReleaseFile], Dict[str, SourceFile]]:
+    """! Brief Gets the binary and source files from the Github Release server
+
+    @param[in] `tag_name` Git tag of the current release
+    @param[in] `config`   confi metadata set in main.py
+
+    @returns `List[ReleaseFile]`        List of release files
+    @returns `Dict[str, SourceFile]`    Dictionary of source files
+
+    @details Sends an `HTTP GET` request to github using their REST API to retrieve metadata.  The files are not
+        actually downloaded here, just their metadata is gathered and organized in their respective container for later
+        use.
+    """
+
+    @retry_multi(5)	# retry at most 5 times
+    def execute_request(path):
+        """!
+        @brief Performs a GET request with the given path. To be used with Github's REST API.
+        @returns If successful, returns a .JSON object
+        """
+        headers = {
+            "Accept": "application/vnd.github.v3+json"
+        }
+        url = "https://api.github.com" + path
+
+        # GET https://api.github.com/<path> Accept: "application/vnd.github.v3+json"
+
+        response = requests.get(url, headers=headers, timeout=GLOBAL_TIMEOUT)
+
+        response.raise_for_status() # Raise a RequestException if we failed, and trigger retry
+
+        return response.json()
+
+    build_group_regex = re.compile("fs2_open_.*-builds-([^.-]*)(-([^.]*))?.*")  # regex for matching binary .zip's and .7z's
+    source_file_regex = re.compile("fs2_open_.*-source-([^.]*)?.*") # regex for matching source .zip's and .7z's
+
+    # Get the github release metadata of the given tag name
+    response = execute_request(
+        "/repos/{}/releases/tags/{}".format(config["github"]["repo"], tag_name))
+
+    # Extract the binary and source files from the response["asset"] metadata
+    binary_files = []
+    source_files = {}
+    for asset in response["assets"]:
+        url = asset["browser_download_url"]
+        name = asset["name"]
+
+        group_match = build_group_regex.match(name)
+
+        if group_match is not None:
+            platform = group_match.group(1)
+            # x64 is the Visual Studio name but for consistency we need Win64
+            if platform == "x64":
+                platform = "Win64"
+
+            binary_files.append(ReleaseFile(name, url, platform, group_match.group(3)))
+        else:
+            group_match = source_file_regex.match(name)
+
+            if group_match is None:
+                continue
+
+            group = group_match.group(1)
+
+            source_files[group] = SourceFile(name, url, group)
+
+    binary_files.sort(key=lambda ReleaseFile: ReleaseFile.name)
+
+    return binary_files, source_files
+
+
+def get_ftp_files(build_type, tag_name, config) -> List[ReleaseFile] :
+    """!
+    @brief Gets file metadata for nightlies hosted on FTP, as determined by config["ftp"] attributes
+    
+    @param [in] `build_type` Unknown str
+    @param [in] `tag_name`   Github tag name of the release
+    @param [in] `config`     config metadata set in main.py
+    """
+
+    tag_regex = re.compile("nightly_(.*)")
+    build_group_regex = re.compile("nightly_.*-builds-([^.]+).*")
+
+    files = []
+    try:
+        with FTP(config["ftp"]["host"], config["ftp"]["user"], config["ftp"]["pass"]) as ftp:
+            # extract version
+            version_str = tag_regex.match(tag_name).group(1)
+
+            # extract filepath w/ version
+            # then list all ftp hits with that path
+            path_template = config["ftp"]["path"]
+            path = path_template.format(type=build_type, version=version_str)
+            file_entries = list(ftp.mlsd(path, ["type"]))
+
+            # get all ftp hits of type file
+            for entry in file_entries:
+                if entry[1]["type"] == "file":
+                    files.append(entry[0])
+    except error_perm:
+        print("Received permanent FTP error!")
+        return []
+
+    out_data = []
+    for file in files:
+        # from the file list, extract only nightly files
+        file_match = build_group_regex.match(file)
+        if file_match is None:
+            print("Ignoring non nightly file '{}'".format(file))
+            continue
+
+        group_match = file_match.group(1)
+        primary_url = None
+        mirrors = []
+
+        # x64 is the name Visual Studio uses but Win64 works better for us since that gets displayed in the nightly post
+        if "x64" in group_match:
+            group_match = group_match.replace("x64", "Win64")
+
+        # construct the download URL list for all mirrors.  The first listed ftp location is taken as the Primary
+        for mirror in config["ftp"]["mirrors"]:
+            download_url = mirror.format(type=build_type, version=version_str, file=file)
+            if primary_url is None:
+                primary_url = download_url
+            else:
+                mirrors.append(download_url)
+
+        # Form the List[ReleaseFile] list with the download URL links
+        out_data.append(ReleaseFile(file, primary_url, group_match, None, mirrors))
+
+    return out_data
---
+++ ci/post/main.py
@@ -0,0 +1,238 @@
+# Post-build python script
+# Uploads build packages to Nebula and creates a forum post on https://www.hard-light.net
+# Uses the following environment variables (Set by the github action workflow before execution)
+
+# FSO versioning
+# FSO_VERSION_MAJOR
+# FSO_VERSION_MINOR
+# FSO_VERSION_BUILD
+
+# Credentials into IndieGames file host
+# INDIEGAMES_USER:
+# INDIEGAMES_SSHPASS:
+
+# Credentials into Nebula Mod Database
+# NEBULA_USER:
+# NEBULA_PASSWORD:
+
+# Credentials into hard-light.net for forum post
+# HLP_API:
+# HLP_KEY:
+
+# LINUX_RESULT: bool, if the linux builds were sucessfully uploaded
+# WINDOWS_RESULT: bool, if the windows builds were successfully uploaded
+
+import sys
+import os
+import re
+from subprocess import check_output
+from datetime import datetime
+from itertools import groupby
+
+import semantic_version
+
+# local .py files
+import file_list
+import installer
+import nebula
+import forum
+
+# Compile regexes for extracting version components
+MAJOR_VERSION_PATTERN = re.compile("set_if_not_defined\(FSO_VERSION_MAJOR (\d+)\)")
+MINOR_VERSION_PATTERN = re.compile("set_if_not_defined\(FSO_VERSION_MINOR (\d+)\)")
+BUILD_VERSION_PATTERN = re.compile("set_if_not_defined\(FSO_VERSION_BUILD (\d+)\)")
+
+LOG_FORMAT = """
+------------------------------------------------------------------------%n
+commit %h%nAuthor: %an <%ad>%n
+Commit: %cn <%cd>%n%n    %s
+"""
+DATEFORMAT_VERSION = "%Y%m%d"
+DATEFORMAT_FORUM = "%d %B %Y"
+
+def _match_version_number(text, regex):
+	"""! Extracts the version component represented by `regex` from `text`
+	
+	@param [in] `text`  Filename with version string 'MAJOR.MINOR.BUILD'
+	@param [in] `regex` Regex pattern of component to look for
+
+	@return The version component as integer
+	"""
+	match = regex.search(text)
+	return int(match.group(1))
+	
+
+def get_source_version(date_version):
+	"""! Retrieves the build's version from `version.cmake`, forms it as a string, and appends the date
+
+	@param[in] `date_version` Date this build is published for release
+
+	@return String of the form `MAJOR_VERSION.MINOR_VERSION.BUILD_VERSION-date`
+	"""
+	
+	with open(os.path.join("..", "..", "cmake", "version.cmake"), "r") as f:
+		filetext = f.read()
+
+		major = _match_version_number(filetext, MAJOR_VERSION_PATTERN)
+		minor = _match_version_number(filetext, MINOR_VERSION_PATTERN)
+		build = _match_version_number(filetext, BUILD_VERSION_PATTERN)
+
+		return semantic_version.Version("{}.{}.{}-{}".format(major, minor, build, date_version))
+
+
+def main():
+	os.chdir(os.path.dirname(__file__))	# Change working directory to this file's directory
+
+	# Aggregate configuration data in a dictionary
+	config = {
+		"github": {
+			"repo": os.environ["GITHUB_REPO"],
+		},
+		"ftp": {
+			"host": "scp.indiegames.us",
+			"user": os.environ["INDIEGAMES_USER"],
+			"pass": os.environ["INDIEGAMES_SSHPASS"],
+			"path": "public_html/builds/{type}/{version}/",
+			"mirrors": (
+				"https://porphyrion.feralhosting.com/datacorder/builds/{type}/{version}/{file}",
+				"http://scp.indiegames.us/builds/{type}/{version}/{file}",
+			),
+		},
+		"nebula": {
+			"user": os.environ["NEBULA_USER"],
+			"password": os.environ["NEBULA_PASSWORD"],
+		},
+		"hlp": {
+			"api": os.environ["HLP_API"],
+			"key": os.environ["HLP_KEY"],
+		},
+		"templates": {
+			"nightly": "nightly.mako",
+			"release": "release.mako",
+		},
+		"nightly": {
+			"hlp_board": 173,
+		},
+		"release": {
+			"hlp_board": 50,
+		},
+	}
+
+	# bail if this script is run from a workflow that doesn't identify as a release or nightly
+	if sys.argv[1] not in ("release", "nightly"):
+		print("ERROR: Invalid release mode %s passed. Expected release or nightly!" % sys.argv[1])
+		sys.exit(1)
+
+	tag_name = os.environ["RELEASE_TAG"]	##!< commit tag string
+	date = datetime.now()	##!< current date
+	version = get_source_version(date.strftime(DATEFORMAT_VERSION))	##!< form full version string
+	success = os.environ["LINUX_RESULT"] == "success" and os.environ["WINDOWS_RESULT"] == "success"	##!< true if both linux and windows builds successful
+
+	# check that tag_name is actually in the git repo and find the previous tag release
+	tags = check_output(("git", "for-each-ref", "--sort=-taggerdate", "--format='%(tag)'", "refs/tags"), text=True).splitlines()	# retrieve all tags in the repo by using git on the shell
+	# NOTE: check_output returns canonical string representation. use repr() on strings its being compared to
+	previous_tag = None
+	found = False
+
+	for tag in tags:
+		if found:
+			# Look for the "previous" tag with the same configuration ('release_' or 'nightly_')
+			# Assumes older tags have a higher index than newer
+			if tag.startswith("\'" + sys.argv[1] + "_"):
+				previous_tag = tag
+				break
+		elif tag == repr(tag_name):
+			# Ok, found the tag
+			found = True
+
+	if not found:
+		# couldn't find tag, bail
+		print("ERROR: Couldn't find tag %s in repo!" % tag_name)
+		sys.exit(1)
+
+	if not previous_tag:
+		# couldn't find previous tag, bail
+		print("ERROR: Could not find a previous tag for %s!" % tag_name)
+		sys.exit(1)
+
+	if sys.argv[1] == "release":
+		# Release specific action
+		if not tag_name.startswith("release_"):
+			# Safety check tag is from a release
+			print("ERROR: Invalid tag name detected. Expected release_ prefix for release builds but found %s (full ref = %s)" % tag_name, os.environ["GITHUB_REF"])
+			sys.exit(1)
+
+		files, sources = file_list.get_release_files(tag_name, config)
+
+		print("Hashing files")
+		for file in files:
+			installer.get_hashed_file_list(file)
+
+		# Construct the file groups
+		# split the file list into a dictionary of {group: files} pairs using `groupby()`
+		# Since `groupby()` returns a iterator reference to the original files list, we have to do a weird hand toss
+		# to save it as a dictionary
+		## NOTE z64: files need to be sorted or else key duplication will occur!
+		groups = {key: file_list.FileGroup(key, list(group)) for key, group in groupby(files, lambda g: g.group)}
+		
+		# Error check for all group keys and subkeys to be used in the mako.  Do this here for better debugging QoL
+		if "Win32" in groups.keys():
+			if "AVX" not in groups["Win32"].subFiles.keys():
+				print("ERROR: No Win32-AVX builds were detected!")
+				sys.exit(1)
+		else:
+			print("ERROR: No Win32 builds were detected!")
+			sys.exit(1)
+
+		if "Win64" in groups.keys():
+			if "AVX" not in groups["Win64"].subFiles.keys():
+				print("ERROR: No x64-AVX builds were detected!")
+				sys.exit(1)
+		else:
+			print("ERROR: Now x64 builds were detected!")
+			sys.exit(1)
+		
+		if "Linux" not in groups.keys():
+			print("ERROR: No Linux builds were detected!")
+			sys.exit(1)
+		
+		# z64: What dose this do???
+		# print(installer.render_installer_config(version, groups, config))
+
+		# Publish release builds to Nebula
+		nebula.submit_release(
+			nebula.render_nebula_release(version, "rc" if version.prerelease else "stable", files, config),
+			config)
+
+		# Publish forum post, using the release.mako template
+		fapi = forum.ForumAPI(config)
+		fapi.post_release(date.strftime(DATEFORMAT_FORUM), version, groups, sources)
+
+	else:
+		# Dead for now, nightly publish is done elsewhere
+		# Nightly specific action
+		if not tag_name.startswith("nightly_"):
+			# safety check tag is from a nightly
+			print("ERROR: Invalid tag name detected. Expected nightly_ prefix for nightly builds but found %s (full ref = %s)" % tag_name, os.environ["GITHUB_REF"])
+			sys.exit(1)
+
+		print("Retrieving file list")
+		files = file_list.get_ftp_files("nightly", tag_name, config)
+
+		print("Hashing files")
+		for file in files:
+			installer.get_hashed_file_list(file)
+
+		# Publish nightly builds to Nebula
+		nebula.submit_release(
+			nebula.render_nebula_release(version, "nightly", files, config),
+			config)
+
+		# Compile commit messages into a log with all commits between this nightly and the previous nightly
+		log = check_output(("git", "log", "%s^..%s^" % (previous_tag, tag_name), "--no-merges", "--stat", "--pretty=format:\"%s\"" % LOG_FORMAT.strip()))
+
+		# Publish forum post, using the nighty.mako template
+		fapi = forum.ForumAPI(config)
+		fapi.post_nightly(date.strftime(DATEFORMAT_FORUM), os.environ["GITHUB_SHA"], files, log, success)
+
+main()
