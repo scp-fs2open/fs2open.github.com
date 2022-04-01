1d3e62f00737b80c4b3e1e75cf76723a0e010064
Tyler Blount <tylerrblount@gmail.com>
2022-04-01 14:18:23 -0700

    Bump FSO version to 22.0

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -6,8 +6,8 @@ if (EXISTS "${PROJECT_SOURCE_DIR}/version_override.cmake")
     include("${PROJECT_SOURCE_DIR}/version_override.cmake")
 endif()
 
-set_if_not_defined(FSO_VERSION_MAJOR 21)
-set_if_not_defined(FSO_VERSION_MINOR 5)
+set_if_not_defined(FSO_VERSION_MAJOR 22)
+set_if_not_defined(FSO_VERSION_MINOR 0)
 set_if_not_defined(FSO_VERSION_BUILD 0)
 
 # A numeric revision e.g. the CI build number
