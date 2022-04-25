e3927104bb3f1f71fd733f2d077dfb13911a8e28
David Gibson <EatThePath@Gmail.com>
2022-04-25 09:27:50 -0500

    Implemented optional parse into bool parsehi

--- code/parse/parsehi.cpp
+++ code/parse/parsehi.cpp
@@ -17,3 +17,20 @@ bool parse_optional_float_into(const SCP_string& field_name, float* value_target
 	}
 	return false;
 }
+
+/**
+ * @brief Parses an optional table value into a field if the name is found
+ *
+ * @param field_name The name of the table field
+ * @param value_target Pointer to the variable to assign any parsed value to
+ *
+ * @return True if a value was parsed, false if not
+ */
+bool parse_optional_bool_into(const SCP_string& field_name, bool* value_target)
+{
+	if (optional_string(field_name.c_str())) {
+		stuff_boolean(value_target);
+		return true;
+	}
+	return false;
+}
--- code/parse/parsehi.h
+++ code/parse/parsehi.h
@@ -5,3 +5,5 @@
 //and also for collection of standardized parsing of complex types where appropriate
 
 extern bool parse_optional_float_into(const SCP_string& field_name, float* value_target);
+
+extern bool parse_optional_bool_into(const SCP_string& field_name, bool* value_target);
