3f39cbd76ab4b3d5fa5e48e3f5f97d66d48a6913
Adam Leach <qazwsxalan@gmail.com>
2022-04-29 21:41:04 +0100

    Clearer Paste function names and saner defaults

--- fred2/sexp_tree.cpp
+++ fred2/sexp_tree.cpp
@@ -1921,11 +1921,11 @@ BOOL sexp_tree::OnCommand(WPARAM wParam, LPARAM lParam)
 			NodeCopy();
 			return 1;
 
-		case ID_EDIT_PASTE:
-			NodePaste();
+		case ID_EDIT_PASTE_SPECIAL:
+			NodeReplacePaste();
 			return 1;
 
-		case ID_EDIT_PASTE_SPECIAL:  // add paste, instead of replace.
+		case ID_EDIT_PASTE:  // add paste, instead of replace.
 			NodeAddPaste();
 			return 1;
 
@@ -2072,7 +2072,7 @@ void sexp_tree::NodeCopy()
 	sexp_mark_persistent(Sexp_clipboard);
 }
 
-void sexp_tree::NodePaste()
+void sexp_tree::NodeReplacePaste()
 {
 	if (item_index < 0)
 		return;
@@ -4274,13 +4274,13 @@ void sexp_tree::OnKeyDown(NMHDR *pNMHDR, LRESULT *pResult)
 			else if (key == 'V')
 			{
 				update_item(GetSelectedItem());
-				NodePaste();
+				NodeAddPaste();
 			}
 			else if (key == 'P')
 			{
 				update_item(GetSelectedItem());
 				auto orig_handle = item_handle;
-				NodeAddPaste();
+				NodeReplacePaste();
 
 				// when using the keyboard shortcut, stay on the original node after pasting
 				SelectItem(orig_handle);
