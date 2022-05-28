45b6362777d2802965d46fd361d9f0334d53298c
David Gibson <EatThePath@Gmail.com>
2022-05-28 16:08:18 -0500

    Applied automatic formatting to the new debug_sphere files Restructured debug sphere code for efficiency

--- code/graphics/debug_sphere.cpp
+++ code/graphics/debug_sphere.cpp
@@ -1,15 +1,20 @@
 #include "debug_sphere.h"
+
 #include "globalincs/vmallocator.h"
+
 #include "render/3d.h"
+
 
 SCP_vector<debug_sphere> debug_sphere::Spheres;
 
-void debug_sphere::add(vec3d pos, float rad)
+debug_sphere::debug_sphere(const vec3d& pos_in, float rad_in)
 {
-	debug_sphere s;
-	s.pos = pos;
-	s.radius = rad;
-	debug_sphere::Spheres.push_back(s);
+	pos = pos_in;
+	radius = rad_in;
+}
+void debug_sphere::add(const vec3d& pos, float rad)
+{
+	debug_sphere::Spheres.emplace_back(debug_sphere(pos, rad));
 }
 
 void debug_sphere::clear()
@@ -19,8 +24,8 @@ void debug_sphere::clear()
 
 void debug_sphere::render()
 {
-	for(auto s : Spheres){
-		g3_draw_sphere_ez(&(s.pos),s.radius);
+	for (auto& s : Spheres) {
+		g3_draw_sphere_ez(&(s.pos), s.radius);
 	}
 	clear();
 }
--- code/graphics/debug_sphere.h
+++ code/graphics/debug_sphere.h
@@ -2,13 +2,13 @@
 
 #include "globalincs/pstypes.h"
 #include "globalincs/vmallocator.h"
-class debug_sphere
-{
-	public:
-	static void add(vec3d pos, float radius);
+class debug_sphere {
+  public:
+	static void add(const vec3d& pos, float radius);
 	static void render();
+	debug_sphere(const vec3d& pos, float radius);
 
-	private:
+  private:
 	static SCP_vector<debug_sphere> Spheres;
 	static void clear();
 	vec3d pos;
