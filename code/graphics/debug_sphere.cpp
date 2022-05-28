859d44f3a387d36d33586c16718c6370d49af96e
David Gibson <EatThePath@Gmail.com>
2022-05-28 16:33:12 -0500

    Rendering changes to stablize debug_sphere coloration

--- code/graphics/debug_sphere.cpp
+++ code/graphics/debug_sphere.cpp
@@ -4,7 +4,6 @@
 
 #include "render/3d.h"
 
-
 SCP_vector<debug_sphere> debug_sphere::Spheres;
 
 debug_sphere::debug_sphere(const vec3d& pos_in, float rad_in)
@@ -24,8 +23,16 @@ void debug_sphere::clear()
 
 void debug_sphere::render()
 {
+	color uicolor = gr_screen.current_color;
+	color render_color;
+	render_color.red = 255;
+	render_color.green = 255;
+	render_color.blue = 255;
+	render_color.alpha = 64;
+	gr_set_color_fast(&render_color);
 	for (auto& s : Spheres) {
 		g3_draw_sphere_ez(&(s.pos), s.radius);
 	}
+	gr_set_color_fast(&uicolor);
 	clear();
 }
