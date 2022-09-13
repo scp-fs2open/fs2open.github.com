142920aea72d3c3648470cd7f0a8b559e6ab820d
mjn-mixael <mjn.other@gmail.com>
2022-09-13 12:27:13 -0500

    Cleanup Fiction, Red Alert, and Loop Brief based on feedback on other PRs

--- code/scripting/api/objs/redalert.cpp
+++ code/scripting/api/objs/redalert.cpp
@@ -3,27 +3,27 @@
 namespace scripting {
 namespace api {
 
-//**********HANDLE: red alert
-ADE_OBJ(l_RedAlertStage, brief_stage, "red_alert_stage", "Red Alert stage handle");
+redalert_stage_h::redalert_stage_h() : ra_brief(-1), ra_stage(-1) {}
 
-ADE_VIRTVAR(Text, l_RedAlertStage, nullptr, "The briefing text of the stage", "red_alert_stage", "The text string")
+redalert_stage_h::redalert_stage_h(int brief, int stage) : ra_brief(brief), ra_stage(stage) {}
+
+bool redalert_stage_h::IsValid() const
 {
-	brief_stage* current = nullptr;
-	if (!ade_get_args(L, "o", l_RedAlertStage.GetPtr(&current))) {
-		return ADE_RETURN_NIL;
-	}
-
-	if (ADE_SETTING_VAR) {
-		LuaError(L, "This property is read only.");
-	}
-
-	return ade_set_args(L, "s", current->text.c_str());
+	return ra_brief >= 0 && ra_stage >= 0;
 }
 
-ADE_VIRTVAR(AudioFilename, l_RedAlertStage, nullptr, "The audio file of the stage", "red_alert_stage", "The audio file")
+brief_stage* redalert_stage_h::getStage() const
 {
-	brief_stage* current = nullptr;
-	if (!ade_get_args(L, "o", l_RedAlertStage.GetPtr(&current))) {
+	return &Briefings[ra_brief].stages[ra_stage];
+}
+
+//**********HANDLE: red alert
+ADE_OBJ(l_RedAlertStage, redalert_stage_h, "red_alert_stage", "Red Alert stage handle");
+
+ADE_VIRTVAR(Text, l_RedAlertStage, nullptr, "The briefing text of the stage", "string", "The text string")
+{
+	redalert_stage_h current;
+	if (!ade_get_args(L, "o", l_RedAlertStage.Get(&current))) {
 		return ADE_RETURN_NIL;
 	}
 
@@ -31,7 +31,21 @@ ADE_VIRTVAR(AudioFilename, l_RedAlertStage, nullptr, "The audio file of the stag
 		LuaError(L, "This property is read only.");
 	}
 
-	return ade_set_args(L, "s", current->voice);
+	return ade_set_args(L, "s", current.getStage()->text.c_str());
+}
+
+ADE_VIRTVAR(AudioFilename, l_RedAlertStage, nullptr, "The audio file of the stage", "string", "The audio file")
+{
+	redalert_stage_h current;
+	if (!ade_get_args(L, "o", l_RedAlertStage.Get(&current))) {
+		return ADE_RETURN_NIL;
+	}
+
+	if (ADE_SETTING_VAR) {
+		LuaError(L, "This property is read only.");
+	}
+
+	return ade_set_args(L, "s", current.getStage()->voice);
 }
 
 } // namespace api
--- code/scripting/api/objs/redalert.h
+++ code/scripting/api/objs/redalert.h
@@ -6,7 +6,15 @@
 namespace scripting {
 namespace api {
 
-DECLARE_ADE_OBJ(l_RedAlertStage, brief_stage);
+struct redalert_stage_h {
+	int ra_brief, ra_stage;
+	redalert_stage_h();
+	explicit redalert_stage_h(int brief, int stage);
+	bool IsValid() const;
+	brief_stage* getStage() const;
+};
+
+DECLARE_ADE_OBJ(l_RedAlertStage, redalert_stage_h);
 
 } // namespace api
 } // namespace scripting
