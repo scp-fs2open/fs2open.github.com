9b16acb47aea3dc43f95464b919655ea76bac513
Fabian Woltermann <fabian.woltermann@peakwork.com>
2022-09-29 17:20:21 +0200

    Add a syntactic sugar extension for imgui

--- code/lab/labv2_internal.h
+++ code/lab/labv2_internal.h
@@ -3,6 +3,6 @@
 #include "imgui.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_sdl.h"
-
+#include "extensions/imgui_sugar.hpp"
 
 const std::unique_ptr<LabManager> &getLabManager();
