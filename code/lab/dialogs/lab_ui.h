638453b1339075b65ea6fdd29947bad70be018cf
Fabian Woltermann <fabian.woltermann@peakwork.com>
2022-10-01 13:14:56 +0200

    refactor some of this for greater cleanness

--- code/lab/dialogs/lab_ui.h
+++ code/lab/dialogs/lab_ui.h
@@ -10,8 +10,36 @@ class LabUi {
 	void buildShipList() const;
 	void buildWeaponList() const;
 	void buildBackgroundList() const;
-	void showRenderOptions() const;
+	void showRenderOptions();
 	void showObjectOptions() const;
 
 	bool rebuildAfterObjectChange = false;
+
+	bool show_render_options = false;
+	bool show_object_selector = true;
+	bool show_object_options = false;
+	bool close_lab = false;
+
+	bool enable_model_rotation = false;
+	bool show_insignia = false;
+	bool show_damage_lightning = false;
+	bool animate_subsystems = false;
+	bool hide_post_processing = false;
+	bool diffuse_map = true;
+	bool glow_map = true;
+	bool spec_map = true;
+	bool reflect_map = true;
+	bool env_map = true;
+	bool normal_map = true;
+	bool height_map = true;
+	bool misc_map = true;
+	bool ao_map = true;
+	bool no_glowpoints = false;
+	bool use_wireframe_rendering = false;
+	bool no_lighting = false;
+	bool show_full_detail = false;
+	bool show_thrusters = false;
+	bool show_afterburners = false;
+	bool show_weapons = false;
+	bool show_emissive_lighting = false;
 };
