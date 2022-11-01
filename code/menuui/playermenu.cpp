e6ef239ae6519ebd52a9c992094e4fe17f873fee
MjnMixael <mjn.other@gmail.com>
2022-11-01 07:46:22 -0500

    Modular tips (#4843)
    * vectorize tips
    
    * make tips.tbl extendable with -tip.tbm

--- code/menuui/playermenu.cpp
+++ code/menuui/playermenu.cpp
@@ -1370,33 +1370,25 @@ DCF(bastion, "Temporarily sets the player to be on the Bastion (or any other mai
 	}
 }
 
-#define MAX_PLAYER_TIPS			40
-
-SCP_string Player_tips[MAX_PLAYER_TIPS];
-int Num_player_tips;
+SCP_vector<SCP_string> Player_tips;
 int Player_tips_shown = 0;
 
 // tooltips
-void player_tips_init()
+void parse_tips_table(const char* filename)
 {
-	Num_player_tips = 0;
 	
 	try
 	{
-		read_file_text("tips.tbl", CF_TYPE_TABLES);
+		read_file_text(filename, CF_TYPE_TABLES);
 		reset_parse();
 
 		while (!optional_string("#end")) {
 			required_string("+Tip:");
 
-			if (Num_player_tips >= MAX_PLAYER_TIPS) {
-				break;
-			}
-
 			SCP_string buf;
 			stuff_string(buf, F_MESSAGE);
-
-			Player_tips[Num_player_tips++] = message_translate_tokens(buf.c_str());
+			
+			Player_tips.push_back(message_translate_tokens(buf.c_str()));
 
 		}
 	}
@@ -1405,6 +1397,15 @@ void player_tips_init()
 		mprintf(("TABLES: Unable to parse '%s'!  Error message = %s.\n", "tips.tbl", e.what()));
 		return;
 	}
+}
+
+void player_tips_init()
+{
+	// first parse the default table
+	parse_tips_table("tips.tbl");
+
+	// parse any modular tables
+	parse_modular_table("*-tip.tbm", parse_tips_table);
 }
 
 void player_tips_popup()
@@ -1422,7 +1423,7 @@ void player_tips_popup()
 	Player_tips_shown = 1;
 
 	// randomly pick one
-	tip = (int)frand_range(0.0f, (float)Num_player_tips - 1.0f);
+	tip = (int)frand_range(0.0f, (float)Player_tips.size() - 1.0f);
 
 	char all_txt[2048];
 
@@ -1434,7 +1435,7 @@ void player_tips_popup()
 		switch(ret){
 		// next
 		case 1:
-			if(tip >= Num_player_tips - 1) {
+			if (tip >= (int)Player_tips.size() - 1) {
 				tip = 0;
 			} else {
 				tip++;
