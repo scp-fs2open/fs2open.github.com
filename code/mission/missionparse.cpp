scp-fs2open/fs2open.github.com

commit 735b41631deef5a917762f53d360d97288fbaecf

ship flag to exempt it from targeting restrictions (#4878)
* ship flag to exempt it from nebula targeting restrictions

* Make this flag more generic

* address feedback

code/mission/missionparse.cpp
@@ -327,6 +327,7 @@ flag_def_list_new<Mission::Parse_Object_Flags> Parse_object_flags[] = {
     { "fail-sound-locked-secondary",		Mission::Parse_Object_Flags::SF_Fail_sound_locked_secondary, true, false },
     { "aspect-immune",						Mission::Parse_Object_Flags::SF_Aspect_immune, true, false },
 	{ "cannot-perform-scan",			Mission::Parse_Object_Flags::SF_Cannot_perform_scan,	true, false },
+	{ "no-targeting-limits",				Mission::Parse_Object_Flags::SF_No_targeting_limits, true, false},
 };
 
 const size_t Num_parse_object_flags = sizeof(Parse_object_flags) / sizeof(flag_def_list_new<Mission::Parse_Object_Flags>);
@@ -2688,6 +2689,9 @@ void resolve_parse_flags(object *objp, flagset<Mission::Parse_Object_Flags> &par
 		shipp->flags.set(Ship::Ship_Flags::Aspect_immune);
 
 	if (parse_flags[Mission::Parse_Object_Flags::SF_Cannot_perform_scan])
+		shipp->flags.set(Ship::Ship_Flags::Cannot_perform_scan);
+
+	if (parse_flags[Mission::Parse_Object_Flags::SF_No_targeting_limits])
 		shipp->flags.set(Ship::Ship_Flags::Cannot_perform_scan);
 }
 

code/mission/mission_flags.h
@@ -107,6 +107,7 @@ namespace Mission {
 		SF_Fail_sound_locked_secondary,	// Kiloku - Plays fail sound when firing with locked weapons
 		SF_Aspect_immune,				// Kiloku - Ship cannot be locked onto by aspect seeking weapons
 		SF_Cannot_perform_scan,			// Goober5000 - ship cannot scan other ships
+		SF_No_targeting_limits,			// MjnMixael - Ship is always targetable regardless of AWACS or targeting range limits
 
 		NUM_VALUES
 	};

code/ship/awacs.cpp
@@ -184,8 +184,8 @@ float awacs_get_level(object *target, ship *viewer, int use_awacs)
 	float test;
 	int closest_index = -1;
 	int idx, stealth_ship = 0, check_huge_ship = 0, friendly_stealth_invisible = 0;
-	ship *shipp = NULL;
-	ship_info *sip = NULL;
+	ship *shipp = nullptr;
+	ship_info *sip = nullptr;
 
 	int viewer_has_primitive_sensors = (viewer->flags[Ship::Ship_Flags::Primitive_sensors]);
 
@@ -203,11 +203,6 @@ float awacs_get_level(object *target, ship *viewer, int use_awacs)
 	if ((viewer == Player_ship) && (Game_mode & GM_MULTIPLAYER) && (Net_player != NULL) && MULTI_OBSERVER(Net_players[MY_NET_PLAYER_NUM]))
 		return ALWAYS_TARGETABLE;
 
-	// check the targeting threshold
-	if ((Hud_max_targeting_range > 0) && (distance > Hud_max_targeting_range)) {
-		return UNTARGETABLE;
-	}
-
 	if (target->type == OBJ_SHIP) {
 		// if no valid target then bail as never viewable
 		if (target->instance < 0)
@@ -220,6 +215,19 @@ float awacs_get_level(object *target, ship *viewer, int use_awacs)
 		friendly_stealth_invisible = (shipp->flags[Ship::Ship_Flags::Friendly_stealth_invis]);
 
 		check_huge_ship = (sip->is_huge_ship());
+	}
+
+	// check for an exempt ship. Exempt ships are _always_ visible
+	if (target->type == OBJ_SHIP) {
+		Assert(shipp != nullptr);
+		int target_is_exempt = (shipp->flags[Ship::Ship_Flags::No_targeting_limits]);
+		if (target_is_exempt)
+			return FULLY_TARGETABLE;
+	}
+
+	// check the targeting threshold
+	if ((Hud_max_targeting_range > 0) && (distance > Hud_max_targeting_range)) {
+		return UNTARGETABLE;
 	}
 	
 	int nebula_enabled = (The_mission.flags[Mission::Mission_Flags::Fullneb]);
@@ -235,7 +243,7 @@ float awacs_get_level(object *target, ship *viewer, int use_awacs)
 	// check for a tagged ship. TAG'd ships are _always_ visible
 	if (target->type == OBJ_SHIP)
 	{
-		Assert( shipp != NULL );
+		Assert( shipp != nullptr );
 		if (shipp->tag_left > 0.0f || shipp->level2_tag_left > 0.0f)
 			return FULLY_TARGETABLE;
 	}

code/ship/ship.cpp
@@ -542,6 +542,7 @@ ship_flag_name Ship_flag_names[] = {
 	{ Ship_Flags::Fail_sound_locked_secondary, 	"fail-sound-locked-secondary"},
 	{ Ship_Flags::Aspect_immune, 				"aspect-immune"},
 	{ Ship_Flags::Cannot_perform_scan,			"cannot-perform-scan"},
+	{ Ship_Flags::No_targeting_limits,			"no-targeting-limits"}
 };
 
 extern const size_t Num_ship_flag_names = sizeof(Ship_flag_names) / sizeof(ship_flag_name);

code/ship/ship_flags.h
@@ -136,6 +136,7 @@ namespace Ship {
 		Subsystem_cache_valid,		// Goober5000 - whether the subsystem list index caches can be used
 		Aspect_immune,						// Kiloku -- Ship cannot be targeted by Aspect Seekers.
 		Cannot_perform_scan,		// Goober5000 - ship cannot scan other ships
+		No_targeting_limits,				//MjnMixael -- Ship is always targetable regardless of AWACS or targeting range limits
 
 		NUM_VALUES
 

fred2/missionsave.cpp
@@ -3505,6 +3505,8 @@ int CFred_mission_save::save_objects()
 				fout(" \"aspect-immune\"");
 			if (shipp->flags[Ship::Ship_Flags::Cannot_perform_scan])
 				fout(" \"cannot-perform-scan\"");
+			if (shipp->flags[Ship::Ship_Flags::No_targeting_limits])
+				fout(" \"no-targeting-limits\"");
 			fout(" )");
 		}
 		// -----------------------------------------------------------

qtfred/src/mission/missionsave.cpp
@@ -3294,6 +3294,9 @@ int CFred_mission_save::save_objects()
 			if (shipp->flags[Ship::Ship_Flags::Cannot_perform_scan]) {
 				fout(" \"cannot-perform-scan\"");
 			}
+			if (shipp->flags[Ship::Ship_Flags::No_targeting_limits]) {
+				fout(" \"no-targeting-limits\"");
+			}
 			fout(" )");
 		}
 		// -----------------------------------------------------------

commit e62c148c53a0099ea874b4d02246cd8ecfd2645d

fix a copy+paste error
This was accidentally introduced in #4878.

code/mission/missionparse.cpp
@@ -2692,7 +2692,7 @@ void resolve_parse_flags(object *objp, flagset<Mission::Parse_Object_Flags> &par
 		shipp->flags.set(Ship::Ship_Flags::Cannot_perform_scan);
 
 	if (parse_flags[Mission::Parse_Object_Flags::SF_No_targeting_limits])
-		shipp->flags.set(Ship::Ship_Flags::Cannot_perform_scan);
+		shipp->flags.set(Ship::Ship_Flags::No_targeting_limits);
 }
 
 void fix_old_special_explosions(p_object *p_objp, int variable_index) 