fd27dd494d93ffb8461c5cf602653a762fd14fcf
wookieejedi <azthepaleo@gmail.com>
2022-12-06 21:55:31 -0500

    same for type ship and damage

--- code/model/modelrender.cpp
+++ code/model/modelrender.cpp
@@ -845,7 +845,19 @@ void model_render_add_lightning( model_draw_list *scene, model_render_params* in
 			}
 
 			secondary = Arc_color_damage_s1;
+			
+			// try and scale the size a bit so that it looks equally well on smaller vessels
+			width = Arc_width_default_damage;
+			if (pm->rad < Arc_width_no_multiply_over_radius_damage) {
+				width *= (pm->rad * Arc_width_radius_multiplier_damage);
+
+				if (width < Arc_width_minimum_damage) {
+					width = Arc_width_minimum_damage;
+				}
+			}
+			
 			break;
+
 		case MARC_TYPE_SHIP:
 			if ( Random::flip_coin() )	{
 				primary = smi->arc_primary_color_1[i];
