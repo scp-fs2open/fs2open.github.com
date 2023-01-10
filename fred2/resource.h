8a1145c441998982997de046276eb71c291a27b6
mjn-mixael <mjn.other@gmail.com>
2023-01-10 14:44:53 -0600

    revert this change

--- fred2/resource.h
+++ fred2/resource.h
@@ -1467,7 +1467,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        331
+#define _APS_NEXT_RESOURCE_VALUE        330
 #define _APS_NEXT_COMMAND_VALUE         33102
 #define _APS_NEXT_CONTROL_VALUE         1665
 #define _APS_NEXT_SYMED_VALUE           105
