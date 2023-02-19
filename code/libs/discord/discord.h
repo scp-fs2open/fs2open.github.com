13e28a3102210d87c559b13216a3e410d2a71a47
mjn-mixael <mjn.other@gmail.com>
2023-02-19 14:58:17 -0600

    const the header, dummy

--- code/libs/discord/discord.h
+++ code/libs/discord/discord.h
@@ -5,7 +5,7 @@ namespace discord {
 
 void init();
 void shutdown();
-void set_presence_string(SCP_string text);
+void set_presence_string(const SCP_string &text);
 void set_presence_gameplay();
 }
 } // namespace libs
