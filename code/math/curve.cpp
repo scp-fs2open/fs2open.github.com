26fb24b964b10cdf6bd96c6d881aa64855b888a3
Asteroth <sshyrda@gmail.com>
2023-04-17 19:39:08 -0400

    pretty sure i dont need this anymore

--- code/math/curve.cpp
+++ code/math/curve.cpp
@@ -1,11 +1,3 @@
-/*
- * Copyright (C) Volition, Inc. 1999.  All rights reserved.
- *
- * All source code herein is the property of Volition, Inc. You may not sell 
- * or otherwise commercially exploit the source or things you created based on the 
- * source.
- *
-*/ 
 
 #include "math/curve.h"
 #include "parse/parselo.h"
