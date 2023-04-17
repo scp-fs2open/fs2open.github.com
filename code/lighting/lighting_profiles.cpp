a8035fec36b2036f083c6edd3c975ea8e8b15d8e
ETP <EatThePath@Gmail.com>
2023-04-17 14:06:21 -0500

    Applied formatting to lighting profiles files

--- code/lighting/lighting_profiles.cpp
+++ code/lighting/lighting_profiles.cpp
@@ -1,13 +1,17 @@
+// clang-format on
+#include "lighting/lighting_profiles.h"
+
 #include "globalincs/pstypes.h"
 #include "globalincs/safe_strings.h"
 #include "globalincs/vmallocator.h"
+
 #include "cmdline/cmdline.h"
 #include "def_files/def_files.h"
 #include "lighting/lighting.h"
-#include "lighting/lighting_profiles.h"
 #include "osapi/dialogs.h"
 #include "parse/parsehi.h"
 #include "parse/parselo.h"
+
 
 //*************************************************
 //			lighting_profile_value funcs
@@ -22,7 +26,7 @@ float lighting_profile_value::handle(float input)
 		input = base;
 	if (has_multiplier)
 		input *= multipier;
-	//handling adjust after multiplier makes it possible to multiply by 0 and still adjust.
+	// handling adjust after multiplier makes it possible to multiply by 0 and still adjust.
 	if (has_adjust)
 		input += adjust;
 	if (has_minimum)
@@ -56,12 +60,9 @@ void lighting_profile_value::set_multiplier(float in)
 
 void lighting_profile_value::stack_multiplier(float in)
 {
-	if(has_multiplier)
-	{
-		multipier*= in;
-	}
-	else 
-	{
+	if (has_multiplier) {
+		multipier *= in;
+	} else {
 		multipier = in;
 		has_multiplier = true;
 	}
@@ -81,22 +82,19 @@ void lighting_profile_value::set_minimum(float in)
 
 void lighting_profile_value::stack_minimum(float in)
 {
-	if(has_minimum)
-	{
-		minimum = MAX(minimum,in);
-	}
-	else
-	{
+	if (has_minimum) {
+		minimum = MAX(minimum, in);
+	} else {
 		minimum = in;
 		has_minimum = true;
 	}
 }
-bool lighting_profile_value::read_adjust(float *out) const
+bool lighting_profile_value::read_adjust(float* out) const
 {
 	*out = adjust;
 	return has_adjust;
 }
-bool lighting_profile_value::read_multiplier(float *out) const
+bool lighting_profile_value::read_multiplier(float* out) const
 {
 	*out = multipier;
 	return has_multiplier;
@@ -105,7 +103,7 @@ bool lighting_profile_value::read_multiplier(float *out) const
  * @brief for use during the parsing of a light profile to attempt to read in an LPV
  *
  * @param filename for error reporting primairly
- * @param valuename Text of the field 
+ * @param valuename Text of the field
  * @param profile_name The name of the profile being parsed, for error reporting
  * @param value_target The profile object parsing into
  * @param required If false it is an error for this to not find a value.
@@ -178,13 +176,13 @@ void lighting_profile::reset()
 {
 	name = "";
 
-    tonemapper = tnm_Uncharted;
+	tonemapper = tnm_Uncharted;
 
 	ppc_values.toe_strength = 0.5f;
-    ppc_values.toe_length = 0.5f;
-    ppc_values.shoulder_strength = 0.0f;
-    ppc_values.shoulder_length = 0.5f;
-    ppc_values.shoulder_angle = 0.1f;
+	ppc_values.toe_length = 0.5f;
+	ppc_values.shoulder_strength = 0.0f;
+	ppc_values.shoulder_length = 0.5f;
+	ppc_values.shoulder_angle = 0.1f;
 
 	exposure = 4.0f;
 
@@ -200,7 +198,6 @@ void lighting_profile::reset()
 	beam_light_radius.reset();
 	beam_light_radius.base = 37.5f;
 
-
 	tube_light_brightness.reset();
 	tube_light_radius.reset();
 
@@ -214,46 +211,45 @@ void lighting_profile::reset()
 
 	directional_light_brightness.reset();
 
-
 	overall_brightness.reset();
-	//Default Multiplier to maintain legacy visual compatibility
-	overall_brightness.set_multiplier(3.5f); 
+	// Default Multiplier to maintain legacy visual compatibility
+	overall_brightness.set_multiplier(3.5f);
 	overall_brightness.set_minimum(0.0f);
 	overall_brightness.stack_multiplier(Cmdline_light_power);
 
 	ambient_light_brightness.reset();
-	//Inverse of the overall brightness boost
-	ambient_light_brightness.set_multiplier(0.286f); 
+	// Inverse of the overall brightness boost
+	ambient_light_brightness.set_multiplier(0.286f);
 	ambient_light_brightness.stack_multiplier(Cmdline_ambient_power);
-	ambient_light_brightness.set_adjust(MAX(0.0f,Cmdline_emissive_power));
+	ambient_light_brightness.set_adjust(MAX(0.0f, Cmdline_emissive_power));
 
 	cockpit_light_radius_modifier.reset();
 	cockpit_light_radius_modifier.set_multiplier(1.0f);
 }
 
-TonemapperAlgorithm lighting_profile::name_to_tonemapper(SCP_string &name)
+TonemapperAlgorithm lighting_profile::name_to_tonemapper(SCP_string& name)
 {
 	SCP_tolower(name);
 	TonemapperAlgorithm r = tnm_Invalid;
-	if(name == "uncharted" || name == "uncharted 2" ){
+	if (name == "uncharted" || name == "uncharted 2") {
 		r = tnm_Uncharted;
 	}
-	if(name == "linear"){
+	if (name == "linear") {
 		r = tnm_Linear;
 	}
-	if(name == "aces"){
+	if (name == "aces") {
 		r = tnm_Aces;
 	}
-	if(name == "aces approximate"){
+	if (name == "aces approximate") {
 		r = tnm_Aces_Approx;
 	}
-	if(name == "cineon"){
+	if (name == "cineon") {
 		r = tnm_Cineon;
 	}
-	if(name == "reinhard jodie"){
+	if (name == "reinhard jodie") {
 		r = tnm_Reinhard_Jodie;
 	}
-	if(name == "reinhard extended"){
+	if (name == "reinhard extended") {
 		r = tnm_Reinhard_Extended;
 	}
 	if (name == "ppc" || name == "piecewise power curve") {
@@ -293,12 +289,12 @@ SCP_string lighting_profile::tonemapper_to_name(TonemapperAlgorithm tnm)
 	}
 }
 
-
 lighting_profile lighting_profile::default_profile;
 
-lighting_profile * lighting_profile::current(){
-	//in time this may host more complex logic to allow such things as blending between two different profiles and managing a temporary profile.
-	//put in place now so that access can be generally future-proofed from here out.
+lighting_profile* lighting_profile::current()
+{
+	// in time this may host more complex logic to allow such things as blending between two different profiles and
+	// managing a temporary profile. put in place now so that access can be generally future-proofed from here out.
 	return &lighting_profile::default_profile;
 }
 
@@ -307,12 +303,12 @@ void lighting_profile::load_profiles()
 	parse_all();
 }
 
-//The logic for grabbing all the parseable files
+// The logic for grabbing all the parseable files
 
 void lighting_profile::parse_all()
 {
 	default_profile.reset();
-	if (cf_exists_full("lighting_profiles.tbl", CF_TYPE_TABLES)){
+	if (cf_exists_full("lighting_profiles.tbl", CF_TYPE_TABLES)) {
 		mprintf(("TABLES: Starting parse of lighting profiles.tbl\n"));
 		lighting_profile::parse_file("lighting_profiles.tbl");
 	}
@@ -321,114 +317,144 @@ void lighting_profile::parse_all()
 	parse_modular_table("*-ltp.tbm", lighting_profile::parse_file);
 }
 
-void lighting_profile::parse_default_section(const char *filename)
+void lighting_profile::parse_default_section(const char* filename)
 {
 	bool parsed;
 	SCP_string buffer;
 	TonemapperAlgorithm tn;
 	const char* profile_name = "Default Profile";
-	while(!optional_string("#END DEFAULT PROFILE")){
+	while (!optional_string("#END DEFAULT PROFILE")) {
 		parsed = false;
-		if(optional_string("$Tonemapper:")){
-			stuff_string(buffer,F_NAME);
+		if (optional_string("$Tonemapper:")) {
+			stuff_string(buffer, F_NAME);
 			tn = lighting_profile::name_to_tonemapper(buffer);
 			default_profile.tonemapper = tn;
 			parsed = true;
 		}
-		parsed |= parse_optional_float_into("$PPC Toe Strength:",&default_profile.ppc_values.toe_strength);
-		parsed |= parse_optional_float_into("$PPC Toe Length:",&default_profile.ppc_values.toe_length);
-		parsed |= parse_optional_float_into("$PPC Shoulder Length:",&default_profile.ppc_values.shoulder_length);
-		parsed |= parse_optional_float_into("$PPC Shoulder Strength:",&default_profile.ppc_values.shoulder_strength);
-		parsed |= parse_optional_float_into("$PPC Shoulder Angle:",&default_profile.ppc_values.shoulder_angle);
-		parsed |= parse_optional_float_into("$Exposure:",&default_profile.exposure);
+		parsed |= parse_optional_float_into("$PPC Toe Strength:", &default_profile.ppc_values.toe_strength);
+		parsed |= parse_optional_float_into("$PPC Toe Length:", &default_profile.ppc_values.toe_length);
+		parsed |= parse_optional_float_into("$PPC Shoulder Length:", &default_profile.ppc_values.shoulder_length);
+		parsed |= parse_optional_float_into("$PPC Shoulder Strength:", &default_profile.ppc_values.shoulder_strength);
+		parsed |= parse_optional_float_into("$PPC Shoulder Angle:", &default_profile.ppc_values.shoulder_angle);
+		parsed |= parse_optional_float_into("$Exposure:", &default_profile.exposure);
 
-		parsed |= lighting_profile_value::parse(filename,"$Missile light brightness:",profile_name,
-										&default_profile.missile_light_brightness);
-		parsed |= lighting_profile_value::parse(filename,"$Missile light radius:",profile_name,
-										&default_profile.missile_light_radius);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Missile light brightness:",
+			profile_name,
+			&default_profile.missile_light_brightness);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Missile light radius:",
+			profile_name,
+			&default_profile.missile_light_radius);
 
-		parsed |= lighting_profile_value::parse(filename,"$Laser light brightness:",profile_name,
-										&default_profile.laser_light_brightness);
-		parsed |= lighting_profile_value::parse(filename,"$Laser light radius:",profile_name,
-										&default_profile.laser_light_radius);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Laser light brightness:",
+			profile_name,
+			&default_profile.laser_light_brightness);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Laser light radius:",
+			profile_name,
+			&default_profile.laser_light_radius);
 
-		parsed |= lighting_profile_value::parse(filename,"$Beam light brightness:",profile_name,
-										&default_profile.beam_light_brightness);
-		parsed |= lighting_profile_value::parse(filename,"$Beam light radius:",profile_name,
-										&default_profile.beam_light_radius);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Beam light brightness:",
+			profile_name,
+			&default_profile.beam_light_brightness);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Beam light radius:",
+			profile_name,
+			&default_profile.beam_light_radius);
 
-		parsed |= lighting_profile_value::parse(filename,"$Tube light brightness:",profile_name,
-										&default_profile.tube_light_brightness);
-		parsed |= lighting_profile_value::parse(filename,"$Tube light radius:",profile_name,
-										&default_profile.tube_light_radius);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Tube light brightness:",
+			profile_name,
+			&default_profile.tube_light_brightness);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Tube light radius:",
+			profile_name,
+			&default_profile.tube_light_radius);
 
-		parsed |= lighting_profile_value::parse(filename,"$Point light brightness:",profile_name,
-										&default_profile.point_light_brightness);
-		parsed |= lighting_profile_value::parse(filename,"$Point light radius:",profile_name,
-										&default_profile.point_light_radius);
-		parsed |= lighting_profile_value::parse(filename,"$Directional light brightness:",profile_name,
-										&default_profile.directional_light_brightness);
-		parsed |= lighting_profile_value::parse(filename,"$Cone light radius:",profile_name,
-										&default_profile.cone_light_radius);
-		parsed |= lighting_profile_value::parse(filename,"$Cone light brightness:",profile_name,
-										&default_profile.cone_light_brightness);
-		if(lighting_profile_value::parse(filename,"$Ambient light brightness:",profile_name, &default_profile.ambient_light_brightness))
-		{
+		parsed |= lighting_profile_value::parse(filename,
+			"$Point light brightness:",
+			profile_name,
+			&default_profile.point_light_brightness);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Point light radius:",
+			profile_name,
+			&default_profile.point_light_radius);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Directional light brightness:",
+			profile_name,
+			&default_profile.directional_light_brightness);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Cone light radius:",
+			profile_name,
+			&default_profile.cone_light_radius);
+		parsed |= lighting_profile_value::parse(filename,
+			"$Cone light brightness:",
+			profile_name,
+			&default_profile.cone_light_brightness);
+		if (lighting_profile_value::parse(filename,
+				"$Ambient light brightness:",
+				profile_name,
+				&default_profile.ambient_light_brightness)) {
 			parsed = true;
 			default_profile.ambient_light_brightness.stack_multiplier(Cmdline_ambient_power);
 			default_profile.ambient_light_brightness.stack_multiplier(Cmdline_light_power);
 			default_profile.ambient_light_brightness.stack_minimum(Cmdline_emissive_power);
 		}
 
-		if(lighting_profile_value::parse(filename,"$Overall brightness:",profile_name, &default_profile.overall_brightness))
-		{
+		if (lighting_profile_value::parse(filename,
+				"$Overall brightness:",
+				profile_name,
+				&default_profile.overall_brightness)) {
 			parsed = true;
 			default_profile.overall_brightness.stack_multiplier(Cmdline_light_power);
 		}
 
-		parsed |= parse_optional_float_into("$Exposure:",&default_profile.exposure);
+		parsed |= parse_optional_float_into("$Exposure:", &default_profile.exposure);
 
-		parsed |= lighting_profile_value::parse(filename, "$Cockpit light radius modifier:", profile_name,
+		parsed |= lighting_profile_value::parse(filename,
+			"$Cockpit light radius modifier:",
+			profile_name,
 			&default_profile.cockpit_light_radius_modifier);
 
-		if(!parsed){
-			stuff_string(buffer,F_RAW);
-			Warning(LOCATION,"Unhandled line in lighting profile\n\t%s",buffer.c_str());
+		if (!parsed) {
+			stuff_string(buffer, F_RAW);
+			Warning(LOCATION, "Unhandled line in lighting profile\n\t%s", buffer.c_str());
 		}
 	}
 }
 
-//Handle an individual file.
-void lighting_profile::parse_file(const char *filename)
+// Handle an individual file.
+void lighting_profile::parse_file(const char* filename)
 {
-	try
-	{
-		if (filename == nullptr){
-			//All defaults currently handled by lighting_profile.reset()
+	try {
+		if (filename == nullptr) {
+			// All defaults currently handled by lighting_profile.reset()
 			return;
 		}
 		read_file_text(filename, CF_TYPE_TABLES);
 		reset_parse();
-		if(optional_string("#DEFAULT PROFILE")){
+		if (optional_string("#DEFAULT PROFILE")) {
 			parse_default_section(filename);
 		}
-    }	catch (const parse::ParseException& e)
-	{
-		mprintf(("TABLES: Unable to parse '%s'!  Error message = %s.\n", (filename) ? filename : "<default ai_profiles.tbl>", e.what()));
+	} catch (const parse::ParseException& e) {
+		mprintf(("TABLES: Unable to parse '%s'!  Error message = %s.\n",
+			(filename) ? filename : "<default ai_profiles.tbl>",
+			e.what()));
 		return;
 	}
 }
 
-
-//these accessor stubs are futureproofing abstraction
-
+// these accessor stubs are futureproofing abstraction
 
 TonemapperAlgorithm lighting_profile::current_tonemapper()
 {
 	return default_profile.tonemapper;
 }
 
-const piecewise_power_curve_values & lighting_profile::current_piecewise_values()
+const piecewise_power_curve_values& lighting_profile::current_piecewise_values()
 {
 	return default_profile.ppc_values;
 }
@@ -437,87 +463,96 @@ float lighting_profile::current_exposure()
 {
 	return default_profile.exposure;
 }
-piecewise_power_curve_intermediates lighting_profile::current_piecewise_intermediates(){
+piecewise_power_curve_intermediates lighting_profile::current_piecewise_intermediates()
+{
 	return calc_intermediates(default_profile.ppc_values);
 }
-piecewise_power_curve_intermediates lighting_profile::calc_intermediates(piecewise_power_curve_values input){
+piecewise_power_curve_intermediates lighting_profile::calc_intermediates(piecewise_power_curve_values input)
+{
 
 	piecewise_power_curve_intermediates ppci;
 
-	//Some safety clamping based on John Hable's implementation: https://github.com/johnhable/fw-public
+	// Some safety clamping based on John Hable's implementation: https://github.com/johnhable/fw-public
 	CLAMP(input.toe_length, 0.0f, 1.0f);
 	CLAMP(input.toe_strength, 0.0f, 1.0f);
 	CLAMP(input.shoulder_angle, 0.0f, 1.0f);
 	CLAMP(input.shoulder_length, 0.0f, 1.0f);
 	input.shoulder_strength = fmax(0.0f, input.shoulder_strength);
 
-	ppci.x0 = input.toe_length * 0.5f; //L,F,P
-	ppci.y0 = (1.0f - input.toe_strength) * ppci.x0; //L
-	float remainingY = 1.0f - ppci.y0; 
+	ppci.x0 = input.toe_length * 0.5f;               // L,F,P
+	ppci.y0 = (1.0f - input.toe_strength) * ppci.x0; // L
+	float remainingY = 1.0f - ppci.y0;
 	float initialW = ppci.x0 + remainingY;
 	float y1_offset = (1.0f - input.shoulder_length) * remainingY;
-	ppci.x1 = ppci.x0 + y1_offset; //F,P
+	ppci.x1 = ppci.x0 + y1_offset; // F,P
 	float y1 = ppci.y0 + y1_offset;
 	float extraW = exp2f(input.shoulder_length) - 1.0f;
 	float W = initialW + extraW;
-	float overshootX = (W * 2.0f) * input.shoulder_strength + (ppci.x0-ppci.y0);
+	float overshootX = (W * 2.0f) * input.shoulder_strength + (ppci.x0 - ppci.y0);
 	float overshootY = 0.5f * input.shoulder_angle;
 
-	ppci.toe_B = ppci.x0/ppci.y0; //T
-	ppci.toe_lnA = log(ppci.y0) - ppci.toe_B*log(ppci.x0);//T
+	ppci.toe_B = ppci.x0 / ppci.y0;                          // T
+	ppci.toe_lnA = log(ppci.y0) - ppci.toe_B * log(ppci.x0); // T
 
 	float sh_x0 = (1.0f + overshootX) - ppci.x1;
 	float sh_y0 = (1.0f + overshootY) - y1;
 
-	ppci.sh_B = sh_x0/sh_y0;//S
-	ppci.sh_lnA = log(sh_y0) - ppci.sh_B*log(sh_x0);//S
-	ppci.sh_offsetX = 1.0f + overshootX; //F,P,S
-	ppci.sh_offsetY = 1.0f + overshootY; //F,P,S
+	ppci.sh_B = sh_x0 / sh_y0;                         // S
+	ppci.sh_lnA = log(sh_y0) - ppci.sh_B * log(sh_x0); // S
+	ppci.sh_offsetX = 1.0f + overshootX;               // F,P,S
+	ppci.sh_offsetY = 1.0f + overshootY;               // F,P,S
 	return ppci;
-
 }
-void lighting_profile::lab_set_exposure(float exIn){
+void lighting_profile::lab_set_exposure(float exIn)
+{
 	default_profile.exposure = exIn;
 }
 
-
-void lighting_profile::lab_set_tonemapper(TonemapperAlgorithm tnin){
+void lighting_profile::lab_set_tonemapper(TonemapperAlgorithm tnin)
+{
 	default_profile.tonemapper = tnin;
 }
 
-void lighting_profile::lab_set_ppc(piecewise_power_curve_values ppcin ){
+void lighting_profile::lab_set_ppc(piecewise_power_curve_values ppcin)
+{
 	default_profile.ppc_values = ppcin;
-
 }
 
-piecewise_power_curve_values lighting_profile::lab_get_ppc(){
+piecewise_power_curve_values lighting_profile::lab_get_ppc()
+{
 	return default_profile.ppc_values;
 }
 
-float lighting_profile::lab_get_light(){
+float lighting_profile::lab_get_light()
+{
 	float r;
 	default_profile.overall_brightness.read_multiplier(&r);
 
 	return r;
 }
-float lighting_profile::lab_get_ambient(){
+float lighting_profile::lab_get_ambient()
+{
 	float r;
 	default_profile.ambient_light_brightness.read_multiplier(&r);
 
 	return r;
 }
-float lighting_profile::lab_get_emissive(){
+float lighting_profile::lab_get_emissive()
+{
 	float r;
 	default_profile.ambient_light_brightness.read_adjust(&r);
 
 	return r;
 }
-void lighting_profile::lab_set_light(float in){
+void lighting_profile::lab_set_light(float in)
+{
 	default_profile.overall_brightness.set_multiplier(in);
 }
-void lighting_profile::lab_set_ambient(float in){
+void lighting_profile::lab_set_ambient(float in)
+{
 	default_profile.ambient_light_brightness.set_multiplier(in);
 }
-void lighting_profile::lab_set_emissive(float in){
-	default_profile.ambient_light_brightness.set_adjust(MAX(0.0f,in));
+void lighting_profile::lab_set_emissive(float in)
+{
+	default_profile.ambient_light_brightness.set_adjust(MAX(0.0f, in));
 }
--- code/lighting/lighting_profiles.h
+++ code/lighting/lighting_profiles.h
@@ -1,15 +1,18 @@
+// clang-format on
 #pragma once
 
 #include "globalincs/vmallocator.h"
 
-//This file handles the lighting_profiles.tbl and -ltp.tbm files. The purpose of these files is to provide control over the HDR lighting pipeline and enviroment, and related matters.
+// This file handles the lighting_profiles.tbl and -ltp.tbm files. The purpose of these files is to provide control over
+// the HDR lighting pipeline and enviroment, and related matters.
 
-//Tonemapping options, aside from the previous standard UC2, pulled from wookiejedi and qazwsxal's
-//work on testing them for FSO, which itself was based on these references:
-//https://64.github.io/tonemapping/ Delta - Blog by 64: Tone Mapping
-//http://filmicworlds.com/blog/filmic-tonemapping-operators/ Filmic Worlds: Filmic Tonemapping Operators by John Hable
+// Tonemapping options, aside from the previous standard UC2, pulled from wookiejedi and qazwsxal's
+// work on testing them for FSO, which itself was based on these references:
+// https://64.github.io/tonemapping/ Delta - Blog by 64: Tone Mapping
+// http://filmicworlds.com/blog/filmic-tonemapping-operators/ Filmic Worlds: Filmic Tonemapping Operators by John Hable
 
-enum TonemapperAlgorithm :int {
+enum TonemapperAlgorithm : int
+{
 	tnm_Invalid = -1,
 	tnm_Linear = 0,
 	tnm_Uncharted = 1,
@@ -19,10 +22,10 @@ enum TonemapperAlgorithm :int {
 	tnm_Reinhard_Jodie = 5,
 	tnm_Reinhard_Extended = 6,
 	tnm_PPC = 7,
-	tnm_PPC_RGB= 8
+	tnm_PPC_RGB = 8
 };
 
-struct piecewise_power_curve_values{
+struct piecewise_power_curve_values {
 	float toe_strength;
 	float toe_length;
 	float shoulder_strength;
@@ -30,7 +33,7 @@ struct piecewise_power_curve_values{
 	float shoulder_angle;
 };
 
-struct piecewise_power_curve_intermediates{
+struct piecewise_power_curve_intermediates {
 	float x0;
 	float y0;
 	float x1;
@@ -45,7 +48,7 @@ struct piecewise_power_curve_intermediates{
 /**
  * @brief To hold configuration of, and handle application of, modifiers to lighting values.
  */
-struct lighting_profile_value{
+struct lighting_profile_value {
 	float base;
 	bool only_positive = true;
 
@@ -58,14 +61,18 @@ struct lighting_profile_value{
 	void set_minimum(const float in);
 	void stack_minimum(const float in);
 
-	static bool parse(const char *filename, const char * valuename, const SCP_string &profile_name, lighting_profile_value* value_target, bool required=false);
-	bool read_multiplier(float *out) const;
-	bool read_adjust(float *out) const;
+	static bool parse(const char* filename,
+		const char* valuename,
+		const SCP_string& profile_name,
+		lighting_profile_value* value_target,
+		bool required = false);
+	bool read_multiplier(float* out) const;
+	bool read_adjust(float* out) const;
 
-private:
+  private:
 	bool has_adjust = false;
 	float adjust;
-	bool has_multiplier=false;
+	bool has_multiplier = false;
 	float multipier;
 	bool has_minimum = false;
 	float minimum;
@@ -73,20 +80,20 @@ private:
 	float maximum;
 };
 
-class lighting_profile{
-public:
-	static lighting_profile * current();
-	static enum TonemapperAlgorithm name_to_tonemapper(SCP_string &name);
+class lighting_profile {
+  public:
+	static lighting_profile* current();
+	static enum TonemapperAlgorithm name_to_tonemapper(SCP_string& name);
 	static SCP_string tonemapper_to_name(TonemapperAlgorithm tnm);
 	static void load_profiles();
 	static TonemapperAlgorithm current_tonemapper();
-	static const piecewise_power_curve_values & current_piecewise_values();
+	static const piecewise_power_curve_values& current_piecewise_values();
 	static piecewise_power_curve_intermediates current_piecewise_intermediates();
 	static piecewise_power_curve_intermediates calc_intermediates(piecewise_power_curve_values input);
 	static float current_exposure();
 	static void lab_set_exposure(float exIn);
 	static void lab_set_tonemapper(TonemapperAlgorithm tnin);
-	static void lab_set_ppc(piecewise_power_curve_values ppcin );
+	static void lab_set_ppc(piecewise_power_curve_values ppcin);
 	static piecewise_power_curve_values lab_get_ppc();
 	static float lab_get_light();
 	static void lab_set_light(float in);
@@ -96,7 +103,7 @@ public:
 	static void lab_set_emissive(float in);
 
 	SCP_string name;
-    TonemapperAlgorithm tonemapper;
+	TonemapperAlgorithm tonemapper;
 	piecewise_power_curve_values ppc_values;
 	float exposure;
 	lighting_profile_value missile_light_brightness;
@@ -114,16 +121,15 @@ public:
 	lighting_profile_value cone_light_radius;
 	lighting_profile_value directional_light_brightness;
 	lighting_profile_value ambient_light_brightness;
-	//Strictly speaking this should be handled by postproc but we need something for the non-postproc people.
+	// Strictly speaking this should be handled by postproc but we need something for the non-postproc people.
 	lighting_profile_value overall_brightness;
 	lighting_profile_value cockpit_light_radius_modifier;
 
-    void reset();
+	void reset();
 
-private:
-
+  private:
 	static lighting_profile default_profile;
 	static void parse_all();
-	static void parse_file(const char *filename);
-	static void parse_default_section(const char *filename);
+	static void parse_file(const char* filename);
+	static void parse_default_section(const char* filename);
 };
