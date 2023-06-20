f94e1b2b83a939817ac87a1748d89da8ee18ff90
Birk Magnussen <6238428+BMagnu@users.noreply.github.com>
2023-06-20 18:41:15 +0200

    Decouple MSAA from deferred end

--- code/graphics/opengl/gropengldeferred.cpp
+++ code/graphics/opengl/gropengldeferred.cpp
@@ -99,6 +99,63 @@ void gr_opengl_deferred_lighting_begin(bool clearNonColorBufs)
 	}
 }
 
+void gr_opengl_deferred_lighting_msaa()
+{
+	if (!Deferred_lighting)
+		return;
+
+	if (Cmdline_msaa_enabled <= 0)
+		return;
+	
+	GR_DEBUG_SCOPE("MSAA Pass");
+	GL_state.BindFrameBuffer(Scene_framebuffer);
+
+	GLenum buffers[] = {GL_COLOR_ATTACHMENT0,
+		GL_COLOR_ATTACHMENT1,
+		GL_COLOR_ATTACHMENT2,
+		GL_COLOR_ATTACHMENT3,
+		GL_COLOR_ATTACHMENT4};
+	glDrawBuffers(5, buffers);
+
+	int msaa_resolve_flags = 0;
+	switch (Cmdline_msaa_enabled) {
+	case 4:
+		msaa_resolve_flags = SDR_FLAG_MSAA_SAMPLES_4;
+		break;
+	case 8:
+		msaa_resolve_flags = SDR_FLAG_MSAA_SAMPLES_8;
+		break;
+	case 16:
+		msaa_resolve_flags = SDR_FLAG_MSAA_SAMPLES_16;
+		break;
+	default:
+		UNREACHABLE("Disallowed MSAA shader sample count!");
+		break;
+	}
+
+	opengl_shader_set_current(gr_opengl_maybe_create_shader(SDR_TYPE_MSAA_RESOLVE, msaa_resolve_flags));
+	GL_state.Texture.Enable(0, GL_TEXTURE_2D_MULTISAMPLE, Scene_color_texture_ms);
+	GL_state.Texture.Enable(1, GL_TEXTURE_2D_MULTISAMPLE, Scene_position_texture_ms);
+	GL_state.Texture.Enable(2, GL_TEXTURE_2D_MULTISAMPLE, Scene_normal_texture_ms);
+	GL_state.Texture.Enable(3, GL_TEXTURE_2D_MULTISAMPLE, Scene_specular_texture_ms);
+	GL_state.Texture.Enable(4, GL_TEXTURE_2D_MULTISAMPLE, Scene_emissive_texture_ms);
+	GL_state.Texture.Enable(5, GL_TEXTURE_2D_MULTISAMPLE, Scene_depth_texture_ms);
+	Current_shader->program->Uniforms.setTextureUniform("texColor", 0);
+	Current_shader->program->Uniforms.setTextureUniform("texPos", 1);
+	Current_shader->program->Uniforms.setTextureUniform("texNormal", 2);
+	Current_shader->program->Uniforms.setTextureUniform("texSpecular", 3);
+	Current_shader->program->Uniforms.setTextureUniform("texEmissive", 4);
+	Current_shader->program->Uniforms.setTextureUniform("texDepth", 5);
+	opengl_set_generic_uniform_data<graphics::generic_data::msaa_data>(
+		[&](graphics::generic_data::msaa_data* data) {
+			data->samples = Cmdline_msaa_enabled;
+			data->fov = Proj_fov;
+		});
+	GL_state.SetAlphaBlendMode(gr_alpha_blend::ALPHA_BLEND_NONE);
+	GL_state.SetZbufferType(ZBUFFER_TYPE_WRITE);
+	opengl_draw_full_screen_textured(0, 0, 1, 1);
+}
+
 void gr_opengl_deferred_lighting_end()
 {
 	if(!Deferred_lighting)
@@ -106,57 +163,7 @@ void gr_opengl_deferred_lighting_end()
 
 	GR_DEBUG_SCOPE("Deferred lighting end");
 
-	if (Cmdline_msaa_enabled > 0) {
-		GR_DEBUG_SCOPE("MSAA Pass");
-		GL_state.BindFrameBuffer(Scene_framebuffer);
-
-		GLenum buffers[] = {GL_COLOR_ATTACHMENT0,
-			GL_COLOR_ATTACHMENT1,
-			GL_COLOR_ATTACHMENT2,
-			GL_COLOR_ATTACHMENT3,
-			GL_COLOR_ATTACHMENT4};
-		glDrawBuffers(5, buffers);
-
-		int msaa_resolve_flags = 0;
-		switch (Cmdline_msaa_enabled) {
-		case 4:
-			msaa_resolve_flags = SDR_FLAG_MSAA_SAMPLES_4;
-			break;
-		case 8:
-			msaa_resolve_flags = SDR_FLAG_MSAA_SAMPLES_8;
-			break;
-		case 16:
-			msaa_resolve_flags = SDR_FLAG_MSAA_SAMPLES_16;
-			break;
-		default:
-			UNREACHABLE("Disallowed MSAA shader sample count!");
-			break;
-		}
-
-		opengl_shader_set_current(gr_opengl_maybe_create_shader(SDR_TYPE_MSAA_RESOLVE, msaa_resolve_flags));
-		GL_state.Texture.Enable(0, GL_TEXTURE_2D_MULTISAMPLE, Scene_color_texture_ms);
-		GL_state.Texture.Enable(1, GL_TEXTURE_2D_MULTISAMPLE, Scene_position_texture_ms);
-		GL_state.Texture.Enable(2, GL_TEXTURE_2D_MULTISAMPLE, Scene_normal_texture_ms);
-		GL_state.Texture.Enable(3, GL_TEXTURE_2D_MULTISAMPLE, Scene_specular_texture_ms);
-		GL_state.Texture.Enable(4, GL_TEXTURE_2D_MULTISAMPLE, Scene_emissive_texture_ms);
-		GL_state.Texture.Enable(5, GL_TEXTURE_2D_MULTISAMPLE, Scene_depth_texture_ms);
-		Current_shader->program->Uniforms.setTextureUniform("texColor", 0);
-		Current_shader->program->Uniforms.setTextureUniform("texPos", 1);
-		Current_shader->program->Uniforms.setTextureUniform("texNormal", 2);
-		Current_shader->program->Uniforms.setTextureUniform("texSpecular", 3);
-		Current_shader->program->Uniforms.setTextureUniform("texEmissive", 4);
-		Current_shader->program->Uniforms.setTextureUniform("texDepth", 5);
-		opengl_set_generic_uniform_data<graphics::generic_data::msaa_data>([&](graphics::generic_data::msaa_data* data) {
-			data->samples = Cmdline_msaa_enabled;
-			data->fov = Proj_fov;
-		});
-		GL_state.SetAlphaBlendMode(gr_alpha_blend::ALPHA_BLEND_NONE);
-		GL_state.SetZbufferType(ZBUFFER_TYPE_WRITE);
-		opengl_draw_full_screen_textured(0, 0, 1, 1);
-	}
-
 	Deferred_lighting = false;
-
 
 	glDrawBuffer(GL_COLOR_ATTACHMENT0);
 
