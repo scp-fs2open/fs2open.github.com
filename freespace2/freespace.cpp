1ce9d468224248b01f8ca4cc6c9763a046d9c84d
mjn-mixael <mjn.other@gmail.com>
2023-10-04 09:41:27 -0500

    Intro about to play hook

--- freespace2/freespace.cpp
+++ freespace2/freespace.cpp
@@ -6653,6 +6653,10 @@ int game_main(int argc, char *argv[])
 		return 0;
 	}
 
+	if (scripting::hooks::OnIntroAboutToPlay->isActive()) {
+		scripting::hooks::OnIntroAboutToPlay->run();
+	}
+
 	if (!Is_standalone) {
 		movie::play("intro.mve");
 	}
