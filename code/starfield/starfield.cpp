e53137473d3c72cb0b35f567feb13ef925d6bd6d
John Fernandez <jafernandez.tampabay@gmail.com>
2023-11-03 22:34:39 -0400

    Move this declaration
    So that linters and coverity don't complain about the initial value being overwritten.
    
    Coverity 1320531

--- code/starfield/starfield.cpp
+++ code/starfield/starfield.cpp
@@ -1762,7 +1762,6 @@ void stars_draw_stars()
 
 	int i;
 	star *sp;
-	float dist = 0.0f;
 	float ratio;
 	vDist vDst;
 	vertex p1, p2;
@@ -1813,7 +1812,7 @@ void stars_draw_stars()
 		}
 
 		if ( can_draw && (Star_flags & (STAR_FLAG_TAIL|STAR_FLAG_DIM)) ) {
-			dist = vm_vec_dist_quick( &sp->last_star_pos, &p2.world );
+			float dist = vm_vec_dist_quick( &sp->last_star_pos, &p2.world );
 
 			if ( dist > Star_max_length ) {
  				ratio = Star_max_length / dist;
