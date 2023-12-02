e61aa1a59dfd6caeaf7209b64898d8265ad3e704
Taylor Richards <taylor@notimaginative.com>
2023-12-01 20:34:56 -0500

    update version hash

--- lib/prebuilt.cmake
+++ lib/prebuilt.cmake
@@ -1,5 +1,5 @@
 
-set(PREBUILT_VERSION_NAME "8a54f1d")
+set(PREBUILT_VERSION_NAME "f81df85")
 
 set(FSO_PREBUILT_OVERRIDE "" CACHE PATH "Path to the prebuilt binaries, if empty the binaries will be downloaded.")
 set(PREBUILT_LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/prebuilt")
