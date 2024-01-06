7c15adec1ab8014376d6c3590c9f197fe7a3a0df
John Fernandez <jafernandez.tampabay@gmail.com>
2024-01-06 18:38:12 -0500

    Finish Loop for saving variables

--- qtfred/src/mission/dialogs/VariableDialogModel.cpp
+++ qtfred/src/mission/dialogs/VariableDialogModel.cpp
@@ -19,8 +19,17 @@ void VariableDialogModel::apply()
 {
     memset(Sexp_variables, 0, MAX_SEXP_VARIABLES * size_of(sexp_variable));
 
-    for (const auto& variable : _variableItems){
+    for (int i = 0; i < static_cast<int>(_variableItems.size()); ++i){
+        Sexp_variables[i].type = _variableItems[i].flags;
+        strcpy_s(Sexp_variables[i].variable_name, _variableItems[i].name.c_str());
         
+        if (_variableItems[i].flags & SEXP_VARIABLE_STRING){
+            strcpy_s(Sexp_variables[i].text, _variableItems[i].stringValue);
+        } else {
+            strcpy_s(Sexp_variables[i].text, std::to_string(_variableItems[i].numberValue).c_str())
+        }
     }
+
+    
 
 }
