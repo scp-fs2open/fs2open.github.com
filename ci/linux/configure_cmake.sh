0da34f0bc84cb38b55f627f9400361b1b0321392
Taylor Richards <taylor@notimaginative.com>
2024-01-29 14:57:38 -0500

    switch to new linux build image
    Upgrades to Ubuntu 20.04 to fix Node v20 problem.
    Bumps compiler to gcc-9.
    
    NOTE: This breaks support for Linux distros older than Ubuntu 20.04

--- ci/linux/configure_cmake.sh
+++ ci/linux/configure_cmake.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$COMPILER" = "gcc-5" ]; then
-    export CC=gcc-5
+if [ "$COMPILER" = "gcc-9" ]; then
+    export CC=gcc-9
     export CXX=g++-5
 fi
 if [ "$COMPILER" = "gcc-13" ]; then
