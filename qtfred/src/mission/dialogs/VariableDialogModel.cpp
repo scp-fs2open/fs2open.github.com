64b54d1022940631fc90ab099b7ec5032d9d7151
John Fernandez <jafernandez.tampabay@gmail.com>
2024-04-01 17:58:07 -0400

    Start writing apply and reject methods

--- qtfred/src/mission/dialogs/VariableDialogModel.cpp
+++ qtfred/src/mission/dialogs/VariableDialogModel.cpp
@@ -1,7 +1,26 @@
 #include "VariableDialogModel.h"
+#include "parse/sexp.h"
+#include "parse/sexp_container.h"
+
 
 VariableDialogModel::VariableDialogModel(QObject* parent, EditorViewport* viewport) 
 		: AbstractDialogModel(parent, viewport)
 {
 		initializeData();
+}
+
+void VariableDialogModel::reject() 
+{
+    _variableItems.clear();
+    _containerItems.clear();
+}
+
+void VariableDialogModel::apply() 
+{
+    memset(Sexp_variables, 0, MAX_SEXP_VARIABLES * size_of(sexp_variable));
+
+    for (const auto& variable : _variableItems){
+        
+    }
+
 }
