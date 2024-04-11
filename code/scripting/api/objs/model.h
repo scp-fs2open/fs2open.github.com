0080a9d141be8f3a9e075723436d7ae6b9a210af
Goober5000 <ipw47@mailbox.org>
2024-04-11 15:15:26 -0400

    add API for glow point banks
    Previously, a model API existed for glow points mounted on thrusters, but not glow points mounted on glow point banks.  This adds glow point bank support.  Also, this refactors and cleans up the implementation of the model API a bit.

--- code/scripting/api/objs/model.h
+++ code/scripting/api/objs/model.h
@@ -58,11 +58,11 @@ DECLARE_ADE_OBJ(l_ModelThrusters, model_h);
 // Thrusterbank:
 struct thrusterbank_h
 {
-	thruster_bank *bank;
+	model_h modelh;
+	int thrusterbank_index;
 
 	thrusterbank_h();
-
-	thrusterbank_h(thruster_bank* ba);
+	thrusterbank_h(int in_model_num, int in_thrusterbank_index);
 
 	thruster_bank *Get() const;
 
@@ -70,19 +70,37 @@ struct thrusterbank_h
 };
 DECLARE_ADE_OBJ(l_Thrusterbank, thrusterbank_h);
 
+// Glow points:
+DECLARE_ADE_OBJ(l_ModelGlowpointbanks, model_h);
+
+// Glowpointbank:
+struct glowpointbank_h
+{
+	model_h modelh;
+	int glowpointbank_index;
+
+	glowpointbank_h();
+	glowpointbank_h(int in_model_num, int in_glowpointbank_index);
+
+	glow_point_bank *Get() const;
+
+	bool isValid() const;
+};
+DECLARE_ADE_OBJ(l_Glowpointbank, glowpointbank_h);
+
 // Glowpoint:
 struct glowpoint_h
 {
-	glow_point *point;
+	glowpointbank_h glowpointbankh;
+	thrusterbank_h thrusterbankh;
+	int glowpoint_index;
 
 	glowpoint_h();
-
-	glowpoint_h(glow_point* np);
+	glowpoint_h(int in_model_num, int in_glowpointbank_index, int in_thrusterbank_index, int in_glowpoint_index);
 
 	glow_point* Get() const;
 
 	bool isValid() const;
-
 };
 DECLARE_ADE_OBJ(l_Glowpoint, glowpoint_h);
 
@@ -99,10 +117,9 @@ class dockingbay_h
 	dockingbay_h(polymodel *pm, int dock_idx);
 	dockingbay_h();
 
-	bool isValid() const;
-
-	model_h* getModelH() const;
 	dock_bay* getDockingBay() const;
+
+	bool isValid() const;
 };
 DECLARE_ADE_OBJ(l_Dockingbay, dockingbay_h);
 
