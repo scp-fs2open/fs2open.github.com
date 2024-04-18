4c34566f60c53c58838c83f1fca1c123abe24f7a
Asteroth <sshyrda@gmail.com>
2024-04-18 16:38:26 -0400

    switch to per frame management

--- code/scripting/api/objs/ship_bank.cpp
+++ code/scripting/api/objs/ship_bank.cpp
@@ -399,18 +399,14 @@ ADE_VIRTVAR(FOFCooldown, l_WeaponBank, "number", "The FOF cooldown value. A valu
 	{
 		case SWH_PRIMARY:
 		{
-			auto wif = &Weapon_info[bh->typeh.sw->primary_bank_weapons[bh->bank]];
-			float reset_amount = (timestamp_until(bh->typeh.sw->last_primary_fire_stamp[bh->bank]) / 1000.0f) * wif->fof_reset_rate;
-			auto val = bh->typeh.sw->primary_bank_fof_cooldown[bh->bank] + reset_amount;
-			CLAMP(val, 0.0f, 1.0f);
+			auto val = bh->typeh.sw->primary_bank_fof_cooldown[bh->bank];
 
 			if (ADE_SETTING_VAR) {
 				CLAMP(newValue, 0.0f, 1.0f);
 				bh->typeh.sw->primary_bank_fof_cooldown[bh->bank] = newValue;
-				val = newValue;
 			}
 
-			return ade_set_args(L, "f", val);
+			return ade_set_args(L, "f", bh->typeh.sw->primary_bank_fof_cooldown[bh->bank]);
 		}
 		case SWH_SECONDARY:
 		case SWH_TERTIARY:
