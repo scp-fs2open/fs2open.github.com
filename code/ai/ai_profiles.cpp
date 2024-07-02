44e1f2ea0641d947b95d8d8231e654ee807214e7
Kestrellius <63537900+Kestrellius@users.noreply.github.com>
2024-07-02 13:46:38 -0700

    Adds flags.

--- code/ai/ai_profiles.cpp
+++ code/ai/ai_profiles.cpp
@@ -184,6 +184,18 @@ void parse_ai_profiles_tbl(const char *filename)
 				if (optional_string("$Max Beam Friendly Fire Damage:"))
 					parse_float_list(profile->beam_friendly_damage_cap, NUM_SKILL_LEVELS);
 
+				if (optional_string("$Max Weapon Friendly Fire Damage:")) {
+					std::array<float, NUM_SKILL_LEVELS> temp;
+					parse_float_list(temp.data(), NUM_SKILL_LEVELS);
+					profile->weapon_friendly_damage_cap.emplace(std::move(temp));
+				}
+					
+				if (optional_string("$Max Weapon Self Damage:")) {
+					std::array<float, NUM_SKILL_LEVELS> temp;
+					parse_float_list(temp.data(), NUM_SKILL_LEVELS);
+					profile->weapon_self_damage_cap.emplace(std::move(temp));
+				}
+
 				if (optional_string("$Player Countermeasure Life Scale:"))
 					parse_float_list(profile->cmeasure_life_scale, NUM_SKILL_LEVELS);
 
@@ -762,7 +774,9 @@ void ai_profile_t::reset()
 
         subsys_damage_scale[i] = 0;
         beam_friendly_damage_cap[i] = 0;
-        turn_time_scale[i] = 0;
+		weapon_friendly_damage_cap[i] = tl::nullopt;
+        weapon_self_damage_cap[i] = tl::nullopt;
+		turn_time_scale[i] = 0;
         glide_attack_percent[i] = 0;
         circle_strafe_percent[i] = 0;
         glide_strafe_percent[i] = 0;
