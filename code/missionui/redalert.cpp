40e8b6b5e108bdc65dcc35de94c6baf7792e41d1
JohnAFernandez <jafernandez.tampabay@gmail.com>
2024-07-21 00:32:33 -0400

    Add Debug warning to modders

--- code/missionui/redalert.cpp
+++ code/missionui/redalert.cpp
@@ -938,7 +938,9 @@ void red_alert_bash_ship_status()
 				wingp->current_wave = rws->latest_wave;
 				
 				// make sure that num_waves can accomodate having extra wings.
+				// this would happen if the mission we are entering has too few waves.
 				if (wingp->num_waves < wingp->current_wave){
+					mprintf(("Red alert warning: make sure that the current mission has at least as many waves as exited last mission. Bashing the number of waves of wing %s to %i!\n", wingp->name, wingp->current_wave));					
 					wingp->num_waves = wingp->current_wave;
 				}
 
