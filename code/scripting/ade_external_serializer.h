7caef527d4c4218a52c6a82c4de333bc58a2266f
Birk Magnussen <6238428+BMagnu@users.noreply.github.com>
2024-07-28 11:15:02 +0200

    Slightly clean up RPC API

---
+++ code/scripting/ade_external_serializer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace scripting {
+	namespace internal {
+		template <typename T>
+		struct ade_serializable_external : std::false_type { };
+
+		/*
+		 * If you need a multi serializer that shouldn't be part of the struct (i.e. because the struct isn't just a lua handler, but also used elsewhere),
+		 * then you can declare and external serializer here and implement it in the cpp of the respective scripting object
+		 * */
+
+		template<>
+		struct ade_serializable_external<vec3d> : std::true_type {
+			static void serialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, const luacpp::LuaValue& value, ubyte* data, int& packet_size);
+			static void deserialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, char* data_ptr, ubyte* data, int& offset);
+		};
+
+		template<>
+		struct ade_serializable_external<object_ship_wing_point_team> : std::true_type {
+			static void serialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, const luacpp::LuaValue& value, ubyte* data, int& packet_size);
+			static void deserialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, char* data_ptr, ubyte* data, int& offset);
+		};
+
+		template<>
+		struct ade_serializable_external<object_h> : std::true_type {
+			static void serialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, const luacpp::LuaValue& value, ubyte* data, int& packet_size);
+			static void deserialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, char* data_ptr, ubyte* data, int& offset);
+		};
+	}
+}
