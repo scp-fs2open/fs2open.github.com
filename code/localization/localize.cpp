6ed64e796dfcfaed8cde0c1035949428d0bdf81a
Goober5000 <ipw47@mailbox.org>
2024-09-30 15:41:59 -0400

    make malformed XSTR a release warning
    1. Tweak the API of `error_display` so that an error level of 2 displays a `ReleaseWarning`.
    2. Use this error level for the malformed XSTR warnings in the localization code.
    
    Resolves #6374.

--- code/localization/localize.cpp
+++ code/localization/localize.cpp
@@ -1040,9 +1040,9 @@ bool lcl_ext_localize_sub(const char *in, char *text_str, char *out, size_t max_
 	if (id != nullptr)
 		*id = xstr_id;
 
-	// if we made an attempt but failed, let the modder know
+	// if we made an attempt but failed, let the modder know (using a ReleaseWarning)
 	if (xstr_id == -2 && attempted_xstr)
-		error_display(0, "Malformed XSTR detected:\n\n%s\n", in);
+		error_display(2, "Malformed XSTR detected:\n\n%s\n", in);
 
 	// copy the entire string (or as much as we can)
 	auto str_len = strlen(xstr_str);
@@ -1202,9 +1202,9 @@ bool lcl_ext_localize_sub(const SCP_string &in, SCP_string &text_str, SCP_string
 	if (id != nullptr)
 		*id = xstr_id;
 
-	// if we made an attempt but failed, let the modder know
+	// if we made an attempt but failed, let the modder know (using a ReleaseWarning)
 	if (xstr_id == -2 && attempted_xstr)
-		error_display(0, "Malformed XSTR detected:\n\n%s\n", in.c_str());
+		error_display(2, "Malformed XSTR detected:\n\n%s\n", in.c_str());
 
 	// copy the entire string
 	out = xstr_str;
