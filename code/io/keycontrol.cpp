0eae457a57a6ae4ae7de7e5a20fc22ebc3ef0ab3
Kestrellius <63537900+Kestrellius@users.noreply.github.com>
2024-12-04 17:12:01 -0800

    Returns extern do_subobj_hit_stuff

--- code/io/keycontrol.cpp
+++ code/io/keycontrol.cpp
@@ -221,6 +221,8 @@ int Tool_enabled = 0;
 extern int AI_watch_object;
 extern int Countermeasures_enabled;
 
+extern float do_subobj_hit_stuff(object *ship_obj, object *other_obj, vec3d *hitpos, int submodel_num, float damage, bool *hull_should_apply_armor);
+
 extern void mission_goal_mark_all_true( int type );
 
 int Normal_key_set[] = {
