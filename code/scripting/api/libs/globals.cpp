585815b4fea0b44a2654a1394b2b6c624d43651c
Mike Nelson <mjn.other@gmail.com>
2024-12-18 10:43:03 -0600

    add global variables library

---
+++ code/scripting/api/libs/globals.cpp
@@ -0,0 +1,72 @@
+//
+//
+
+#include "globalincs/version.h"
+
+#include "globals.h"
+
+#include "missionui/missionscreencommon.h"
+#include "playerman/managepilot.h"
+
+namespace scripting {
+namespace api {
+
+//**********LIBRARY: Globals
+ADE_LIB(l_GlobalVariables, "GlobalVariables", "gl", "Library of global values");
+
+ADE_VIRTVAR(MAX_PILOTS,
+	l_GlobalVariables,
+	nullptr,
+	"Gets the maximum number of possible pilots.",
+	"number",
+            "The maximum number of pilots")
+{
+	return ade_set_args(L, "i", MAX_PILOTS);
+}
+
+ADE_VIRTVAR(MAX_WING_SLOTS,
+	l_GlobalVariables,
+	nullptr,
+	"The maximum ships that can be in a loadout wing",
+	"number",
+	"A maximum number of ships")
+{
+
+	return ade_set_args(L, "i", MAX_WING_SLOTS);
+}
+
+ADE_VIRTVAR(MAX_WING_BLOCKS,
+	l_GlobalVariables,
+	nullptr,
+	"The maximum wings that can be in a loadout",
+	"number",
+	"A maximum number of wings")
+{
+
+	return ade_set_args(L, "i", MAX_WING_BLOCKS);
+}
+
+ADE_VIRTVAR(MAX_SHIP_PRIMARY_BANKS,
+	l_GlobalVariables,
+	nullptr,
+	"The maximum primary banks a ship can have",
+	"number",
+	"A maximum number of primary banks")
+{
+
+	return ade_set_args(L, "i", MAX_SHIP_PRIMARY_BANKS);
+}
+
+ADE_VIRTVAR(MAX_SHIP_SECONDARY_BANKS,
+	l_GlobalVariables,
+	nullptr,
+	"The maximum secondary banks a ship can have",
+	"number",
+	"A maximum number of secondary banks")
+{
+
+	return ade_set_args(L, "i", MAX_SHIP_SECONDARY_BANKS);
+}
+
+} // namespace api
+} // namespace scripting
