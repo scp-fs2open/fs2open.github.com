cb6356620fc2acdafa5bd5a381e8984570883c0d
mjn-mixael <mjn.other@gmail.com>
2024-12-31 08:53:12 -0600

    lua set mainhall

--- code/scripting/api/libs/ui.cpp
+++ code/scripting/api/libs/ui.cpp
@@ -487,6 +487,32 @@ ADE_FUNC(toggleHelp,
 	return ADE_RETURN_NIL;
 }
 
+ADE_FUNC(setMainhall, l_UserInterface_MainHall, "string mainhall, bool enforce", "The name of the mainhall to try to set. Will immediately change if the player is currently in the mainhall menu. "
+	"Use enforce to set this as the mainhall on next mainhall load if setting from outside the mainhall menu. NOTE: If enforce is true then the player will always return back to this mainhall forever. "
+	"Call this with a blank string and enforce false to unset enforce without changing the current mainhall that is loaded.", nullptr, "nothing")
+{
+	const char* name = nullptr;
+	bool enforce = false;
+	if (!ade_get_args(L, "s|b", &name, &enforce)) {
+		return ADE_RETURN_NIL;
+	}
+
+	if (enforce) {
+		Enforced_main_hall = name;
+	} else {
+		Enforced_main_hall.clear();
+	}
+
+	if (gameseq_get_state() == GS_STATE_MAIN_MENU) {
+		if (name[0] != '\0') {
+			main_hall_close();
+			main_hall_init(name);
+		}
+	}
+
+	return ADE_RETURN_NIL;
+}
+
 //**********SUBLIBRARY: UserInterface/Barracks
 ADE_LIB_DERIV(l_UserInterface_Barracks, "Barracks", nullptr,
               "API for accessing values specific to the Barracks UI.",
