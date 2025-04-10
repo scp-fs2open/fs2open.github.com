77cbee7bcb6d13d95999cd526aff249221b0e9b1
Goober5000 <ipw47@mailbox.org>
2025-04-10 19:57:06 -0400

    fix bogus direction vector in particle code
    This was discovered thanks to the new assertions from #6596.  For particles with 0 velocity, the code would attempt to normalize a 0,0,0 vector with predictably undesirable results.  So, pick the global `z` vector in these cases, while normalizing in the usual case.

--- code/particle/hosts/EffectHostParticle.cpp
+++ code/particle/hosts/EffectHostParticle.cpp
@@ -26,15 +26,21 @@ std::pair<vec3d, matrix> EffectHostParticle::getPositionAndOrientation(bool /*re
 		pos = particle->pos;
 	}
 
-	if (tabled_offset){
-		vec3d particle_dir;
-		vm_vec_copy_normalize(&particle_dir, &particle->velocity);
+	// find the particle direction (normalized vector)
+	// note: this can't be computed for particles with 0 velocity, so arbitrarily pick the global Z vector
+	vec3d particle_dir;
+	float mag = vm_vec_mag(&particle->velocity);
+	if (fl_near_zero(mag))
+		particle_dir = vmd_z_vector;
+	else
+		particle_dir = particle->velocity / mag;
+
+	if (tabled_offset)
 		pos += particle_dir * tabled_offset->xyz.z;
-	}
 
 	matrix orientation;
 	//As there's no sensible uvec in this particle orientation, relative override orientation is not that sensible. Nonetheless, allow it for compatibility, or future orientation-aware particles
-	orientation = m_orientationOverrideRelative ? m_orientationOverride * *vm_vector_2_matrix(&orientation, &particle->velocity) : m_orientationOverride;
+	orientation = m_orientationOverrideRelative ? m_orientationOverride * *vm_vector_2_matrix_norm(&orientation, &particle_dir) : m_orientationOverride;
 
 	return { pos, orientation };
 }
