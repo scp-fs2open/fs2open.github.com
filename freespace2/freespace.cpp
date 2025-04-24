1a3835079786e90f56ee126532baae582dd753f0
Asteroth <sshyrda@gmail.com>
2025-04-24 13:59:21 -0400

    2d camera fix

--- freespace2/freespace.cpp
+++ freespace2/freespace.cpp
@@ -3159,7 +3159,7 @@ camid game_render_frame_setup()
 	//These replace the normal player in-cockpit view with a topdown view.
 	if(The_mission.flags[Mission::Mission_Flags::Mission_2d])
 	{
-		if(!Viewer_mode)
+		if(!(Viewer_mode & ~VM_CAMERA_LOCKED))
 		{
 			Viewer_mode = VM_TOPDOWN;
 		}
