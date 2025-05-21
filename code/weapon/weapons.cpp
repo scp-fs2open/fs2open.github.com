381f9fde581989f48e3d1f55173931b06353215f
wookieejedi <azthepaleo@gmail.com>
2025-05-21 17:56:55 -0400

    Mod Specific Visual Culling Size for Lasers (#6729)
    * Mod Specific Visual Culling Size for Lasers
    
    Currently, lasers will not render if the length or both head and tail radii are < `0.0001`. This is very small, and in many mods the smallest visual size is `0.01`, so this PR exposes that culling size value to allow mods to optimize and tune visual culling of lasers, which can be quite useful especially in laser heavy missions.
    
    * update names
    
    * actually push all files

--- code/weapon/weapons.cpp
+++ code/weapon/weapons.cpp
@@ -9215,17 +9215,19 @@ void weapon_render(object* obj, model_draw_list *scene)
 			head_radius *= head_radius_mult * radius_mult;
 			tail_radius *= tail_radius_mult * radius_mult;
 
-			if (laser_length < 0.0001f)
+			if (laser_length < Do_not_render_lasers_below_length) {
 				return;
-
-			if (head_radius < 0.0001f && tail_radius < 0.0001f)
-				return;
-
-			if (head_radius <= 0.0001f) {
-				head_radius = 0.0001f;
 			}
-			if (tail_radius <= 0.0001f) {
-				tail_radius = 0.0001f;
+
+			if (head_radius < Do_not_render_lasers_below_radius && tail_radius < Do_not_render_lasers_below_radius) {
+				return;
+			}
+
+			if (head_radius <= Do_not_render_lasers_below_radius) {
+				head_radius = Do_not_render_lasers_below_radius;
+			}
+			if (tail_radius <= Do_not_render_lasers_below_radius) {
+				tail_radius = Do_not_render_lasers_below_radius;
 			}
 
 
