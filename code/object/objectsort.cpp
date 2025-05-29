e41694f2bf5ff971a65ee2903df48c39bb0c998f
Kestrellius <902X@comcast.net>
2025-05-29 12:47:00 -0700

    bugfixing

--- code/object/objectsort.cpp
+++ code/object/objectsort.cpp
@@ -149,17 +149,17 @@ int obj_in_view_cone( object * objp )
 	if (objp->type == OBJ_WEAPON && Weapon_info[Weapons[objp->instance].weapon_info_index].render_type == WRT_LASER) {
 		auto wp = &Weapons[objp->instance];
 		auto wip = &Weapon_info[wp->weapon_info_index];
-		float length_scalar = wip->weapon_curves.get_output(weapon_info::WeaponCurveOutputs::LASER_LENGTH_MULT, *wp, wp->modular_curves_instance);
+		float length_scalar = wip->weapon_curves.get_output(weapon_info::WeaponCurveOutputs::LASER_LENGTH_MULT, *wp, &wp->modular_curves_instance);
 		if (wip->laser_length_by_frametime) {
 			length_scalar *= flFrametime;
 		}
-		obj_size = wip->laser_length * 
+		obj_size = wip->laser_length * length_scalar;
 	} else {
 		obj_size = objp->radius;
 	}
 
 	for (i=0; i<8; i++ ) {
-		vm_vec_scale_add( &pt, &objp->pos, &check_offsets[i], objp->radius );
+		vm_vec_scale_add( &pt, &objp->pos, &check_offsets[i], obj_size );
 		codes=g3_rotate_vector(&tmp,&pt);
 		if ( !codes ) {
 			//mprintf(( "A point is inside, so render it.\n" ));
