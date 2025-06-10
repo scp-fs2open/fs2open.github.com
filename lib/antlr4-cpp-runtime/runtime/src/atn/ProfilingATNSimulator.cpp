808fa0e3f77fb28d0778e0032adb5c73d8785701
Birk Magnussen <6238428+BMagnu@users.noreply.github.com>
2025-06-10 21:25:00 +0200

    Include Mess for MSVC

--- lib/antlr4-cpp-runtime/runtime/src/atn/ProfilingATNSimulator.cpp
+++ lib/antlr4-cpp-runtime/runtime/src/atn/ProfilingATNSimulator.cpp
@@ -2,6 +2,7 @@
  * Use of this file is governed by the BSD 3-clause license that
  * can be found in the LICENSE.txt file in the project root.
  */
+#include <chrono>
 
 #include "atn/PredicateEvalInfo.h"
 #include "atn/LookaheadEventInfo.h"
