e0d7a5cfc480cd7d6ed04d600603a4836c983c2a
Mike Nelson <mjn.other@gmail.com>
2025-06-12 16:56:50 -0500

    lua table prop support

---
+++ code/scripting/api/objs/prop.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "object/object.h"
+#include "scripting/ade_api.h"
+
+namespace scripting {
+namespace api {
+
+//**********HANDLE: Prop
+DECLARE_ADE_OBJ(l_Prop, object_h);
+} // namespace api
+} // namespace scripting
