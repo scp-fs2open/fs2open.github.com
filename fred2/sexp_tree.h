379eea1df70bffb435add8eae2815e27f35a74eb
Mike Nelson <mjn.other@gmail.com>
2025-06-12 13:27:15 -0500

    prop sexps

--- fred2/sexp_tree.h
+++ fred2/sexp_tree.h
@@ -220,6 +220,7 @@ public:
 	sexp_list_item *get_listing_opf_positive();
 	sexp_list_item *get_listing_opf_number();
 	sexp_list_item *get_listing_opf_ship(int parent_node = -1);
+	sexp_list_item *get_listing_opf_prop(int parent_node = -1);
 	sexp_list_item *get_listing_opf_wing();
 	sexp_list_item *get_listing_opf_subsystem(int parent_node, int arg_index);
 	sexp_list_item *get_listing_opf_subsystem_type(int parent_node);
@@ -249,6 +250,7 @@ public:
 	sexp_list_item *get_listing_opf_medal_name();
 	sexp_list_item *get_listing_opf_weapon_name();
 	sexp_list_item *get_listing_opf_ship_class_name();
+	sexp_list_item *get_listing_opf_prop_class_name();
 	sexp_list_item *get_listing_opf_huge_weapon();
 	sexp_list_item *get_listing_opf_ship_not_player();
 	sexp_list_item *get_listing_opf_jump_nodes();
