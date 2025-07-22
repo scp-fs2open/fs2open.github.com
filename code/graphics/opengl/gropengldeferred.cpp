802c05d1eeac7669bba1ae6891494c4836bf276b
combatlombax <78774622+combatlombax@users.noreply.github.com>
2025-07-21 18:42:24 -0600

    Removing block to allow volumetric and background nebulas to render at the same time

--- code/graphics/opengl/gropengldeferred.cpp
+++ code/graphics/opengl/gropengldeferred.cpp
@@ -522,7 +522,7 @@ void gr_opengl_deferred_lighting_finish()
 
 		opengl_draw_full_screen_textured(0.0f, 0.0f, 1.0f, 1.0f);
 	}
-	else if (The_mission.volumetrics && The_mission.volumetrics->get_enabled() && !override_fog) {
+	if (The_mission.volumetrics && The_mission.volumetrics->get_enabled() && !override_fog) {
 		GR_DEBUG_SCOPE("Volumetric Nebulae");
 		TRACE_SCOPE(tracing::Volumetrics);
 
