9ff8344c1cecf77b8389c739b50a97eebfa1bbd7
Asteroth <sshyrda@gmail.com>
2025-07-24 23:33:27 -0400

    fix single segmen trails when interrupted on both ends

--- code/weapon/trails.cpp
+++ code/weapon/trails.cpp
@@ -159,20 +159,27 @@ void trail_render( trail * trailp )
 
 		float speed = vm_vec_mag(&trailp->vel[front]);
 
-		float total_len = speed * ti->max_life;
-		float t = vm_vec_dist(&trailp->pos[front], &trailp->pos[back]) / total_len;
-		CLAMP(t, 0.0f, 1.0f);
 		float f_alpha, b_alpha, f_width, b_width;
+
+		float t = trailp->val[front] - trailp->val[back];
+
 		if (trailp->object_died) {
-			f_alpha = t * (ti->a_start - ti->a_end) + ti->a_end;
-			b_alpha = ti->a_end;
-			f_width = t * (ti->w_start - ti->w_end) + ti->w_end;
-			b_width = ti->w_end;
+			if (trailp->val[0] < 0.0f) {
+				f_alpha = t * (ti->a_start - ti->a_end) + ti->a_end;
+				b_alpha = -trailp->val[back] * (ti->a_start - ti->a_end) + ti->a_end;
+				f_width = t * (ti->w_start - ti->w_end) + ti->w_end;
+				b_width = -trailp->val[back] * (ti->w_start - ti->w_end) + ti->w_end;
+			} else {
+				f_alpha = t * (ti->a_start - ti->a_end) + ti->a_end;
+				b_alpha = ti->a_end;
+				f_width = t * (ti->w_start - ti->w_end) + ti->w_end;
+				b_width = ti->w_end;
+			}
 		} else {
 			f_alpha = ti->a_start;
-			b_alpha = t * (ti->a_end - ti->a_start) + ti->a_start;
+			b_alpha = -trailp->val[back] * (ti->a_start - ti->a_end) + ti->a_end;
 			f_width = ti->w_start;
-			b_width = t * (ti->w_end - ti->w_start) + ti->w_start;
+			b_width = -trailp->val[back] * (ti->w_start - ti->w_end) + ti->w_end;
 		}
 
 		vec3d trail_direction, ftop, fbot, btop, bbot;
@@ -191,12 +198,14 @@ void trail_render( trail * trailp )
 		verts[2].world = bbot;
 		verts[3].world = btop;
 
+		float total_len = speed * ti->max_life;
 		float uv_scale = (total_len / speed / (i2fl(ti->spew_duration) / MILLISECONDS_PER_SECOND)) / ti->texture_stretch;
+		uv_scale *= 0.5;
 
 		verts[0].texture_position.u = trailp->val[front] * uv_scale;
 		verts[1].texture_position.u = trailp->val[front] * uv_scale;
-		verts[2].texture_position.u = trailp->val[back] * uv_scale;
-		verts[3].texture_position.u = trailp->val[back] * uv_scale;
+		verts[2].texture_position.u = MAX(trailp->val[back], 0.0f) * uv_scale;
+		verts[3].texture_position.u = MAX(trailp->val[back], 0.0f) * uv_scale;
 
 		verts[0].texture_position.v = verts[3].texture_position.v = 0.0f;
 		verts[1].texture_position.v = verts[2].texture_position.v = 1.0f;
@@ -372,6 +381,9 @@ void trail_add_segment( trail *trailp, vec3d *pos , const matrix* orient, vec3d*
 
 	if (trailp->single_segment && velocity) {
 		trailp->vel[next] = *velocity;
+		if (next == 0) {
+			trailp->val[next] = -1.0;
+		}
 	} else if (orient != nullptr && trailp->info.spread > 0.0f) {
 		vm_vec_random_in_circle(&trailp->vel[next], &vmd_zero_vector, orient, trailp->info.spread, false, true);
 	} else 
@@ -404,10 +416,15 @@ void trail_move_all(float frametime)
 		time_delta = frametime / trailp->info.max_life;
 
 		if (trailp->single_segment) {
-			if (trailp->object_died) {
-				trailp->pos[0] += trailp->vel[0] * frametime; // only back keeps going...
-				trailp->val[0] += time_delta; 
+			trailp->val[0] += time_delta;
 
+			if (trailp->val[0] > 0.0f) {
+				// finished 'unfurling'
+				// back end moves too now
+				trailp->pos[0] += trailp->vel[0] * frametime;
+			}
+
+			if (trailp->object_died) {
 				if (trailp->val[0] >= trailp->val[1])
 					num_alive_segments = 0; // back has caught up to front and were dead
 				else
@@ -415,13 +432,6 @@ void trail_move_all(float frametime)
 			} else {
 				trailp->pos[1] += trailp->vel[1] * frametime;
 				trailp->val[1] += time_delta;
-
-				if (trailp->val[1] > 1.0f) {
-					// finished 'unfurling'
-					// back end moves too now
-					trailp->pos[0] += trailp->vel[0] * frametime;
-					trailp->val[0] += time_delta;
-				}
 
 				num_alive_segments = 2;
 			}
