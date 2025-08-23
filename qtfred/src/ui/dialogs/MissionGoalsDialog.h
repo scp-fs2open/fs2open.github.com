9db567ea4025cf277cb39fd09caf946978462ed7
Mike Nelson <mjn.other@gmail.com>
2025-08-23 09:58:14 -0500

    QtFRED Mission Goals Dialog Cleanup (#6951)
    * cleanup and bugfixes of mission goals editor
    
    * redundant specifier
    
    * static casts

--- qtfred/src/ui/dialogs/MissionGoalsDialog.h
+++ qtfred/src/ui/dialogs/MissionGoalsDialog.h
@@ -1,5 +1,4 @@
-#ifndef MISSIONGOALSDIALOG_H
-#define MISSIONGOALSDIALOG_H
+#pragma once
 
 #include <QDialog>
 
@@ -8,11 +7,7 @@
 
 #include "ui/widgets/sexp_tree.h"
 
-#include <memory>
-
-namespace fso {
-namespace fred {
-namespace dialogs {
+namespace fso::fred::dialogs {
 
 namespace Ui {
 class MissionGoalsDialog;
@@ -26,15 +21,34 @@ public:
     explicit MissionGoalsDialog(QWidget *parent, EditorViewport* viewport);
     ~MissionGoalsDialog() override;
 
+	void accept() override;
+	void reject() override;
+
  protected:
 	void closeEvent(QCloseEvent* event) override;
-   void rejectHandler();
 
- private:
-	void updateUI();
+private slots:
+	void on_okAndCancelButtons_accepted();
+	void on_okAndCancelButtons_rejected();
 
+	void on_displayTypeCombo_currentIndexChanged(int index);
+	void on_goalTypeCombo_currentIndexChanged(int index);
+	void on_goalName_textChanged(const QString& text);
+	void on_goalDescription_textChanged(const QString& text);
+	void on_goalScore_valueChanged(int value);
+	void on_goalTeamCombo_currentIndexChanged(int team);
+	void on_objectiveInvalidCheck_stateChanged(bool checked);
+	void on_noCompletionMusicCheck_stateChanged(bool checked);
+	void on_newObjectiveBtn_clicked();
+
+	void on_goalEventTree_selectedRootChanged(int formula);
+	void on_goalEventTree_rootNodeDeleted(int node);
+	void on_goalEventTree_rootNodeFormulaChanged(int old, int node);
+	void on_goalEventTree_helpChanged(const QString& help);
+
+ private: // NOLINT(readability-redundant-access-specifiers)
+	void updateUi();
 	void createNewObjective();
-
 	void changeGoalCategory(int type);
 
     std::unique_ptr<Ui::MissionGoalsDialog> ui;
@@ -45,8 +59,4 @@ public:
 	void recreate_tree();
 };
 
-}
-}
-}
-
-#endif // MISSIONGOALSDIALOG_H
+} // namespace fso::fred::dialogs
