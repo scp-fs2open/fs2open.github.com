080aa65ec8fc6cfa0d2ef6c042947c37fa7ec332
Mike Nelson <mjn.other@gmail.com>
2025-08-23 09:57:18 -0500

    QtFRED Object Orientation Editor Dialog (#6932)
    * refactor and cleanup the qtfred object dialog
    
    * clang is being pedantic
    
    * BRAAAACE FOR IMPACT
    
    * clang
    
    * switch statement

--- qtfred/src/ui/dialogs/ObjectOrientEditorDialog.h
+++ qtfred/src/ui/dialogs/ObjectOrientEditorDialog.h
@@ -6,49 +6,68 @@
 
 #include <mission/dialogs/ObjectOrientEditorDialogModel.h>
 
-namespace fso {
-namespace fred {
-namespace dialogs {
+namespace fso::fred::dialogs {
 
 namespace Ui {
 class ObjectOrientEditorDialog;
 }
 
 class ObjectOrientEditorDialog : public QDialog {
+	Q_OBJECT
  public:
 	ObjectOrientEditorDialog(FredView* parent, EditorViewport* viewport);
 	~ObjectOrientEditorDialog() override;
 
+	void accept() override;
+	void reject() override;
 
 protected:
-	void closeEvent(QCloseEvent*) override;
-  void rejectHandler();
+	void closeEvent(QCloseEvent* e) override; // funnel all Window X presses through reject()
 
-private:
+private slots:
+	// dialog controls
+	void on_okAndCancelButtons_accepted();
+	void on_okAndCancelButtons_rejected();
+	// Position
+	void on_positionXSpinBox_valueChanged(double value);
+	void on_positionYSpinBox_valueChanged(double value);
+	void on_positionZSpinBox_valueChanged(double value);
+	// Orientation
+	void on_orientationPSpinBox_valueChanged(double value);
+	void on_orientationBSpinBox_valueChanged(double value);
+	void on_orientationHSpinBox_valueChanged(double value);
+	// Settings
+	void on_setAbsoluteRadioButton_toggled(bool checked);
+	void on_setRelativeRadioButton_toggled(bool checked);
+	void on_transformIndependentlyRadioButton_toggled(bool checked);
+	void on_transformRelativelyRadioButton_toggled(bool checked);
+	// Point to
+	void on_pointToCheckBox_toggled(bool checked);
+	void on_objectRadioButton_toggled(bool checked);
+	void on_objectComboBox_currentIndexChanged(int index);
+	void on_locationRadioButton_toggled(bool checked);
+	void on_locationXSpinBox_valueChanged(double value);
+	void on_locationYSpinBox_valueChanged(double value);
+	void on_locationZSpinBox_valueChanged(double value);
+
+
+private: // NOLINT(readability-redundant-access-specifiers)
+	void initializeUi();
+	void updateUi();
+	void enableOrDisableControls();
+
+	// Boilerplate
 	std::unique_ptr<Ui::ObjectOrientEditorDialog> ui;
 	std::unique_ptr<ObjectOrientEditorDialogModel> _model;
 	EditorViewport* _viewport;
 
-	void updateUI();
+	// Group updates
+	void updatePosition();
+	void updateOrientation();
+	void updatePointTo();
 	void updateComboBox();
-
-	void objectSelectionChanged(int index);
-
-	void objectRadioToggled(bool enabled);
-	void locationRadioToggled(bool enabled);
-
-	void pointToChecked(bool checked);
-
-	void positionValueChangedX(double value);
-	void positionValueChangedY(double value);
-	void positionValueChangedZ(double value);
-
-	void locationValueChangedX(double value);
-	void locationValueChangedY(double value);
-	void locationValueChangedZ(double value);
+	void updateLocation();
 };
 
 
-}
-}
-}
+} // namespace fso::fred::dialogs
