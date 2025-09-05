ea855dc7be0c0a0f60a0e489c93211943dbd8e7f
Mike Nelson <mjn.other@gmail.com>
2025-09-05 15:06:45 -0500

    refactor variables/containers model and variables ui

---
+++ qtfred/src/ui/widgets/LineEditDelegate.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QStyledItemDelegate>
+
+namespace fso::fred::dialogs {
+
+class LineEditDelegate : public QStyledItemDelegate {
+	Q_OBJECT
+  public:
+	explicit LineEditDelegate(QObject* parent = nullptr);
+
+	QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+};
+
+} // namespace fso::fred::dialogs
