47d8a87624f7f86918eff8950a76f766897ffa0d
Mike Nelson <mjn.other@gmail.com>
2025-09-10 11:25:55 -0500

    rough in more code by copying from debrief

---
+++ qtfred/src/mission/dialogs/BriefingEditorDialogModel.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "globalincs/pstypes.h"
+
+#include "AbstractDialogModel.h"
+
+#include "mission/missionbriefcommon.h"
+#include "missionui/missioncmdbrief.h" //TODO remove this?
+
+namespace fso::fred::dialogs {
+
+class BriefingEditorDialogModel : public AbstractDialogModel {
+  public:
+	BriefingEditorDialogModel(QObject* parent, EditorViewport* viewport);
+
+	bool apply() override;
+	void reject() override;
+
+	int getCurrentTeam() const;
+	void setCurrentTeam(int teamIn);
+	int getCurrentStage() const;
+	int getTotalStages();
+
+	SCP_string getStageText();
+	void setStageText(const SCP_string& text);
+	SCP_string getRecommendationText();
+	void setRecommendationText(const SCP_string& text);
+	SCP_string getSpeechFilename();
+	void setSpeechFilename(const SCP_string& speechFilename);
+	int getFormula() const;
+	void setFormula(int formula);
+
+	void gotoPreviousStage();
+	void gotoNextStage();
+	void addStage();
+	void insertStage();
+	void deleteStage();
+	void testSpeech();
+	void copyToOtherTeams();
+	const SCP_vector<std::pair<SCP_string, int>>& getTeamList();
+	static bool getMissionIsMultiTeam();
+
+	static SCP_vector<SCP_string> getMusicList();
+	int getSuccessMusicTrack() const;
+	void setSuccessMusicTrack(int trackIndex);
+	int getAverageMusicTrack() const;
+	void setAverageMusicTrack(int trackIndex);
+	int getFailureMusicTrack() const;
+	void setFailureMusicTrack(int trackIndex);
+
+  private:
+	void initializeData();
+	void stopSpeech();
+	void initializeTeamList();
+
+	debriefing _wipDebriefing[MAX_TVT_TEAMS];
+	int _successMusic;
+	int _averageMusic;
+	int _failureMusic;
+
+	int _currentTeam;
+	int _currentStage;
+	int _waveId;
+	SCP_vector<std::pair<SCP_string, int>> _teamList;
+};
+
+} // namespace fso::fred::dialogs
