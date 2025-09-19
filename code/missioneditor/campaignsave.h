6b22b311cc5556b70b6ece1e2120b37654d59474
Mike Nelson <mjn.other@gmail.com>
2025-09-19 08:44:43 -0500

    have the campaign editor pass the links to save properly

--- code/missioneditor/campaignsave.h
+++ code/missioneditor/campaignsave.h
@@ -1,6 +1,18 @@
 #pragma once
 #include "missionsave.h"
 
+typedef struct campaign_link {
+	int from;                         // index of source mission
+	int to;                           // index of mission link leads to
+	int sexp;                         // sexp index of condition that allows this branch
+	int node;                         // node tracker when link is in sexp tree window
+	bool is_mission_loop;             // whether link leads to mission loop
+	bool is_mission_fork;             // whether link leads to mission fork
+	char* mission_branch_txt;         // text describing mission loop
+	char* mission_branch_brief_anim;  // filename of anim to play in the brief
+	char* mission_branch_brief_sound; // filename of anim to play in the brief
+} campaign_link;
+
 class Fred_campaign_save : public Fred_mission_save {
   public:
 	/**
@@ -15,7 +27,7 @@ class Fred_campaign_save : public Fred_mission_save {
 	 *
 	 * @see save_mission_internal()
 	 */
-	int save_campaign_file(const char* pathname);
+	int save_campaign_file(const char* pathname, const SCP_vector<campaign_link>& links);
 
   private:
 	/**
