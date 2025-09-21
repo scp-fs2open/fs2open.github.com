c1444781e8825b064d105fa40c3202155ecb39f5
Goober5000 <ipw47@mailbox.org>
2025-09-21 15:53:15 -0400

    check missions for missing hangar bays
    It's possible for ships to be designated as arrival or departure anchors when these ships do not actually have hangar bays.  This cannot normally happen in the interface but can happen when missions are edited in a text editor, or are imported, or the modpack is changed.  So add warnings and checks for this situation.

--- qtfred/src/mission/Editor.cpp
+++ qtfred/src/mission/Editor.cpp
@@ -1964,6 +1964,8 @@ int Editor::global_error_check_impl() {
 	int bs, i, j, n, s, t, z, ai, count, ship, wing, obj, team, point, multi;
 	object* ptr;
 	brief_stage* sp;
+	SCP_string anchor_message;
+	SCP_set<int> anchor_shipnums_checked;
 
 	g_err = multi = 0;
 	if (The_mission.game_type & MISSION_TYPE_MULTI) {
@@ -2158,11 +2160,23 @@ int Editor::global_error_check_impl() {
 						return 1;
 					}
 				}
+				if (Ships[i].arrival_location == ArrivalLocation::FROM_DOCK_BAY) {
+					check_anchor_for_hangar_bay(anchor_message, anchor_shipnums_checked, Ships[i].arrival_anchor, Ships[i].ship_name, true, true);
+					if (!anchor_message.empty() && error("%s", anchor_message.c_str())) {
+						return 1;
+					}
+				}
 			}
 
 			if (Ships[i].departure_location != DepartureLocation::AT_LOCATION) {
 				if (Ships[i].departure_anchor < 0) {
 					if (error("Ship \"%s\" requires a valid departure target", Ships[i].ship_name)) {
+						return 1;
+					}
+				}
+				if (Ships[i].departure_location == DepartureLocation::TO_DOCK_BAY) {
+					check_anchor_for_hangar_bay(anchor_message, anchor_shipnums_checked, Ships[i].departure_anchor, Ships[i].ship_name, true, false);
+					if (!anchor_message.empty() && error("%s", anchor_message.c_str())) {
 						return 1;
 					}
 				}
@@ -2374,11 +2388,23 @@ int Editor::global_error_check_impl() {
 						return 1;
 					}
 				}
+				if (Wings[i].arrival_location == ArrivalLocation::FROM_DOCK_BAY) {
+					check_anchor_for_hangar_bay(anchor_message, anchor_shipnums_checked, Wings[i].arrival_anchor, Wings[i].name, false, true);
+					if (!anchor_message.empty() && error("%s", anchor_message.c_str())) {
+						return 1;
+					}
+				}
 			}
 
 			if (Wings[i].departure_location != DepartureLocation::AT_LOCATION) {
 				if (Wings[i].departure_anchor < 0) {
 					if (error("Wing \"%s\" requires a valid departure target", Wings[i].name)) {
+						return 1;
+					}
+				}
+				if (Wings[i].departure_location == DepartureLocation::TO_DOCK_BAY) {
+					check_anchor_for_hangar_bay(anchor_message, anchor_shipnums_checked, Wings[i].departure_anchor, Wings[i].name, false, false);
+					if (!anchor_message.empty() && error("%s", anchor_message.c_str())) {
 						return 1;
 					}
 				}
