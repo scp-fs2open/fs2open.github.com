b8fa52097ce55171a0742525a3f3c0b9de2a3b59
Goober5000 <ipw47@mailbox.org>
2025-10-07 21:17:33 -0400

    patch cancel-future-waves to handle absent waves
    If the `cancel-future-waves` sexp is called when a wave is not present, it would previously not handle wing cleanup, such as setting flags and updating the mission log.

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -8881,6 +8881,43 @@ void ship_wing_cleanup( int shipnum, wing *wingp )
 			wingp->special_ship_ship_info_index = Ships[wingp->ship_index[0]].ship_info_index;
 	}
 
+	wing_maybe_cleanup(wingp, team);
+}
+
+// Assume the team of the wing is the same as the team of the most recently exited ship from that wing.
+// Returns -1 if no ship from that wing has exited.
+int wing_determine_team(const wing *wingp)
+{
+	int wingnum = WING_INDEX(wingp);
+	int team = -1;
+	fix latest_exit_time = -1;
+
+	// Grab the team from the most recent ship in this wing to have exited the mission.  If there are no exited ships from this wing,
+	// that means they all vanished.  In that case we must return -1, but it doesn't matter for logging because vanished wings aren't logged.
+	for (const auto &entry : Ships_exited)
+	{
+		if ((entry.wingnum == wingnum) && (entry.time > latest_exit_time))
+		{
+			latest_exit_time = entry.time;
+			team = entry.team;
+		}
+	}
+
+	return team;
+}
+
+/**
+ * This was originally part of ::ship_wing_cleanup, but it can now be called separately.  It sets various flags and mission log entries
+ * associated with a wing no longer being in a mission.
+ * 
+ * The team parameter is used for logging.  If it is not supplied (i.e. is not >= 0), it will be derived using wing_determine_team().
+ */
+void wing_maybe_cleanup( wing *wingp, int team )
+{
+	// not if the wing is already gone or has not yet arrived
+	if (wingp->flags[Ship::Wing_Flags::Gone] || wingp->total_arrived_count == 0)
+		return;
+
 	// if the current count is 0, check to see if the wing departed or was destroyed.
 	if (wingp->current_count == 0)
 	{
@@ -8905,7 +8942,7 @@ void ship_wing_cleanup( int shipnum, wing *wingp )
 				// first, be sure to mark a wing destroyed event if all members of wing were destroyed and on
 				// the last wave.  This circumvents a problem where the wing could be marked as departed and
 				// destroyed if the last ships were destroyed after the wing's departure cue became true.
-				mission_log_add_entry(LOG_WING_DESTROYED, wingp->name, NULL, team);
+				mission_log_add_entry(LOG_WING_DESTROYED, wingp->name, nullptr, team >= 0 ? team : wing_determine_team(wingp));
 			}
 			// if some ships escaped, log it as departed
 			else if (wingp->total_vanished != wingp->total_arrived_count)
@@ -8913,7 +8950,7 @@ void ship_wing_cleanup( int shipnum, wing *wingp )
 				// if the wing wasn't destroyed, and it is departing, then mark it as departed -- in this
 				// case, there had better be ships in this wing with departure entries in the log file.  The
 				// logfile code checks for this case.  
-				mission_log_add_entry(LOG_WING_DEPARTED, wingp->name, NULL, team);
+				mission_log_add_entry(LOG_WING_DEPARTED, wingp->name, nullptr, team >= 0 ? team : wing_determine_team(wingp));
 			}
 
 #ifndef NDEBUG
