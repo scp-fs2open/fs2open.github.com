5ee313e556db2e3508e3b10e2ba6e40614ac7b0b
Mara van der Laan <126646+laanwj@users.noreply.github.com>
2026-02-08 18:27:07 +0100

    Route ImGui calls through gr_screen function pointers
    Replace direct `ImGui_ImplOpenGL3` calls in game code with
    backend-agnostic `gr_imgui_new_frame` and `gr_imgui_render_draw_data`
    function pointers, matching the pattern used by all other `gr_*`
    functions. This makes it possible for the Vulkan backend
    to provide its own ImGui implemantation.

--- code/options/manager/ingame_options_manager.cpp
+++ code/options/manager/ingame_options_manager.cpp
@@ -138,8 +138,7 @@ void OptConfigurator::offer_save_options_popup()
 
 // The main Imgui rendering happens here as well as any i/o checking
 void OptConfigurator::onFrame() {
-	if (gr_screen.mode == GR_OPENGL)
-		ImGui_ImplOpenGL3_NewFrame();
+	gr_imgui_new_frame();
 	ImGui_ImplSDL2_NewFrame(gr_screen.max_w, gr_screen.max_h);
 	ImGui::NewFrame();
 
@@ -178,8 +177,7 @@ void OptConfigurator::onFrame() {
 	if (Cmdline_show_imgui_debug)
 		ImGui::ShowDemoWindow();
 	ImGui::Render();
-	if (gr_screen.mode == GR_OPENGL)
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	gr_imgui_render_draw_data();
 
 	if (CloseThis) {
 		close();
