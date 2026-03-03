f18330fe10dce3e74a61f87644925b04f03eefc1
Mike Nelson <mjn.other@gmail.com>
2026-03-03 08:58:00 -0600

    typo

--- code/scripting/api/objs/volumetric.h
+++ code/scripting/api/objs/volumetric.h
@@ -12,7 +12,7 @@ struct volumetric_h {
 	int index = -1;
 
 	bool isValid() const;
-	volumetric_nebula* get(); const
+	volumetric_nebula* get() const;
 
 	static void serialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, const luacpp::LuaValue& value, ubyte* data, int& packet_size);
 	static void deserialize(lua_State* /*L*/, const scripting::ade_table_entry& /*tableEntry*/, char* data_ptr, ubyte* data, int& offset); // NOLINT(readability-non-const-parameter)
