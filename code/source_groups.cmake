258ff507a831623697bfbf4ae745f24f77157c04
Salvador Cipolla <shivanuo@hotmail.com>
2026-06-02 01:41:37 -0300

    add tts support

--- code/source_groups.cmake
+++ code/source_groups.cmake
@@ -1641,6 +1641,11 @@ elseif (APPLE)
 		${file_root_sound}
 		sound/speech_mac.mm
 	)
+elseif (ANDROID)
+	add_file_folder("Sound"
+		${file_root_sound}
+		sound/speech_android.cpp
+	)
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
 	add_file_folder("Sound"
 		${file_root_sound}
