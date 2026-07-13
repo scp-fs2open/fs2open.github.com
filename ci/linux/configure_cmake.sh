284b1baf4aab4331a5ff4db9b2d79d94c3d78904
Fabian Woltermann <fabian.woltermann@trivago.com>
2026-07-13 08:57:25 +0200

    Remove unused LD_LIBRARY_PATH export and fix shader shadow map sampling.

--- ci/linux/configure_cmake.sh
+++ ci/linux/configure_cmake.sh
@@ -13,7 +13,6 @@ if [ "$COMPILER" = "clang-16" ]; then
     export CXX=clang++-16
 fi
 
-LD_LIBRARY_PATH=$Qt5_DIR/lib:$LD_LIBRARY_PATH
 if [ "$RUNNER_OS" = "macOS" ]; then
     CXXFLAGS="-mtune=generic -pipe -Wno-unknown-pragmas"
     CFLAGS="-mtune=generic -pipe -Wno-unknown-pragmas"
