640ee1c09d570ca2312256aaf7beb2b184c5b1f8
Salvador Cipolla <shivanuo@hotmail.com>
2026-08-14 21:39:58 -0300

    Remove calculate_max_resize_max_size

--- code/ddsutils/ddsutils.cpp
+++ code/ddsutils/ddsutils.cpp
@@ -68,28 +68,6 @@ static bool conversion_needed(const DDS_HEADER &dds_header)
 	return false;
 }
 
-
-// Auto-determine max texture size based on avail device ram for software decompression
-// Desktop OS: 1024
-// Android OS: 128 for <4GB, 256 for < 8GB, 512 for < 12GB and finally 1024
-static size_t calculate_resize_max_size()
-{
-#ifdef __ANDROID__
-	size_t ram_mib = SDL_GetSystemRAM();
-
-	if (ram_mib > 0)
-	{
-		if (ram_mib < 4 * 1024) // < 4GB
-			return 128;
-		if (ram_mib < 8 * 1024) // < 8GB
-			return 256;
-		if (ram_mib < 12 * 1024) // < 12GB
-			return 512;
-	}
-#endif
-	return 1024;
-}
-
 // Memory usage for uncompressed textures is quite high. Some MVP assets can
 // require well over a GB of VRAM for a single ship after conversion. To help
 // alleviate this we need to resize those textures where possible. At 1024x1024
@@ -101,8 +79,7 @@ static size_t calculate_resize_max_size()
 // returns: number of mipmap levels to skip
 static uint conversion_resize(DDS_HEADER &dds_header)
 {
-	const size_t MAX_SIZE = calculate_resize_max_size();
-
+	const size_t MAX_SIZE = 1024;
 	uint width, height, depth, offset = 0;
 
 	if (dds_header.dwMipMapCount <= 1) {
